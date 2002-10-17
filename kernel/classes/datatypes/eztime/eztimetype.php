3b8bde64adaaf8b676387a5bc06e0eca219d41d0
Wenyue Yu <wy@ez.no>
2002-10-17 13:03:24 +0000

    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@300 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ kernel/classes/datatypes/eztime/eztimetype.php
@@ -0,0 +1,127 @@
+<?php
+//
+// Definition of eZTimeType class
+//
+//
+// Copyright (C) 1999-2002 eZ systems as. All rights reserved.
+//
+// This source file is part of the eZ publish (tm) Open Source Content
+// Management System.
+//
+// This file may be distributed and/or modified under the terms of the
+// "GNU General Public License" version 2 as published by the Free
+// Software Foundation and appearing in the file LICENSE.GPL included in
+// the packaging of this file.
+//
+// Licencees holding valid "eZ publish professional licences" may use this
+// file in accordance with the "eZ publish professional licence" Agreement
+// provided with the Software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+// THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE.
+//
+// The "eZ publish professional licence" is available at
+// http://ez.no/home/licences/professional/. For pricing of this licence
+// please contact us via e-mail to licence@ez.no. Further contact
+// information is available at http://ez.no/home/contact/.
+//
+// The "GNU General Public License" (GPL) is available at
+// http://www.gnu.org/copyleft/gpl.html.
+//
+// Contact licence@ez.no if any conditions of this licencing isn't clear to
+// you.
+//
+
+//!! eZKernel
+//! The class eZTimeType
+/*!
+
+*/
+include_once( "kernel/classes/ezdatatype.php" );
+
+define( "EZ_DATATYPESTRING_TIME", "eztime" );
+
+
+class eZTimeType extends eZDataType
+{
+    function eZTimeType()
+    {
+        $this->eZDataType( EZ_DATATYPESTRING_TIME, "Time" );
+    }
+
+    function hasAttribute( $attr )
+    {
+        return $attr == "hour" or
+            $attr == "minute" or
+            eZDataType::hasAttribute( $attr ) ;
+    }
+
+    function &attribute( $attr )
+    {
+        $time = $this->attribute( 'data_text' );
+        list ( $hour, $minute ) = split ('[:]', $time);
+        switch( $attr )
+        {
+            case "hour":
+            {
+                return $hour;
+            } break;
+             case "minute":
+            {
+                return $minute;
+            } break;
+            default:
+                return eZDataType::attribute( $attr );
+        }
+    }
+
+    /*!
+     Validates the input and returns true if the input was
+     valid for this datatype.
+    */
+    function validateObjectAttributeHTTPInput( &$http, $base, &$contentObjectAttribute )
+    {
+        $hour = $http->postVariable( $base . "_time_hour_" . $contentObjectAttribute->attribute( "id" ) );
+        $minute = $http->postVariable( $base . "_time_minute_" . $contentObjectAttribute->attribute( "id" ) );
+        $time = $hour.':'.$minute;
+        $classAttribute =& $contentObjectAttribute->contentClassAttribute();
+        if( ( $classAttribute->attribute( "is_required" ) == false ) &&  ( $time == ":" ) )
+        {
+            return EZ_INPUT_VALIDATOR_STATE_ACCEPTED;
+        }
+        if ( preg_match( "#[0-9]+[:][0-9]+#", $time ) )
+            return EZ_INPUT_VALIDATOR_STATE_ACCEPTED;
+        return EZ_INPUT_VALIDATOR_STATE_INVALID;
+    }
+
+    /*!
+     Fetches the http post var integer input and stores it in the data instance.
+    */
+    function fetchObjectAttributeHTTPInput( &$http, $base, &$contentObjectAttribute )
+    {
+        $hour = $http->postVariable( $base . "_time_hour_" . $contentObjectAttribute->attribute( "id" ) );
+        $minute = $http->postVariable( $base . "_time_minute_" . $contentObjectAttribute->attribute( "id" ) );
+        $time = $hour.':'.$minute;
+        $contentObjectAttribute->setAttribute( "data_text", $time );
+    }
+
+    /*!
+     Returns the meta data used for storing search indeces.
+    */
+    function metaData( $contentObjectAttribute )
+    {
+        return $contentObjectAttribute->attribute( 'data_text' );
+    }
+
+    /*!
+     Returns the date.
+    */
+    function title( &$contentObjectAttribute )
+    {
+        return $contentObjectAttribute->attribute( "data_text" );
+    }
+}
+eZDataType::register( EZ_DATATYPESTRING_TIME, "eztimetype" );
+
+?>
