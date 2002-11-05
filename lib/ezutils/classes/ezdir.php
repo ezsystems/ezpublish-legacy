5bb4463f0e8d7a3ea86821724a6478f5e2816832
Bård Farstad <bf@ez.no>
2002-11-05 16:22:48 +0000

    Fixed problem with creating recursive directory sctructures on windows.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@462 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezutils/classes/ezdir.php
+++ lib/ezutils/classes/ezdir.php
@@ -93,7 +93,9 @@ class eZDir
         {
             $new_dir = preg_replace( "#/+#", "/", $dir );
             if ( $dir[0] != "/" )
-                $new_dir = realpath( "." ) . "/$new_dir";
+                $new_dir = "$new_dir";
+// Fix to make this work on windows.. To quick ?
+//                $new_dir = realpath( "." ) . "/$new_dir";
             if ( preg_match( "#^(.+/)([^/]+)/?$#", $new_dir, $regs ) )
             {
                 $new_dir = $regs[1];
