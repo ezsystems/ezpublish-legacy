8e822b31c3e74ca72186e4bf18b67125ee5bbe20
Bård Farstad <bf@ez.no>
2002-11-05 07:59:52 +0000

    Removed hio code.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@452 a01eee8c-daba-0310-acae-fa49f3023285

--- index.php
+++ index.php
@@ -352,6 +352,7 @@ if ( $show_page_layout )
             $show_page_layout = "pagelayout.tpl";
         }
 
+        /*
         /// HiO special menu code tmp
         eZDebug::writeWarning( "Temporary HiO specific code, remove", "index.php" );
 
@@ -516,6 +517,7 @@ if ( $show_page_layout )
 //      }
         $tpl->setVariable( 'menuitems', $pathArray );
         /// end HiO code
+        */
 
         $tpl->display( $resource . $show_page_layout );
     }
