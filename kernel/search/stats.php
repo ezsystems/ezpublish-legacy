7b7ca0f3ace0db2cfea48d3adf99ae8006b61331
Jan Borsodi <jb@ez.no>
2002-11-18 10:36:33 +0000

    - Added path.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@584 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/search/stats.php
+++ kernel/search/stats.php
@@ -50,5 +50,7 @@ $tpl->setVariable( "most_frequent_phrase_array", $mostFrequentPhraseArray );
 
 $Result = array();
 $Result['content'] =& $tpl->fetch( "design:search/stats.tpl" );
+$Result['path'] = array( array( 'text' => 'Search stats',
+                                'url' => false ) );
 
 ?>
