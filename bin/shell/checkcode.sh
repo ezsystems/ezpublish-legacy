49adb15c0dd4ba76eceed628bca0e67a0c62029f
Jan Borsodi <jb@ez.no>
2002-11-19 13:50:31 +0000

    - Added tab check for code. - New file TEMPLATE_BUGS which lists bugs for the template engine.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@598 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/checkcode.sh
+++ bin/shell/checkcode.sh
@@ -2,43 +2,46 @@
 
 . ./bin/shell/common.sh
 
-# echo "Searching for PHP files that doesn't conform to 'short_open_tag = Off'"
+echo "Searching for PHP files that doesn't conform to 'short_open_tag = Off'"
 
-# find . -name \*.php -exec grep -n -H '<?[ '"\t"']*$' {} \;
+find . -name \*.php -exec grep -n -H '<?[ '"\t"']*$' {} \;
+
+echo
+echo "Check for tabs"
 
 echo
 echo "Check for unneeded print statements"
 find . -name \*.php -exec ./bin/shell/checkprint.sh {} \;
 
-# echo
-# echo "Check codingstandard"
-# echo "Searching for bad 'foreach' statements"
-# find . -name \*.php -exec ./bin/shell/checkforeach.sh {} \;
+echo
+echo "Check codingstandard"
+echo "Searching for bad 'foreach' statements"
+find . -name \*.php -exec ./bin/shell/checkforeach.sh {} \;
 
-# echo "Check for elsif"
+echo "Check for elsif"
 
-# echo "Check for && and ||"
+echo "Check for && and ||"
 
-# echo
-# echo "Check for global variable access"
-# find . -name \*.php -exec grep -n -H 'getenv' {} \;
+echo
+echo "Check for global variable access"
+find . -name \*.php -exec grep -n -H 'getenv' {} \;
 
-# echo "Check for REQUEST_URI"
-# rgrep -R '*.php' -n 'REQUEST_URI' .
+echo "Check for REQUEST_URI"
+rgrep -R '*.php' -n 'REQUEST_URI' .
 
-# echo "Checking doxygen code"
-# echo "Check for proper use of \return"
-# echo "Checking for old doc code"
-# # //!! eZKernel
-# # //! The class eZURLOperator does
-# # /*!
+echo "Checking doxygen code"
+echo "Check for proper use of \return"
+echo "Checking for old doc code"
+# //!! eZKernel
+# //! The class eZURLOperator does
+# /*!
 
-# # */
+# */
 
-# echo "Check for URIs which will not work with nVH setups"
-# #  <a href=\"\">
-# rgrep -R '*.tpl' -n -i '<a  *href="' .
-# rgrep -R '*.php' -n -i 'header' .
+echo "Check for URIs which will not work with nVH setups"
+#  <a href=\"\">
+rgrep -R '*.tpl' -n -i '<a  *href="' .
+rgrep -R '*.php' -n -i 'header' .
 
-# echo "Check for bad prints"
-# rgrep -R '*.php' -n -i 'print' .
+echo "Check for bad prints"
+rgrep -R '*.php' -n -i 'print' .
