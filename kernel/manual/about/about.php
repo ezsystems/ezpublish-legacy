4cbfb2929bf100b4ba4a79649d22c65b3cd13a2f
Ole Morten Halvorsen <oh@ez.no>
2002-12-11 15:06:52 +0000

    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@977 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ kernel/manual/about/about.php
@@ -0,0 +1,40 @@
+<h1>About eZ publish </h1>
+
+<h2>What is eZ publish?</h2>
+
+<p>
+eZ publish is known around the world as an open source content management
+system. eZ publish 3 is much more than a content management system. It is a
+general php development framework; with functionality allowing you to develop
+professional Internet applications. It will of course also have some standard
+implementations, like the CMS, which are solutions that are ready for you to
+use.
+</p>
+
+<p>
+eZ publish comes with an SDK, a software development kit, available at sdk.ez.no
+and in every eZ publish distribution. The SDK is a toolkit with documentation,
+examples and reference documentation on the eZ publish libraries and eZ publish
+kernel.
+</p>
+
+<p>
+eZ publish SDK is a tool helping you develop your own application or software or
+to set up a solution on the eZ publish framework. Since eZ publish has most
+features ready it allows you to develop a solution in no time.
+</p>
+
+<p>
+With eZ publish you can either follow the GPL licence, making open source
+applications and contributing to the development of open software. You can also
+purchase the eZ publish professional licence, allowing you to make commercial
+software with eZ publish.
+</p>
+
+<p>
+eZ publish is database and platform independent. That means that you can use eZ
+publish on Linux, Windows, Mac, FreeBSD, UNIX and etc. It also means that you
+can use different kinds of databases.
+eZ publish is also browser based which means that you can access your site and
+update it as long as you have internet access without worrying about location.
+</p>
