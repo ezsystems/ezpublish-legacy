56254a696831cbe5fa08310063d960c51f7e81ae
Ole Morten Halvorsen <oh@ez.no>
2002-12-12 11:28:48 +0000

    Added requirements section
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@1037 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/manual/install/without_installer.php
+++ kernel/manual/install/without_installer.php
@@ -1,6 +1,7 @@
 <h1>eZ publish installation without installers</h1>
 
 <ul>
+	<li><a href="#Requirements">Requirements</a></li>
 	<li><a href="#Linux_FreeBSD_Installation">Installation on Linux and FreeBSD</a></li>
 	<ul>
 		<li><a href="#Linux_FreeBSD_Database">Database setup</a></li>
@@ -26,6 +27,27 @@
 		<li><a href="#MacOS_VirtualHost">VirtualHost</a></li>
 	</ul>
 </ul>
+
+<h2 id="Requirements"> eZ publish 3 requirements</h2>
+<p>
+	You need to have Apache, MySQL, ImageMagick and PHP installed to run eZ publish 3.
+	PHP needs to have compiled-in support for either MySQL or PostgreSQL.
+	For image conversion support you need to have GD compiled in PHP or
+	ImageMagick installed on your system.
+</p>
+
+<ul>
+	<li>PHP >= 4.1.x <br /><a href="http://www.php.net">http://www.php.net </a> </li>
+	<li>Apache 1.3 (apache 2.0 might/might not work) <br /><a href="http://www.apache.org">http://www.apache.org</a></li>
+	<li>Either MySQL or PostgreSQL <br /><a href="http://www.mysql.org">http://www.mysql.org</a><br /><a href="http://www.postgresql.org">http://www.postgresql.org</a></li>
+	<li> ImageMagick and/or GD compiled in PHP for image conversion (not required) <br /><a href="http://www.imagemagick.org">http://www.imagemagick.org</a></li>
+</ul>
+
+<p>
+	The installation process for the above programs are very well documented on
+	their homepage, so we will not include that here.
+</p>
+
 
 <h2>Installation on Linux and FreeBSD without installers</h2>
 <ol>
