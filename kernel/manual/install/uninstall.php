a1b3b14289d42faa4cb095f67c623b5e8dc00355
Terje Gunrell-Kaste <tg@ez.no>
2002-12-13 10:15:59 +0000

    updated manual
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@1165 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/manual/install/uninstall.php
+++ kernel/manual/install/uninstall.php
@@ -21,8 +21,8 @@
 <h3 id="Windows_installer">Installer</h3>
 <p>
 	Uninstalling eZ publish should be very easy. In windows you simply
-	Click Start ? Settings ? Control panel. Open ?Add/Remove programs? and find eZ
-	publish on the list. Highlight ?eZ publish? and click the ?Change/Remove?
+	Click Start - Settings - Control panel. Open <b>Add/Remove programs</b> and find eZ
+	publish on the list. Highlight <b>eZ publish</b> and click the <b>Change/Remove</b>
 	button. This will delete eZ publish 3.
 </p>
 <p>
