e1f2511e8201ef0eb558c759886907b81ccc5045
Ole Morten Halvorsen <oh@ez.no>
2003-01-08 15:06:20 +0000

    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@1291 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/manual/install/setup_guide.php
+++ kernel/manual/install/setup_guide.php
@@ -48,8 +48,6 @@
 	<li>Become the PostgreSQL super user (normally called postgres)<pre class="example">$ su &lt;postgres_super_user&gt;</pre></li>
 	li>Create a postgresql user<pre class="example">$ createuser &lt;user&gt;</pre></li>
 	<li>Create a database <pre class="example">$ createdb &lt;name_of_database&gt;</pre></li>
-	<li>Demodata is not available for PostgreSQL at the moment, so we have to install the kernel_clean.sql file<pre class="example">$ psql -U &lt;ezpublish_user&gt;  &lt;name_of_database&gt;
-	&lt; &lt;httproot&gt;/ezpublish-xxx/kernel/sql/postgresql/kernel_clean.sql</pre></li>
 </ol>
 
 <ol>
