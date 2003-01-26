f07efc810b471f3da6c8d69311c1bd2e006578c1
Bård Farstad <bf@ez.no>
2003-01-26 13:11:17 +0000

    Added object index script
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@1504 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ scrap/index_objects.php
@@ -0,0 +1,88 @@
+<?php
+//
+// Created on: <28-Nov-2002 12:45:40 bf>
+//
+// Copyright (C) 1999-2002 eZ systems as. All rights reserved.
+//
+// This source file is part of the eZ publish (tm) Open Source Content
+// Management System.
+//
+// This file may be distributed and/or modified under the terms of the
+// "GNU General Public License" version 2 as published by the Free
+// Software Foundation and appearing in the file LICENSE.GPL included in
+// the packaging of this file.
+//
+// Licencees holding valid "eZ publish professional licences" may use this
+// file in accordance with the "eZ publish professional licence" Agreement
+// provided with the Software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+// THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE.
+//
+// The "eZ publish professional licence" is available at
+// http://ez.no/home/licences/professional/. For pricing of this licence
+// please contact us via e-mail to licence@ez.no. Further contact
+// information is available at http://ez.no/home/contact/.
+//
+// The "GNU General Public License" (GPL) is available at
+// http://www.gnu.org/copyleft/gpl.html.
+//
+// Contact licence@ez.no if any conditions of this licencing isn't clear to
+// you.
+//
+
+set_time_limit( 0 );
+include_once( "lib/ezutils/classes/ezdebug.php" );
+
+eZINI::setIsCacheEnabled( false );
+eZDebug::setHandleType( EZ_HANDLE_TO_PHP );
+// eZDebug::setHandleType( EZ_HANDLE_FROM_PHP );
+
+header( 'X-Powered-By: eZ publish' );
+// header( 'Content-Type: text/plain' );
+
+include_once( 'kernel/classes/ezcontentobjecttreenode.php' );
+include_once( 'kernel/classes/ezsearch.php' );
+include_once( 'lib/ezutils/classes/ezdebugsetting.php' );
+
+$db =& eZDB::instance();
+$db->setIsSQLOutputEnabled( false );
+
+// Get top node
+$node =& eZContentObjectTreeNode::fetch( 5082 );
+
+$endl = "\n";
+// $endl = "<br/>";
+
+$subTreeCount =& $node->subTreeCount();
+print( "Number of objects to index: $subTreeCount $endl" );
+$subTree =& $node->subTree();
+
+$updateCount = $subTreeCount / 100;
+if ( $updateCount < 1 )
+    $updateCount = 1;
+
+$i = 0;
+$updateCounter = 0;
+foreach ( $subTree as $node )
+{
+    $object = $node->attribute( 'object' );
+    eZSearch::removeObject( $object );
+    eZSearch::addObject( $object );
+    ++$i;
+    ++$updateCounter;
+    if ( $updateCounter > $updateCount )
+    {
+        $updateCounter = 0;
+        $percent = ($i*100.0) / $subTreeCount;
+        print( $percent . "%" . $endl );
+    }
+    ob_end_flush();
+}
+
+print( $endl . "done" . $endl );
+
+eZDebug::printReport( );
+
+?>
