8acf1632e90e1e8cc2e12ce6de52b65eed2bd05e
Gunnstein Lye <gl@ez.no>
2003-01-28 15:23:18 +0000

    sdk update
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@1547 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/eztemplate/sdk/operator_list.php
+++ lib/eztemplate/sdk/operator_list.php
@@ -63,16 +63,25 @@ for eZ publish.
 <dd>Localizes values, e.g. {42.50|l10n(currency)}. Allowed types are: time, shorttime, date, shortdate, currency or number.</dd>
 </dl>
 
+<h2>Dates</h2>
+<dl>
+<dt>datetime</dt>
+<dd>Formats dates and times according to formats defined in datetime.ini. Custom formats in the template itself are also possible. E.g. {$date|datetime(mydate)}, {$date|datetime(custom,"%m %y")}</dd>
+<dt>currentdate</dt>
+<dd>Returns the timestamp of the current datetime. E.g. {currentdate()}</dd>
+</dl>
+
 <h2>Debug</h2>
 <dl>
 <dt>attribute</dt>
-<dd>Helper attribute to print available methods in objects. E.g. {$node|attribute(show)}</dd>
+<dd>Helper attribute to print available methods in objects and arrays, by default it only shows the array keys and object attribute names but by passing show as parameter it will fetch the values. The second parameter can be used to controlled the number of children to expand, default is no limit which may give problems with values that loop. The returned result is an HTML table unless false() is passed as the third parameter. E.g. {$node|attribute(show)}, {$node|attribute(show,2,false)}
+</dd>
 </dl>
 
 <h2>Output washing</h2>
 <dl>
 <dt>wash</dt>
-<dd>Will convert special characters to valid XHTML characters.</dd>
+<dd>General character/string washing operator. The washing type is the first parameter, for now it only supports XHTML characters.</dd>
 </dl>
 
 <h2>Text operations</h2>
@@ -84,25 +93,32 @@ for eZ publish.
 <h2>Unit operators</h2>
 <dl>
 <dt>si</dt>
-<dd>values </dd>
+<dd>Handles unit display of values, most often used for showing sizes of files but can also be used for displaying units on other items such as meters, grams etc. The operator reads two parameters. The first tells the kind of unit type we're dealing with, for instance: byte, length. The second determines the behaviour of prefixes and is optional.</dd>
+<dd>E.g.<br>
+{1025|si(byte)}<br>
+{1025|si(byte,binary)}<br>
+{1025|si(byte,decimal)}<br>
+{1025|si(byte,none)}<br>
+{1025|si(byte,auto)}<br>
+{1025|si(byte,mebi)}</dd>
 </dl>
 
 <h2>Logical operators</h2>
 <dl>
 <dt>lt</dt>
-<dd>Returns true if the value to the left is less than the value to the right. E.g. {1|lt(2)} returns true.</dd>
+<dd>Returns true if the input value is less than the first parameter. E.g. {1|lt(2)} returns true.</dd>
 <dt>gt</dt>
-<dd>Returns true if the value to the left is greater than the value to the right. E.g. {2|gt(1)} returns true.</dd>
+<dd>Returns true if the input value is greater than the first parameter. E.g. {2|gt(1)} returns true.</dd>
 <dt>le</dt>
-<dd>Returns true if the value to the left is less than or equal to the value to the right. E.g. {1|le(1)} and {1|le(2)} returns true.</dd>
+<dd>Returns true if the input value is less than or equal to the first parameter. E.g. {1|le(1)} and {1|le(2)} returns true.</dd>
 <dt>ge</dt>
-<dd>Returns true if the value to the left is greater than or equal to the value to the right. E.g. {1|le(1)} and {2|le(1)} returns true.</dd>
+<dd>Returns true if the input value is greater than or equal to the first parameter. E.g. {1|le(1)} and {2|le(1)} returns true.</dd>
 <dt>eq</dt>
-<dd>Returns true if the value to the left is equal to the value to the right. E.g. {1|le(1)} returns true.</dd>
+<dd>Returns true if the input value is equal to the first parameter, or if no input value is available it returns true if all parameters are equal E.g. {1|eq(1)} returns true. {eq(1,true(),false()|not,0|inc)} returns true.</dd>
 <dt>null</dt>
-<dd>Returns true if the value to the left is null, which is not the same as 0. E.g. {0|null()} returns false.</dd>
+<dd>Returns true if the input value is null, which is not the same as 0. E.g. {0|null()} returns false.</dd>
 <dt>not</dt>
-<dd>Returns true if the value to the left is false. E.g. {false()|not()} returns true.</dd>
+<dd>Returns true if the input value is false. E.g. {false()|not()} returns true.</dd>
 <dt>true</dt>
 <dd>Returns a true boolean.</dd>
 <dt>false</dt>
@@ -110,13 +126,13 @@ for eZ publish.
 <dt>or</dt>
 <dd>Evaluates all parameter values until one is found to be true, then returns that value. The remaining parameters are not evaluated at all. If there are no parameters or all elements were false it returns false. E.g. {or(false(),false(),true(),false())} returns true.</dd>
 <dt>and</dt>
-<dd>Evaluates all parameter values until one is found to be false, then returns that false. The remaining parameters are not evaluated at all. If there are no parameters it returns false, if no elements were false it returns the last parameter value. E.g. {or(false(),false(),true(),false())} returns false.</dd>
+<dd>Evaluates all parameter values until one is found to be false, then returns that value. The remaining parameters are not evaluated at all. If there are no parameters it returns false, if no elements were false it returns the last parameter value. E.g. {or(false(),false(),true(),false())} returns false.</dd>
 <dt>choose</dt>
 <dd>Uses the input count to pick one of the parameter elements. The input count equals the parameter index. E.g. {0|choose("a","b","c")} returns "a".</dd>
 </dl>
 
 <h2>Type operators</h2>
-<p>These operators correspond to the same PHP functions.</p>
+<p>These operators correspond to the PHP functions of the same name.</p>
 <dl>
 <dt>is_array</dt>
 <dd></dd>
@@ -149,80 +165,79 @@ for eZ publish.
 <h2>Control operators</h2>
 <dl>
 <dt>cond</dt>
-<dd></dd>
+<dd>Evaluates clauses and returns the value of the first clause whose condition is true. Pairs of parameters are treated as clauses where the first is the condition and the second is the body. The last clause may have one parameter only in which case it is used as condition and body. E.g. {cond($b|ne(0),div($a,$b),0)} returns the result of $a/$b if $b is not 0, or 0 if it is.
+</dd>
 <dt>first_set</dt>
-<dd></dd>
+<dd>Returns the first value which exists, this is useful if you want to make sure that you always have a value to work with, since you can put a constant as the last parameter and constants always exist. E.g. {first_set($a,$b,$c,"&nbsp;")} returns "&nbsp;" if none of the variables are set.</dd>
 </dl>
 
 <h2>Arithmentic operators</h2>
 <dl>
 <dt>sum</dt>
-<dd></dd>
+<dd>Returns the sum of all parameters.</dd>
 <dt>sub</dt>
-<dd></dd>
+<dd>Subtracts all extra parameters from the first parameter, e.g. sub($a,$b,$c) equal $a - $b - $c.</dd>
 <dt>inc</dt>
-<dd></dd>
+<dd>Increases either the input value or the first parameter with one.</dd>
 <dt>dec</dt>
-<dd></dd>
+<dd>Decreases either the input value or the first parameter with one.</dd>
 <dt>div</dt>
-<dd></dd>
+<dd>Divides all extra parameters with the first parameter, e.g. div($a,$b,$c) equal $a / $b / $c.</dd>
 <dt>mod</dt>
-<dd></dd>
+<dd>Returns the modulo of the first input parameter divided by the second. E.g. mod(5,3) returns 2.</dd>
 <dt>mul</dt>
-<dd></dd>
+<dd>Multiplies all parameters and returns the result.</dd>
 <dt>max</dt>
-<dd></dd>
+<dd>Returns the largest value of all parameters.</dd>
 <dt>min</dt>
-<dd></dd>
+<dd>Returns the smallest value of all parameters.</dd>
 <dt>abs</dt>
-<dd></dd>
+<dd>Returns a positive value of either the input value or the first parameter. E.g. {abs(-1)} returns 1 and {abs(200)} returns 200.</dd>
 <dt>ceil</dt>
-<dd></dd>
+<dd>Returns the next highest integer value by rounding up input value if necessary.</dd>
 <dt>floor</dt>
-<dd></dd>
+<dd>Returns the next lowest integer value by rounding down input value if necessary.</dd>
 <dt>round</dt>
-<dd></dd>
+<dd>Returns the rounded value of input value.</dd>
 <dt>count</dt>
-<dd></dd>
+<dd>Returns the count of the input value.</dd>
+<dd>How counts are interpreted:<br>
+  - If the data is an array the array count is used<br>
+  - If the data is an object the object attribute count is used<br>
+  - If the data is a numeric the value is used<br>
+  - If the data is a boolean false is 0 and true is 1<br>
+  - For all other data 0 is used</dd>
 </dl>
 
 <h2>Image handling operators</h2>
+<p>These operators require the ImageMagick or ImageGD extension to work.</p>
 <dl>
-<dt>texttoimage</dt>
-<dd></dd>
 <dt>image</dt>
 <dd></dd>
 <dt>imagefile</dt>
 <dd></dd>
 <dt>imagelabel</dt>
 <dd></dd>
+<dt>texttoimage</dt>
+<dd></dd>
 </dl>
 
-<h2>eZ publish operators</h2>
+<h2>eZ publish kernel operators</h2>
 <dl>
 <dt>ezurl</dt>
-<dd></dd>
+<dd>Makes sure that the url works for both virtual hosts and non-virtual host setups.</dd>
 <dt>ezroot</dt>
-<dd></dd>
+<dd>Similar to ezurl but does not include the index.php.</dd>
 <dt>ezsys</dt>
-<dd></dd>
+<dd>Returns values from ezsys. The available values are wwwdir, sitedir, indexfile, indexdir and imagesize.</dd>
 <dt>ezdesign</dt>
-<dd></dd>
+<dd>Similar to ezroot but prefixes with the design directory.</dd>
 <dt>ezimage</dt>
-<dd></dd>
+<dd>Similar to ezroot but prefixes image directory in the design.</dd>
 <dt>exturl</dt>
-<dd></dd>
+<dd>Not finished. Will be used for storing a URL in the database with a unique id, this means that similar URLs across templates can be updated centrally without changing templates. This is useful if URLs are wrong or become dead.</dd>
 <dt>i18n</dt>
 <dd>Marks a string for translation. E.g. {"I like %1!"|i18n("design/standard",,array($food))} See the tutorial <a href="/sdk/sdk/tutorials/view/translation">Translation and i18n</a> for more information.</dd>
 <dt>x18n</dt>
 <dd>Marks a string in an extension template for translation. E.g. {"I like %1!"|i18n("myextension","design/standard",,array($food))} See the tutorial <a href="/sdk/sdk/tutorials/view/translation">Translation and i18n</a> for more information.</dd>
-<dt>upcase</dt>
-<dd></dd>
-<dt>upcase</dt>
-<dd></dd>
-<dt>reverse</dt>
-<dd></dd>
-<dt>nl2br</dt>
-<dd></dd>
 </dl>
-
