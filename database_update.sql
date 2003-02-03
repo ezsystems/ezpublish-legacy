a7be12cdba9fc8ee1296a781f8cbbbab68d1c700
Sergiy Pushchin <sp@ez.no>
2003-02-03 12:22:57 +0000

    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@1611 a01eee8c-daba-0310-acae-fa49f3023285

--- database_update.sql
+++ database_update.sql
@@ -344,4 +344,4 @@ alter table ezoperation_memento drop main_key;
 alter table ezoperation_memento add main_key varchar(32) NOT NULL;
 
 # Fixed a bug with a lacking field in ezmedia
-alter table ezmedia add controls var(char 50);
+alter table ezmedia add controls varchar(50);
