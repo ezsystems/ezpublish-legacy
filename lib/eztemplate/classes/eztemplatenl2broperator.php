911232b52c9f6bf799b9e4811f09b1b6fc17f372
Bård Farstad <bf@ez.no>
2003-03-10 14:48:55 +0000

    Added nl2br text operator.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@2030 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ lib/eztemplate/classes/eztemplatenl2broperator.php
@@ -0,0 +1,86 @@
+<?php
+//
+// Definition of eZTemplateNl2BrOperator class
+//
+// Created on: <10-Mar-2003 11:22:29 bf>
+//
+// Copyright (C) 1999-2003 eZ systems as. All rights reserved.
+//
+// This source file is part of the eZ publish (tm) Open Source Content
+// Management System.
+//
+// This file may be distributed and/or modified under the terms of the
+// "GNU General Public License" version 2 as published by the Free
+// Software Foundation and appearing in the file LICENSE.GPL included in
+// the packaging of this file.
+//
+// Licencees holding valid "eZ publish professional licences" may use this
+// file in accordance with the "eZ publish professional licence" Agreement
+// provided with the Software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+// THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE.
+//
+// The "eZ publish professional licence" is available at
+// http://ez.no/home/licences/professional/. For pricing of this licence
+// please contact us via e-mail to licence@ez.no. Further contact
+// information is available at http://ez.no/home/contact/.
+//
+// The "GNU General Public License" (GPL) is available at
+// http://www.gnu.org/copyleft/gpl.html.
+//
+// Contact licence@ez.no if any conditions of this licencing isn't clear to
+// you.
+//
+
+/*!
+  \class eZTemplateNl2BrOperator eztemplatenl2broperator.php
+  \ingroup eZTemplateOperators
+\code
+
+\endcode
+
+*/
+
+class eZTemplateWashOperator
+{
+    /*!
+     Initializes the object with the name $name, default is "nl2br".
+    */
+    function eZTemplateNl2BrOperator( $name = "nl2br" )
+    {
+        $this->Operators = array( $name );
+    }
+
+    /*!
+     Returns the template operators.
+    */
+    function &operatorList()
+    {
+        return $this->Operators;
+    }
+
+    /*!
+     See eZTemplateOperator::namedParameterList()
+    */
+    function namedParameterList()
+    {
+        return array( );
+    }
+
+    /*!
+     Display the variable.
+    */
+    function modify( &$tpl, &$operatorName, &$operatorParameters, &$rootNamespace, &$currentNamespace, &$operatorValue, &$namedParameters )
+    {
+        $operatorValue = str_replace( "\n"
+                                      "<br />",
+                                      $operatorValue );
+    }
+
+    /// The array of operators, used for registering operators
+    var $Operators;
+}
+
+?>
