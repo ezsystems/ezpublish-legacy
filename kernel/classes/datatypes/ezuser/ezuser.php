06ded46858fc6faef68960dff989359218f3ce16
Bård Farstad <bf@ez.no>
2003-04-25 09:45:20 +0000

    - Fixed cache bug in eZ user. instance() returned a common user object for all instances
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@2378 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezuser/ezuser.php
+++ kernel/classes/datatypes/ezuser/ezuser.php
@@ -447,7 +447,8 @@ class eZUser extends eZPersistentObject
             eZDebugSetting::writeDebug( 'kernel-user', $userRow, 'user row' );
             $user =& new eZUser( $userRow );
             eZDebugSetting::writeDebug( 'kernel-user', $user, 'user' );
-            $GLOBALS["eZUserGlobalInstance"] =& $user;
+            $userID = $user->attribute( 'contentobject_id' );
+            $GLOBALS["eZUserGlobalInstance_$userID"] =& $user;
             $http->setSessionVariable( 'eZUserLoggedInID', $userRow['contentobject_id'] );
             return $user;
         }
@@ -483,7 +484,7 @@ class eZUser extends eZPersistentObject
     */
     function &instance( $id = false )
     {
-        $currentUser =& $GLOBALS["eZUserGlobalInstance"];
+        $currentUser =& $GLOBALS["eZUserGlobalInstance_$id"];
         if ( get_class( $currentUser ) == 'ezuser' )
         {
             return $currentUser;
