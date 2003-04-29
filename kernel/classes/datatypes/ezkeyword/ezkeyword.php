d8e0d65217c7056924cda0f02901d7dc3b815c38
Bård Farstad <bf@ez.no>
2003-04-29 13:19:50 +0000

    Added new datatype ezkeyword.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@2392 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ kernel/classes/datatypes/ezkeyword/ezkeyword.php
@@ -0,0 +1,139 @@
+<?php
+//
+// Definition of eZKeyword class
+//
+// Created on: <29-Apr-2003 15:18:15 bf>
+//
+// Copyright (C) 1999-2003 eZ systems as. All rights reserved.
+//
+// This source file is part of the eZ publish (tm) Open Source Content
+// Management System.
+//
+// This file may be distributed and/or modified under the terms of the
+// "GNU General Public License" version 2 as published by the Free
+// Software Foundation and appearing in the file LICENSE.GPL included in
+// the packaging of this file.
+//
+// Licencees holding valid "eZ publish professional licences" may use this
+// file in accordance with the "eZ publish professional licence" Agreement
+// provided with the Software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+// THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE.
+//
+// The "eZ publish professional licence" is available at
+// http://ez.no/products/licences/professional/. For pricing of this licence
+// please contact us via e-mail to licence@ez.no. Further contact
+// information is available at http://ez.no/home/contact/.
+//
+// The "GNU General Public License" (GPL) is available at
+// http://www.gnu.org/copyleft/gpl.html.
+//
+// Contact licence@ez.no if any conditions of this licencing isn't clear to
+// you.
+//
+
+/*!
+  \class eZKeyword ezkeyword.php
+  \ingroup eZKernel
+  \brief A content datatype which handles keyword index instances
+
+*/
+
+class eZKeyword
+{
+    /*!
+     Construct a new keyword instance
+    */
+    function eZKeyword( )
+    {
+    }
+
+    /*!
+     Initialze the keyword index
+    */
+    function initializeKeyword( $keywordString )
+    {
+        $keywordArray =& explode( ",", $keywordString );
+        foreach ( array_keys( $keywordArray ) as $key )
+        {
+            if ( trim( $keywordArray[$key] ) != "" )
+                $this->KeywordArray[$key] = trim( $keywordArray[$key] );
+        }
+    }
+
+    /*!
+     Stores the keyword index to database
+    */
+    function store( &$attribute )
+    {
+        $db =& eZDB::instance();
+
+        $object =& $attribute->attribute( 'object' );
+        $classID = $object->attribute( 'contentclass_id' );
+        // Get already existing keywords
+        $wordArray = array();
+        $wordsString = implode( '\',\'', $this->KeywordArray );
+        $existingWords =& $db->arrayQuery( "SELECT * FROM ezkeyword WHERE keyword IN ( '$wordsString' ) AND class_id='$classID' " );
+
+        $keywordDiff = array();
+        // Find out which words to store
+        foreach ( $this->KeywordArray as $keyword )
+        {
+            $wordExists = false;
+            foreach ( $existingWords as $existingKeyword )
+            {
+                if ( $keyword == $existingKeyword['keyword'] )
+                {
+                     $wordExists = true;
+                     break;
+                }
+            }
+
+            if ( $wordExists == false )
+            {
+                $keywordDiff[] = $keyword;
+            }
+        }
+
+        // Store every new keyword
+        foreach ( $keywordDiff as $keyword )
+        {
+            $keyword = trim( $keyword );
+            $keyword = $db->escapeString( $keyword );
+            $db->query( "INSERT INTO ezkeyword ( keyword, class_id ) VALUES ( '$keyword', '$classID' )" );
+
+            $keywordID = $db->lastSerialID();
+            $existingWords[] = array( 'keyword' => $keywordID, 'id' => $keywordID );
+        }
+
+        // Only store relation to new keywords
+        // Store relations to keyword for this content object
+        foreach ( $existingWords as $keywordArray )
+        {
+            $db->query( "INSERT INTO ezkeyword_attribute_link ( keyword_id, objectattribute_id ) VALUES ( '" . $keywordArray['id'] ."', '" . $attribute->attribute( 'id' ) . "' )" );
+        }
+    }
+
+    /*!
+     Sets the keyword index
+    */
+    function setKeywordArray( $keywords )
+    {
+        $this->KeywordArray =& $keywords;
+    }
+
+    /*!
+     Returns the keyword index
+    */
+    function &keywordArray( )
+    {
+        return $this->KeywordArray;
+    }
+
+    /// Contains the keywords
+    var $KeywordArray = array();
+}
+
+?>
---
+++ kernel/classes/datatypes/ezkeyword/ezkeywordtype.php
@@ -0,0 +1,186 @@
+<?php
+//
+// Definition of eZKeywordType class
+//
+// Created on: <29-Apr-2003 14:59:12 bf>
+//
+// Copyright (C) 1999-2003 eZ systems as. All rights reserved.
+//
+// This source file is part of the eZ publish (tm) Open Source Content
+// Management System.
+//
+// This file may be distributed and/or modified under the terms of the
+// "GNU General Public License" version 2 as published by the Free
+// Software Foundation and appearing in the file LICENSE.GPL included in
+// the packaging of this file.
+//
+// Licencees holding valid "eZ publish professional licences" may use this
+// file in accordance with the "eZ publish professional licence" Agreement
+// provided with the Software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+// THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE.
+//
+// The "eZ publish professional licence" is available at
+// http://ez.no/products/licences/professional/. For pricing of this licence
+// please contact us via e-mail to licence@ez.no. Further contact
+// information is available at http://ez.no/home/contact/.
+//
+// The "GNU General Public License" (GPL) is available at
+// http://www.gnu.org/copyleft/gpl.html.
+//
+// Contact licence@ez.no if any conditions of this licencing isn't clear to
+// you.
+//
+
+/*!
+  \class eZKeywordType ezkeywordtype.php
+  \ingroup eZKernel
+  \brief A content datatype which handles keyword indexes
+
+*/
+
+include_once( 'kernel/classes/ezdatatype.php' );
+include_once( 'kernel/common/i18n.php' );
+
+include_once( 'kernel/classes/datatypes/ezkeyword/ezkeyword.php' );
+
+define( 'EZ_DATATYPESTRING_KEYWORD', 'ezkeyword' );
+
+class eZKeywordType extends eZDataType
+{
+    /*!
+     Initializes with a keyword id and a description.
+    */
+    function eZKeywordType()
+    {
+        $this->eZDataType( EZ_DATATYPESTRING_KEYWORD, 'Keyword' );
+    }
+
+    /*!
+     Sets the default value.
+    */
+    function initializeObjectAttribute( &$contentObjectAttribute, $currentVersion, &$originalContentObjectAttribute )
+    {
+    }
+
+    /*!
+     Validates the input and returns true if the input was
+     valid for this datatype.
+    */
+    function validateObjectAttributeHTTPInput( &$http, $base, &$contentObjectAttribute )
+    {
+        return EZ_INPUT_VALIDATOR_STATE_VALID;
+    }
+
+    /*!
+     Fetches the http post var keyword input and stores it in the data instance.
+    */
+    function fetchObjectAttributeHTTPInput( &$http, $base, &$contentObjectAttribute )
+    {
+        if ( $http->hasPostVariable( $base . '_ezkeyword_data_text_' . $contentObjectAttribute->attribute( 'id' ) ) )
+        {
+            $data =& $http->postVariable( $base . '_ezkeyword_data_text_' . $contentObjectAttribute->attribute( 'id' ) );
+            $keyword = new eZKeyword();
+            $keyword->initializeKeyword( $data );
+            $contentObjectAttribute->setContent( $keyword );
+            return true;
+        }
+        return false;
+    }
+
+    /*!
+     Does nothing since it uses the data_text field in the content object attribute.
+     See fetchObjectAttributeHTTPInput for the actual storing.
+    */
+    function storeObjectAttribute( &$attribute )
+    {
+        // create keyword index
+        $keyword =& $attribute->content();
+        $keyword->store( $attribute );
+    }
+
+    function storeClassAttribute( &$attribute, $version )
+    {
+    }
+
+    function storeDefinedClassAttribute( &$attribute )
+    {
+    }
+
+    /*!
+     \reimp
+    */
+    function validateClassAttributeHTTPInput( &$http, $base, &$classAttribute )
+    {
+        return EZ_INPUT_VALIDATOR_STATE_ACCEPTED;
+    }
+
+    /*!
+     \reimp
+    */
+    function fixupClassAttributeHTTPInput( &$http, $base, &$classAttribute )
+    {
+    }
+
+    /*!
+     \reimp
+    */
+    function fetchClassAttributeHTTPInput( &$http, $base, &$classAttribute )
+    {
+        return true;
+    }
+
+    /*!
+     Returns the content.
+    */
+    function &objectAttributeContent( &$attribute )
+    {
+        return "test";
+    }
+
+    /*!
+     Returns the meta data used for storing search indeces.
+    */
+    function metaData( &$contentObjectAttribute )
+    {
+        return "KEYWORDS to index 42";
+    }
+
+    /*!
+     \reuturn the collect information action if enabled
+    */
+    function contentActionList( &$classAttribute )
+    {
+        return array();
+    }
+
+    /*!
+     Returns the content of the keyword for use as a title
+    */
+    function title( &$contentObjectAttribute )
+    {
+        return "FIX ME";
+    }
+
+    /*!
+     \reimp
+    */
+    function isIndexable()
+    {
+        return true;
+    }
+
+    /*!
+     \reimp
+    */
+    function isInformationCollector()
+    {
+        return false;
+    }
+}
+
+eZDataType::register( EZ_DATATYPESTRING_KEYWORD, 'ezkeywordtype' );
+
+?>
