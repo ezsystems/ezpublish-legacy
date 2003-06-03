7c40b1c98b07ada65199128c41b2c95c522b4416
Bård Farstad <bf@ez.no>
2003-06-03 08:13:15 +0000

    - Fixed bug in attribute parsing with the - character, e.g. SOAP-ENV
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@2633 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezxml/classes/ezxml.php
+++ lib/ezxml/classes/ezxml.php
@@ -371,12 +371,10 @@ class eZXML
     {
         $ret = false;
 
-        preg_match_all( "/([a-zA-Z0-9:-_]+\s*=\s*(\"|').*?(\\2))/i",  $attributeString, $attributeArray );
+        preg_match_all( "/([a-zA-Z0-9:\-_]+\s*=\s*(\"|').*?(\\2))/i",  $attributeString, $attributeArray );
 
         foreach ( $attributeArray[0] as $attributePart )
         {
-//            $attributePart = $attributePart;
-
             if ( trim( $attributePart ) != "" && trim( $attributePart ) != "/" )
             {
                 $attributeNamespaceURI = false;
