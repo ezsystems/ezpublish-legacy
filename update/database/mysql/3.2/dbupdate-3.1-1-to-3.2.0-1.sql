e1b074304a74c8b50aa9d4d6bfe7cebfb3e8fd61
Wenyue Yu <wy@ez.no>
2003-07-11 06:25:27 +0000

    - Create table ezurl_object_link.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@2843 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/3.2/dbupdate-3.1-1-to-3.2.0-1.sql
+++ update/database/mysql/3.2/dbupdate-3.1-1-to-3.2.0-1.sql
@@ -1,3 +1,10 @@
 alter table ezcontentclass_attribute add column data_text5 text;
 alter table ezsearch_object_word_link add column identifier varchar(255) not null default '';
 alter table ezsearch_object_word_link add column integer_value integer not null default '0';
+
+CREATE TABLE ezurl_object_link (
+  url_id int(11) NOT NULL default '0',
+  contentobject_attribute_id int(11) NOT NULL default '0',
+  contentobject_attribute_version int(11) NOT NULL default '0',
+  PRIMARY KEY (url_id,contentobject_attribute_id,contentobject_attribute_version)
+);
