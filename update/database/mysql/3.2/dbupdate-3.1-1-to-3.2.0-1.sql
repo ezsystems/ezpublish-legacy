f660ea96c916832ed6e2eb17a815e40ff531a1ae
Bård Farstad <bf@ez.no>
2003-07-23 08:35:12 +0000

    Changed order of SQL.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@2920 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/3.2/dbupdate-3.1-1-to-3.2.0-1.sql
+++ update/database/mysql/3.2/dbupdate-3.1-1-to-3.2.0-1.sql
@@ -30,7 +30,7 @@ INSERT INTO ezsite_data (name, value) VALUES('ezpublish-version', '3.2.0');
 INSERT INTO ezsite_data (name, value) VALUES('ezpublish-release', '1');
 
 
+drop index ezcontentobject_tree_depth on ezcontentobject_tree;
 create index ezsearch_word_object_count on ezsearch_word(object_count);
 create index ezcontentobject_status on ezcontentobject( status );
 create index ezcontentobject_tree_path_depth on ezcontentobject_tree( path_string, depth );
-drop index ezcontentobject_tree_depth on ezcontentobject_tree;
