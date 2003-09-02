8cdb2de016adbfbf0004eb0eb5eeab3d5a1e5cfd
Jan Borsodi <jb@ez.no>
2003-09-02 06:43:49 +0000

    - Updated version information to reflect that this is a 3.3 alpha branch.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@3268 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/common.sh
+++ bin/shell/common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION="3.2.0-2"
-VERSION_NICK="3.2 beta2"
+VERSION="3.3.0-1"
+VERSION_NICK="3.3 alpha"
 
 RES_COL=60
 # terminal sequence to move to that column. You could change this
---
+++ update/database/mysql/3.3/dbupdate-3.2-1-to-3.3.0-1.sql
@@ -0,0 +1,2 @@
+UPDATE ezsite_data SET value='3.3.0' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
