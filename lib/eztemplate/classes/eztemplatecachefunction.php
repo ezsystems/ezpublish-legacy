16366456256c2cd1ab3c18a933f7ffaad7be5ec0
Jan Borsodi <jb@ez.no>
2003-09-10 13:35:11 +0000

    - Changed cache block code to only rely on TemplateCache setting, not TemplateCompile also.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.2@3432 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/eztemplate/classes/eztemplatecachefunction.php
+++ lib/eztemplate/classes/eztemplatecachefunction.php
@@ -71,8 +71,7 @@ class eZTemplateCacheFunction
             {
                 // Check for disabled cache.
                 $ini =& eZINI::instance();
-                if ( $ini->variable( 'TemplateSettings', 'TemplateCache' ) == 'disabled' or
-                     $ini->variable( 'TemplateSettings', 'TemplateCompile' ) != 'enabled' )
+                if ( $ini->variable( 'TemplateSettings', 'TemplateCache' ) == 'disabled' )
                 {
                     $children = $functionChildren;
 
