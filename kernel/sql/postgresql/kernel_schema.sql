b9dbfbcab654a37e0b1f37b799c3a8f07aaf4f93
Jan Borsodi <jb@ez.no>
2003-09-25 11:06:41 +0000

    - Fixed index creation in postgresql.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.2@3538 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/sql/postgresql/kernel_schema.sql
+++ kernel/sql/postgresql/kernel_schema.sql
@@ -3052,5 +3052,5 @@ CREATE TABLE ezsite_data (
 
 CREATE INDEX ezorder_item_order_id ON ezorder_item( order_id );
 CREATE INDEX ezproductcollection_item_productcollection_id ON ezproductcollection_item( productcollection_id );
-CREATE INDEX ezurlalias_source_url ON ezurlalias(source_url(255));
+CREATE INDEX ezurlalias_source_url ON ezurlalias(source_url);
 CREATE INDEX ezcontentobject_attribute_co_id_ver_lang_code ON ezcontentobject_attribute( contentobject_id, version, language_code);
