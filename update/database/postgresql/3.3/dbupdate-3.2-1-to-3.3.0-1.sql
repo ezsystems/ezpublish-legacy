96e31999f18996b1c24f442aa5d2bea782f90b80
Jan Borsodi <jb@ez.no>
2003-10-01 12:27:13 +0000

    - Added postgresql SQL update for new things in 3.3.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@3568 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/3.3/dbupdate-3.2-1-to-3.3.0-1.sql
+++ update/database/postgresql/3.3/dbupdate-3.2-1-to-3.3.0-1.sql
@@ -1,14 +1,23 @@
 UPDATE ezsite_data SET value='3.3.0' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
-ALTER TABLE ezinfocollection ADD COLUMN user_identifier varchar(34);
-ALTER TABLE ezinfocollection ADD COLUMN modified int NOT NULL DEFAULT 0;
-ALTER TABLE ezinfocollection_attribute ADD COLUMN contentobject_attribute_id int;
-ALTER TABLE ezinfocollection_attribute ADD COLUMN contentobject_id int;
+ALTER TABLE ezinfocollection ADD COLUMN user_identifier VARCHAR(34);
+ALTER TABLE ezinfocollection ADD COLUMN modified INT;
+ALTER TABLE ezinfocollection ALTER modified SET DEFAULT 0;
+ALTER TABLE ezinfocollection ALTER modified SET NOT NULL;
+ALTER TABLE ezinfocollection_attribute ADD COLUMN contentobject_attribute_id INT;
+ALTER TABLE ezinfocollection_attribute ADD COLUMN contentobject_id INT;
 
+
+CREATE SEQUENCE ezrss_export_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
 
 CREATE TABLE ezrss_export (
-  id integer NOT NULL auto_increment,
+  id integer DEFAULT nextval('ezrss_export_s'::text) NOT NULL,
   title varchar(255) default NULL,
   modifier_id integer default NULL,
   modified integer default NULL,
@@ -22,22 +31,36 @@ CREATE TABLE ezrss_export (
   status integer default NULL,
   site_access varchar(255) default NULL,
   PRIMARY KEY  (id)
-) TYPE=MyISAM;
+);
+
+CREATE SEQUENCE ezrss_export_item_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
 
 CREATE TABLE ezrss_export_item (
-  id integer NOT NULL auto_increment,
+  id integer DEFAULT nextval('ezrss_export_item_s'::text) NOT NULL,
   rssexport_id integer default NULL,
   source_node_id integer default NULL,
   class_id integer default NULL,
   title varchar(255) default NULL,
   description varchar(255) default NULL,
   PRIMARY KEY  (id)
-) TYPE=MyISAM;
+);
 
 CREATE INDEX ezrss_export_rsseid ON ezrss_export_item( rssexport_id );
 
+CREATE SEQUENCE ezrss_import_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
 CREATE TABLE ezrss_import (
-  id integer NOT NULL auto_increment,
+  id integer DEFAULT nextval('ezrss_import_s'::text) NOT NULL,
   name varchar(255) default NULL,
   url text,
   destination_node_id integer default NULL,
@@ -53,4 +76,4 @@ CREATE TABLE ezrss_import (
   status integer default NULL,
   object_owner_id integer default NULL,
   PRIMARY KEY  (id)
-) TYPE=MyISAM;
+);
