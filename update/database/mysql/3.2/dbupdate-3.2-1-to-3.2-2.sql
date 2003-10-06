aaec5d902e584163c12f5afbba41c2382d2370d0
Jan Borsodi <jb@ez.no>
2003-10-06 13:47:45 +0000

    - Added some new indexes to the database update.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.2@3593 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/3.2/dbupdate-3.2-1-to-3.2-2.sql
+++ update/database/mysql/3.2/dbupdate-3.2-1-to-3.2-2.sql
@@ -1,1 +1,8 @@
 UPDATE ezsite_data SET value='2' WHERE name='ezpublish-release';
+
+CREATE INDEX ezproductcollection_item_opt_item_id ON  ezproductcollection_item_opt(item_id);
+CREATE INDEX ezproductcollection_item_contentobject_id ON ezproductcollection_item(productcollection_id);
+CREATE INDEX ezbasket_session_id ON ezbasket(session_id);
+CREATE INDEX ezoperation_memento_memento_key_main ON ezoperation_memento(memento_key,main);
+CREATE INDEX eztrigger_fetch ON eztrigger(name(25),module_name(50),function_name(50));
+CREATE INDEX ezworkflow_process_process_key ON ezworkflow_process(process_key);
