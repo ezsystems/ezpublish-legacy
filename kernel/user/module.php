9e2cd7c11ae1b73388523dc206ade3c732fb3949
Sergiy Pushchin <sp@ez.no>
2003-10-09 12:59:17 +0000

    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@3649 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/user/module.php
+++ kernel/user/module.php
@@ -88,6 +88,7 @@ $ViewList["forgotpassword"] = array(
     "params" => array( "HashKey" ) );
 
 $ViewList["edit"] = array(
+    "function" => array( 'login' ),
     "script" => "edit.php",
     'single_post_actions' => array( 'ChangePasswordButton' => 'ChangePassword',
                                     'ChangeSettingButton' => 'ChangeSetting',
@@ -100,7 +101,9 @@ $ViewList["register"] = array(
     "params" => array( ),
     "default_navigation_part" => 'ezmynavigationpart',
     'single_post_actions' => array( 'PublishButton' => 'Publish',
-                                    'CancelButton' => 'Cancel' ) );
+                                    'CancelButton' => 'Cancel',
+                                    'CustomActionButton' => 'CustomAction'
+                                    ) );
 $ViewList["activate"] = array(
     "script" => "activate.php",
     "params" => array( "Hash" ) );
