a8787b2f95a0751d3c20a9e790424fe08aa21e09
Wenyue Yu <wy@ez.no>
2003-11-26 16:48:31 +0000

    - Added files to pkgs.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@4285 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ packages/news/files/default/file/var/news/cache/expiry.php
@@ -0,0 +1,12 @@
+<?php
+// Array of expiry timestamps for various system parts
+// Can be set to current date to force a total expiry
+$Timestamps = array( "user-role-cache" => 1069855195,
+                     "content-cache" => 1069841357,
+                     "urlalias-wildcard" => 1069245323,
+                     "content-complex-viewmode-cache" => 1069841359,
+                     "user-class-cache" => 1069416212,
+                     "user-info-cache" => 1069239780,
+                     "user-groups-cache" => 1069239780,
+                     "image-manager-alias" => 1069855785 );
+?>
