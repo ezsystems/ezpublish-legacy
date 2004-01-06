fcd690dc1b6bba90120cd865c4a1f2e596fc1109
Kåre Køhler Høvik <kk@ez.no>
2004-01-06 11:11:22 +0000

    - Fixed mysql server and client version information.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.2@4658 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezdb/classes/ezmysqldb.php
+++ lib/ezdb/classes/ezmysqldb.php
@@ -597,7 +597,7 @@ class eZMySQLDB extends eZDBInterface
     */
     function databaseServerVersion()
     {
-        $versionInfo = mysql_get_client_info();
+        $versionInfo = mysql_get_server_info();
 
         $versionArray = explode( '.', $versionInfo );
 
@@ -610,7 +610,7 @@ class eZMySQLDB extends eZDBInterface
     */
     function databaseClientVersion()
     {
-        $versionInfo = mysql_get_server_info();
+        $versionInfo = mysql_get_client_info();
 
         $versionArray = explode( '.', $versionInfo );
 
