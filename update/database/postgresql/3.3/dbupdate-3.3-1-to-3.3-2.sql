dde59c0ca76b73b468a17a0f0f3e235503c6b7c1
Kåre Køhler Høvik <kk@ez.no>
2004-01-28 08:30:02 +0000

    - Merged changes for 3.3-3 release from stable/3.3 branch.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@4929 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/3.3/dbupdate-3.3-1-to-3.3-2.sql
+++ update/database/postgresql/3.3/dbupdate-3.3-1-to-3.3-2.sql
@@ -1,2 +1,2 @@
-SELECT pg_catalog.setval ('ezcontentclass_s', (select max(id) from ezcontentclass), false);
+SELECT pg_catalog.setval ('ezcontentclass_s', (select max(id)+1 from ezcontentclass), false);
 UPDATE ezsite_data SET value='2' WHERE name='ezpublish-release';
