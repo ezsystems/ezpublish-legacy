74ef3c253b7d2837e62069d484ec3e69bfd0acee
Jan Borsodi <jb@ez.no>
2004-02-02 14:43:15 +0000

    - Removed some commented code from updatedtranslations.php,   also fixed the text output a bit.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.2@4967 a01eee8c-daba-0310-acae-fa49f3023285

--- update/common/scripts/updatetranslations.php
+++ update/common/scripts/updatetranslations.php
@@ -296,7 +296,7 @@ $complexTypes = array();
 
 $objectCount = eZContentObject::fetchListCount();
 
-print( "count=" . $objectCount . "\n" );
+$cli->output( "Going to update " . $objectCount . " objects" );
 
 $maxFetch = 30;
 $index = 0;
@@ -359,89 +359,6 @@ while ( $index < $objectCount )
         }
     }
 }
-
-
-// for ( $i = 0; $i < count( $attributeList ); ++$i )
-// {
-//     $attribute =& $attributeList[$i];
-//     $classDataTypeString = $attribute->attribute( 'data_type_string' );
-//     if ( !isset( $classDataTypeList[$classDataTypeString] ) )
-//     {
-//         $classDataType =& $attribute->attribute( 'data_type' );
-//         $sortKeyType = $classDataType->sortKeyType();
-//         if ( $sortKeyType )
-//         {
-//             $classDataTypeList[$classDataTypeString] = array( 'sort_key_type' => $sortKeyType );
-//         }
-//         else
-//             $classDataTypeList[$classDataTypeString] = false;
-//     }
-//     if ( $classDataTypeList[$classDataTypeString] )
-//     {
-//         $isSimpleType = false;
-//         foreach ( $simpleTypes as $simpleTypeKey => $simpleType )
-//         {
-//             if ( in_array( $classDataTypeString, $simpleType['datatypes'] ) )
-//             {
-//                 $isSimpleType = true;
-//                 if ( !isset( $simpleTypes[$simpleTypeKey]['id_list'] ) )
-//                     $simpleTypes[$simpleTypeKey]['id_list'] = array();
-//                 $simpleTypes[$simpleTypeKey]['id_list'][] = $attribute->attribute( 'id' );
-//                 break;
-//             }
-//         }
-//         if ( !$isSimpleType )
-//         {
-//             if ( !isset( $complexTypes[$classDataTypeString] ) )
-//                 $complexTypes[$classDataTypeString] = array();
-//             $complexTypes[$classDataTypeString][] = $attribute->attribute( 'id' );
-//         }
-//     }
-// }
-// unset( $attributeList );
-
-// foreach ( $simpleTypes as $simpleType )
-// {
-//     $cli->output( "Will update " . count( $simpleType['id_list'] )  ." class attribute(s) for datatype(s) " . implode( ", ", $simpleType['datatypes'] ) . " using field " . $simpleType['field'] );
-//     if ( count( $simpleType['id_list'] ) > 0 )
-//     {
-//         $sortField = $simpleType['sort_field'];
-//         $field = $simpleType['field'];
-//         $db =& eZDB::instance();
-//         $sql = "UPDATE ezcontentobject_attribute SET $sortField=$field WHERE contentclassattribute_id IN (" . implode( ",", $simpleType['id_list'] ) . ")";
-//         $db->query( $sql );
-//     }
-// }
-
-// foreach ( $complexTypes as $complexTypeString => $complexTypeList )
-// {
-//     $cli->output( "Will update " . count( $complexTypeList )  ." class attribute(s) for datatype " . $complexTypeString );
-//     $counter = 0;
-//     $column = 0;
-//     $objectAttributeList =& eZContentObjectAttribute::fetchListByClassID( $complexTypeList );
-//     $objectAttributeCount = count( $objectAttributeList );
-//     $cli->output( "Total of " . $objectAttributeCount . " attributes" );
-//     for ( $i = 0; $i < $objectAttributeCount; ++$i )
-//     {
-//         $objectAttribute =& $objectAttributeList[$i];
-//         // re-store the attribute
-//         $objectAttribute->store();
-//         $cli->output( ".", false );
-//         flush();
-//         ++$counter;
-//         ++$column;
-//         if ( $column > 70 or $counter == $objectAttributeCount )
-//         {
-//             $percent = ( $counter * 100 ) / ( $objectAttributeCount );
-//             $percentText = number_format( $percent, 2 );
-//             $cli->output( " $percentText%" );
-//             $column = 0;
-//         }
-//     }
-//     $cli->output();
-//     unset( $objectAttributeList );
-// }
-
 
 $script->shutdown();
 
