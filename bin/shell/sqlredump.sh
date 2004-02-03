553670c159b43d8f45acaf03c3a60c80290e17dd
Jan Borsodi <jb@ez.no>
2004-02-03 14:09:08 +0000

    - Added new script phpcheck.php which will do syntax check (lint) on   all PHP files it finds in the given paths. - Made sure makedist.sh runs phpcheck.sh before it starts the creation   of the distribution, if an error is found it exits with error code 1. - Added check for php executable in makedist.sh, sqlredump.sh,   redumpall.sh and phpcheck.sh.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@4982 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/sqlredump.sh
+++ bin/shell/sqlredump.sh
@@ -10,6 +10,11 @@ SCHEMAFILES=""
 USE_MYSQL=""
 USE_POSTGRESQL=""
 
+if ! which php &>/dev/null; then
+    echo "No PHP executable found, please add it to the path"
+    exit 1
+fi
+
 function help
 {
 	    echo "Usage: $0 [options] DBNAME SQLFILE [SQLFILE]..."
