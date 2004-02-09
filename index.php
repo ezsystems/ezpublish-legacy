34bb35da67d76da6b705392ecee141db260c4ce9
Derick Rethans <dr@ez.no>
2004-02-09 21:06:52 +0000

    - If sessionRequired is true, it set dbRequired to true five lines above, so   remove redundant check here.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@5050 a01eee8c-daba-0310-acae-fa49f3023285

--- index.php
+++ index.php
@@ -315,11 +315,12 @@ $check = eZHandlePreChecks( $siteBasics, $uri );
 include_once( 'kernel/common/i18n.php' );
 
 if ( $sessionRequired )
-    $dbRequired = true;
+{
+	$dbRequired = true;
+}
 
 $db = false;
-if ( $dbRequired or
-     $sessionRequired )
+if ( $dbRequired )
 {
     include_once( 'lib/ezdb/classes/ezdb.php' );
     $db =& eZDB::instance();
