6c3c3bb27a381c4e91999336cc0b8280559dc216
Vidar Langseid <vl@ez.no>
2004-02-26 14:39:15 +0000

    - Merged in bugfix from 32stable (Rev. 5306)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.3@5307 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobject.php
+++ kernel/classes/ezcontentobject.php
@@ -2263,11 +2263,14 @@ class eZContentObject extends eZPersistentObject
     function hasContentAction( $name )
     {
         $return = false;
-        foreach ( $this->ContentActionList as $action )
+        if ( is_array ( $this->ContentActionList ) )
         {
-            if ( $action['action'] == $name )
+            foreach ( $this->ContentActionList as $action )
             {
-                $return = true;
+                if ( $action['action'] == $name )
+                {
+                    $return = true;
+                }
             }
         }
         return $return;
