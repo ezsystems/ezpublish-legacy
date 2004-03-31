61af670955ae25d1197db2644b4c3682d3c6bce4
Derick Rethans <dr@ez.no>
2004-03-31 14:59:48 +0000

    - Fixed script to allow for filenames with spaces being checked. - Replaced tabs by spaces.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@5725 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/phpcheck.sh
+++ bin/shell/phpcheck.sh
@@ -18,35 +18,35 @@ PRINT=1
 
 for arg in $*; do
     case $arg in
-	--help|-h)
-	    echo "Usage: $0 [options] DIR [DIR]..."
-	    echo
-	    echo "Options: -h"
-	    echo "         --help                     This message"
-	    echo "         --exit-on-error            Exit on the first error"
-	    echo "         --errors-only              Will only print bad files, other texts are ommited"
-	    echo "         -q                         Quiet, do not print anything"
+        --help|-h)
+            echo "Usage: $0 [options] DIR [DIR]..."
+            echo
+            echo "Options: -h"
+            echo "         --help                     This message"
+            echo "         --exit-on-error            Exit on the first error"
+            echo "         --errors-only              Will only print bad files, other texts are ommited"
+            echo "         -q                         Quiet, do not print anything"
             echo
             echo "Example:"
             echo "$0 lib"
-	    exit 1
-	    ;;
-	--exit-on-error)
-	    EXIT_ON_ERROR=1
-	    ;;
-	--errors-only)
-	    ERRORS_ONLY=1
-	    ;;
-	-q)
-	    PRINT=0
-	    ;;
-	-*)
-	    echo "$arg: unkown option specified"
+            exit 1
+            ;;
+        --exit-on-error)
+            EXIT_ON_ERROR=1
+            ;;
+        --errors-only)
+            ERRORS_ONLY=1
+            ;;
+        -q)
+            PRINT=0
+            ;;
+        -*)
+            echo "$arg: unkown option specified"
             $0 -h
-	    exit 1
-	    ;;
-	*)
-	    DIRS="$DIRS $arg"
+            exit 1
+            ;;
+        *)
+            DIRS="$DIRS $arg"
     esac;
 done
 
@@ -61,52 +61,54 @@ global_error=0
 
 for dir in $DIRS; do
     if [ $PRINT -eq 1 ]; then
-	if [ $ERRORS_ONLY -eq 0 ]; then
-	    echo "Checking $dir"
-	fi
+        if [ $ERRORS_ONLY -eq 0 ]; then
+            echo "Checking $dir"
+        fi
     fi
-    files=`find $dir -name \*.php -type f`
+    files=`find $dir -name \*.php -type f -printf '%p;'`
     error=0
+    IFS=";"
     for file in $files; do
-	if [ $PRINT -eq 1 ]; then
-	    if [ $ERRORS_ONLY -eq 0 ]; then
-		echo -n $file
-	    fi
-	fi
-	php -l $file &>/dev/null
-	if [ $? -eq 0 ]; then
-	    if [ $PRINT -eq 1 ]; then
-		if [ $ERRORS_ONLY -eq 0 ]; then
-		    echo "`$MOVE_TO_COL`[`$SETCOLOR_SUCCESS`OK`$SETCOLOR_NORMAL`]"
-		fi
-	    fi
-	else
-	    error=1
-	    BAD_FILES="$BAD_FILES $file"
-	    if [ $PRINT -eq 1 ]; then
-		if [ $ERRORS_ONLY -eq 0 ]; then
-		    echo "`$MOVE_TO_COL`[`$SETCOLOR_FAILURE`Bad`$SETCOLOR_NORMAL`]";
-		else
-		    echo "`$SETCOLOR_FAILURE`$file`$SETCOLOR_NORMAL`";
-		fi
-	    fi
-	    if [ $EXIT_ON_ERROR -eq 1 ]; then
-		exit 1
-	    fi
-	fi
+        if [ $PRINT -eq 1 ]; then
+            if [ $ERRORS_ONLY -eq 0 ]; then
+                echo -n $file
+            fi
+        fi
+        IFS=" "
+        php -l $file &>/dev/null
+        if [ $? -eq 0 ]; then
+            if [ $PRINT -eq 1 ]; then
+                if [ $ERRORS_ONLY -eq 0 ]; then
+                    echo "`$MOVE_TO_COL`[`$SETCOLOR_SUCCESS`OK`$SETCOLOR_NORMAL`]"
+                fi
+            fi
+        else
+            error=1
+            BAD_FILES="$BAD_FILES $file"
+            if [ $PRINT -eq 1 ]; then
+                if [ $ERRORS_ONLY -eq 0 ]; then
+                    echo "`$MOVE_TO_COL`[`$SETCOLOR_FAILURE`Bad`$SETCOLOR_NORMAL`]";
+                else
+                    echo "`$SETCOLOR_FAILURE`$file`$SETCOLOR_NORMAL`";
+                fi
+            fi
+            if [ $EXIT_ON_ERROR -eq 1 ]; then
+                exit 1
+            fi
+        fi
     done
     if [ $error -eq 1 ]; then
-	global_error=1
+        global_error=1
     fi
 done
 
 if [ $global_error -eq 1 ]; then
     if [ $ERRORS_ONLY -eq 0 ]; then
-	if [ $PRINT -eq 1 ]; then echo; fi
-	if [ $PRINT -eq 1 ]; then echo "The following files have parser errors in them:"; fi
-	for file in $BAD_FILES; do
-	    if [ $PRINT -eq 1 ]; then echo "`$SETCOLOR_FAILURE`$file`$SETCOLOR_NORMAL`"; fi
-	done
+        if [ $PRINT -eq 1 ]; then echo; fi
+        if [ $PRINT -eq 1 ]; then echo "The following files have parser errors in them:"; fi
+        for file in $BAD_FILES; do
+            if [ $PRINT -eq 1 ]; then echo "`$SETCOLOR_FAILURE`$file`$SETCOLOR_NORMAL`"; fi
+        done
     fi
 fi
 
