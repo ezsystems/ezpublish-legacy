6ce4e5425a230a4d3cbf8591eec92fb9becee7a2
Jan Borsodi <jb@ez.no>
2004-04-13 12:14:47 +0000

    - Changed default setting for setup node to 48 (which is in cleandata.sql) - Fixed the update SQLs for PostgreSQL, the NOT NULL fields must be set with   separate commands.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@5883 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/3.4/unstable/dbupdate-3.4.0alpha2-to-3.4.0alpha3.sql
+++ update/database/postgresql/3.4/unstable/dbupdate-3.4.0alpha2-to-3.4.0alpha3.sql
@@ -1,11 +1,14 @@
 UPDATE ezsite_data SET value='3.4.0alpha3' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='3' WHERE name='ezpublish-release';
 
-ALTER TABLE ezcontentclass ADD COLUMN remote_id varchar(100) NOT NULL;
+ALTER TABLE ezcontentclass ADD COLUMN remote_id varchar(100);
+ALTER TABLE ezcontentclass ALTER remote_id SET NOT NULL;
 ALTER TABLE ezcontentclass ALTER remote_id SET default '';
 
-ALTER TABLE ezcontentobject_tree ADD COLUMN remote_id varchar(100) NOT NULL;
+ALTER TABLE ezcontentobject_tree ADD COLUMN remote_id varchar(100);
+ALTER TABLE ezcontentobject_tree ALTER remote_id SET NOT NULL;
 ALTER TABLE ezcontentobject_tree ALTER remote_id SET default '';
 
-ALTER TABLE eznode_assignment ADD COLUMN node_remote_id varchar(100) NOT NULL;
+ALTER TABLE eznode_assignment ADD COLUMN node_remote_id varchar(100);
+ALTER TABLE eznode_assignment ALTER node_remote_id NOT NULL;
 ALTER TABLE eznode_assignment ALTER node_remote_id SET default '';
