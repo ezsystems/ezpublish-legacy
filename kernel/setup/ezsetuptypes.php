d9078f4771953bd064b486dd16acc9efcb1f0591
Jan Borsodi <jb@ez.no>
2004-04-16 14:08:14 +0000

    - New setup file to handle site types.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@6000 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ kernel/setup/ezsetuptypes.php
@@ -0,0 +1,55 @@
+<?php
+//
+// Created on: <16-Apr-2004 10:04:58 amos>
+//
+// Copyright (C) 1999-2004 eZ systems as. All rights reserved.
+//
+// This source file is part of the eZ publish (tm) Open Source Content
+// Management System.
+//
+// This file may be distributed and/or modified under the terms of the
+// "GNU General Public License" version 2 as published by the Free
+// Software Foundation and appearing in the file LICENSE.GPL included in
+// the packaging of this file.
+//
+// Licencees holding valid "eZ publish professional licences" may use this
+// file in accordance with the "eZ publish professional licence" Agreement
+// provided with the Software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+// THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE.
+//
+// The "eZ publish professional licence" is available at
+// http://ez.no/products/licences/professional/. For pricing of this licence
+// please contact us via e-mail to licence@ez.no. Further contact
+// information is available at http://ez.no/home/contact/.
+//
+// The "GNU General Public License" (GPL) is available at
+// http://www.gnu.org/copyleft/gpl.html.
+//
+// Contact licence@ez.no if any conditions of this licencing isn't clear to
+// you.
+//
+
+function eZSetupTypes()
+{
+    return array( 'blog' => array( 'name' => 'Blog',
+                                   'identifier' => 'blog',
+                                   'thumbnail' => 'blog_thumbnail.png' ),
+                  'corporate' => array( 'name' => 'Corporate',
+                                        'identifier' => 'corporate',
+                                        'thumbnail' => 'corporate_thumbnail.png' ),
+                  'forum' => array( 'name' => 'Forum',
+                                    'identifier' => 'forum',
+                                    'thumbnail' => 'forum_thumbnail.png' ),
+                  'gallery' => array( 'name' => 'Gallery',
+                                      'identifier' => 'gallery',
+                                      'thumbnail' => 'gallery_thumbnail.png' ),
+                  'intranet' => array( 'name' => 'Intranet',
+                                       'identifier' => 'intranet',
+                                       'thumbnail' => 'intranet_thumbnail.png' )
+                  );
+}
+
+?>
