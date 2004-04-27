2eccfcdc9d468554d8cd9b62dfcfb28a8a369a6f
Bård Farstad <bf@ez.no>
2004-04-27 14:09:55 +0000

    - Fixed bug with user redirection in basket with default account handler   http://ez.no/community/bug_reports/redirectafteruserregister_set_by_ezdefaultshopaccounthandler_but_never_used ( Merged from stable/3.3 r6285 )
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@6286 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/shopaccounthandlers/ezdefaultshopaccounthandler.php
+++ kernel/classes/shopaccounthandlers/ezdefaultshopaccounthandler.php
@@ -83,7 +83,7 @@ class eZDefaultShopAccountHandler
 
     function fetchAccountInformation( &$module )
     {
-        eZHTTPTool::setSessionVariable( 'RedirectAfterUserRegister', '/shop/basket/' );
+        eZHTTPTool::setSessionVariable( 'RedirectAfterLogin', '/shop/basket/' );
         eZHTTPTool::setSessionVariable( 'DoCheckoutAutomatically', true );
         $module->redirectTo( '/user/login/' );
     }
