e5b7655f24edeb18b6f722a03ab717d7b9fe9608
Jan Borsodi <jb@ez.no>
2004-05-04 19:57:25 +0000

    - Changed the names and summaries of all the created packages.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@6368 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/makestylepackages.sh
+++ bin/shell/makestylepackages.sh
@@ -28,7 +28,7 @@ for arg in $*; do
 	    echo
 	    echo "Options: -h"
 	    echo "         --help                     This message"
-	    echo "         --build-root=DIR           Set build root, default is /tmp"
+	    echo "         --export-path=DIR          Where to export the packages, default is 'packages/styles'"
 	    echo
 	    echo "* Warning: Using these options will not make a valid distribution"
 	    exit 1
@@ -66,103 +66,103 @@ mkdir -p "$SITE_PACKAGES_EXPORT" || exit 1
 
 
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-	create t1 "T1 CSS" "$VERSION" import  -- \
-	set t1 type sitestyle -- \
-	add t1 file -n sitecssfile design/base/stylesheets/t1/site-colors.css -- \
-	add t1 file -n classescssfile design/base/stylesheets/t1/classes-colors.css -- \
-	add t1 thumbnail -n thumbnail design/standard/images/setup/t1.png -- \
-	add t1 file -n image design/base/images/t1/t1-tab-selected-right.gif -- \
-	add t1 file -n image design/base/images/t1/t1-tab-selected-left.gif -- \
-	add t1 file -n image design/base/images/t1/t1-arrow.gif -- \
-	add t1 file -n image design/base/images/t1/t1-bin.gif -- \
-	add t1 file -n image design/base/images/t1/t1-button.gif -- \
-	add t1 file -n image design/base/images/t1/t1-tab-normal-left.gif -- \
-	add t1 file -n image design/base/images/t1/t1-tab-normal-right.gif -- \
-	export t1 -d "$EXPORT_PATH" || exit 1
+	create t01 "Theme 01" "$VERSION" import  -- \
+	set t01 type sitestyle -- \
+	add t01 file -n sitecssfile design/base/stylesheets/t1/site-colors.css -- \
+	add t01 file -n classescssfile design/base/stylesheets/t1/classes-colors.css -- \
+	add t01 thumbnail -n thumbnail design/standard/images/setup/t1.png -- \
+	add t01 file -n image design/base/images/t1/t1-tab-selected-right.gif -- \
+	add t01 file -n image design/base/images/t1/t1-tab-selected-left.gif -- \
+	add t01 file -n image design/base/images/t1/t1-arrow.gif -- \
+	add t01 file -n image design/base/images/t1/t1-bin.gif -- \
+	add t01 file -n image design/base/images/t1/t1-button.gif -- \
+	add t01 file -n image design/base/images/t1/t1-tab-normal-left.gif -- \
+	add t01 file -n image design/base/images/t1/t1-tab-normal-right.gif -- \
+	export t01 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-	create t2 "T2 CSS" "$VERSION" import -- \
-	set t2 type sitestyle -- \
-	add t2 file -n sitecssfile design/base/stylesheets/t2/site-colors.css -- \
-	add t2 file -n classescssfile design/base/stylesheets/t2/classes-colors.css -- \
-	add t2 thumbnail -n thumbnail design/standard/images/setup/t2.png -- \
-	add t2 file -n image design/base/images/t2/ratingstar.png -- \
-	add t2 file -n image design/base/images/t2/arrow.gif -- \
-	add t2 file -n image design/base/images/t2/bin.gif -- \
-	add t2 file -n image design/base/images/t2/button.gif -- \
-	export t2 -d "$EXPORT_PATH" || exit 1
+	create t02 "Theme 02" "$VERSION" import -- \
+	set t02 type sitestyle -- \
+	add t02 file -n sitecssfile design/base/stylesheets/t2/site-colors.css -- \
+	add t02 file -n classescssfile design/base/stylesheets/t2/classes-colors.css -- \
+	add t02 thumbnail -n thumbnail design/standard/images/setup/t2.png -- \
+	add t02 file -n image design/base/images/t2/ratingstar.png -- \
+	add t02 file -n image design/base/images/t2/arrow.gif -- \
+	add t02 file -n image design/base/images/t2/bin.gif -- \
+	add t02 file -n image design/base/images/t2/button.gif -- \
+	export t02 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t3 "T3 CSS" "$VERSION" import -- \
-	set t3 type sitestyle -- \
-	add t3 file -n sitecssfile design/base/stylesheets/t3/site-colors.css -- \
-	add t3 file -n classescssfile design/base/stylesheets/t3/classes-colors.css -- \
-	add t3 thumbnail -n thumbnail design/standard/images/setup/t3.png -- \
-	add t3 file -n image design/base/images/t3/ratingstar.png -- \
-	add t3 file -n image design/base/images/t3/arrow.gif -- \
-	add t3 file -n image design/base/images/t3/bin.gif -- \
-	add t3 file -n image design/base/images/t3/button.gif -- \
-	export t3 -d "$EXPORT_PATH" || exit 1
+        create t03 "Theme 03" "$VERSION" import -- \
+	set t03 type sitestyle -- \
+	add t03 file -n sitecssfile design/base/stylesheets/t3/site-colors.css -- \
+	add t03 file -n classescssfile design/base/stylesheets/t3/classes-colors.css -- \
+	add t03 thumbnail -n thumbnail design/standard/images/setup/t3.png -- \
+	add t03 file -n image design/base/images/t3/ratingstar.png -- \
+	add t03 file -n image design/base/images/t3/arrow.gif -- \
+	add t03 file -n image design/base/images/t3/bin.gif -- \
+	add t03 file -n image design/base/images/t3/button.gif -- \
+	export t03 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t4 "T4 CSS" "$VERSION" import -- \
-	set t4 type sitestyle -- \
-	add t4 file -n sitecssfile design/base/stylesheets/t4/site-colors.css -- \
-	add t4 file -n classescssfile design/base/stylesheets/t4/classes-colors.css -- \
-	add t4 thumbnail -n thumbnail design/standard/images/setup/t4.png -- \
-	add t4 file -n image design/base/images/t4/ratingstar.png -- \
-	add t4 file -n image design/base/images/t4/arrow.gif -- \
-	add t4 file -n image design/base/images/t4/bin.gif -- \
-	add t4 file -n image design/base/images/t4/button.gif -- \
-	export t4 -d "$EXPORT_PATH" || exit 1
+        create t04 "Theme 04" "$VERSION" import -- \
+	set t04 type sitestyle -- \
+	add t04 file -n sitecssfile design/base/stylesheets/t4/site-colors.css -- \
+	add t04 file -n classescssfile design/base/stylesheets/t4/classes-colors.css -- \
+	add t04 thumbnail -n thumbnail design/standard/images/setup/t4.png -- \
+	add t04 file -n image design/base/images/t4/ratingstar.png -- \
+	add t04 file -n image design/base/images/t4/arrow.gif -- \
+	add t04 file -n image design/base/images/t4/bin.gif -- \
+	add t04 file -n image design/base/images/t4/button.gif -- \
+	export t04 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t5 "T5 CSS" "$VERSION" import -- \
-	set t5 type sitestyle -- \
-	add t5 file -n sitecssfile design/base/stylesheets/t5/site-colors.css -- \
-	add t5 file -n classescssfile design/base/stylesheets/t5/classes-colors.css -- \
-	add t5 thumbnail -n thumbnail design/standard/images/setup/t5.png -- \
-	add t5 file -n image design/base/images/t5/ratingstar.png -- \
-	add t5 file -n image design/base/images/t5/arrow.gif -- \
-	add t5 file -n image design/base/images/t5/bin.gif -- \
-	add t5 file -n image design/base/images/t5/button.gif -- \
-	export t5 -d "$EXPORT_PATH" || exit 1
+        create t05 "Theme 05" "$VERSION" import -- \
+	set t05 type sitestyle -- \
+	add t05 file -n sitecssfile design/base/stylesheets/t5/site-colors.css -- \
+	add t05 file -n classescssfile design/base/stylesheets/t5/classes-colors.css -- \
+	add t05 thumbnail -n thumbnail design/standard/images/setup/t5.png -- \
+	add t05 file -n image design/base/images/t5/ratingstar.png -- \
+	add t05 file -n image design/base/images/t5/arrow.gif -- \
+	add t05 file -n image design/base/images/t5/bin.gif -- \
+	add t05 file -n image design/base/images/t5/button.gif -- \
+	export t05 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t6 "T6 CSS" "$VERSION" import -- \
-	set t6 type sitestyle -- \
-	add t6 file -n sitecssfile design/base/stylesheets/t6/site-colors.css -- \
-	add t6 file -n classescssfile design/base/stylesheets/t6/classes-colors.css -- \
-	add t6 thumbnail -n thumbnail design/standard/images/setup/t6.png -- \
-	add t6 file -n image design/base/images/t6/arrow.gif -- \
-	add t6 file -n image design/base/images/t6/button.gif -- \
-	export t6 -d "$EXPORT_PATH" || exit 1
+        create t06 "Theme 06" "$VERSION" import -- \
+	set t06 type sitestyle -- \
+	add t06 file -n sitecssfile design/base/stylesheets/t6/site-colors.css -- \
+	add t06 file -n classescssfile design/base/stylesheets/t6/classes-colors.css -- \
+	add t06 thumbnail -n thumbnail design/standard/images/setup/t6.png -- \
+	add t06 file -n image design/base/images/t6/arrow.gif -- \
+	add t06 file -n image design/base/images/t6/button.gif -- \
+	export t06 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t7 "T7 CSS" "$VERSION" import -- \
-	set t7 type sitestyle -- \
-	add t7 file -n sitecssfile design/base/stylesheets/t7/site-colors.css -- \
-	add t7 file -n classescssfile design/base/stylesheets/t7/classes-colors.css -- \
-	add t7 thumbnail -n thumbnail design/standard/images/setup/t7.png -- \
-	add t7 file -n image design/base/images/t7/arrow.gif -- \
-	add t7 file -n image design/base/images/t7/button.gif -- \
-	export t7 -d "$EXPORT_PATH" || exit 1
+        create t07 "Theme 07" "$VERSION" import -- \
+	set t07 type sitestyle -- \
+	add t07 file -n sitecssfile design/base/stylesheets/t7/site-colors.css -- \
+	add t07 file -n classescssfile design/base/stylesheets/t7/classes-colors.css -- \
+	add t07 thumbnail -n thumbnail design/standard/images/setup/t7.png -- \
+	add t07 file -n image design/base/images/t7/arrow.gif -- \
+	add t07 file -n image design/base/images/t7/button.gif -- \
+	export t07 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t8 "T8 CSS" "$VERSION" import -- \
-	set t8 type sitestyle -- \
-	add t8 file -n sitecssfile design/base/stylesheets/t8/site-colors.css -- \
-	add t8 file -n classescssfile design/base/stylesheets/t8/classes-colors.css -- \
-	add t8 thumbnail -n thumbnail design/standard/images/setup/t8.png -- \
-	add t8 file -n image design/base/images/t8/arrow.gif -- \
-	add t8 file -n image design/base/images/t8/button.gif -- \
-        add t8 file -n image design/base/images/t8/bg.gif -- \
-        add t8 file -n image design/base/images/t8/bin.gif -- \
-	export t8 -d "$EXPORT_PATH" || exit 1
+        create t08 "Theme 08" "$VERSION" import -- \
+	set t08 type sitestyle -- \
+	add t08 file -n sitecssfile design/base/stylesheets/t8/site-colors.css -- \
+	add t08 file -n classescssfile design/base/stylesheets/t8/classes-colors.css -- \
+	add t08 thumbnail -n thumbnail design/standard/images/setup/t8.png -- \
+	add t08 file -n image design/base/images/t8/arrow.gif -- \
+	add t08 file -n image design/base/images/t8/button.gif -- \
+        add t08 file -n image design/base/images/t8/bg.gif -- \
+        add t08 file -n image design/base/images/t8/bin.gif -- \
+	export t08 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t9 "T9 CSS" "$VERSION" import -- \
-	set t9 type sitestyle -- \
-	add t9 file -n sitecssfile design/base/stylesheets/t9/site-colors.css -- \
-	add t9 file -n classescssfile design/base/stylesheets/t9/classes-colors.css -- \
-	add t9 thumbnail -n thumbnail design/standard/images/setup/t9.png -- \
-	add t9 file -n image design/base/images/t9/arrow.gif -- \
-	add t9 file -n image design/base/images/t9/button.gif -- \
-	export t9 -d "$EXPORT_PATH" || exit 1
+        create t09 "Theme 09" "$VERSION" import -- \
+	set t09 type sitestyle -- \
+	add t09 file -n sitecssfile design/base/stylesheets/t9/site-colors.css -- \
+	add t09 file -n classescssfile design/base/stylesheets/t9/classes-colors.css -- \
+	add t09 thumbnail -n thumbnail design/standard/images/setup/t9.png -- \
+	add t09 file -n image design/base/images/t9/arrow.gif -- \
+	add t09 file -n image design/base/images/t9/button.gif -- \
+	export t09 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t10 "T10 CSS" "$VERSION" import -- \
+        create t10 "Theme 10" "$VERSION" import -- \
 	set t10 type sitestyle -- \
 	add t10 file -n sitecssfile design/base/stylesheets/t10/site-colors.css -- \
 	add t10 file -n classescssfile design/base/stylesheets/t10/classes-colors.css -- \
@@ -171,7 +171,7 @@ mkdir -p "$SITE_PACKAGES_EXPORT" || exit 1
 	add t10 file -n image design/base/images/t10/button.gif -- \
 	export t10 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t11 "T11 CSS" "$VERSION" import -- \
+        create t11 "Theme 11" "$VERSION" import -- \
 	set t11 type sitestyle -- \
 	add t11 file -n sitecssfile design/base/stylesheets/t11/site-colors.css -- \
 	add t11 file -n classescssfile design/base/stylesheets/t11/classes-colors.css -- \
@@ -180,7 +180,7 @@ mkdir -p "$SITE_PACKAGES_EXPORT" || exit 1
 	add t11 file -n image design/base/images/t11/button.gif -- \
 	export t11 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t12 "T12 CSS" "$VERSION" import -- \
+        create t12 "Theme 12" "$VERSION" import -- \
 	set t12 type sitestyle -- \
 	add t12 file -n sitecssfile design/base/stylesheets/t12/site-colors.css -- \
 	add t12 file -n classescssfile design/base/stylesheets/t12/classes-colors.css -- \
@@ -189,7 +189,7 @@ mkdir -p "$SITE_PACKAGES_EXPORT" || exit 1
 	add t12 file -n image design/base/images/t12/button.gif -- \
 	export t12 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t13 "T13 CSS" "$VERSION" import -- \
+        create t13 "Theme 13" "$VERSION" import -- \
 	set t13 type sitestyle -- \
 	add t13 file -n sitecssfile design/base/stylesheets/t13/site-colors.css -- \
 	add t13 file -n classescssfile design/base/stylesheets/t13/classes-colors.css -- \
@@ -198,7 +198,7 @@ mkdir -p "$SITE_PACKAGES_EXPORT" || exit 1
 	add t13 file -n image design/base/images/t13/button.gif -- \
 	export t13 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t14 "T14 CSS" "$VERSION" import -- \
+        create t14 "Theme 14" "$VERSION" import -- \
 	set t14 type sitestyle -- \
 	add t14 file -n sitecssfile design/base/stylesheets/t14/site-colors.css -- \
 	add t14 file -n classescssfile design/base/stylesheets/t14/classes-colors.css -- \
@@ -207,7 +207,7 @@ mkdir -p "$SITE_PACKAGES_EXPORT" || exit 1
 	add t14 file -n image design/base/images/t14/button.gif -- \
 	export t14 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t15 "T15 CSS" "$VERSION" import -- \
+        create t15 "Theme 15" "$VERSION" import -- \
 	set t15 type sitestyle -- \
 	add t15 file -n sitecssfile design/base/stylesheets/t15/site-colors.css -- \
 	add t15 file -n classescssfile design/base/stylesheets/t15/classes-colors.css -- \
@@ -218,7 +218,7 @@ mkdir -p "$SITE_PACKAGES_EXPORT" || exit 1
 	add t15 file -n image design/base/images/t15/carpet-background.gif -- \
 	export t15 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t16 "T16 CSS" "$VERSION" import -- \
+        create t16 "Theme 16" "$VERSION" import -- \
 	set t16 type sitestyle -- \
 	add t16 file -n sitecssfile design/base/stylesheets/t16/site-colors.css -- \
 	add t16 file -n classescssfile design/base/stylesheets/t16/classes-colors.css -- \
@@ -229,7 +229,7 @@ mkdir -p "$SITE_PACKAGES_EXPORT" || exit 1
 	add t16 file -n image design/base/images/t16/freeze-background.jpg -- \
 	export t16 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t17 "T17 CSS" "$VERSION" import -- \
+        create t17 "Theme 17" "$VERSION" import -- \
 	set t17 type sitestyle -- \
 	add t17 file -n sitecssfile design/base/stylesheets/t17/site-colors.css -- \
 	add t17 file -n classescssfile design/base/stylesheets/t17/classes-colors.css -- \
@@ -238,7 +238,7 @@ mkdir -p "$SITE_PACKAGES_EXPORT" || exit 1
 	add t17 file -n image design/base/images/t17/button.gif -- \
 	export t17 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t18 "T18 CSS" "$VERSION" import -- \
+        create t18 "Theme 18" "$VERSION" import -- \
 	set t18 type sitestyle -- \
 	add t18 file -n sitecssfile design/base/stylesheets/t18/site-colors.css -- \
 	add t18 file -n classescssfile design/base/stylesheets/t18/classes-colors.css -- \
@@ -249,7 +249,7 @@ mkdir -p "$SITE_PACKAGES_EXPORT" || exit 1
 	add t18 file -n image design/base/images/t18/background-menu.png -- \
 	export t18 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t19 "T19 CSS" "$VERSION" import -- \
+        create t19 "Theme 19" "$VERSION" import -- \
 	set t19 type sitestyle -- \
 	add t19 file -n sitecssfile design/base/stylesheets/t19/site-colors.css -- \
 	add t19 file -n classescssfile design/base/stylesheets/t19/classes-colors.css -- \
@@ -258,7 +258,7 @@ mkdir -p "$SITE_PACKAGES_EXPORT" || exit 1
 	add t19 file -n image design/base/images/t19/button.gif -- \
 	export t19 -d "$EXPORT_PATH" || exit 1
     ./ezpm.php -r "$SITE_PACKAGES" $QUIET \
-        create t20 "T20 CSS" "$VERSION" import -- \
+        create t20 "Theme 20" "$VERSION" import -- \
 	set t20 type sitestyle -- \
 	add t20 file -n sitecssfile design/base/stylesheets/t20/site-colors.css -- \
 	add t20 file -n classescssfile design/base/stylesheets/t20/classes-colors.css -- \
