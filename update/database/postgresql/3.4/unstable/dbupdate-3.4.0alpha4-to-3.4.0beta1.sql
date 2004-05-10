0bfaff8dbadb12c34ae2de530b9bf15e3cbc1798
Jan Borsodi <jb@ez.no>
2004-05-10 09:45:30 +0000

    - Added update to 3.4.0beta1 which adds a NOT NULL to one of the new   fields in 3.4 which was missing from PostgreSQL schema.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@6434 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/3.4/unstable/dbupdate-3.4.0alpha4-to-3.4.0beta1.sql
+++ update/database/postgresql/3.4/unstable/dbupdate-3.4.0alpha4-to-3.4.0beta1.sql
@@ -1,2 +1,9 @@
 UPDATE ezsite_data SET value='3.4.0beta1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='5' WHERE name='ezpublish-release';
+
+ALTER TABLE eznode_assignment RENAME COLUMN parent_remote_id TO parent_remote_id_tmp;
+ALTER TABLE eznode_assignment ADD COLUMN parent_remote_id character varying(100) ;
+ALTER TABLE eznode_assignment ALTER parent_remote_id SET DEFAULT '' ;
+ALTER TABLE eznode_assignment ALTER parent_remote_id SET NOT NULL ;
+UPDATE eznode_assignment SET parent_remote_id=parent_remote_id_tmp;
+ALTER TABLE eznode_assignment DROP COLUMN parent_remote_id_tmp;
