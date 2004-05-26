cb2414b9ddad1bad836f7f3e4fdd6b70d9f6181f
Jan Borsodi <jb@ez.no>
2004-05-26 09:07:19 +0000

    - Updated the 3.3 sql files to make sure the PostgreSQL and MySQL schema   is the same (NOT NULL problem).
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.3@6632 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/3.3/dbupdate-3.3-5-to-3.3-6.sql
+++ update/database/postgresql/3.3/dbupdate-3.3-5-to-3.3-6.sql
@@ -4,12 +4,13 @@ UPDATE ezsite_data SET value='6' WHERE name='ezpublish-release';
 CREATE LOCAL TEMPORARY TABLE temp_sort_key_string
 (
     id               integer NOT NULL,
-    sort_key_string  character varying(50) DEFAULT ''::character varying
+    sort_key_string  character varying(50) DEFAULT ''::character varying NOT NULL
 );
 
 INSERT INTO temp_sort_key_string SELECT id, sort_key_string FROM ezcontentobject_attribute;
 ALTER TABLE ezcontentobject_attribute DROP COLUMN sort_key_string;
 ALTER TABLE ezcontentobject_attribute ADD COLUMN sort_key_string character varying(255);
+ALTER TABLE ezcontentobject_attribute ALTER COLUMN sort_key_string SET NOT NULL;
 ALTER TABLE ezcontentobject_attribute ALTER COLUMN sort_key_string SET DEFAULT ''::character varying;
 UPDATE ezcontentobject_attribute SET sort_key_string=temp_sort_key_string.sort_key_string WHERE temp_sort_key_string.id=ezcontentobject_attribute.id;
 CREATE INDEX sort_key_string367 ON ezcontentobject_attribute USING btree (sort_key_string);
