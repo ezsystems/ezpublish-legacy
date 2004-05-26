35db184f02253436cd1910fce00ff4f589ee4549
Jan Borsodi <jb@ez.no>
2004-05-26 08:18:47 +0000

    - Added version numbers to 3.3-5 to 3.4.0 db update files.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@6624 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ update/database/mysql/3.4/dbupdate-3.3-5-to-3.4.0.sql
@@ -0,0 +1,4 @@
+UPDATE ezsite_data SET value='3.4.0' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='7' WHERE name='ezpublish-release';
+
+-- NOTE: This file will be generated from all the development releases, do not add entries
