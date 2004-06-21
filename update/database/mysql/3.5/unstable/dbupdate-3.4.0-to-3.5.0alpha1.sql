feb3ddc97d9c348820ef169649ff88d3b9c613fa
Vadym Savchuk <vs@ez.no>
2004-06-21 10:31:18 +0000

    - Modified the upgrade scripts to reflect recent changes in cleandata.sql   fixing the following bug (merged from stable/3.4 rev. 6877, 6879):   http://ez.no/community/bug_reports/users_in_edit_group_can_create_new_admin_users
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@6880 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/3.5/unstable/dbupdate-3.4.0-to-3.5.0alpha1.sql
+++ update/database/mysql/3.5/unstable/dbupdate-3.4.0-to-3.5.0alpha1.sql
@@ -1,2 +1,12 @@
 UPDATE ezsite_data SET value='3.5.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+-- We allow users from the "Editors" group 
+-- access only to "Root Folder" and "Media" trees.
+DELETE FROM ezuser_role WHERE id=30 AND role_id=3;  
+INSERT INTO ezuser_role
+       (role_id, contentobject_id, limit_identifier,limit_value)
+       VALUES (3,13,'Subtree','/1/2/');
+INSERT INTO ezuser_role
+       (role_id, contentobject_id, limit_identifier,limit_value)
+       VALUES (3,13,'Subtree','/1/43/');
