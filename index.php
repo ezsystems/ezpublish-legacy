fc65a945cce85e61763b7f005f2d14b325c02404
Bård Farstad <bf@ez.no>
2004-06-28 10:44:49 +0000

    - Changed the format of Last-Modified header to confirm to RFC 2616.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@6940 a01eee8c-daba-0310-acae-fa49f3023285

--- index.php
+++ index.php
@@ -354,7 +354,7 @@ if ( $phpLocale != '' )
 
 // send header information
 header( 'Expires: Mon, 26 Jul 1997 05:00:00 GMT' );
-header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . 'GMT' );
+header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
 header( 'Cache-Control: no-cache, must-revalidate' );
 header( 'Pragma: no-cache' );
 header( 'X-Powered-By: eZ publish' );
