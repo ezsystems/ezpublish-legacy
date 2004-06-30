319821eb992f0d3a62cd2d0c1ef4b4e8e8470167
Jan Kudlicka <jk@ez.no>
2004-06-30 13:16:51 +0000

    - Fixed bug causing that the siteaccess names could not be written in capitals   http://ez.no/community/bug_reports/changing_the_siteaccess_name_causes_a_module_not_found_error
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@6972 a01eee8c-daba-0310-acae-fa49f3023285

--- access.php
+++ access.php
@@ -205,7 +205,7 @@ function accessType( &$uri, $host, $port, $file )
 
         if ( ( isset( $name ) && $name != '' ) || $type == EZ_ACCESS_TYPE_URI )
         {
-            $name = preg_replace( array( '/[^a-z0-9]+/',
+            $name = preg_replace( array( '/[^a-zA-Z0-9]+/',
                                          '/_+/',
                                          '/^_/',
                                          '/_$/' ),
@@ -213,7 +213,7 @@ function accessType( &$uri, $host, $port, $file )
                                          '_',
                                          '',
                                          '' ),
-                                  strtolower( $name ) );
+                                  $name );
 
             if ( in_array( $name, $siteAccessList ) )
             {
