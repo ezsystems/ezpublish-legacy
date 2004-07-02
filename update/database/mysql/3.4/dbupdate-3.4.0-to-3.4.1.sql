9fb1970e8e31eef922e6cf3255d20088e9b83ab3
Jan Borsodi <jb@ez.no>
2004-07-02 12:19:38 +0000

    - Merged in new index for table ezcontentobject_version from stable/3.4   rev. 7002, 7003 and 7004.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@7005 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ update/database/mysql/3.4/dbupdate-3.4.0-to-3.4.1.sql
@@ -0,0 +1,16 @@
+UPDATE ezsite_data SET value='3.4.1' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='8' WHERE name='ezpublish-release';
+
+-- We allow users from the "Editors" group
+-- access only to "Root Folder" and "Media" trees.
+DELETE FROM ezuser_role WHERE id=30 AND role_id=3;  
+INSERT INTO ezuser_role
+       (role_id, contentobject_id, limit_identifier,limit_value)
+       VALUES (3,13,'Subtree','/1/2/');
+INSERT INTO ezuser_role
+       (role_id, contentobject_id, limit_identifier,limit_value)
+       VALUES (3,13,'Subtree','/1/43/');
+
+
+-- Missing index
+CREATE INDEX idx_object_version_objver ON ezcontentobject_version ( contentobject_id, version );
