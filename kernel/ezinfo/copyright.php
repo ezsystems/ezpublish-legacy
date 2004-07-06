5f49f780fd4d88f8a79d65689047574b64938fb5
Jan Borsodi <jb@ez.no>
2004-07-06 14:37:28 +0000

    - Merged in profesinal licence update from stable/3.4 rev. 7042.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@7043 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/ezinfo/copyright.php
+++ kernel/ezinfo/copyright.php
@@ -38,21 +38,22 @@ $text = "<p>Copyright (C) 1999-2004 eZ systems as. All rights reserved.</p>
 
 <p>This file may be distributed and/or modified under the terms of the
 \"GNU General Public License\" version 2 as published by the Free
-Software Foundation and appearing in the file LICENSE.GPL included in
+Software Foundation and appearing in the file LICENSE included in
 the packaging of this file.</p>
 
-<p>Licencees holding valid \"eZ publish professional licences\" may use this
-file in accordance with the \"eZ publish professional licence\" Agreement
-provided with the Software.</p>
+<p>Licencees holding a valid \"eZ publish professional licence\" version 2
+may use this file in accordance with the \"eZ publish professional licence\"
+version 2 Agreement provided with the Software.</p>
 
 <p>This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
 THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
 PURPOSE.</p>
 
-<p>The \"eZ publish professional licence\" is available at
-<a href=\"http://ez.no/products/licences/professional/\">http://ez.no/products/licences/professional/</a>. For pricing of this licence
-please contact us via e-mail. Further contact
-information is available at <a href=\"http://ez.no/home/contact/\">http://ez.no/home/contact/</a>.</p>
+<p>The \"eZ publish professional licence\" version 2 is available at
+<a href=\"http://ez.no/ez_publish/licences/professional/\">http://ez.no/ez_publish/licences/professional/</a> and in the file
+PROFESSIONAL_LICENCE included in the packaging of this file.
+For pricing of this licence please contact us via e-mail to licence@ez.no.
+Further contact information is available at <a href=\"http://ez.no/company/contact/\">http://ez.no/company/contact/</a>.</p>
 
 <p>The \"GNU General Public License\" (GPL) is available at
 <a href=\"http://www.gnu.org/copyleft/gpl.html\">http://www.gnu.org/copyleft/gpl.html</a>.</p>
