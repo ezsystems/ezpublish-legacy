f0be7d8c2015d0b42fdf3ca4678fe2fcc9bb0782
Jan Borsodi <jb@ez.no>
2004-07-14 14:38:52 +0000

    - Commented out the lines in the 3.4.0 to 3.4.1 update sqls   which removes and adds role information. They use hardcoded   ids and could easily break an existing system.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.4@7159 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/3.4/dbupdate-3.4.0-to-3.4.1.sql
+++ update/database/mysql/3.4/dbupdate-3.4.0-to-3.4.1.sql
@@ -3,13 +3,16 @@ UPDATE ezsite_data SET value='8' WHERE name='ezpublish-release';
 
 -- We allow users from the "Editors" group
 -- access only to "Root Folder" and "Media" trees.
-DELETE FROM ezuser_role WHERE id=30 AND role_id=3;  
-INSERT INTO ezuser_role
-       (role_id, contentobject_id, limit_identifier,limit_value)
-       VALUES (3,13,'Subtree','/1/2/');
-INSERT INTO ezuser_role
-       (role_id, contentobject_id, limit_identifier,limit_value)
-       VALUES (3,13,'Subtree','/1/43/');
+-- If you want to fix this you need to figure out the ids of these roles and modify
+-- the following SQLs
+--
+-- DELETE FROM ezuser_role WHERE id=30 AND role_id=3;  
+-- INSERT INTO ezuser_role
+--        (role_id, contentobject_id, limit_identifier,limit_value)
+--        VALUES (3,13,'Subtree','/1/2/');
+-- INSERT INTO ezuser_role
+--        (role_id, contentobject_id, limit_identifier,limit_value)
+--        VALUES (3,13,'Subtree','/1/43/');
 
 
 -- Missing index
