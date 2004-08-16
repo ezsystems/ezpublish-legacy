8775e8d90f3bfc91eb7912f3104fef8b8105a2b7
Vadym Savchuk <vs@ez.no>
2004-08-16 09:18:27 +0000

    - Added DB modification queries for a couple of typos fixed previously in   mysql/kernel_schema.sql  (merged from trunk (3.5), rev. 7568).
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.4@7569 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/3.4/dbupdate-3.4.1-to-3.4.2.sql
+++ update/database/mysql/3.4/dbupdate-3.4.1-to-3.4.2.sql
@@ -1,2 +1,10 @@
 UPDATE ezsite_data SET value='3.4.2' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='9' WHERE name='ezpublish-release';
+
+-- Fix typos in the indexes creation queries.
+DROP   INDEX ezproductcollection_item_contentobject_id ON
+             ezproductcollection_item;                 
+CREATE INDEX ezproductcollection_item_contentobject_id ON
+             ezproductcollection_item (contentobject_id);
+
+DROP INDEX ezsubtree_notification_rule_id ON ezsubtree_notification_rule;
