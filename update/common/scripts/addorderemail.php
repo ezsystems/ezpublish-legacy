1ec5e99b05f329d2357c564b0f2e67e44ec0a002
Wenyue Yu <wy@ez.no>
2004-08-18 11:22:35 +0000

    - Added file back.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.4@7602 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ update/common/scripts/addorderemail.php
@@ -0,0 +1,163 @@
+#!/usr/bin/env php
+<?php
+//
+// Created on: <27-Feb-2004 13:12:40 wy>
+//
+// Copyright (C) 1999-2004 eZ systems as. All rights reserved.
+//
+// This source file is part of the eZ publish (tm) Open Source Content
+// Management System.
+//
+// This file may be distributed and/or modified under the terms of the
+// "GNU General Public License" version 2 as published by the Free
+// Software Foundation and appearing in the file LICENSE included in
+// the packaging of this file.
+//
+// Licencees holding a valid "eZ publish professional licence" version 2
+// may use this file in accordance with the "eZ publish professional licence"
+// version 2 Agreement provided with the Software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+// THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE.
+//
+// The "eZ publish professional licence" version 2 is available at
+// http://ez.no/ez_publish/licences/professional/ and in the file
+// PROFESSIONAL_LICENCE included in the packaging of this file.
+// For pricing of this licence please contact us via e-mail to licence@ez.no.
+// Further contact information is available at http://ez.no/company/contact/.
+//
+// The "GNU General Public License" (GPL) is available at
+// http://www.gnu.org/copyleft/gpl.html.
+//
+// Contact licence@ez.no if any conditions of this licencing isn't clear to
+// you.
+//
+
+set_time_limit( 0 );
+
+include_once( 'lib/ezutils/classes/ezcli.php' );
+include_once( 'kernel/classes/ezscript.php' );
+
+$cli =& eZCLI::instance();
+$endl = $cli->endlineString();
+
+$script =& eZScript::instance( array( 'description' => ( "eZ publish add order email.\n\n" .
+                                                         "Fetch email value from eZShopAccountHandler and insert into table ezorder\n" .
+                                                         "This script only need to be run when updating from eZ publish 3.3 to eZ publish 3.4\n" .
+                                                         "\n" .
+                                                         "addorderemail.php"),
+                                      'use-session' => true,
+                                      'use-modules' => true,
+                                      'use-extensions' => true ) );
+
+$script->startup();
+
+$options = $script->getOptions( "[db-user:][db-password:][db-database:][db-driver:][sql]",
+                                "",
+                                array( 'db-host' => "Database host",
+                                       'db-user' => "Database user",
+                                       'db-password' => "Database password",
+                                       'db-database' => "Database name",
+                                       'db-driver' => "Database driver",
+                                       'sql' => "Display sql queries"
+                                       ) );
+$script->initialize();
+
+$dbUser = $options['db-user'] ? $options['db-user'] : false;
+$dbPassword = $options['db-password'] ? $options['db-password'] : false;
+$dbHost = $options['db-host'] ? $options['db-host'] : false;
+$dbName = $options['db-database'] ? $options['db-database'] : false;
+$dbImpl = $options['db-driver'] ? $options['db-driver'] : false;
+$showSQL = $options['sql'] ? true : false;
+$siteAccess = $options['siteaccess'] ? $options['siteaccess'] : false;
+
+if ( $siteAccess )
+{
+    changeSiteAccessSetting( $siteaccess, $siteAccess );
+}
+
+function changeSiteAccessSetting( &$siteaccess, $optionData )
+{
+    global $isQuiet;
+    $cli =& eZCLI::instance();
+    if ( file_exists( 'settings/siteaccess/' . $optionData ) )
+    {
+        $siteaccess = $optionData;
+        if ( !$isQuiet )
+            $cli->notice( "Using siteaccess $siteaccess for adding order email" );
+    }
+    else
+    {
+        if ( !$isQuiet )
+            $cli->notice( "Siteaccess $optionData does not exist, using default siteaccess" );
+    }
+}
+
+print( "Starting add email into table ezorder\n" );
+
+//eZDebug::setHandleType( EZ_HANDLE_FROM_PHP );
+
+include_once( "lib/ezutils/classes/ezmodule.php" );
+// eZModule::setGlobalPathList( array( "kernel" ) );
+include_once( 'lib/ezutils/classes/ezexecution.php' );
+include_once( "lib/ezutils/classes/ezdebug.php" );
+include_once( 'kernel/classes/ezorder.php' );
+
+$db =& eZDB::instance();
+
+if ( $dbHost or $dbName or $dbUser or $dbImpl )
+{
+    $params = array();
+    if ( $dbHost !== false )
+        $params['server'] = $dbHost;
+    if ( $dbUser !== false )
+    {
+        $params['user'] = $dbUser;
+        $params['password'] = '';
+    }
+    if ( $dbPassword !== false )
+        $params['password'] = $dbPassword;
+    if ( $dbName !== false )
+        $params['database'] = $dbName;
+    $db =& eZDB::instance( $dbImpl, $params, true );
+    eZDB::setInstance( $db );
+}
+
+$db->setIsSQLOutputEnabled( $showSQL );
+
+$orderArray = eZOrder::fetchList();
+$orderCount = count( $orderArray );
+
+print( $endl . $orderCount . " order email will be updated" . $endl );
+// Fetch the shop account handler
+include_once( 'kernel/classes/ezshopaccounthandler.php' );
+$accountHandler =& eZShopAccountHandler::instance();
+
+$i = 0;
+$dotMax = 70;
+$dotCount = 0;
+$limit = 50;
+
+foreach ( array_keys ( $orderArray ) as $key  )
+{
+    $order =& $orderArray[$key];
+
+    $email = $accountHandler->email( $order );
+    $order->setAttribute( 'email', $email );
+    $order->store();
+    ++$i;
+    ++$dotCount;
+    if ( $dotCount >= $dotMax or $i >= $orderCount )
+    {
+        $dotCount = 0;
+        $percent = (float)( ($i*100.0) / $orderCount );
+        print( " " . $percent . "%" . $endl );
+    }
+}
+
+print( $endl . "done" . $endl );
+
+$script->shutdown();
+
+?>
