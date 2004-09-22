b04b5aa57760ae2c695e53cc35581369fb13bef9
Jan Borsodi <jb@ez.no>
2004-09-22 10:11:06 +0000

    - Fixed DROP CONSTRAINT statement in PostgreSQL db update file (3.4.2),   the table name was missing.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.4@8125 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/3.4/dbupdate-3.4.1-to-3.4.2.sql
+++ update/database/postgresql/3.4/dbupdate-3.4.1-to-3.4.2.sql
@@ -8,7 +8,7 @@ CREATE INDEX ezproductcollection_item_contentobject_id ON ezproductcollection_it
 DROP   INDEX ezsubtree_notification_rule_id;
 
 -- Fix faulty index on ezurl_object_link
-ALTER TABLE DROP CONSTRAINT ezurl_object_link1039_key;
+ALTER TABLE ezurl_object_link DROP CONSTRAINT ezurl_object_link1039_key;
 CREATE INDEX ezurl_ol_url_id ON ezurl_object_link (url_id);
 CREATE INDEX ezurl_ol_coa_id ON ezurl_object_link (contentobject_attribute_id);
 CREATE INDEX ezurl_ol_coa_version ON ezurl_object_link (contentobject_attribute_version);
