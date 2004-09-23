da38d46bda7c32eb61f301380d5c18c80e306ed4
Jan Borsodi <jb@ez.no>
2004-09-23 15:08:58 +0000

    - Added 3.4.3 release to checkdbfiles.php script.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.4@8163 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/php/checkdbfiles.php
+++ bin/php/checkdbfiles.php
@@ -94,6 +94,7 @@ $versions34 = array( 'unstable' => array( array( '3.3-3', '3.4.0alpha1' )
                      'stable' => array( array( '3.3-5', '3.4.0' )
 		                                ,array( '3.4.0', '3.4.1' )
 		                                ,array( '3.4.1', '3.4.2' )
+		                                ,array( '3.4.2', '3.4.3' )
                                         ) );
 $versions['3.0'] = $versions30;
 $versions['3.1'] = $versions31;
