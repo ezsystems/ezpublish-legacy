2e1995db12e10f7cbc7e9efd24849aed406852fe
Balazs Halasy <bh@ez.no>
2004-10-12 08:19:25 +0000

    - Admin: fixed wrong link/path in RSS list.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@8594 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/rss/list.php
+++ kernel/rss/list.php
@@ -101,7 +101,7 @@ $tpl->setVariable( 'rssimport_list', $importList );
 
 $Result = array();
 $Result['content'] =& $tpl->fetch( "design:rss/list.tpl" );
-$Result['path'] = array( array( 'url' => 'kernel/rss',
+$Result['path'] = array( array( 'url' => 'rss/list',
                                 'text' => ezi18n( 'kernel/rss', 'Really Simple Syndication' ) ) );
 
 
