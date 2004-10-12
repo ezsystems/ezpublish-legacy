0f0c827d2e638817fd5f9745f67b5e5f56f1a1f1
Vadym Savchuk <vs@ez.no>
2004-10-12 12:47:47 +0000

    - Fixed bug: index.php didn't set LastAccessesURI session variable when viewing node #2.   (Merged from stable/3.3 rev. 8611, also in stable/3.4 rev. 8612)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@8613 a01eee8c-daba-0310-acae-fa49f3023285

--- index.php
+++ index.php
@@ -459,7 +459,7 @@ while ( $moduleRunRequired )
             $lastAccessedURI = $http->sessionVariable( "LastAccessesURI" );
         if ( $currentURI != $lastAccessedURI )
         {
-            if ( preg_match( "/\/content\/view\/.*/", $currentURI  ) )
+            if ( preg_match( "/\/content\/view\/.*/", $currentURI  ) or $currentURI === '/' )
             {
                 $http->setSessionVariable( "LastAccessesURI", $currentURI );
             }
