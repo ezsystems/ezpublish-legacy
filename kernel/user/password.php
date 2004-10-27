153905f05ea61a358313d7fdd70aba71679e7030
Vadym Savchuk <vs@ez.no>
2004-10-27 14:19:34 +0000

    - Fixed bug: New password was not checked for validity when changing a user's password.   (Manually merged from stable/3.3 rev. 9192)   http://ez.no/community/bug_reports/password_change_blank_passwords_allowed
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.4@9193 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/user/password.php
+++ kernel/user/password.php
@@ -43,6 +43,7 @@ $Module =& $Params["Module"];
 $message = 0;
 $oldPasswordNotValid = 0;
 $newPasswordNotMatch = 0;
+$newPasswordTooShort = 0;
 
 if ( is_numeric( $Params["UserID"] ) )
     $UserID = $Params["UserID"];
@@ -80,13 +81,20 @@ if ( $http->hasPostVariable( "OKButton" ) )
     {
         if (  $newPassword ==  $confirmPassword )
         {
+            if ( strlen( $newPassword ) < 3 )
+            {
+                $newPasswordTooShort = 1;
+            }
+            else
+            {
+                $newHash = $user->createHash( $login, $newPassword, $site, $type );
+                $user->setAttribute( "password_hash", $newHash );
+                $user->store();
+            }
             $message = true;
-            $newHash = $user->createHash( $login, $newPassword, $site, $type );
-            $user->setAttribute( "password_hash", $newHash );
-            $user->store();
-            $oldPassword = "";
-            $newPassword = "";
-            $confirmPassword = "";
+            $oldPassword = '';
+            $newPassword = '';
+            $confirmPassword = '';
         }
         else
         {
@@ -110,6 +118,14 @@ if ( $http->hasPostVariable( "CancelButton" ) )
     return;
 }
 
+// prevent PHP warnings about undefined variables
+if ( !isset( $oldPassword ) )
+    $oldPassword = '';
+if ( !isset( $newPassword ) )
+    $newPassword = '';
+if ( !isset( $confirmPassword ) )
+    $confirmPassword = '';
+
 $Module->setTitle( "Edit user information" );
 // Template handling
 include_once( "kernel/common/template.php" );
@@ -123,6 +139,7 @@ $tpl->setVariable( "newPassword", $newPassword );
 $tpl->setVariable( "confirmPassword", $confirmPassword );
 $tpl->setVariable( "oldPasswordNotValid", $oldPasswordNotValid );
 $tpl->setVariable( "newPasswordNotMatch", $newPasswordNotMatch );
+$tpl->setVariable( "newPasswordTooShort", $newPasswordTooShort );
 $tpl->setVariable( "message", $message );
 
 $Result = array();
