7ae2dc576d1c49c430d9116700a918c03bdb45f4
Jan Borsodi <jb@ez.no>
2004-11-02 10:44:11 +0000

    - Updated version numbers in cleandata to 3.4.4.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.4@9301 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/sql/common/cleandata.sql
+++ kernel/sql/common/cleandata.sql
@@ -1585,8 +1585,8 @@ INSERT INTO ezsection (id, name, locale, navigation_part_identifier) VALUES (4,'
 
 
 
-INSERT INTO ezsite_data (name, value) VALUES ('ezpublish-version','3.4.3');
-INSERT INTO ezsite_data (name, value) VALUES ('ezpublish-release','10');
+INSERT INTO ezsite_data (name, value) VALUES ('ezpublish-version','3.4.4');
+INSERT INTO ezsite_data (name, value) VALUES ('ezpublish-release','11');
 
 
 
