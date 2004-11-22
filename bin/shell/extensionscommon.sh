b887f1a42b36762e253dae53070dd50bb61f6ae3
Jan Borsodi <jb@ez.no>
2004-11-22 16:08:00 +0000

    - Added a list of svn urls that are extensions that will be made in makedist.sh.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@9948 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/extensionscommon.sh
+++ bin/shell/extensionscommon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-EXTENSIONS=""
+EXTENSIONS="http://zev.ez.no/svn/extensions/ezpaypal/ http://zev.ez.no/svn/commercial/projects/paymentgateways/ezpaynet/ http://zev.ez.no/svn/commercial/projects/ezoracle/ http://zev.ez.no/svn/commercial/projects/ezdhtml/trunk/dhtml/ http://zev.ez.no/svn/commercial/projects/survey/"
