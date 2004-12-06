57222725acc74d66c384f89b4dd99171618a148d
Peter Kudlicka <pk@ez.no>
2004-12-06 13:34:28 +0000

    - Solved problem with unusual text/rtf and application/rtf mime type. General solution.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@10211 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezbinaryfile/ezbinaryfiletype.php
+++ kernel/classes/datatypes/ezbinaryfile/ezbinaryfiletype.php
@@ -293,7 +293,7 @@ class eZBinaryFileType extends eZDataType
                 $mime = $binaryFile->attribute( "mime_type" );
             }
             $extension = preg_replace('/.*\.(.+?)$/', '\\1', $binaryFile->attribute( "original_filename" ) );
-            if ( !$binaryFile->store( "original", $extension ) )
+            if ( !$binaryFile->store( "original", $extension, $mime ) )
             {
                 eZDebug::writeError( "Failed to store http-file: " . $binaryFile->attribute( "original_filename" ),
                                      "eZBinaryFileType" );
