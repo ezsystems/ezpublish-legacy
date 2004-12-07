7aecba0d0c60721d8696b5312854344e02cfc6c3
Sandro Zic <sz@ez.no>
2004-12-07 10:44:52 +0000

    - Updated url_path unit test to check for empty urls, urls with just a slash   and urls with anchors (#). - Duplicated url_path unit test into url_path_vhost (tests virtual-host +   rewrite setup) and url_path_vhost_access (tests virtual-host + rewrite +   siteaccess in path setup).
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.4@10232 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ tests/eztemplate/operators/url_path_vhost_access.php
@@ -0,0 +1,11 @@
+<?php
+
+$sys =& eZSys::instance();
+
+$sys->AccessPath = array( 'blog' );
+$sys->SiteDir = '/var/www/html';
+$sys->WWWDir = '';
+$sys->IndexFile = '';
+$sys->RequestURI = '/admin/content/view/full/2';
+
+?>
