4f66fe3178f03cd576ba9ea117298665196da1c6
Jan Borsodi <jb@ez.no>
2004-12-21 14:33:54 +0000

    - Disabled the building of the survey and paynet extension for now.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.5@10431 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/extensionscommon.sh
+++ bin/shell/extensionscommon.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-EXTENSIONS="http://zev.ez.no/svn/extensions/ezpaypal/ http://zev.ez.no/svn/commercial/projects/paymentgateways/ezpaynet/ http://zev.ez.no/svn/commercial/projects/ezoracle/ http://zev.ez.no/svn/commercial/projects/ezdhtml/trunk/dhtml/ http://zev.ez.no/svn/commercial/projects/survey/"
+EXTENSIONS="http://zev.ez.no/svn/extensions/ezpaypal/
+http://zev.ez.no/svn/commercial/projects/ezoracle/
+http://zev.ez.no/svn/commercial/projects/ezdhtml/trunk/dhtml/"
+
+# These are disabled for now
+#EXTENSIONS="$EXTENSIONS http://zev.ez.no/svn/commercial/projects/paymentgateways/ezpaynet/"
+#EXTENSIONS="$EXTENSIONS http://zev.ez.no/svn/commercial/projects/survey/"
