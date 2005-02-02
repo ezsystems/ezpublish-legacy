9ac550ced2e9977fa65b640749cf1f0efa2c9e13
Jan Borsodi <jb@ez.no>
2005-02-02 10:17:08 +0000

    - Fixed bug #6034: eZMYSQLDB::query() unproper error handling
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.3@10746 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezdb/classes/ezmysqldb.php
+++ lib/ezdb/classes/ezmysqldb.php
@@ -225,7 +225,7 @@ class eZMySQLDB extends eZDBInterface
             }
 
             $result =& mysql_query( $sql, $connection );
-            if ( $this->RecordError )
+            if ( $this->RecordError and !$result )
                 $this->setError();
 
             if ( $this->OutputSQL )
@@ -243,9 +243,11 @@ class eZMySQLDB extends eZDBInterface
             else
             {
                 eZDebug::writeError( "Query error: " . mysql_error( $connection ) . ". Query: ". $sql, "eZMySQLDB"  );
+                $oldRecordError = $this->RecordError;
+                // Turn off error handling while we unlock
                 $this->RecordError = false;
                 $this->unlock();
-                $this->RecordError = true;
+                $this->RecordError = $oldRecordError;
 
                 return false;
             }
