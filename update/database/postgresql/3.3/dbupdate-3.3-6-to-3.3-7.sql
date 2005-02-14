805be0702da952fc124ee19565d9fef2a7a8967f
Jan Borsodi <jb@ez.no>
2005-02-14 10:01:43 +0000

    #- Update SQL to fix wrong sort-key for some entries bundled with eZ publish.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/3.3-7@10875 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/3.3/dbupdate-3.3-6-to-3.3-7.sql
+++ update/database/postgresql/3.3/dbupdate-3.3-6-to-3.3-7.sql
@@ -7,3 +7,5 @@ CREATE TABLE eztipafriend_request (
 create index eztipafriend_request_email_receiver on eztipafriend_request(email_receiver);
 create index eztipafriend_request_created on eztipafriend_request(created);
 
+UPDATE ezcontentobject_attribute SET sort_key_string = data_text WHERE data_type_string = 'ezstring';
+
