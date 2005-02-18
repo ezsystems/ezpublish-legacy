7b6c74662688961176c7df5574b9f5110c2ee63e
Jan Borsodi <jb@ez.no>
2005-02-18 10:09:36 +0000

    - Fixed bug #6217: Wrong package version.   (Merged from release/3.4.5 (3.4.5) rev. 10919) #- Updated checkversionumbers.sh to check for the package version.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/3.5.1@10924 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezpackage.php
+++ kernel/classes/ezpackage.php
@@ -51,8 +51,8 @@ include_once( 'lib/ezfile/classes/ezfile.php' );
 include_once( 'lib/ezfile/classes/ezdir.php' );
 include_once( 'lib/ezfile/classes/ezfilehandler.php' );
 
-define( 'EZ_PACKAGE_VERSION', '3.4.0beta1' );
-define( 'EZ_PACKAGE_DEVELOPMENT', true );
+define( 'EZ_PACKAGE_VERSION', '3.5.1' );
+define( 'EZ_PACKAGE_DEVELOPMENT', false );
 define( 'EZ_PACKAGE_USE_CACHE', true );
 define( 'EZ_PACKAGE_CACHE_CODEDATE', 1069339607 );
 
