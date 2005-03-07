5eefd747e807967976aac344c24335ff93c19644
Jan Borsodi <jb@ez.no>
2005-03-07 10:11:23 +0000

    #- Added 3.4.6 entry to db check.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.4@11014 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/php/checkdbfiles.php
+++ bin/php/checkdbfiles.php
@@ -97,6 +97,7 @@ $versions34 = array( 'unstable' => array( array( '3.3-3', '3.4.0alpha1' )
 		                                ,array( '3.4.2', '3.4.3' )
 		                                ,array( '3.4.3', '3.4.4' )
 		                                ,array( '3.4.4', '3.4.5' )
+		                                ,array( '3.4.5', '3.4.6' )
                                         ) );
 $versions['3.0'] = $versions30;
 $versions['3.1'] = $versions31;
