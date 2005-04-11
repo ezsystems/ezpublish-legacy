4d7fb948cd95ce128a174647921ddbf7ebd12a51
Jan Borsodi <jb@ez.no>
2005-04-11 13:50:31 +0000

    - Updated: Order status   Adding update SQL for changes in DB schema.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@11334 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/3.6/unstable/dbupdate-3.5.0-to-3.6.0alpha1.sql
+++ update/database/mysql/3.6/unstable/dbupdate-3.5.0-to-3.6.0alpha1.sql
@@ -26,3 +26,52 @@ CREATE INDEX ezco_link_from     ON ezcontentobject_link ( from_contentobject_id,
                                                           contentclassattribute_id );
 
 DROP TABLE ezsubtree_expiry;
+
+
+-- Add missing index for orders
+ALTER TABLE ezorder ADD INDEX ezorder_is_tmp (is_temporary);
+
+-- New feature: Status on orders
+
+ALTER TABLE ezorder ADD status_id int DEFAULT 0;
+ALTER TABLE ezorder ADD status_modified int DEFAULT 0;
+ALTER TABLE ezorder ADD status_modifier_id int DEFAULT 0;
+
+CREATE TABLE ezorder_status (
+    id int(11) NOT NULL auto_increment,
+    status_id int(11) NOT NULL DEFAULT 0,
+    name varchar(255) NOT NULL DEFAULT '',
+    is_active int(11) NOT NULL DEFAULT 1,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE ezorder_status ADD INDEX ezorder_status_sid (status_id);
+ALTER TABLE ezorder_status ADD INDEX ezorder_status_name (name);
+ALTER TABLE ezorder_status ADD INDEX ezorder_status_active (is_active);
+
+INSERT INTO ezorder_status (status_id, name, is_active)
+VALUES( 1, 'Pending', 1 );
+INSERT INTO ezorder_status (status_id, name, is_active)
+VALUES( 2, 'Processing', 1 );
+INSERT INTO ezorder_status (status_id, name, is_active)
+VALUES( 3, 'Delivered', 1 );
+
+CREATE TABLE ezorder_status_history (
+    id int(11) NOT NULL auto_increment,
+    order_id int(11) NOT NULL DEFAULT 0,
+    status_id int(11) NOT NULL DEFAULT 0,
+    modifier_id int(11) NOT NULL DEFAULT 0,
+    modified int(11) NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE ezorder_status_history ADD INDEX ezorder_status_history_oid (order_id);
+ALTER TABLE ezorder_status_history ADD INDEX ezorder_status_history_sid (status_id);
+ALTER TABLE ezorder_status_history ADD INDEX ezorder_status_history_mod (modified);
+
+
+-- Make sure each order has a history element with Pending status
+INSERT INTO ezorder_status_history (order_id, status_id, modifier_id, modified)
+SELECT order_nr AS order_id, 1 AS status_id, user_id AS modifier_id, created AS modified FROM ezorder WHERE status_id = 0;
+-- Update status of all orders to Pending
+UPDATE ezorder SET status_id = 1, status_modifier_id = user_id, status_modified = created WHERE status_id = 0;
--- update/database/postgresql/3.6/unstable/dbupdate-3.5.0-to-3.6.0alpha1.sql
+++ update/database/postgresql/3.6/unstable/dbupdate-3.5.0-to-3.6.0alpha1.sql
@@ -34,3 +34,75 @@ CREATE INDEX ezco_link_from     ON ezcontentobject_link ( from_contentobject_id,
                                                           contentclassattribute_id );
 
 DROP TABLE ezsubtree_expiry;
+
+
+-- Add missing index for orders
+CREATE INDEX ezorder_is_tmp ON ezorder USING btree (is_temporary);
+
+ALTER TABLE ezorder ADD status_id integer;
+ALTER TABLE ezorder ALTER status_id SET DEFAULT 0;
+
+ALTER TABLE ezorder ADD status_modified integer;
+ALTER TABLE ezorder ALTER status_modified SET DEFAULT 0;
+
+ALTER TABLE ezorder ADD status_modifier_id integer;
+ALTER TABLE ezorder ALTER status_modifier_id SET DEFAULT 0;
+
+
+CREATE SEQUENCE ezorder_status_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
+CREATE TABLE ezorder_status (
+    id integer NOT NULL DEFAULT nextval('ezorder_status_s'::text),
+    status_id integer NOT NULL DEFAULT 0,
+    name varchar(255) NOT NULL DEFAULT '',
+    is_active integer NOT NULL DEFAULT 1
+);
+
+ALTER TABLE ONLY ezorder_status
+    ADD CONSTRAINT ezorder_status_pkey PRIMARY KEY (id);
+
+CREATE INDEX ezorder_status_sid ON ezorder_status USING btree  (status_id);
+CREATE INDEX ezorder_status_name ON ezorder_status USING btree (name);
+CREATE INDEX ezorder_status_active ON ezorder_status USING btree  (is_active);
+
+INSERT INTO ezorder_status (status_id, name, is_active)
+VALUES( 1, 'Pending', 1 );
+INSERT INTO ezorder_status (status_id, name, is_active)
+VALUES( 2, 'Processing', 1 );
+INSERT INTO ezorder_status (status_id, name, is_active)
+VALUES( 3, 'Delivered', 1 );
+
+CREATE SEQUENCE ezorder_status_history_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
+CREATE TABLE ezorder_status_history (
+    id integer NOT NULL DEFAULT nextval('ezorder_status_history_s'::text),
+    order_id integer NOT NULL DEFAULT 0,
+    status_id integer NOT NULL DEFAULT 0,
+    modifier_id integer NOT NULL DEFAULT 0,
+    modified integer NOT NULL DEFAULT 0
+);
+
+ALTER TABLE ONLY ezorder_status_history
+    ADD CONSTRAINT ezorder_status_history_pkey PRIMARY KEY (id);
+
+CREATE INDEX ezorder_status_history_oid ON ezorder_status_history USING btree (order_id);
+CREATE INDEX ezorder_status_history_sid ON ezorder_status_history USING btree  (status_id);
+CREATE INDEX ezorder_status_history_mod ON ezorder_status_history USING btree (modified);
+
+
+-- Make sure each order has a history element with Pending status
+INSERT INTO ezorder_status_history (order_id, status_id, modifier_id, modified)
+SELECT order_nr AS order_id, 1 AS status_id, user_id AS modifier_id, created AS modified FROM ezorder WHERE status_id = 0;
+
+-- Update status of all orders to Pending
+UPDATE ezorder SET status_id = 1, status_modifier_id = user_id, status_modified = created WHERE status_id = 0;
