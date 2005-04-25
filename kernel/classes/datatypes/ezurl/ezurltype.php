d7b766a755d8b69c7cda28b75641ddd92dafbb13
Kirill Subbotin <ks@ez.no>
2005-04-25 14:31:54 +0000

    - Fixed bug #6508: Cannot remove URL Attribute.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@11531 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezurl/ezurltype.php
+++ kernel/classes/datatypes/ezurl/ezurltype.php
@@ -186,6 +186,11 @@ class eZURLType extends eZDataType
                  !eZURLObjectLink::hasObjectLinkList( $oldURLID ) )
                     eZURL::removeByID( $oldURLID );
         }
+        else
+        {
+            $attribute->setAttribute( 'data_int', 0 );
+        }
+
     }
 
     function storeClassAttribute( &$attribute, $version )
@@ -217,6 +222,9 @@ class eZURLType extends eZDataType
 
     function hasObjectAttributeContent( &$contentObjectAttribute )
     {
+        if ( $contentObjectAttribute->attribute( 'data_int' ) == 0 )
+            return false;
+
         $url =& eZURL::fetch( $contentObjectAttribute->attribute( 'data_int' ) );
         if ( is_object( $url ) and
              trim( $url->attribute( 'url' ) ) != '' and
