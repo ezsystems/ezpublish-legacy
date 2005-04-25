221f7ce685757d0038e360aded5fc4dfd7ba75d9
Jan Borsodi <jb@ez.no>
2005-04-25 11:32:58 +0000

    #- Made sure only ezpaypal is built with makedist.sh (The others are built #  separately).
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.5@11528 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/extensionscommon.sh
+++ bin/shell/extensionscommon.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-EXTENSIONS="http://zev.ez.no/svn/extensions/ezpaypal/
-http://zev.ez.no/svn/commercial/projects/ezoracle/
-http://zev.ez.no/svn/commercial/projects/ezdhtml/trunk/dhtml/"
+EXTENSIONS="http://zev.ez.no/svn/extensions/ezpaypal/"
 
 # These are disabled for now
+#EXTENSIONS="$EXTENSIONS http://zev.ez.no/svn/commercial/projects/ezoracle/trunk/"
+#EXTENSIONS="$EXTENSIONS http://zev.ez.no/svn/commercial/projects/ezdhtml/trunk/dhtml/
+
 #EXTENSIONS="$EXTENSIONS http://zev.ez.no/svn/commercial/projects/paymentgateways/ezpaynet/"
 #EXTENSIONS="$EXTENSIONS http://zev.ez.no/svn/commercial/projects/survey/"
