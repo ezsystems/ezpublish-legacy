d94bb89b5f68a323c37c61357a273761b10b6594
Jan Borsodi <jb@ez.no>
2005-05-11 09:25:17 +0000

    #- Updated version numbers to reflect the 3.6.0rc1 release.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@11754 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/sql/common/cleandata.sql
+++ kernel/sql/common/cleandata.sql
@@ -21878,14 +21878,14 @@ INSERT INTO ezsite_data (
   value
 ) VALUES (
   'ezpublish-version',
-  '3.6.0beta1'
+  '3.6.0rc1'
 );
 INSERT INTO ezsite_data (
   name,
   value
 ) VALUES (
   'ezpublish-release',
-  '1'
+  '2'
 );
 
 INSERT INTO ezurl (
---
+++ update/database/mysql/3.6/unstable/dbupdate-3.6.0beta1-to-3.6.0rc1.sql
@@ -0,0 +1,3 @@
+UPDATE ezsite_data SET value='3.6.0rc1' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='2' WHERE name='ezpublish-release';
+
