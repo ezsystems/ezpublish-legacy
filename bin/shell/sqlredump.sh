7430b073847583f17f39de73ce4676a8d3a2a7ef
Jan Borsodi <jb@ez.no>
2005-05-11 13:33:51 +0000

    #- Fixed db parameter passing, they were not passed with --db- prefix. #- Fixed update scripts to also allow --db-type to specify database type.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@11762 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/sqlredump.sh
+++ bin/shell/sqlredump.sh
@@ -172,7 +172,7 @@ function ez_cleanup_db
 # 	[ -n "$DB_HOST" ] && DB_HOST_OPT="--db-server=$DB_HOST"
 # 	[ -n "$DB_PWD" ] && DB_PWD_OPT="--db-password=$DB_PWD"
 	echo -n "Flattening objects"
-	ezdist_db_prepare_params_from_mysql "1"
+	ezdist_db_prepare_params_from_mysql ""
 	./update/common/scripts/flatten.php --db-driver=mysql $PARAM_EZ_DB_ALL --db-database=$DBNAME all &>.mysql.log
 	ez_result_file $? .mysql.log || exit 1
 	if [ $CLEAN_SEARCH ]; then
