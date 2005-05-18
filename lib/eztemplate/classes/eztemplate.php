5e0380951727fed986d76d6091ba20b020e007ea
Vadym Savchuk <vs@ez.no>
2005-05-18 15:13:43 +0000

    - Fixed eZTemplate::elementValue() so that it does not complain about   absence of an array element when the element exists and its value is null.   (Merged from trunk (3.6.0rc2) rev. 11831)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.5@11879 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/eztemplate/classes/eztemplate.php
+++ lib/eztemplate/classes/eztemplate.php
@@ -1079,7 +1079,7 @@ class eZTemplate
                         }
                         if ( is_array( $value ) )
                         {
-                            if ( isset( $value[$attributeValue] ) )
+                            if ( array_key_exists( $attributeValue, $value ) )
                             {
                                 unset( $tempValue );
                                 $tempValue =& $value[$attributeValue];
