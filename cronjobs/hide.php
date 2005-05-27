630d30fa3b7f25e96e012f3ce01e6a1bb6cb32c1
Vadym Savchuk <vs@ez.no>
2005-05-27 10:34:20 +0000

    - Fixed fatal error in the hide cronjob that happens when the hide   attribute is of time eztime and value of that attribute is not set.   (Merged from stable/3.6 (3.6.0) rev. 12009)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@12010 a01eee8c-daba-0310-acae-fa49f3023285

--- cronjobs/hide.php
+++ cronjobs/hide.php
@@ -81,7 +81,7 @@ foreach( $rootNodeIDList as $nodeID )
 
             $dateAttribute =& $dataMap[$dateAttributeName];
 
-            if ( is_null( $dateAttribute ) )
+            if ( is_null( $dateAttribute ) || !$dateAttribute->hasContent() )
             {
                 continue;
             }
