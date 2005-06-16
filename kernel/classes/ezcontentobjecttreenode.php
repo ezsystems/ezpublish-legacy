c178668a595fa4158830218eca92978559736dbc
Ruslan Lisnyk <rl@ez.no>
2005-06-16 11:41:49 +0000

    #- Fixed bug: static function used $this inside and was used as function attribute.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@12242 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobjecttreenode.php
+++ kernel/classes/ezcontentobjecttreenode.php
@@ -157,7 +157,6 @@ class eZContentObjectTreeNode extends eZPersistentObject
                                                       'data_map' => 'dataMap',
                                                       "object" => "object",
                                                       "subtree" => "subTree",
-                                                      "subtree_multi_paths" => "subTreeMultiPaths",
                                                       "children" => "children",
                                                       "children_count" => "childrenCount",
                                                       'contentobject_version_object' => 'contentObjectVersionObject',
@@ -1256,16 +1255,17 @@ class eZContentObjectTreeNode extends eZPersistentObject
         {
             if ( $nodeID == 0 )
             {
-                $nodeID = $treeNode->attribute( 'node_id' );
-                $node   =& $treeNode;
+                $node =& $treeNode;
             }
             else
             {
-                $node   =& eZContentObjectTreeNode::fetch( $nodeID );
+                $node =& eZContentObjectTreeNode::fetch( $nodeID );
             }
 
             if ( !is_object( $node ) )
                 return false;
+
+            $nodeID     = $treeNode->attribute( 'node_id' );
 
             $nodePath   = $node->attribute( 'path_string' );
             $nodeDepth  = $node->attribute( 'depth' );
@@ -1791,7 +1791,7 @@ class eZContentObjectTreeNode extends eZPersistentObject
             $pathStringCond     = '';
             $notEqParentString  = '';
             // If the node(s) doesn't exist we return null.
-            if ( !eZContentObjectTreeNode::createPathConditionAndNotEqParentSQLStrings( $pathStringCond, $notEqParentString, $this, $nodeID, $depth, $depthOperator ) )
+            if ( !eZContentObjectTreeNode::createPathConditionAndNotEqParentSQLStrings( $pathStringCond, $notEqParentString, null, $nodeID, $depth, $depthOperator ) )
             {
                 return null;
             }
