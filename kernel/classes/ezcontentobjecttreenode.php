a6678c2906c8b3371485d3f6fe4a9c9d795ea1dd
Björn Dieding <bjoern@xrow.de>
2005-06-29 13:01:16 +0000

    - Fixed bug #6838: Contentobject receives remote_id of nodeobject
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.5@12348 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobjecttreenode.php
+++ kernel/classes/ezcontentobjecttreenode.php
@@ -4248,6 +4248,7 @@ WHERE
             {
                 if ( array_key_exists( 'class_name', $node ) )
                 {
+                    unset( $node['remote_id'] );
                     $contentObject =& new eZContentObject( $node );
 
                     $permissions = array();
