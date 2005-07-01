b2606a88953c553ece757cc5509f17624d43a404
Jan Borsodi <jb@ez.no>
2005-07-01 09:23:39 +0000

    - Fixed bug #6854: Use of 'proxy' objects in template expressions fails with   compilation enabled.   (Merged from stable/3.6 rev. 12382)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@12383 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/eztemplate/classes/eztemplatecompiler.php
+++ lib/eztemplate/classes/eztemplatecompiler.php
@@ -2586,9 +2586,7 @@ END;
                     $textName = eZTemplateCompiler::currentTextName( $parameters );
                     if ( count( $knownTypes ) == 0 or in_array( 'objectproxy', $knownTypes ) )
                     {
-                        $php->addCodePiece( "while " . ( $resourceData['use-comments'] ? ( "/*TC:" . __LINE__ . "*/" ) : "" ) . "( is_object( \$$generatedVariableName ) and method_exists( \$$generatedVariableName, 'templateValue' ) )\n" .
-                                            "    \$$generatedVariableName = \$$generatedVariableName" . "->templateValue();\n" .
-                                            "\$$textName .= ( is_object( \$$generatedVariableName ) ? compiledFetchText( \$tpl, \$rootNamespace, \$currentNamespace, false, \$$generatedVariableName ) : \$$generatedVariableName );\n" .
+                        $php->addCodePiece( "\$$textName .= ( is_object( \$$generatedVariableName ) ? compiledFetchText( \$tpl, \$rootNamespace, \$currentNamespace, false, \$$generatedVariableName ) : \$$generatedVariableName );" .  ( $resourceData['use-comments'] ? ( "/*TC:" . __LINE__ . "*/" ) : "" ) . "\n" .
                                             "unset( \$$generatedVariableName );\n", array( 'spacing' => $spacing ) );
                     }
                     else
@@ -2658,12 +2656,7 @@ END;
                 }
                 else if ( $variableAssignmentName !== false and !$isStaticElement and !$treatVariableDataAsNonObject )
                 {
-                    if ( count( $knownTypes ) == 0 or in_array( 'objectproxy', $knownTypes ) )
-                    {
-                        $php->addCodePiece( "while " . ( $resourceData['use-comments'] ? ( "/*TC:" . __LINE__ . "*/" ) : "" ) . "( is_object( \$$generatedVariableName ) and method_exists( \$$generatedVariableName, 'templateValue' ) )\n" .
-                                            "    \$$generatedVariableName = \$$generatedVariableName" . "->templateValue();\n",
-                                            array( 'spacing' => $spacing ) );
-                    }
+                    // Normal assignment from an expression, no need to anything extra
                 }
                 unset( $dataInspection );
             }
@@ -3267,6 +3260,9 @@ unset( \$" . $variableAssignmentName . "Data );\n",
                 $php->addVariableUnsetList( $unsetList, array( 'spacing' => $spacing ) );
             }
         }
+        // After the entire expression line is done we try to extract the actual value if proxies are used
+        $php->addCodePiece( "while " . ( $resourceData['use-comments'] ? ( "/*TC:" . __LINE__ . "*/" ) : "" ) . "( is_object( \$$variableAssignmentName ) and method_exists( \$$variableAssignmentName, 'templateValue' ) )\n" .
+                            "    \$$variableAssignmentName = \$$variableAssignmentName" . "->templateValue();\n" );
     }
 }
 
