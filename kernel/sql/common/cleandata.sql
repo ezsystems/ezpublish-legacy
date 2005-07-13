c6f3fab868dd4975e70721fe7c423be59370366a
Dmitriy Lakhtyuk <dl@ez.no>
2005-07-13 14:38:57 +0000

    #- Updated sql-files to reflect 3.6.1 release
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@12515 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/sql/common/cleandata.sql
+++ kernel/sql/common/cleandata.sql
@@ -21878,14 +21878,14 @@ INSERT INTO ezsite_data (
   value
 ) VALUES (
   'ezpublish-version',
-  '3.6.1rc1'
+  '3.6.1'
 );
 INSERT INTO ezsite_data (
   name,
   value
 ) VALUES (
   'ezpublish-release',
-  '4'
+  '5'
 );
 
 INSERT INTO ezurl (
