587fdceafe51185fec9127318d779d6259570f99
Dmitriy Lakhtyuk <dl@ez.no>
2005-07-13 15:26:36 +0000

    #- Configured trunk branch
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@12517 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/sql/common/cleandata.sql
+++ kernel/sql/common/cleandata.sql
@@ -21878,14 +21878,14 @@ INSERT INTO ezsite_data (
   value
 ) VALUES (
   'ezpublish-version',
-  '3.6.0'
+  '3.8.0alpha1'
 );
 INSERT INTO ezsite_data (
   name,
   value
 ) VALUES (
   'ezpublish-release',
-  '3'
+  '1'
 );
 
 INSERT INTO ezurl (
--- update/database/mysql/3.8/unstable/dbupdate-3.6.0-to-3.8.0alpha1.sql
+++ update/database/mysql/3.8/unstable/dbupdate-3.6.0-to-3.8.0alpha1.sql
@@ -1,4 +1,4 @@
-UPDATE ezsite_data SET value='3.7.0alpha1' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='3.8.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
 ALTER TABLE ezorder ADD is_archived INT DEFAULT '0' NOT NULL ;
