0674e83df751e28cfb9bd561cfd34f4449eca662
Jan Borsodi <jb@ez.no>
2005-07-15 13:39:45 +0000

    #- Removed 3.4.8rc1 entry in db-check file which was forgotten.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.4@12550 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/php/checkdbfiles.php
+++ bin/php/checkdbfiles.php
@@ -122,7 +122,6 @@ $versions34 = array( 'unstable' => array( array( '3.3-3', '3.4.0alpha1' )
                                           ,array( '3.4.0alpha4', '3.4.0beta1' )
                                           ,array( '3.4.0beta1', '3.4.0beta2' )
                                           ,array( '3.4.0beta2', '3.4.0' )
-                                          ,array( '3.4.7', '3.4.8rc1' )                               
                                           ),
                      'unstable_subdir' => 'unstable',
                      'stable' => array( array( '3.3-5', '3.4.0' )
