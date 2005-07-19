c5a64b46173498bfacfd0fafa28fd9775373b0ae
Gurudutt Verma <gv@ez.no>
2005-07-19 14:31:48 +0000

    - Fixed bug #6862: The content-tree will displayed proper subtree while deleting a user or content object.   (merged from unstable/3.6.1 rev. 12694  and stable/3.6.2 rev 12614)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.5@12617 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/content/module.php
+++ kernel/content/module.php
@@ -198,6 +198,12 @@ $ViewList['removeobject'] = array(
     'script' => 'removeobject.php',
     'params' => array( ) );
 
+$ViewList['removeuserobject'] = array(
+    'functions' => array( 'read' ),
+    'default_navigation_part' => 'ezusernavigationpart',
+    'script' => 'removeobject.php',
+    'params' => array( ) );
+
 $ViewList['removeeditversion'] = array(
     'functions' => array( 'read' ),
     'default_navigation_part' => 'ezcontentnavigationpart',
