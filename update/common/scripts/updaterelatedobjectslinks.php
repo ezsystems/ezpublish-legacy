bab6bca0693d2ab779c5a987a5dd7f0db8800a7a
Kirill Subbotin <ks@ez.no>
2005-07-20 17:35:57 +0000

    - Added partial table processing for huge databases.   (Merged from stable/3.6 rev. 12635)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@12653 a01eee8c-daba-0310-acae-fa49f3023285

--- update/common/scripts/updaterelatedobjectslinks.php
+++ update/common/scripts/updaterelatedobjectslinks.php
@@ -41,9 +41,11 @@
   Updates 'ezcontentobject_link' table by adding related objects links made with
   attributes of 'ezobjectrelation' and 'ezobjectrelationlist' types.
 
-  You should run this script before using database created with eZ publish
+  You should run this script before using a database created with eZ publish
   version 3.5.* or lower.
 */
+
+define( "QUERY_LIMIT", 100 );
 
 if( !file_exists( 'update/common/scripts' ) || !is_dir( 'update/common/scripts' ) )
 {
@@ -59,8 +61,8 @@ $cli =& eZCLI::instance();
 $script =& eZScript::instance( array( 'description' => ( "\nDatabase converter for eZ publish 3.6.\n" .
                                                          "Updates 'ezcontentobject_link' table by adding related objects links made with\n" .
                                                          "attributes of 'ezobjectrelation' and 'ezobjectrelationlist' types.\n\n" .
-                                                         "Run this script before using database created with eZ publish version 3.5.* or lower.\n" .
-                                                         "Don't forget to update database structure before running this script." ),
+                                                         "Run this script before using a database created with eZ publish version 3.5.* or lower.\n" .
+                                                         "Don't forget to update database's structure before running this script." ),
                                       'use-session' => false,
                                       'use-modules' => false,
                                       'use-extensions' => false ) );
@@ -83,6 +85,7 @@ $dbHost = $options['db-host'] ? $options['db-host'] : false;
 $dbName = $options['db-database'] ? $options['db-database'] : false;
 $dbImpl = $options['db-driver'] ? $options['db-driver'] : false;
 $siteAccess = $options['siteaccess'] ? $options['siteaccess'] : false;
+$isQuiet = $script->isQuiet();
 
 if ( $siteAccess )
 {
@@ -142,84 +145,126 @@ include_once( 'kernel/classes/ezcontentobjectattribute.php' );
 include_once( 'lib/ezxml/classes/ezxml.php' );
 
 $links = array();
-$result = $db->arrayQuery( "SELECT attr.* 
-                            FROM ezcontentobject_attribute attr
-                            WHERE attr.data_type_string='ezobjectrelation'" );
-foreach( $result as $row )
-{
-    $attr = new eZContentObjectAttribute( $row );
-    if ( $attr->attribute( 'data_int' ) )
-    {
-        $links[] = array( 'fromObjectID' => $attr->attribute( 'contentobject_id' ),
-                          'fromObjectVersion' => $attr->attribute( 'version' ),
-                          'toObjectID' => $attr->attribute( 'data_int' ),
-                          'attributeID' => $attr->attribute( 'contentclassattribute_id' ) );
-    }
-}
+$added_links_count = 0;
+$links_total = 0;
 
-$result = $db->arrayQuery( "SELECT attr.* 
-                            FROM ezcontentobject_attribute attr
-                            WHERE attr.data_type_string='ezobjectrelationlist'" );
-foreach( $result as $row )
+function updateLinks()
 {
-    $attr = new eZContentObjectAttribute( $row );
-    if ( $attr->attribute( 'data_text' ) )
-    {
-        $xml = new eZXML();
-        $dom =& $xml->domTree( $attr->attribute( 'data_text' ) );
+    global $added_links_count, $links, $db, $isQuiet, $cli;
 
-        $root =& $dom->root();
-        $relationList =& $root->elementByName( 'relation-list' );
-        if ( $relationList )
+    foreach( $links as $link )
+    {
+        $fromObjectID = $link['fromObjectID'];
+        $fromObjectVersion = $link['fromObjectVersion'];
+        $toObjectID = $link['toObjectID'];
+        $attributeID = $link['attributeID'];
+    
+        $result = $db->arrayQuery( "SELECT ( COUNT( ezcontentobject_link.id ) ) AS count
+                               FROM ezcontentobject_link
+                               WHERE from_contentobject_id=$fromObjectID AND
+                                     from_contentobject_version=$fromObjectVersion AND
+                                     to_contentobject_id=$toObjectID AND
+                                     contentclassattribute_id=$attributeID" );
+    
+        $count = $result[0]['count'];
+        if ( !$count )
         {
-            $relationItems =& $relationList->elementsByName( 'relation-item' );
-            if ( count( $relationItems ) )
-            {
-                foreach( $relationItems as $relationItem )
-                {
-                    $links[] = array( 'fromObjectID' => $attr->attribute( 'contentobject_id' ),
-                                      'fromObjectVersion' => $attr->attribute( 'version' ),
-                                      'toObjectID' => $relationItem->attributeValue( 'contentobject-id' ),
-                                      'attributeID' => $attr->attribute( 'contentclassattribute_id' ) );
-                }
-            }
+            $db->query( "INSERT INTO ezcontentobject_link ( from_contentobject_id, from_contentobject_version, to_contentobject_id, contentclassattribute_id )
+                         VALUES ( $fromObjectID, $fromObjectVersion, $toObjectID, $attributeID )" );
+            $added_links_count++;
+            if ( !$isQuiet )
+                $cli->notice( "Added link from object $fromObjectID, version $fromObjectVersion with attribute $attributeID to object $toObjectID" );
         }
     }
 }
 
-$links_total = count( $links );
-$cli->notice( "Found $links_total references to related objects in attributes." );
+$query = "SELECT attr.* 
+                 FROM ezcontentobject_attribute attr
+                 WHERE attr.data_type_string='ezobjectrelation'";
 
-$added_links_count = 0;
+$result = $db->arrayQuery( $query, array( "limit" => QUERY_LIMIT ) );
+$pass = 1;
 
-foreach( $links as $link )
+while( count( $result ) )
 {
-    $fromObjectID = $link['fromObjectID'];
-    $fromObjectVersion = $link['fromObjectVersion'];
-    $toObjectID = $link['toObjectID'];
-    $attributeID = $link['attributeID'];
+    $links = array();
 
-    $result = $db->arrayQuery( "SELECT ( COUNT( ezcontentobject_link.id ) ) AS count
-                           FROM ezcontentobject_link
-                           WHERE from_contentobject_id=$fromObjectID AND
-                                 from_contentobject_version=$fromObjectVersion AND
-                                 to_contentobject_id=$toObjectID AND
-                                 contentclassattribute_id=$attributeID" );
-
-    $count = $result[0]['count'];
-    if ( !$count )
+    foreach( $result as $row )
     {
-        $db->query( "INSERT INTO ezcontentobject_link ( from_contentobject_id, from_contentobject_version, to_contentobject_id, contentclassattribute_id )
-                     VALUES ( $fromObjectID, $fromObjectVersion, $toObjectID, $attributeID )" );
-        $added_links_count++;
-        $cli->notice( "Added link from object $fromObjectID, version $fromObjectVersion with attribute $attributeID to object $toObjectID" );
+        $attr = new eZContentObjectAttribute( $row );
+        if ( $attr->attribute( 'data_int' ) )
+        {
+            $links[] = array( 'fromObjectID' => $attr->attribute( 'contentobject_id' ),
+                              'fromObjectVersion' => $attr->attribute( 'version' ),
+                              'toObjectID' => $attr->attribute( 'data_int' ),
+                              'attributeID' => $attr->attribute( 'contentclassattribute_id' ) );
+        }
     }
+    if ( count( $links ) )
+    {
+        $links_total += count( $links );
+        updateLinks();
+    }
+
+    $result = $db->arrayQuery( $query, array( "limit" => QUERY_LIMIT, "offset" => $pass * QUERY_LIMIT ) );
+    $pass++;
 }
 
-if ( $added_links_count )
-    $cli->notice( "$added_links_count links were added to 'ezcontentobject_link' table.\n" );
-else
-    $cli->notice( "No links were added.\n" );
+$query = "SELECT attr.* 
+          FROM ezcontentobject_attribute attr
+          WHERE attr.data_type_string='ezobjectrelationlist'";
+
+$result = $db->arrayQuery( $query, array( "limit" => QUERY_LIMIT ) );
+$pass = 1;
+
+while( count( $result ) )
+{
+    $links = array();
+
+    foreach( $result as $row )
+    {
+        $attr = new eZContentObjectAttribute( $row );
+        if ( $attr->attribute( 'data_text' ) )
+        {
+            $xml = new eZXML();
+            $dom =& $xml->domTree( $attr->attribute( 'data_text' ) );
+    
+            $root =& $dom->root();
+            $relationList =& $root->elementByName( 'relation-list' );
+            if ( $relationList )
+            {
+                $relationItems =& $relationList->elementsByName( 'relation-item' );
+                if ( count( $relationItems ) )
+                {
+                    foreach( $relationItems as $relationItem )
+                    {
+                        $links[] = array( 'fromObjectID' => $attr->attribute( 'contentobject_id' ),
+                                          'fromObjectVersion' => $attr->attribute( 'version' ),
+                                          'toObjectID' => $relationItem->attributeValue( 'contentobject-id' ),
+                                          'attributeID' => $attr->attribute( 'contentclassattribute_id' ) );
+                    }
+                }
+            }
+        }
+    }
+    if ( count( $links ) )
+    {
+        $links_total += count( $links );
+        updateLinks();
+    }
+
+    $result = $db->arrayQuery( $query, array( "limit" => QUERY_LIMIT, "offset" => $pass * QUERY_LIMIT ) );
+    $pass++;
+}
+
+if ( !$isQuiet )
+    $cli->notice( "\nFound $links_total references to related objects in attributes." );
+
+if ( !$isQuiet )
+    if ( $added_links_count )
+        $cli->notice( "$added_links_count links were added to 'ezcontentobject_link' table.\n" );
+    else
+        $cli->notice( "No links were added.\n" );
 
 $script->shutdown();
 ?>
