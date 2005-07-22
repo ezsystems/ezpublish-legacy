f7f21574bb61d233651ca71425c8671a93daf847
Jan Borsodi <jb@ez.no>
2005-07-22 11:42:23 +0000

    #- Made sure SQL is exactly the same as the file in 3.4 branch.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.7@12704 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/3.4/dbupdate-3.4.5-to-3.4.6.sql
+++ update/database/mysql/3.4/dbupdate-3.4.5-to-3.4.6.sql
@@ -6,4 +6,3 @@ DROP TABLE ezsubtree_expiry;
 -- Reduce the total size of the index eztrigger_def_id to make it work with utf-8
 ALTER TABLE eztrigger DROP INDEX eztrigger_def_id;
 ALTER TABLE eztrigger ADD UNIQUE INDEX eztrigger_def_id (module_name( 50 ),function_name( 50 ),connect_type);
-
