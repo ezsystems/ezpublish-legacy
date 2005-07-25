c09e18a261e9131bca496e937eb9a38422b27449
Valentin Doroschuk <vd@ez.no>
2005-07-25 12:50:21 +0000

    #- Minor code cleanups.    (Merged from stable/3.5 (3.5.5) rev 12736)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@12737 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobject.php
+++ kernel/classes/ezcontentobject.php
@@ -1636,6 +1636,8 @@ class eZContentObject extends eZPersistentObject
                 $attributeName = $dataType->attribute( 'information' );
                 $attributeName = $attributeName['name'];
                 $description = $contentObjectAttribute->attribute( 'validation_error' );
+                $validationNameArray[] = $contentClassAttribute->attribute( 'name' );
+                $validationName = implode( '->', $validationNameArray );
                 $hasValidationError = $contentObjectAttribute->attribute( 'has_validation_error' );
                 if ( $hasValidationError )
                 {
@@ -1644,23 +1646,16 @@ class eZContentObject extends eZPersistentObject
                     $validationNameArray = array();
                     if ( $parameters['prefix-name'] )
                         $validationNameArray = $parameters['prefix-name'];
-                    $validationNameArray[] = $contentClassAttribute->attribute( 'name' );
-                    $validationName = implode( '->', $validationNameArray );
-                    $unvalidatedAttributes[] = array( 'id' => $contentObjectAttribute->attribute( 'id' ),
-                                                      'identifier' => $contentClassAttribute->attribute( 'identifier' ),
-                                                      'name' => $validationName,
-                                                      'description' => $description );
-                } else
+                }
+                else
                 {
                     if ( !$description )
                         $description = 'uknown error';
-                    $validationNameArray[] = $contentClassAttribute->attribute( 'name' );
-                    $validationName = implode( '->', $validationNameArray );
-                    $unvalidatedAttributes[] = array( 'id' => $contentObjectAttribute->attribute( 'id' ),
-                                                      'identifier' => $contentClassAttribute->attribute( 'identifier' ),
-                                                      'name' => $validationName,
-                                                      'description' => $description );
                 }
+                $unvalidatedAttributes[] = array( 'id' => $contentObjectAttribute->attribute( 'id' ),
+                                                  'identifier' => $contentClassAttribute->attribute( 'identifier' ),
+                                                  'name' => $validationName,
+                                                  'description' => $description );
             }
             else if ( $status == EZ_INPUT_VALIDATOR_STATE_ACCEPTED )
             {
