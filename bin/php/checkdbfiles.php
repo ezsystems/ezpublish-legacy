52a3c26989d3789bbea45867ecbf8c1fd4bfbb08
Jan Borsodi <jb@ez.no>
2005-07-26 11:34:41 +0000

    #- Fixed wrong entries in checkdbfiles.php
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/3.6.1@12755 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/php/checkdbfiles.php
+++ bin/php/checkdbfiles.php
@@ -149,7 +149,6 @@ $versions35 = array( 'unstable' => array( array( '3.4.2', '3.5.0alpha1' )
 $versions36 = array( 'unstable' => array( array( '3.5.2', '3.6.0beta1' )
                                           ,array( '3.6.0beta1', '3.6.0rc1' )
                                           ,array( '3.6.0rc1', '3.6.0' )
-                                          ,array( '3.6.0', '3.6.1rc1' )
                                           ),
                      'unstable_subdir' => 'unstable',
                      'stable' => array( array( '3.5.2', '3.6.0' )
