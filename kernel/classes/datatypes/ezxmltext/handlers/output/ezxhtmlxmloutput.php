b57f3720ceed04d44cef50494885ec16e3bd21ed
Valentin Doroschuk <vd@ez.no>
2005-08-25 13:37:44 +0000

    - Fixed bug #6042: Paragraph tags included in unordered list.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.5@13046 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezxmltext/handlers/output/ezxhtmlxmloutput.php
+++ kernel/classes/datatypes/ezxmltext/handlers/output/ezxhtmlxmloutput.php
@@ -135,7 +135,19 @@ class eZXHTMLXMLOutput extends eZXMLOutputHandler
     {
         $output = "";
         eZDebugSetting::writeDebug( 'kernel-datatype-ezxmltext', "level " . $section->toString( 0 ) );
-        foreach ( $section->children() as $sectionNode )
+
+        $isBlockTag = false;
+        $listChildren = & $section->children();
+        // If <paragraph> is the one and only child then don't render it as <p>.
+        if ( count( $listChildren ) == 1 )
+        {
+            if ( $listChildren[0]->name() == "paragraph" )
+            {
+                 $listChildren = $listChildren[0]->children();
+            }
+        }
+
+        foreach ( $listChildren as $sectionNode )
         {
             if ( $tdSectionLevel == null )
             {
@@ -147,6 +159,8 @@ class eZXHTMLXMLOutput extends eZXMLOutputHandler
                 $currentSectionLevel = $currentSectionLevel;
             }
             $tagName = $sectionNode->name();
+
+
             switch ( $tagName )
             {
                 // tags with parameters
@@ -196,6 +210,21 @@ class eZXHTMLXMLOutput extends eZXMLOutputHandler
                         $output .= $this->renderXHTMLSection( $tpl, $sectionNode, $sectionLevel );
                     else
                         $output .= $this->renderXHTMLSection( $tpl, $sectionNode, $currentSectionLevel, $sectionLevel );
+                }break;
+                // Supported tags
+                case 'emphasize' :
+                case '#text' :
+                case 'line':
+                case 'strong' :
+                case 'ul' :
+                case 'ol' :
+                case 'literal' :
+                case 'custom' :
+                case 'link' :
+                case 'table' :
+                case 'object' :
+                {
+                    $output .= $this->renderXHTMLTag( $tpl, $sectionNode, $currentSectionLevel, $isBlockTag );
                 }break;
 
                 default :
@@ -453,7 +482,7 @@ class eZXHTMLXMLOutput extends eZXMLOutputHandler
                     foreach ( $tableRow->children() as $tableCell )
                     {
                         $cellContent = "";
-                        $cellContent .= $this->renderXHTMLSection( $tpl, $tableCell, 0, 0);
+                        $cellContent .= $this->renderXHTMLSection( $tpl, $tableCell, 0, 0 );
 
                         $tpl->setVariable( 'content', $cellContent, 'xmltagns' );
                         $cellWidth = $tableCell->attributeValueNS( 'width', "http://ez.no/namespaces/ezpublish3/xhtml/" );
@@ -526,7 +555,19 @@ class eZXHTMLXMLOutput extends eZXMLOutputHandler
                     $listItemContent = "";
 
                     $listSectionLevel = $currentSectionLevel;
-                    foreach ( $listItemNode->children() as $itemChildNode )
+
+                    $listChildren =& $listItemNode->children();
+
+                    // If <paragraph> is the one and only child then don't render it as <p>.
+                    if ( count( $listChildren ) == 1 )
+                    {
+                        if ( $listChildren[0]->name() == "paragraph" )
+                        {
+                            $listChildren = $listChildren[0]->children();
+                        }
+                    }
+
+                    foreach ( $listChildren as $itemChildNode )
                     {
                         $listSectionLevel = $currentSectionLevel;
                         if ( $itemChildNode->name() == "section" or $itemChildNode->name() == "paragraph" )
