279db7f4be4f7215d601caf0dc595eef288c7fce
Kåre Køhler Høvik <kk@ez.no>
2005-08-30 12:52:10 +0000

    - Fixed bug #7099: Role cache not invalidated when editing role assignments.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.5@13101 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezrole.php
+++ kernel/classes/ezrole.php
@@ -477,7 +477,7 @@ class eZRole extends eZPersistentObject
         // Expire role cache
         include_once( 'lib/ezutils/classes/ezexpiryhandler.php' );
         $handler =& eZExpiryHandler::instance();
-        $handler->setTimestamp( 'user-role-cache', mktime() );
+        $handler->setTimestamp( 'user-access-cache', mktime() );
         $handler->store();
     }
 
