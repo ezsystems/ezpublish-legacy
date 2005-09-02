7c17fdda189222cfb250aa61858ed4cbd277585f
Kåre Køhler Høvik <kk@ez.no>
2005-09-02 09:29:01 +0000

    - Fixed bug #7079: Fatal error on image.   ( Merged from stable/3.5 rev. 13152 )
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@13153 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezimage/ezimagealiashandler.php
+++ kernel/classes/datatypes/ezimage/ezimagealiashandler.php
@@ -815,23 +815,21 @@ class eZImageAliasHandler
     function parseInformationNode( &$imageInfoNode, &$imageInformation )
     {
         $imageInformation = array();
+
         $attributes =& $imageInfoNode->attributes();
         foreach ( $attributes as $attribute )
         {
-     /* CHECK FOR EZXML */
-            $imageInformation[$attribute->name()] = $attribute->content();
+            $imageInformation[$attribute->name()] = $attribute->value;
         }
+
         $children =& $imageInfoNode->children();
         foreach ( $children as $child )
         {
-     /* CHECK FOR EZXML */
             if ( isset ( $child->name ) )
             {
                 $childName = $child->name;
                 if ( $childName == 'array' )
                 {
-    //                $name = $child->attributeValue( 'name' );
-    //                $items = $child->elementsByName( 'item' );
                     $name = $child->get_attribute( 'name' );
                     $items = $child->get_elements_by_tagname( 'item' );
                     $array = array();
@@ -844,8 +842,6 @@ class eZImageAliasHandler
                 }
                 else if ( $childName == 'serialized' )
                 {
-    //                $name = $child->attributeValue( 'name' );
-    //                $data = $child->attributeValue( 'data' );
                     $name = $child->get_attribute( 'name' );
                     $data = $child->get_attribute( 'data' );
                     $imageInformation[$name] = unserialize( $data );
