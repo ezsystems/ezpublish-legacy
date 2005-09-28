e1491f8277d338d9340ce9e888b3b2e8b569c8fc
Kåre Køhler Høvik <kk@ez.no>
2005-09-28 13:33:16 +0000

    - Fixed bug #7208: ezsoapserver.php: Fix for call to non-object error. - Fixed bug #7207: ezsoapclient.php: Extend if scope to avoid warnings.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.5@13524 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezsoap/classes/ezsoapclient.php
+++ lib/ezsoap/classes/ezsoapclient.php
@@ -106,48 +106,50 @@ class eZSOAPClient
         if ( $this->Timeout != 0 )
         {
             $fp = fsockopen( $this->Server,
-            $this->Port,
-            $this->errorNumber,
-            $this->errorString,
-            $this->Timeout );
+                             $this->Port,
+                             $this->errorNumber,
+                             $this->errorString,
+                             $this->Timeout );
         }
         else
         {
             $fp = fsockopen( $this->Server,
-            $this->Port,
-            $this->errorNumber,
-            $this->errorString );
+                             $this->Port,
+                             $this->errorNumber,
+                             $this->errorString );
         }
 
         $payload =& $request->payload();
 
         eZDebug::writeNotice( $payload, "myload" );
 
-
-        if ( $fp != 0 )
+        if ( $fp == 0 )
         {
-            $authentification = "";
+            $this->ErrorString = '<b>Error:</b> eZSOAPClient::send() : Unable to open connection to ' . $this->Server . '.';
+            return 0;
+        }
 
-            if ( ( $this->login() != "" ) )
-            {
-                $authentification = "Authorization: Basic " . base64_encode( $this->login() . ":" . $this->password() ) . "\r\n" ;
-            }
+        $authentification = "";
 
-            $HTTPRequest = "POST " . $this->Path . " HTTP/1.0\r\n" .
-                 "User-Agent: eZ soap client\r\n" .
-                 "Host: " . $this->Server . "\r\n" .
-                 $authentification .
-                 "Content-Type: text/xml\r\n" .
-                 "Content-Length: " . strlen( $payload ) . "\r\n\r\n" .
-                 $payload;
+        if ( ( $this->login() != "" ) )
+        {
+            $authentification = "Authorization: Basic " . base64_encode( $this->login() . ":" . $this->password() ) . "\r\n" ;
+        }
 
-            eZDebug::writeNotice( $HTTPRequest, "Request" );
+        $HTTPRequest = "POST " . $this->Path . " HTTP/1.0\r\n" .
+            "User-Agent: eZ soap client\r\n" .
+            "Host: " . $this->Server . "\r\n" .
+            $authentification .
+            "Content-Type: text/xml\r\n" .
+            "Content-Length: " . strlen( $payload ) . "\r\n\r\n" .
+            $payload;
 
-            if ( !fputs( $fp, $HTTPRequest, strlen( $HTTPRequest ) ) )
-            {
-                $this->ErrorString = "<b>Error:</b> could not send the SOAP request. Could not write to the socket.";
-                return 0;
-            }
+        eZDebug::writeNotice( $HTTPRequest, "Request" );
+
+        if ( !fputs( $fp, $HTTPRequest, strlen( $HTTPRequest ) ) )
+        {
+            $this->ErrorString = "<b>Error:</b> could not send the SOAP request. Could not write to the socket.";
+            return 0;
         }
 
         $rawResponse = "";
