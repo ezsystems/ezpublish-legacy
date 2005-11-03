94508f98cd3662761554d6b499032ba0367f037e
Dmitriy Lakhtyuk <dl@ez.no>
2005-11-03 15:25:50 +0000

    - Configured.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/3.6.4@13937 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/php/checkdbfiles.php
+++ bin/php/checkdbfiles.php
@@ -156,6 +156,7 @@ $versions36 = array( 'unstable' => array( array( '3.5.2', '3.6.0beta1' )
                                         ,array( '3.6.0', '3.6.1' )
                                         ,array( '3.6.1', '3.6.2' )
                                         ,array( '3.6.2', '3.6.3' )
+                                        ,array( '3.6.3', '3.6.4' )
                                         ) );
 
 $versions['3.0'] = $versions30;
---
+++ update/database/postgresql/3.6/dbupdate-3.6.3-to-3.6.4.sql
@@ -0,0 +1,2 @@
+UPDATE ezsite_data SET value='3.6.4' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='8' WHERE name='ezpublish-release';
