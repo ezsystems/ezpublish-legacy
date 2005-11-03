cbbe8a4d9dcb0680ae00be6be3a47358b2bc29db
Dmitriy Lakhtyuk <dl@ez.no>
2005-11-03 16:37:24 +0000

    - Configured.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/3.7.2@13942 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/sql/common/cleandata.sql
+++ kernel/sql/common/cleandata.sql
@@ -21878,14 +21878,14 @@ INSERT INTO ezsite_data (
   value
 ) VALUES (
   'ezpublish-version',
-  '3.7.1'
+  '3.7.2'
 );
 INSERT INTO ezsite_data (
   name,
   value
 ) VALUES (
   'ezpublish-release',
-  '4'
+  '5'
 );
 
 INSERT INTO ezurl (
