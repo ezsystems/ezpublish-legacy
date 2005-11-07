12a390befaeb30a2d2875ebadb5d9f693eec0171
Ruslan Lisnyk <rl@ez.no>
2005-11-07 06:15:31 +0000

    - Fixed bug #7354: Typo in ezorder.php.   (Merged from stable/3.7 (3.7.3) rev. 13968)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@13969 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezorder.php
+++ kernel/classes/ezorder.php
@@ -1245,7 +1245,7 @@ class eZOrder extends eZPersistentObject
     function setStatus( $status )
     {
         if ( get_class( $status ) == "ezorderstatus" )
-            $this->StatusID = $staus->attribute( 'id' );
+            $this->StatusID = $status->attribute( 'id' );
         else
             $this->StatusID = $status;
         $this->setStatusModified( mktime() );
