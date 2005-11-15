84111a88da14117b50625c9e980f849cac379fe6
Valentin Doroschuk <vd@ez.no>
2005-11-15 14:17:34 +0000

    #- Removed the error: #  ( "Workflow type not found" ).
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.7@14095 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobject.php
+++ kernel/classes/ezcontentobject.php
@@ -1342,7 +1342,15 @@ class eZContentObject extends eZPersistentObject
              WHERE user_id = '$delID'" );
 
         include_once( "kernel/classes/ezworkflowtype.php" );
-        $registeredTypes = eZWorkFlowType::fetchRegisteredTypes();
+        if ( isset( $GLOBALS["eZWorkflowTypeObjects"] ) and is_array( $GLOBALS["eZWorkflowTypeObjects"] ) )
+        {
+            $registeredTypes =& $GLOBALS["eZWorkflowTypeObjects"];
+        }
+        else
+        {
+            $registeredTypes = eZWorkFlowType::fetchRegisteredTypes();
+        }
+
         // Cleanup ezworkflow_evet etc...
         foreach ( array_keys( $registeredTypes ) as $registeredTypeKey )
         {
