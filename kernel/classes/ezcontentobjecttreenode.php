0c0ab1eae19d0684758d2a775b4b25956c6f2afb
Lenara Anafieva <la@ez.no>
2005-12-07 15:31:00 +0000

    - Fixed bug #7516: Bug Unwanted debug output in createExtendedAttributeFilterSQLStrings.   ( Merged from stable/3.5 rev. 14292 )
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.7@14294 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobjecttreenode.php
+++ kernel/classes/ezcontentobjecttreenode.php
@@ -834,7 +834,6 @@ class eZContentObjectTreeNode extends eZPersistentObject
             $filter['tables']   = $sqlResult['tables'];
             $filter['joins']    = $sqlResult['joins'];
 
-            eZDebug::writeDebug( $filter['joins'], 'extendedAttributeFilterJoins' );
         }
 
         return $filter;
