47b85d663cb9b6a89c7d07e65aeb1750cb031814
Sergey Alexeev <sa@ez.no>
2005-12-29 08:08:43 +0000

    - Fixed bug #7612: Crash when empty eznode with anchor   provided in "href" attribute.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@14435 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobjecttreenode.php
+++ kernel/classes/ezcontentobjecttreenode.php
@@ -2930,6 +2930,12 @@ class eZContentObjectTreeNode extends eZPersistentObject
 
     function &fetchByURLPath( $pathString, $asObject = true )
     {
+        if ( $pathString == "" ) 
+        {
+            eZDebug::writeWarning( 'Can not fetch, given URLPath is empty', 'eZContentObjectTreeNode::fetchByURLPath' );
+            return null;
+        }
+
         return eZContentObjectTreeNode::fetch( false, false, $asObject, array( "path_identification_string" => $pathString ) );
 
         /* return eZPersistentObject::fetchObject( eZContentObjectTreeNode::definition(),
