40373a3717bf4bc22b43f01ea091afc7e4c7bd21
Kåre Køhler Høvik <kk@ez.no>
2006-01-02 14:12:28 +0000

    - Fixed bug #7445: RSS Feed bugs.   ( Merged from stable/3.5 rev. 14447, stable/3.6 rev. 14448 )
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.7@14449 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/rss/edit_functions.php
+++ kernel/rss/edit_functions.php
@@ -38,6 +38,13 @@ include_once( 'kernel/classes/ezrssexportitem.php' );
 include_once( 'kernel/classes/ezrssimport.php' );
 include_once( 'lib/ezutils/classes/ezhttppersistence.php' );
 
+/*!
+ Store RSSExport
+
+ \param Module
+ \param HTTP
+ \param publish ( true/false )
+*/
 function storeRSSExport( &$Module, &$http, $publish = false )
 {
     /* Kill the RSS cache */
@@ -87,7 +94,7 @@ function storeRSSExport( &$Module, &$http, $publish = false )
     $rssExport = eZRSSExport::fetch( $http->postVariable( 'RSSExport_ID' ), true, EZ_RSSEXPORT_STATUS_DRAFT );
     $rssExport->setAttribute( 'title', $http->postVariable( 'title' ) );
     $rssExport->setAttribute( 'url', $http->postVariable( 'url' ) );
-    // $rssExport->setAttribute( 'site_access', $http->postVariable( 'SiteAccess' ) );
+
     $rssExport->setAttribute( 'description', $http->postVariable( 'Description' ) );
     $rssExport->setAttribute( 'rss_version', $http->postVariable( 'RSSVersion' ) );
     $rssExport->setAttribute( 'number_of_objects', $http->postVariable( 'NumberOfObjects' ) );
@@ -100,7 +107,8 @@ function storeRSSExport( &$Module, &$http, $publish = false )
     {
         $rssExport->setAttribute( 'active', 0 );
     }
-    $rssExport->setAttribute( 'access_url', $http->postVariable( 'Access_URL' ) );
+
+    $rssExport->setAttribute( 'access_url', str_replace( array( '/', '?', '&', '>', '<' ), '',  $http->postVariable( 'Access_URL' ) ) );
     if ( $http->hasPostVariable( 'MainNodeOnly' ) )
     {
         $rssExport->setAttribute( 'main_node_only', 1 );
