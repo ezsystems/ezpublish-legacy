a29864bbe86771bfc8bbff27b7c3cbe0d6f733b7
Dmitriy Lakhtyuk <dl@ez.no>
2006-01-13 12:13:51 +0000

    - Configured for the next 3.6.6 release.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@14536 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/php/checkdbfiles.php
+++ bin/php/checkdbfiles.php
@@ -158,6 +158,7 @@ $versions36 = array( 'unstable' => array( array( '3.5.2', '3.6.0beta1' )
                                         ,array( '3.6.2', '3.6.3' )
                                         ,array( '3.6.3', '3.6.4' )
                                         ,array( '3.6.4', '3.6.5' )
+                                        ,array( '3.6.5', '3.6.6' )
                                         ) );
 
 $versions['3.0'] = $versions30;
---
+++ update/database/postgresql/3.6/dbupdate-3.6.5-to-3.6.6.sql
@@ -0,0 +1,2 @@
+UPDATE ezsite_data SET value='3.6.6' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='10' WHERE name='ezpublish-release';
