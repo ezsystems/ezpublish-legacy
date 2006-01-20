9700ffcde450c1849a47b3d3fd995b8062442dae
Valentin Doroschuk <vd@ez.no>
2006-01-20 10:05:09 +0000

    - Fixed bug #7675: Bug /content/action without POTS-data renders blank page.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.5@14597 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/content/action.php
+++ kernel/content/action.php
@@ -1382,6 +1382,10 @@ else if ( $module->isCurrentAction( 'UploadFile' ) )
     $module->redirectTo( $module->functionURI( 'view' ) . '/' . $viewMode . '/' . $topLevelNode . '/' );
     return;
 }*/
+else if ( !isset( $result ) )
+{
+    return $module->handleError( EZ_ERROR_KERNEL_NOT_AVAILABLE, 'kernel' );
+}
 
 
 // return module contents
