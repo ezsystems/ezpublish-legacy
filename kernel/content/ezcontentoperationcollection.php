90e7eb033b8a59ce54a40347527a3902046f6a39
Valentin Doroschuk <vd@ez.no>
2006-01-24 10:09:09 +0000

    #- Removed unwanted debugs and php warning.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@14624 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/content/ezcontentoperationcollection.php
+++ kernel/content/ezcontentoperationcollection.php
@@ -232,7 +232,7 @@ class eZContentOperationCollection
 
         $db =& eZDB::instance();
         $db->begin();
-        
+
         $object->setAttribute( 'modified', mktime() );
         $object->store();
 
@@ -350,7 +350,6 @@ class eZContentOperationCollection
 
         if ( $updateSectionID )
         {
-            eZDebug::writeDebug( "will  update section ID " );
             eZContentOperationCollection::updateSectionID( $objectID, $versionNum );
         }
         $db->commit();
@@ -393,7 +392,8 @@ class eZContentOperationCollection
         list( $newMainAssignment ) = eZNodeAssignment::fetchForObject( $objectID, $versionNum, 1 );
 
         $currentVersion =& $object->attribute( 'current' );
-        list( $oldMainAssignment ) = eZNodeAssignment::fetchForObject( $objectID, $object->attribute( 'current_version' ), 1 );
+        $nodeAssigmentTmpArray = eZNodeAssignment::fetchForObject( $objectID, $object->attribute( 'current_version' ), 1 );
+        list( $oldMainAssignment ) = isset( $nodeAssigmentTmpArray[0] ) ? $nodeAssigmentTmpArray : null;
 
         if ( $newMainAssignment && $oldMainAssignment
              &&  $newMainAssignment->attribute( 'parent_node' ) != $oldMainAssignment->attribute( 'parent_node' ) )
@@ -449,7 +449,7 @@ class eZContentOperationCollection
             $nodeAssignment =& $curentVersionNodeAssignments[$key];
             $versionParentIDList[] = $nodeAssignment->attribute( 'parent_node' );
         }
-        
+
         $db =& eZDB::instance();
         $db->begin();
         foreach ( array_keys( $assignedExistingNodes )  as $key )
