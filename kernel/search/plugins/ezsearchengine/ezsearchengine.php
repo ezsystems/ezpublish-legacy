5386f9ccc01945c0047c4435b23d27be6158d086
Valentin Doroschuk <vd@ez.no>
2006-02-23 11:37:36 +0000

    - Fixed bug #7861: php warnings with AllowEmptySearch and advanced search.   ( Merged from stable/3.7 ( 3.7.4 ) rev. 14924 )
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@14926 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/search/plugins/ezsearchengine/ezsearchengine.php
+++ kernel/search/plugins/ezsearchengine/ezsearchengine.php
@@ -2054,7 +2054,10 @@ class eZSearchEngine
     function &prepareWordIDArrays( $searchText )
     {
         if ( trim( $searchText ) == "" )
-            return array();
+            $ini =& eZINI::instance();
+            if ( $ini->hasVariable( 'SearchSettings', 'AllowEmptySearch' ) and
+                 $ini->variable( 'SearchSettings', 'AllowEmptySearch' ) != 'enabled' )
+                return array();
         $db =& eZDB::instance();
         $searchWordArray = $this->splitString( $searchText );
 
