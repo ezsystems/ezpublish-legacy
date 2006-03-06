c76324d942d5bcf50727d669ac060d088244dba0
Valentin Doroschuk <vd@ez.no>
2006-03-06 09:19:08 +0000

    - Fixed bug #7912: reverseRelatedObjectCount $attributeID = 0.   ( count of reverse related objects is incorrect )
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@14993 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobject.php
+++ kernel/classes/ezcontentobject.php
@@ -2352,7 +2352,7 @@ class eZContentObject extends eZPersistentObject
                            0  - count regular relations (not by attribute)
                            false - count all relations
     */
-    function &reverseRelatedObjectCount( $version = false, $toObjectID = false, $attributeID = 0 )
+    function &reverseRelatedObjectCount( $version = false, $toObjectID = false, $attributeID = false )
     {
         if( !$toObjectID )
             $toObjectID = $this->ID;
