8aba2b8d943954bda91000d120905719efd40b6e
Dmitriy Lakhtyuk <dl@ez.no>
2006-03-31 14:04:27 +0000

    - Fixed wrong sql-update for postgresql.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@15512 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/3.8/unstable/dbupdate-3.8.0alpha2-to-3.8.0beta1.sql
+++ update/database/postgresql/3.8/unstable/dbupdate-3.8.0alpha2-to-3.8.0beta1.sql
@@ -14,21 +14,41 @@ CREATE TABLE ezcontent_language
                     
 DROP TABLE ezcontent_translation;
                  
-ALTER TABLE ezcontentobject ADD COLUMN language_mask int NOT NULL DEFAULT 0;
-ALTER TABLE ezcontentobject ADD COLUMN initial_language_id int NOT NULL DEFAULT 0;
+ALTER TABLE ezcontentobject ADD COLUMN language_mask int; 
+ALTER TABLE ezcontentobject ALTER COLUMN language_mask SET DEFAULT 0;
+ALTER TABLE ezcontentobject ALTER COLUMN language_mask SET NOT NULL;
 
-ALTER TABLE ezcontentobject_name ADD COLUMN language_id int NOT NULL DEFAULT 0;
+ALTER TABLE ezcontentobject ADD COLUMN initial_language_id int;
+ALTER TABLE ezcontentobject ALTER COLUMN initial_language_id SET DEFAULT 0;
+ALTER TABLE ezcontentobject ALTER COLUMN initial_language_id SET NOT NULL;
 
-ALTER TABLE ezcontentobject_attribute ADD COLUMN language_id int NOT NULL DEFAULT 0;
+ALTER TABLE ezcontentobject_name ADD COLUMN language_id int;
+ALTER TABLE ezcontentobject_name ALTER COLUMN language_id SET DEFAULT 0;
+ALTER TABLE ezcontentobject_name ALTER COLUMN language_id SET NOT NULL;
 
-ALTER TABLE ezcontentobject_version ADD COLUMN language_mask int NOT NULL DEFAULT 0;
-ALTER TABLE ezcontentobject_version ADD COLUMN initial_language_id int NOT NULL DEFAULT 0;
+ALTER TABLE ezcontentobject_attribute ADD COLUMN language_id int;
+ALTER TABLE ezcontentobject_attribute ALTER COLUMN language_id SET DEFAULT 0;
+ALTER TABLE ezcontentobject_attribute ALTER COLUMN language_id SET NOT NULL;
 
-ALTER TABLE ezcontentclass ADD COLUMN always_available int NOT NULL DEFAULT 0;
+ALTER TABLE ezcontentobject_version ADD COLUMN language_mask int;
+ALTER TABLE ezcontentobject_version ALTER COLUMN language_mask SET DEFAULT 0;
+ALTER TABLE ezcontentobject_version ALTER COLUMN language_mask SET NOT NULL;
 
-ALTER TABLE ezcontentobject_link ADD COLUMN op_code int NOT NULL DEFAULT 0;
+ALTER TABLE ezcontentobject_version ADD COLUMN initial_language_id int;
+ALTER TABLE ezcontentobject_version ALTER COLUMN initial_language_id SET DEFAULT 0;
+ALTER TABLE ezcontentobject_version ALTER COLUMN initial_language_id SET NOT NULL;
 
-ALTER TABLE eznode_assignment ADD COLUMN op_code int NOT NULL DEFAULT 0;
+ALTER TABLE ezcontentclass ADD COLUMN always_available int;
+ALTER TABLE ezcontentclass ALTER COLUMN always_available SET DEFAULT 0;
+ALTER TABLE ezcontentclass ALTER COLUMN always_available SET NOT NULL;
+
+ALTER TABLE ezcontentobject_link ADD COLUMN op_code int;
+ALTER TABLE ezcontentobject_link ALTER COLUMN op_code SET DEFAULT 0;
+ALTER TABLE ezcontentobject_link ALTER COLUMN op_code SET NOT NULL;
+
+ALTER TABLE eznode_assignment ADD COLUMN op_code int;
+ALTER TABLE eznode_assignment ALTER COLUMN op_code SET DEFAULT 0;
+ALTER TABLE eznode_assignment ALTER COLUMN op_code SET NOT NULL;
 
 -- updates
 -- set correct op_code
