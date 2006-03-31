b162c8bd5f9c48943942b282594cdebfe01d933a
Dmitriy Lakhtyuk <dl@ez.no>
2006-03-31 10:50:01 +0000

    - Fixed '2005' year.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@15483 a01eee8c-daba-0310-acae-fa49f3023285

--- support/lupdate-ezpublish3-qt4/fetchtr_php.cpp
+++ support/lupdate-ezpublish3-qt4/fetchtr_php.cpp
@@ -7,25 +7,28 @@
 // Gunnstein Lye <gl@ez.no>
 // Created on: <10-Dec-2002 18:46:17 gl>
 //
-// Copyright (C) 1999-2005 eZ systems as. All rights reserved.
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: eZ publish
+// SOFTWARE RELEASE: 3.8.x
+// COPYRIGHT NOTICE: Copyright (C) 1999-2006 eZ systems AS
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //
-// The GNU General Public License is also available online at:
-//
-// http://www.gnu.org/copyleft/gpl.html
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 //
 
 #include <metatranslator.h>
