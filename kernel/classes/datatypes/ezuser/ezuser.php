cbb5c88479bb4ea08a1ab9d30d6e62538e49455f
Valentin Doroschuk <vd@ez.no>
2006-04-05 14:52:58 +0000

    - Added site.ini:[shopSettings].ClearBasketOnLogout setting:   Means that the basket will (or NOT) be cleared when a user logs out. - Fixed bug #8015: Clear basket when user logs out.   ( Merged from stable/trunk ( 3.8.0beta2 ) rev. 15573 )
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@15575 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezuser/ezuser.php
+++ kernel/classes/datatypes/ezuser/ezuser.php
@@ -896,6 +896,13 @@ WHERE user_id = '" . $userID . "' AND
         $newUserID = EZ_USER_ANONYMOUS_ID;
         $http->setSessionVariable( 'eZUserLoggedInID', $newUserID );
         eZSessionSetUserID( $newUserID );
+        // Clear current basket if necessary
+        $db =& eZDB::instance();
+        $db->begin();
+        include_once( 'kernel/classes/ezbasket.php' );
+        eZBasket::cleanupCurrentBasket();
+        $db->commit();
+
         if ( $contentObjectID )
             eZUser::cleanup();
     }
