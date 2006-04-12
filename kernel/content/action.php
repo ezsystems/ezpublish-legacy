faf041737a57977bfed97048140f4e1cb664a681
Valentin Doroschuk <vd@ez.no>
2006-04-12 10:37:58 +0000

    - Fixed bug #8121: Possible wrong navigation menu used when deleting objects.   ( Merged from stable/3.7 ( 3.7.6 ) rev. 15657 )
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.5@15660 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/content/action.php
+++ kernel/content/action.php
@@ -934,9 +934,13 @@ else if ( $http->hasPostVariable( 'RemoveButton' ) )
                 $navigationPartIdentifier = $section->attribute( 'navigation_part_identifier' );
             else
                 $navigationPartIdentifier = null;
-            if( $navigationPartIdentifier and $navigationPartIdentifier == 'ezusernavigationpart' )
+            if ( $navigationPartIdentifier and $navigationPartIdentifier == 'ezusernavigationpart' )
             {
                 $module->redirectTo( $module->functionURI( 'removeuserobject' ) . '/' );
+            }
+            elseif ( $navigationPartIdentifier and $navigationPartIdentifier == 'ezmedianavigationpart' )
+            {
+                $module->redirectTo( $module->functionURI( 'removemediaobject' ) . '/' );
             }
             else
             {
@@ -1145,9 +1149,13 @@ else if ( $http->hasPostVariable( "ContentObjectID" )  )
                 $navigationPartIdentifier = $section->attribute( 'navigation_part_identifier' );
             else
                 $navigationPartIdentifier = null;
-            if( $navigationPartIdentifier and $navigationPartIdentifier == 'ezusernavigationpart' )
+            if ( $navigationPartIdentifier and $navigationPartIdentifier == 'ezusernavigationpart' )
             {
                 $module->redirectTo( $module->functionURI( 'removeuserobject' ) . '/' );
+            }
+            elseif ( $navigationPartIdentifier and $navigationPartIdentifier == 'ezmedianavigationpart' )
+            {
+                $module->redirectTo( $module->functionURI( 'removemediaobject' ) . '/' );
             }
             else
             {
--- kernel/content/module.php
+++ kernel/content/module.php
@@ -197,6 +197,12 @@ $ViewList['removeuserobject'] = array(
     'script' => 'removeobject.php',
     'params' => array( ) );
 
+$ViewList['removemediaobject'] = array(
+    'functions' => array( 'read' ),
+    'default_navigation_part' => 'ezmedianavigationpart',
+    'script' => 'removeobject.php',
+    'params' => array( ) );
+
 $ViewList['removeeditversion'] = array(
     'functions' => array( 'read' ),
     'default_navigation_part' => 'ezcontentnavigationpart',
