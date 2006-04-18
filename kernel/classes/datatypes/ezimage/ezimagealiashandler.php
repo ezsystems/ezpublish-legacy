1b56574a35426ff32e9c3fc30f90cbcf772532db
Valentin Doroschuk <vd@ez.no>
2006-04-18 12:25:46 +0000

    - Fixed bug: Image filename is wrong after initializeFromFile().   ( Merged from stable/3.6 ( 3.6.8 ) rev. 15741 ).
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.5@15745 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezimage/ezimagealiashandler.php
+++ kernel/classes/datatypes/ezimage/ezimagealiashandler.php
@@ -1072,7 +1072,7 @@ class eZImageAliasHandler
         }
         eZFileHandler::copy( $filename, $mimeData['url'] );
 
-        return $this->initialize( $mimeData, $filename, $imageAltText );
+        return $this->initialize( $mimeData, $originalFilename, $imageAltText );
     }
 
     /*!
