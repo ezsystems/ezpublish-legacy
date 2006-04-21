810265206d0e0ae47e82f94a69d50d408c647a13
Ruslan Lisnyk <rl@ez.no>
2006-04-21 13:02:08 +0000

    #- Fixed bug: wrong returned array for redirect after precollect info #  (during user check) causes anonymous access to admin interface.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@15812 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezuser/ezuser.php
+++ kernel/classes/datatypes/ezuser/ezuser.php
@@ -1781,7 +1781,7 @@ WHERE user_id = '" . $userID . "' AND
     */
     function preCollectUserInfo()
     {
-        return array( 'user', 'login' );
+        return array( 'module' => 'user', 'function' => 'login' );
     }
 
     /*!
