a935fbd1f94ca0f51b78ee8dbad5f05a5f24ec0f
Ruslan Lisnyk <rl@ez.no>
2006-04-29 09:54:09 +0000

    #- Fixed small issue: debug output in log file didn't contain #  $label given to eZDebug::writeDebug( $var, $label ) method. #  (Merged from trunk (3.8.0) rev. 15930)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@15933 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezutils/classes/ezdebug.php
+++ lib/ezutils/classes/ezdebug.php
@@ -718,6 +718,7 @@ class eZDebug
                 }
                 if ( $this->isLogFileEnabled( $verbosityLevel ) )
                 {
+                    $string = "$label:\n$string";
                     $this->writeFile( $fileName, $string, $verbosityLevel, $alwaysLog );
                 }
             }
