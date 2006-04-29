210f8fc24b9c50ec4522cb915c7495ef3ac82149
Dmitriy Lakhtyuk <dl@ez.no>
2006-04-29 10:36:39 +0000

    - Added conditional warning in 'createClassFilteringSQLString'.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@15935 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobjecttreenode.php
+++ kernel/classes/ezcontentobjecttreenode.php
@@ -805,6 +805,10 @@ class eZContentObjectTreeNode extends eZPersistentObject
                 {
                     $classIDArray[] = $classID;
                 }
+                else
+                {
+                    eZDebugSetting::writeWarning( 'kernel-content-class', "Invalid class identifier in subTree() classfilterarray, classID : " . $originalClassID );
+                }
             }
 
             if ( count( $classIDArray ) > 0  )
@@ -2158,6 +2162,10 @@ class eZContentObjectTreeNode extends eZPersistentObject
                 if ( is_numeric( $classID ) )
                 {
                     $classIDArray[] = $classID;
+                }
+                else
+                {
+                    eZDebugSetting::writeWarning( 'kernel-content-class', "Invalid class identifier in subTree() classfilterarray, classID : " . $originalClassID );
                 }
             }
             if ( count( $classIDArray ) > 0  )
