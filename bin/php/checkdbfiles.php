db5c1724c27271c14f81bbb6ef67654b22965e05
Dmitriy Lakhtyuk <dl@ez.no>
2006-05-06 11:52:45 +0000

    - Configured for the next 3.7.7 release.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.7@16021 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/php/checkdbfiles.php
+++ bin/php/checkdbfiles.php
@@ -160,6 +160,7 @@ $versions37 = array( 'unstable' => array( array( '3.6.0', '3.7.0rc1' )
                                         ,array( '3.7.3', '3.7.4' ) 
                                         ,array( '3.7.4', '3.7.5' ) 
                                         ,array( '3.7.5', '3.7.6' ) 
+                                        ,array( '3.7.6', '3.7.7' ) 
                                         ) );
 
 
--- kernel/sql/common/cleandata.sql
+++ kernel/sql/common/cleandata.sql
@@ -21878,14 +21878,14 @@ INSERT INTO ezsite_data (
   value
 ) VALUES (
   'ezpublish-version',
-  '3.7.6'
+  '3.7.7'
 );
 INSERT INTO ezsite_data (
   name,
   value
 ) VALUES (
   'ezpublish-release',
-  '9'
+  '10'
 );
 
 INSERT INTO ezurl (
