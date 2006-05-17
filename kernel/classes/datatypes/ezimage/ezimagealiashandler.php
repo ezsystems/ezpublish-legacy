eb98d2ce8c1c2cba74edb1c70537221ae29bd94f
Valentin Doroschuk <vd@ez.no>
2006-05-17 14:34:59 +0000

    - Fixed bug #8229: Image Upload in 3.8 doesn't work.   (Merged from trunk (3.9.0alpha1) rev. 16111)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.8@16112 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezimage/ezimagealiashandler.php
+++ kernel/classes/datatypes/ezimage/ezimagealiashandler.php
@@ -1052,6 +1052,8 @@ class eZImageAliasHandler
                 foreach ( $items as $item )
                 {
                     $itemValueNode = $item->first_child();
+                    if ( !is_object ( $itemValueNode ) )
+                        continue;
                     $itemValue = $itemValueNode->node_value();
                     if (  $item->get_attribute( 'base64' ) == '1' )
                     {
