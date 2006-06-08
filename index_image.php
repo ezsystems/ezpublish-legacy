9be4591a93fb711cffb78cd88d9bf7623dfa3eed
Vadym Savchuk <vs@ez.no>
2006-06-08 18:45:32 +0000

    - Fixed bug #8394: Fatal error due to typo in clustering code.   (Merged from stable/3.8 (3.8.1) rev. 16212) - Changed the way clustering settings are stored.   For more details see doc/features/3.8/clustering.txt.   (Merged from stable/3.8 (3.8.1) rev. 16215) - Optimized index_image.php to gain more speed.   (Merged from stable/3.8 (3.8.1) rev. 16215)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@16216 a01eee8c-daba-0310-acae-fa49f3023285

--- index_image.php
+++ index_image.php
@@ -1,28 +1,26 @@
 <?php
-$filename = ltrim( $_SERVER['SCRIPT_URL'], "/");
-
-require_once( 'kernel/classes/ezclusterfilehandler.php' );
-$file = eZClusterFileHandler::instance( $filename );
-
-if ( $file->exists() )
-    $file->passthrough();
-else
-{
-    header( "HTTP/1.1 404 Not Found" );
-?>
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<HTML><HEAD>
-<TITLE>404 Not Found</TITLE>
-</HEAD><BODY>
-<H1>Not Found</H1>
-The requested URL <?=$filename?> was not found on this server.<P>
-</BODY></HTML>
-<?php
-}
-
 /*
-$fp = fopen( '/tmp/index_image.log', 'a+' );
-fwrite( $fp, "$filename\n" );
-fclose( $fp );
+This file should be included from a file containing
+clustering database settings.
+
+Here are sample contents of such file:
+-------------------------------------------
+<?php
+define( 'STORAGE_BACKEND',     'mysql'           );
+define( 'STORAGE_HOST',        'db'              );
+define( 'STORAGE_PORT',        3306              );
+define( 'STORAGE_SOCKET',      '/tmp/mysql.sock' );
+define( 'STORAGE_USER',        'fred'            );
+define( 'STORAGE_PASS',        'secret'          );
+define( 'STORAGE_DB',          'cluster'         );
+define( 'STORAGE_CHUNK_SIZE',  65535             );
+
+include_once( 'index_image.php' );
+?>
+-------------------------------------------
 */
+
+if ( !defined( 'STORAGE_BACKEND' ) )
+    die( "No storage backend chosen.\n" );
+include_once( 'index_image_' . STORAGE_BACKEND . '.php' );
 ?>
---
+++ index_image_mysql.php
@@ -0,0 +1,72 @@
+<?php
+
+define( 'TABLE_METADATA', 'ezdbfile' );
+define( 'TABLE_DATA', 'ezdbfile_data' );
+
+// Connect to storage database.
+$serverString = STORAGE_HOST;
+if ( defined( 'STORAGE_SOCKET' ) )
+    $serverString .= ':' . STORAGE_SOCKET;
+elseif ( defined( 'STORAGE_PORT' ) )
+    $serverString .= ':' . STORAGE_PORT;
+if ( !$db = mysql_connect( $serverString, STORAGE_USER, STORAGE_PASS ) )
+    die( "Unable to connect to storage server.\n" );
+
+if ( !mysql_select_db( STORAGE_DB, $db ) )
+    die( "Unable to connect to storage database.\n" );
+
+$filename = ltrim( $_SERVER['SCRIPT_URL'], "/");
+
+// Fetch file metadata.
+$filePathHash = md5( mysql_real_escape_string( $filename ) );
+$sql = "SELECT * FROM " . TABLE_METADATA . " WHERE name_hash='$filePathHash'" ;
+if ( !$res = mysql_query( $sql, $db ) )
+    die( "Failed to retrive file metadata: $filePath.\n" );
+
+if ( !( $metaData = mysql_fetch_array( $res, MYSQL_ASSOC ) ) )
+{
+    header( "HTTP/1.1 404 Not Found" );
+?>
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<HTML><HEAD>
+<TITLE>404 Not Found</TITLE>
+</HEAD><BODY>
+<H1>Not Found</H1>
+The requested URL <?=$filename?> was not found on this server.<P>
+</BODY></HTML>
+<?php
+    mysql_free_result( $res );
+    mysql_close( $db );
+    exit( 1 );
+}
+
+mysql_free_result( $res );
+
+// Fetch file data.
+$fileID = $metaData['id'];
+$sql = "SELECT filedata FROM " . TABLE_DATA . " WHERE masterid=$fileID";
+if ( $res = mysql_query( $sql, $db ) )
+{
+    // Output HTTP headers.
+    $path     = $metaData['name'];
+    $size     = $metaData['size'];
+    $mimeType = $metaData['datatype'];
+    $mtime    = $metaData['mtime'];
+    $mdate    = gmdate( 'D, d M Y H:i:s T', $mtime );
+
+    header( "Content-Length: $size" );
+    header( "Content-Type: $mimeType" );
+    header( "Last-Modified: $mdate" );
+    header( "Expires: ". gmdate('D, d M Y H:i:s', time() + 6000) . 'GMT' );
+    header( "Connection: close" );
+    header( "X-Powered-By: eZ publish" );
+    header( "Accept-Ranges: bytes" );
+
+    // Output image data.
+    while ( $row = mysql_fetch_row( $res ) )
+        echo $row[0];
+    mysql_free_result( $res );
+}
+mysql_close( $db );
+
+?>
