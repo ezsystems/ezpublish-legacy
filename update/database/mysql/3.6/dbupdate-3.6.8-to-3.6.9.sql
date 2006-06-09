a993598e638aec202dd69360db1f3eccf5d98ef0
Dmitriy Lakhtyuk <dl@ez.no>
2006-06-09 14:43:40 +0000

    - Fixed bug #8259: Performance issue with eZKeyword datatype.   (Merged from trunk (3.9.0alpha1) rev. 16221)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@16224 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/3.6/dbupdate-3.6.8-to-3.6.9.sql
+++ update/database/mysql/3.6/dbupdate-3.6.8-to-3.6.9.sql
@@ -1,2 +1,5 @@
 UPDATE ezsite_data SET value='3.6.9' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='13' WHERE name='ezpublish-release';
+
+CREATE INDEX ezkeyword_keyword_id ON ezkeyword ( keyword, id );
+CREATE INDEX ezkeyword_attr_link_kid_oaid ON ezkeyword_attribute_link ( keyword_id, objectattribute_id );
