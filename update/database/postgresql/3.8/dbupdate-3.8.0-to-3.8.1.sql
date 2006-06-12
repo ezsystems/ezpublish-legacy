a0898ae42307e44cc04fbde837266d2ce3e10928
Dmitriy Lakhtyuk <dl@ez.no>
2006-06-12 10:07:54 +0000

    - Fixed bug #8342: Missing index on ezurlalias, eznode_assignment table.   (Merged from trunk (3.9.0alpha1) rev. 16229)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.8@16230 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/3.8/dbupdate-3.8.0-to-3.8.1.sql
+++ update/database/postgresql/3.8/dbupdate-3.8.0-to-3.8.1.sql
@@ -3,3 +3,9 @@ UPDATE ezsite_data SET value='7' WHERE name='ezpublish-release';
 
 CREATE INDEX ezkeyword_keyword_id ON ezkeyword USING btree ( keyword, id );
 CREATE INDEX ezkeyword_attr_link_kid_oaid ON ezkeyword_attribute_link USING btree ( keyword_id, objectattribute_id );
+
+CREATE INDEX ezurlalias_is_wildcard ON ezurlalias USING btree ( is_wildcard );
+
+CREATE INDEX eznode_assignment_coid_cov ON eznode_assignment USING btree ( contentobject_id,contentobject_version );
+CREATE INDEX eznode_assignment_is_main ON eznode_assignment USING btree ( is_main );
+CREATE INDEX eznode_assignment_parent_node ON eznode_assignment USING btree ( parent_node );
