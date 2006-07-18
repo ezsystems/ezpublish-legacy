c75acbc7b13e3fbee25521dd256b182acd5b1bb6
Valentin Doroschuk <vd@ez.no>
2006-07-18 16:36:15 +0000

    - Fixed bug #8681: ezurl and ezroot operators don't work correctly.   (Merged from stable/3.6 (3.6.9) rev. 16441) - Fixed bug #7619: RSS: Relative image src in XML field.   (Merged from stable/3.6 (3.6.9) rev. 16441)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.7@16447 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezutils/classes/ezuri.php
+++ lib/ezutils/classes/ezuri.php
@@ -330,7 +330,7 @@ class eZURI
      Implementation of an 'ezurl' template operator.
      Makes valid ez publish urls to use in links.
     */
-    function transformURI( &$href )
+    function transformURI( &$href, $ignoreIndexDir = false )
     {
         if ( preg_match( "#^[a-zA-Z0-9]+:#", $href ) || substr( $href, 0, 2 ) == '//' )
             return false;
@@ -349,7 +349,8 @@ class eZURI
 
         include_once( 'lib/ezutils/classes/ezsys.php' );
         $sys =& eZSys::instance();
-        $href = $sys->indexDir() . $href;
+        $dir = !$ignoreIndexDir ? $sys->indexDir() : $sys->wwwDir();
+        $href = $sys->serverURL() . $dir . $href;
         $href = preg_replace( "#^(//)#", "/", $href );
         $href = preg_replace( "#(^.*)(/+)$#", "\$1", $href );
         $href = htmlspecialchars( $href );
