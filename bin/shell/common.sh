82f4f9a168cf6a1fada9fa083e65d2b6eedef697
Dmitriy Lakhtyuk <dl@ez.no>
2006-07-25 13:10:50 +0000

    - Updated REPOSITORY_BRANCH_PATH.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/3.7.8@16520 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/common.sh
+++ bin/shell/common.sh
@@ -24,7 +24,7 @@ TR_REPOSITORY_BASE_URL="http://svn.ez.no/svn/translations"
 # This needs to be set correctly when a new branch is created
 # e.g. stable/3.4 stable/3.5
 #REPOSITORY_BRANCH_PATH="URL: http://svn.ez.no/svn/nextgen/stable/3.7"
-REPOSITORY_BRANCH_PATH="stable/3.7"
+REPOSITORY_BRANCH_PATH="release/3.7.8"
 REPOSITORY_STABLE_BRANCH_PATH="stable"
 
 CURRENT_URL="$REPOSITORY_BASE_URL/$REPOSITORY_BRANCH_PATH"
