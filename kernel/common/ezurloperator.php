77ea8e34d4b77e3c44ce7a439c67618c415e138e
Valentin Doroschuk <vd@ez.no>
2006-07-26 09:25:16 +0000

    - Implemented enhancement #7449: No way to check existance of variable   in ezini or ezhttp. # Added new tpl operators ezini_hasvariable,ezhttp_has_variable and added # new parameters to ezini,ezhttp.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@16531 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/common/ezurloperator.php
+++ kernel/common/ezurloperator.php
@@ -51,9 +51,11 @@ class eZURLOperator
                             $image_name = 'ezimage',
                             $ext_name = 'exturl',
                             $httpName = 'ezhttp',
-                            $iniName = 'ezini' )
+                            $iniName = 'ezini',
+                            $iniNameHasVariable = 'ezini_hasvariable',
+                            $httpNameHasVariable = 'ezhttp_hasvariable' )
     {
-        $this->Operators = array( $url_name, $urlroot_name, $ezsys_name, $design_name, $image_name, $ext_name, $httpName, $iniName );
+        $this->Operators = array( $url_name, $urlroot_name, $ezsys_name, $design_name, $image_name, $ext_name, $httpName, $iniName, $iniNameHasVariable, $httpNameHasVariable );
         $this->URLName = $url_name;
         $this->URLRootName = $urlroot_name;
         $this->SysName = $ezsys_name;
@@ -61,7 +63,9 @@ class eZURLOperator
         $this->ImageName = $image_name;
         $this->ExtName = $ext_name;
         $this->HTTPName = $httpName;
-        $this->ININame=  $iniName;
+        $this->ININame = $iniName;
+        $this->ININameHasVariable = $iniNameHasVariable;
+        $this->HTTPNameHasVariable = $httpNameHasVariable;
         $this->Sys =& eZSys::instance();
     }
 
@@ -115,7 +119,14 @@ class eZURLOperator
                                                'element-transformation' => true,
                                                'transform-parameters' => true,
                                                'input-as-parameter' => true,
-                                               'element-transformation-func' => 'iniTrans')
+                                               'element-transformation-func' => 'iniTrans'),
+                      $this->ININameHasVariable => array( 'input' => true,
+                                                          'output' => true,
+                                                          'parameters' => true,
+                                                          'element-transformation' => true,
+                                                          'transform-parameters' => true,
+                                                          'input-as-parameter' => true,
+                                                          'element-transformation-func' => 'iniTrans' )
                       );
     }
 
@@ -131,19 +142,31 @@ class eZURLOperator
             $iniGroup = eZTemplateNodeTool::elementStaticValue( $parameters[0] );
             $iniVariable = eZTemplateNodeTool::elementStaticValue( $parameters[1] );
 
-            $iniName = false;
-            $iniPath = false;
+            $iniName = isset( $parameters[2] ) ? eZTemplateNodeTool::elementStaticValue( $parameters[2] ) : false;
+            $iniPath = isset( $parameters[3] ) ? eZTemplateNodeTool::elementStaticValue( $parameters[3] ) : false;
 
-            if ( count( $parameters ) > 2 )
+            // If we should check for existence of variable.
+            // You can use like:
+            //     ezini( <BlockName>, <SettingName>, <FileName>, <IniPath>,<Should We Put implamantaion to cache file?: 'inCache' or true()>,
+            //                                                                    <Should We Check for existence: 'hasVariable' or true()> )
+            //     ezini_hasvariable( <BlockName>, <SettingName>, <FileName>, <IniPath>... )
+            if ( $operatorName == $this->ININameHasVariable )
             {
-                $iniName = eZTemplateNodeTool::elementStaticValue( $parameters[2] );
+                $checkExistence = true;
+            }
+            else
+            {
+                $checkExistence = isset( $parameters[5] )
+                                  ? ( eZTemplateNodeTool::elementStaticValue( $parameters[5] ) === true or
+                                      eZTemplateNodeTool::elementStaticValue( $parameters[5] ) == 'hasVariable' ) ? true : false
+                                  : false;
             }
 
-            if ( count( $parameters ) > 3 )
-                $iniPath = eZTemplateNodeTool::elementStaticValue( $parameters[3] );
-
             if ( count( $parameters ) > 4 )
-                if ( eZTemplateNodeTool::elementStaticValue( $parameters[4] ) == 'true' )
+            {
+                $inCache = eZTemplateNodeTool::elementStaticValue( $parameters[4] );
+                // Check if we should put implementaion of parsing ini variable to cache file
+                if ( $inCache == 'inCache' or $inCache === true )
                 {
                     $values = array();
                     $values[] = $parameters[0];
@@ -165,14 +188,17 @@ class eZURLOperator
                     else
                         $code .= '%tmp1% =& eZINI::instance();' . "\n";
 
+                    $checkExist = $checkExistence ? 'true' : 'false';
+
                     $code .= 'if ( %tmp1%->hasVariable( %1%, %2% ) )' . "\n" .
-                        '    %output% = %tmp1%->variable( %1%, %2% );' . "\n" .
+                        '    %output% = ' . "!$checkExist" . ' ? %tmp1%->variable( %1%, %2% ) : true;' . "\n" .
                         "else\n" .
-                        "    %output% = '';\n";
+                        "    %output% = $checkExist ? false : '';\n";
+
 
                     return array( eZTemplateNodeTool::createCodePieceElement( $code, $values, false, 1 ) );
                 }
-
+            }
             include_once( 'lib/ezutils/classes/ezini.php' );
 
             if ( $iniPath !== false )
@@ -185,13 +211,21 @@ class eZURLOperator
             $value = '';
             if ( $ini->hasVariable( $iniGroup, $iniVariable ) )
             {
-                $value = $ini->variable( $iniGroup, $iniVariable );
+                $value = !$checkExistence ? $ini->variable( $iniGroup, $iniVariable ) : true;
             }
             else
             {
-                if ( $iniName === false )
-                    $iniName = 'site.ini';
-                $tpl->error( $operatorName, "No such variable '$iniVariable' in group '$iniGroup' for $iniName" );
+                // If we should check for existence then we return only FALSE
+                if ( $checkExistence )
+                {
+                    $value = false;
+                }
+                else
+                {
+                    if ( $iniName === false )
+                        $iniName = 'site.ini';
+                    $tpl->error( $operatorName, "No such variable '$iniVariable' in group '$iniGroup' for $iniName" );
+                }
             }
             return array( eZTemplateNodeTool::createStringElement( $value ) );
         }
@@ -543,62 +577,76 @@ CODEPIECE;
     {
         switch ( $operatorName )
         {
+            case $this->ININameHasVariable:
             case $this->ININame:
             {
                 if ( count( $operatorParameters ) > 0 )
                 {
                     $iniGroup = $tpl->elementValue( $operatorParameters[0], $rootNamespace, $currentNamespace );
-                    if ( count( $operatorParameters ) > 1 )
+                    if ( count( $operatorParameters ) == 1 )
                     {
-                        $iniVariable = $tpl->elementValue( $operatorParameters[1], $rootNamespace, $currentNamespace );
-                        $iniName = false;
-                        $iniPath = false;
-                        if ( count( $operatorParameters ) > 2 )
-                        {
-                            $iniName = $tpl->elementValue( $operatorParameters[2], $rootNamespace, $currentNamespace );
-                        }
-                        if ( count( $operatorParameters ) > 3 )
-                        {
-                            $iniPath = $tpl->elementValue( $operatorParameters[3], $rootNamespace, $currentNamespace );
-                        }
-
-                        include_once( 'lib/ezutils/classes/ezini.php' );
-
-                        if ( $iniPath !== false )
-                            $ini =& eZINI::instance( $iniName, $iniPath, null, null, null, true );
-                        elseif ( $iniName !== false )
-                            $ini =& eZINI::instance( $iniName );
-                        else
-                            $ini =& eZINI::instance();
-
-                        if ( $ini->hasVariable( $iniGroup, $iniVariable ) )
-                        {
-                            $operatorValue = $ini->variable( $iniGroup, $iniVariable );
-                        }
-                        else
-                        {
-                            if ( $iniPath !== false )
-                            {
-                                // Return empty string instead of displaying error when using 'path' parameter
-                                // and DirectAccess mode for ezini.
-                                $operatorValue = '';
-                            }
-                            else
-                            {
-                                if ( $iniName === false )
-                                    $iniName = 'site.ini';
-                                $tpl->error( $operatorName, "!!!No such variable '$iniVariable' in group '$iniGroup' for $iniName" );
-                            }
-                        }
+                        $tpl->error( $operatorName, "Missing variable name parameter" );
                         return;
                     }
+                    $iniVariable = $tpl->elementValue( $operatorParameters[1], $rootNamespace, $currentNamespace );
+                    $iniName = isset( $operatorParameters[2] ) ? $tpl->elementValue( $operatorParameters[2], $rootNamespace, $currentNamespace ) : false;
+                    $iniPath = isset( $operatorParameters[3] ) ? $tpl->elementValue( $operatorParameters[3], $rootNamespace, $currentNamespace ) : false;
+
+                    // If we should check for existence of variable.
+                    // You can use like:
+                    //     ezini( <BlockName>, <SettingName>, <FileName>, <IniPath>, _use under template compiling mode_ , <Should We Check for existence: 'hasVariable' or true()> )
+                    //     ezini_hasvariable( <BlockName>, <SettingName>, <FileName>, <IniPath>... )
+                    if ( $operatorName == $this->ININameHasVariable )
+                    {
+                        $checkExistence = true;
+                    }
                     else
-                        $tpl->error( $operatorName, "Missing variable name parameter" );
+                    {
+                        $checkExistence = isset( $operatorParameters[5] )
+                                          ? ( $tpl->elementValue( $operatorParameters[5], $rootNamespace, $currentNamespace ) === true or
+                                              $tpl->elementValue( $operatorParameters[5], $rootNamespace, $currentNamespace ) == 'hasVariable' ) ? true : false
+                                          : false;
+                    }
+                    include_once( 'lib/ezutils/classes/ezini.php' );
+
+                    if ( $iniPath !== false )
+                        $ini =& eZINI::instance( $iniName, $iniPath, null, null, null, true );
+                    elseif ( $iniName !== false )
+                        $ini =& eZINI::instance( $iniName );
+                    else
+                        $ini =& eZINI::instance();
+
+                    if ( $ini->hasVariable( $iniGroup, $iniVariable ) )
+                    {
+                        $operatorValue = !$checkExistence ? $ini->variable( $iniGroup, $iniVariable ) : true;
+                    }
+                    else
+                    {
+                        if ( $checkExistence )
+                        {
+                            $operatorValue = false;
+                            return;
+                        }
+                        if ( $iniPath !== false )
+                        {
+                            // Return empty string instead of displaying error when using 'path' parameter
+                            // and DirectAccess mode for ezini.
+                            $operatorValue = '';
+                        }
+                        else
+                        {
+                            if ( $iniName === false )
+                                $iniName = 'site.ini';
+                            $tpl->error( $operatorName, "!!!No such variable '$iniVariable' in group '$iniGroup' for $iniName" );
+                        }
+                    }
+                    return;
                 }
                 else
                     $tpl->error( $operatorName, "Missing group name parameter" );
             } break;
 
+            case $this->HTTPNameHasVariable:
             case $this->HTTPName:
             {
                 include_once( 'lib/ezutils/classes/ezhttptool.php' );
@@ -619,28 +667,66 @@ CODEPIECE;
                         else
                             $tpl->warning( $operatorName, "Unknown http type '$httpTypeName'" );
                     }
+
+                    // If we should check for existence of http variable
+                    // You can use like:
+                    //     ezhttp( <Variable>, <Method: post, get, session>, <Should We Check for existence: 'hasVariable' or true()> )
+                    //     ezhttp_hasvariable( <Variable>, <Method> )
+                    if ( $operatorName == $this->HTTPNameHasVariable )
+                    {
+                        $checkExistence = true;
+                    }
+                    else
+                    {
+                        $checkExistence = isset( $operatorParameters[2] )
+                                          ? ( $tpl->elementValue( $operatorParameters[2], $rootNamespace, $currentNamespace ) === true or
+                                              $tpl->elementValue( $operatorParameters[2], $rootNamespace, $currentNamespace ) == 'hasVariable' ) ? true : false
+                                          : false;
+                    }
                     switch( $httpType )
                     {
                         case EZ_HTTP_OPERATOR_TYPE_POST:
                         {
                             if ( $http->hasPostVariable( $httpName ) )
-                                $operatorValue = $http->postVariable( $httpName );
+                                $operatorValue = !$checkExistence ? $http->postVariable( $httpName ) : true;
                             else
+                            {
+                                // If only check for existence - return false
+                                if ( $checkExistence )
+                                {
+                                    $operatorValue = false;
+                                    return;
+                                }
                                 $tpl->error( $operatorName, "Unknown post variable '$httpName'" );
+                            }
                         } break;
                         case EZ_HTTP_OPERATOR_TYPE_GET:
                         {
                             if ( $http->hasGetVariable( $httpName ) )
-                                $operatorValue = $http->getVariable( $httpName );
+                                $operatorValue = !$checkExistence ? $http->getVariable( $httpName ) : true;
                             else
+                            {
+                                if ( $checkExistence )
+                                {
+                                    $operatorValue = false;
+                                    return;
+                                }
                                 $tpl->error( $operatorName, "Unknown get variable '$httpName'" );
+                            }
                         } break;
                         case EZ_HTTP_OPERATOR_TYPE_SESSION:
                         {
                             if ( $http->hasSessionVariable( $httpName ) )
-                                $operatorValue = $http->sessionVariable( $httpName );
+                                $operatorValue = !$checkExistence ? $http->sessionVariable( $httpName ) : true;
                             else
+                            {
+                                if ( $checkExistence )
+                                {
+                                    $operatorValue = false;
+                                    return;
+                                }
                                 $tpl->error( $operatorName, "Unknown session variable '$httpName'" );
+                            }
                         } break;
                     }
                 }
