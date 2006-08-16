c57defc5b7f18ec2abeb912ec9239ccc9eab9f2d
Valentin Doroschuk <vd@ez.no>
2006-08-16 10:50:58 +0000

    - Fixed bug #8810: {fetch( content , list , hash( parent_node_id , 0 ) )}   causes fatal error.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@16630 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobjecttreenode.php
+++ kernel/classes/ezcontentobjecttreenode.php
@@ -1373,7 +1373,7 @@ class eZContentObjectTreeNode extends eZPersistentObject
         {
             if ( $nodeID == 0 )
             {
-                if ( !is_object( $treeNode ) )
+                if ( !is_object( $treeNode ) or get_class( $treeNode ) != 'ezcontentobjecttreenode' )
                     return false;
 
                 $node =& $treeNode;
