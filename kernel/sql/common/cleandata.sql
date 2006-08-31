7b10375cd35a0190f07d6d90db1c57a8de3b7887
Valentin Doroschuk <vd@ez.no>
2006-08-31 14:41:03 +0000

    - Configured for the next 3.8.5 subrelease.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.8@16745 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/sql/common/cleandata.sql
+++ kernel/sql/common/cleandata.sql
@@ -22198,14 +22198,14 @@ INSERT INTO ezsite_data (
   value
 ) VALUES (
   'ezpublish-version',
-  '3.8.4'
+  '3.8.5'
 );
 INSERT INTO ezsite_data (
   name,
   value
 ) VALUES (
   'ezpublish-release',
-  '10'
+  '11'
 );
 
 INSERT INTO ezurl (
---
+++ update/database/mysql/3.8/dbupdate-3.8.4-to-3.8.5.sql
@@ -0,0 +1,2 @@
+UPDATE ezsite_data SET value='3.8.5' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='11' WHERE name='ezpublish-release';
