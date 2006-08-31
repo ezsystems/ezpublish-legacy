8386dc80fe06f2879cc4254ddf355a9811336119
Valentin Doroschuk <vd@ez.no>
2006-08-31 10:22:40 +0000

    - Fixed bug #8867: Cron and cli scripts do not check transactions.   (Merged from trunk (3.9.0alpha1) rev. 16733)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.7@16736 a01eee8c-daba-0310-acae-fa49f3023285

--- index.php
+++ index.php
@@ -852,12 +852,7 @@ if ( $module->exitStatus() == EZ_MODULE_STATUS_REDIRECT )
     include_once( 'kernel/classes/ezstaticcache.php' );
     eZStaticCache::executeActions();
 
-    $db =& eZDB::instance();
-    while ( $db->TransactionCounter > 0 )
-    {
-        eZDebug::writeError( "Internal transaction counter mismatch : " . $db->TransactionCounter . ". Should be zero." );
-        $db->commit();
-    }
+    eZDB::checkTransactionCounter();
     if ( $automatic_redir )
     {
         eZHTTPTool::redirect( $redirectURI );
@@ -1116,12 +1111,7 @@ eZDebug::addTimingPoint( "End" );
 
 ob_end_flush();
 
-$db =& eZDB::instance();
-while ( $db->TransactionCounter > 0 )
-{
-    eZDebug::writeError( "Internal transaction counter mismatch : " . $db->TransactionCounter . ". Should be zero." );
-    $db->commit();
-}
+eZDB::checkTransactionCounter();
 
 eZDisplayResult( $templateResult );
 
--- runcronjobs.php
+++ runcronjobs.php
@@ -303,6 +303,10 @@ foreach ( $scripts as $cronScript )
         include( $scriptFile );
         eZDebug::addTimingPoint( "Script $scriptFile done" );
         ++$index;
+        // The transaction check
+        $transactionCounterCheck = eZDB::checkTransactionCounter();
+        if ( isset( $transactionCounterCheck['error'] ) )
+            $cli->error( $transactionCounterCheck['error'] );
     }
 }
 
