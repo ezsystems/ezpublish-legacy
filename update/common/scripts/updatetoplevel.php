78d308f4fc2b8f330b8e4fd03398d278c56ce49d
Svetlana Shatokhina <ss@ez.no>
2006-09-05 07:55:18 +0000

    - Changed description for update/common/scripts/updatetoplevel.php
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@16761 a01eee8c-daba-0310-acae-fa49f3023285

--- update/common/scripts/updatetoplevel.php
+++ update/common/scripts/updatetoplevel.php
@@ -58,7 +58,7 @@ include_once( 'lib/ezutils/classes/ezcli.php' );
 include_once( 'kernel/classes/ezscript.php' );
 
 $cli =& eZCLI::instance();
-$script =& eZScript::instance( array( 'description' => ( "eZ publish Setup Node Creator\n\n" .
+$script =& eZScript::instance( array( 'description' => ( "eZ publish Top Level Nodes Creator\n\n" .
                                                          "This script will create the setup top level node if it does not exist,\n" .
                                                          "\n" .
                                                          "updatetoplevel.php" ),
