1713b733c322430413f6ab4d6943b99f5bd50a08
Björn Dieding <bjoern@xrow.de>
2006-09-15 09:19:56 +0000

    - Added new mail transport "file" for debugging.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@16862 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ lib/ezutils/classes/ezfiletransport.php
@@ -0,0 +1,81 @@
+<?php
+//
+// Definition of eZFileTransport class
+//
+// Created on: <10-Dec-2002 14:41:22 amos>
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: eZ publish
+// SOFTWARE RELEASE: 3.8.x
+// COPYRIGHT NOTICE: Copyright (C) 1999-2006 eZ systems AS
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
+
+/*! \file ezsendmailtransport.php
+*/
+
+/*!
+  \class eZFileTransport ezfiletransport.php
+  \brief Sends the email message to a file.
+
+*/
+
+include_once( 'lib/ezutils/classes/ezmailtransport.php' );
+include_once( 'lib/ezfile/classes/ezlog.php' );
+
+class eZFileTransport extends eZMailTransport
+{
+    /*!
+     Constructor
+    */
+    function eZFileTransport()
+    {
+    }
+
+    /*!
+     \reimp
+    */
+    function sendMail( &$mail )
+    {
+        $ini =& eZINI::instance();
+        $sendmailOptions = '';
+        $emailFrom = $mail->sender();
+        $emailSender = $emailFrom['email'];
+        if ( !$emailSender || count( $emailSender) <= 0 )
+            $emailSender = $ini->variable( 'MailSettings', 'EmailSender' );
+        if ( !$emailSender )
+            $emailSender = $ini->variable( 'MailSettings', 'AdminEmail' );
+        if ( !eZMail::validate( $emailSender ) )
+            $emailSender = false;
+
+        $isSafeMode = ini_get( 'safe_mode' );
+        if ( $isSafeMode and
+             $emailSender and
+             $mail->sender() == false )
+            $mail->setSenderText( $emailSender );
+        
+        $filename = mktime() . '-' . rand() . '.mail';
+        
+        $data = preg_replace('/(\r\n|\r|\n)/', "\r\n", $mail->headerText() . "\n" . $mail->body() );
+        return eZFile::create( $filename, 'var/log/mail', $data );
+    }
+}
+
+?>
