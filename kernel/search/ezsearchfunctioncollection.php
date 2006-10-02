d7c562b5b542c38073410101c139989395e45aed
Ruslan Lisnyk <rl@ez.no>
2006-10-02 04:15:07 +0000

    - Implemented 'New search_stats fetch funtions' task.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@16996 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ kernel/search/ezsearchfunctioncollection.php
@@ -0,0 +1,74 @@
+<?php
+//
+// Definition of eZSearchFunctionCollection class
+//
+// Created on: <06-Oct-2002 16:19:31 amos>
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: eZ publish
+// SOFTWARE RELEASE: 3.8.x
+// COPYRIGHT NOTICE: Copyright (C) 1999-2006 eZ systems AS
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
+
+/*! \file ezcontentfunctioncollection.php
+*/
+
+/*!
+  \class eZSearchFunctionCollection ezsearchfunctioncollection.php
+  \brief The class eZSearchFunctionCollection does
+
+*/
+
+include_once( 'kernel/error/errors.php' );
+
+class eZSearchFunctionCollection
+{
+    /*!
+     Constructor
+    */
+    function eZSearchFunctionCollection()
+    {
+    }
+
+    function fetchSearchListCount()
+    {
+        include_once( "kernel/classes/ezsearchlog.php" );
+
+        $db =& eZDB::instance();
+        $query = "SELECT count(*) as count FROM ezsearch_search_phrase";
+        $searchListCount = $db->arrayQuery( $query );
+
+        return array( 'result' => $searchListCount[0]['count'] );
+    }
+
+    function fetchSearchList( $offset, $limit )
+    {
+        include_once( "kernel/classes/ezsearchlog.php" );
+
+        $parameters = array( 'offset' => $offset, 'limit'  => $limit );
+        $mostFrequentPhraseArray =& eZSearchLog::mostFrequentPhraseArray( $parameters );
+
+        return array( 'result' => &$mostFrequentPhraseArray );
+    }
+
+}
+
+?>
---
+++ kernel/search/function_definition.php
@@ -0,0 +1,54 @@
+<?php
+//
+// Created on: <20-Sep-2006 23:23:23 amos>
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: eZ publish
+// SOFTWARE RELEASE: 3.8.x
+// COPYRIGHT NOTICE: Copyright (C) 1999-2006 eZ systems AS
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
+
+/*! \file function_definition.php
+*/
+
+$FunctionList = array();
+$FunctionList['list_count'] = array( 'name' => 'list_count',
+                                     'operation_types' => array( 'read' ),
+                                     'call_method' => array( 'include_file' => 'kernel/search/ezsearchfunctioncollection.php',
+                                                       'class' => 'eZSearchFunctionCollection',
+                                                       'method' => 'fetchSearchListCount' ),
+                                      'parameter_type' => 'standard',
+                                      'parameters' => array() );
+$FunctionList['list'] = array( 'name' => 'list',
+                               'operation_types' => array( 'read' ),
+                               'call_method' => array( 'include_file' => 'kernel/search/ezsearchfunctioncollection.php',
+                                                       'class' => 'eZSearchFunctionCollection',
+                                                       'method' => 'fetchSearchList' ),
+                                      'parameter_type' => 'standard',
+                                      'parameters' => array( array( 'name' => 'offset',
+                                                                    'type' => 'integer',
+                                                                    'required' => false,
+                                                                    'default' => false ),
+                                                             array( 'name' => 'limit',
+                                                                    'type' => 'integer',
+                                                                    'required' => false,
+                                                                    'default' => false ) ) );
+?>
