fb0758af4c542921edda108bbce6e553ef7b2759
Dmitriy Lakhtyuk <dl@ez.no>
2006-10-24 07:34:04 +0000

    - Fixed bug #007575: eZURLAlias | Using PathPrefix causes wrong index page.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@17244 a01eee8c-daba-0310-acae-fa49f3023285

--- index.php
+++ index.php
@@ -9,18 +9,18 @@
 //   This program is free software; you can redistribute it and/or
 //   modify it under the terms of version 2.0  of the GNU General
 //   Public License as published by the Free Software Foundation.
-// 
+//
 //   This program is distributed in the hope that it will be useful,
 //   but WITHOUT ANY WARRANTY; without even the implied warranty of
 //   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //   GNU General Public License for more details.
-// 
+//
 //   You should have received a copy of version 2.0 of the GNU General
 //   Public License along with this program; if not, write to the Free
 //   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 //   MA 02110-1301, USA.
-// 
-// 
+//
+//
 // ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 //
 
@@ -550,7 +550,8 @@ while ( $moduleRunRequired )
     // Check for URL translation
     if ( $urlTranslatorAllowed and
          $ini->variable( 'URLTranslator', 'Translation' ) == 'enabled' and
-         ( !$uri->isEmpty() || ( $ini->hasVariable( 'SiteAccessSettings', 'PathPrefix' ) and $ini->variable( 'SiteAccessSettings', 'PathPrefix' ) != '' ) ) )
+         !$uri->isEmpty() and
+         $ini->hasVariable( 'SiteAccessSettings', 'PathPrefix' ) and $ini->variable( 'SiteAccessSettings', 'PathPrefix' ) != '' )
     {
         include_once( 'kernel/classes/ezurlalias.php' );
         $translateResult = eZURLAlias::translate( $uri );
