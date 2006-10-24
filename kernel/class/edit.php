b426195621a79b3112aa6ad30bae2167454caa81
Dmitriy Lakhtyuk <dl@ez.no>
2006-10-24 15:12:19 +0000

    - Fixed bug #009117: Content class editing is not inside DB transaction.   (Manually merged from stable/3.8 (3.8.5) rev. 17260)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@17261 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/class/edit.php
+++ kernel/class/edit.php
@@ -11,18 +11,18 @@
 //   This program is free software; you can redistribute it and/or
 //   modify it under the terms of version 2.0  of the GNU General
 //   Public License as published by the Free Software Foundation.
-// 
+//
 //   This program is distributed in the hope that it will be useful,
 //   but WITHOUT ANY WARRANTY; without even the implied warranty of
 //   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //   GNU General Public License for more details.
-// 
+//
 //   You should have received a copy of version 2.0 of the GNU General
 //   Public License along with this program; if not, write to the Free
 //   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 //   MA 02110-1301, USA.
-// 
-// 
+//
+//
 // ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 //
 
@@ -624,6 +624,9 @@ if ( $http->hasPostVariable( 'StoreButton' ) && $canStore )
         // Class cleanup, update existing class objects according to new changes
         include_once( 'kernel/classes/ezcontentobject.php' );
 
+        $db =& eZDB::instance();
+        $db->begin();
+
         $objects = null;
         $objectCount = eZContentObject::fetchSameClassListCount( $ClassID );
         if ( $objectCount > 0 )
@@ -693,6 +696,8 @@ if ( $http->hasPostVariable( 'StoreButton' ) && $canStore )
         {
             $class->storeDefined( $attributes );
         }
+
+        $db->commit();
 
         $http->removeSessionVariable( 'CanStoreTicket' );
         return $Module->redirectToView( 'view', array( $ClassID ), array( 'Language' => $EditLanguage ) );
