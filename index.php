9adfcabaed46ce2ea5f8b89a815ea034ed217180
Dmitriy Lakhtyuk <dl@ez.no>
2006-10-24 08:48:44 +0000

    - Fixed bug #007575: eZURLAlias | Using PathPrefix causes wrong index page.   (Additional fix: merged from trunk(3.9.0beta1) rev. 17249)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@17252 a01eee8c-daba-0310-acae-fa49f3023285

--- index.php
+++ index.php
@@ -509,8 +509,7 @@ while ( $moduleRunRequired )
     // Check for URL translation
     if ( $urlTranslatorAllowed and
          $ini->variable( 'URLTranslator', 'Translation' ) == 'enabled' and
-         !$uri->isEmpty() and
-         $ini->hasVariable( 'SiteAccessSettings', 'PathPrefix' ) and $ini->variable( 'SiteAccessSettings', 'PathPrefix' ) != '' )
+         !$uri->isEmpty() )
     {
         include_once( 'kernel/classes/ezurlalias.php' );
         $translateResult =& eZURLAlias::translate( $uri );
