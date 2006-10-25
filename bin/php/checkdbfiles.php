081bca315ae986ecaaad77fb5f3b120f9cfa1eb7
Valentin Doroschuk <vd@ez.no>
2006-10-25 08:57:20 +0000

    - Configured for the next 3.8.6 subrelease.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.8@17272 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/php/checkdbfiles.php
+++ bin/php/checkdbfiles.php
@@ -159,11 +159,12 @@ $versions38 = array( 'unstable' => array( array( '3.6.0', '3.8.0alpha1' )
                                           ),
                      'unstable_subdir' => 'unstable',
                      'stable' => array( array( '3.6.0', '3.8.0' )
-                                      , array( '3.8.0', '3.8.1' ) 
-                                      , array( '3.8.1', '3.8.2' ) 
-                                      , array( '3.8.2', '3.8.3' ) 
-                                      , array( '3.8.3', '3.8.4' ) 
-                                      , array( '3.8.4', '3.8.5' ) 
+                                      , array( '3.8.0', '3.8.1' )
+                                      , array( '3.8.1', '3.8.2' )
+                                      , array( '3.8.2', '3.8.3' )
+                                      , array( '3.8.3', '3.8.4' )
+                                      , array( '3.8.4', '3.8.5' )
+                                      , array( '3.8.5', '3.8.6' )
 				      ) );
 
 $versions['3.0'] = $versions30;
--- kernel/sql/common/cleandata.sql
+++ kernel/sql/common/cleandata.sql
@@ -22198,14 +22198,14 @@ INSERT INTO ezsite_data (
   value
 ) VALUES (
   'ezpublish-version',
-  '3.8.5'
+  '3.8.6'
 );
 INSERT INTO ezsite_data (
   name,
   value
 ) VALUES (
   'ezpublish-release',
-  '11'
+  '12'
 );
 
 INSERT INTO ezurl (
