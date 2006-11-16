cbc5767436775d25a12b83d07809b6b779bf9128
Vladimir Polyanin <vp@ez.no>
2006-11-16 12:54:44 +0000

    - si operator ignores decimals parameter when prefix is "none"   (correction to rev. 17422)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@17501 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/eztemplate/classes/eztemplateunitoperator.php
+++ lib/eztemplate/classes/eztemplateunitoperator.php
@@ -13,18 +13,18 @@
 //   This program is free software; you can redistribute it and/or
 //   modify it under the terms of version 2.0  of the GNU General
 //   Public License as published by the Free Software Foundation.
-// 
+//
 //   This program is distributed in the hope that it will be useful,
 //   but WITHOUT ANY WARRANTY; without even the implied warranty of
 //   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //   GNU General Public License for more details.
-// 
+//
 //   You should have received a copy of version 2.0 of the GNU General
 //   Public License along with this program; if not, write to the Free
 //   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 //   MA 02110-1301, USA.
-// 
-// 
+//
+//
 // ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 //
 
@@ -400,7 +400,7 @@ class eZTemplateUnitOperator
         if ( $namedParameters['decimal_count'] !== false )
             $decimalCount = $namedParameters['decimal_count'];
         elseif ( $prefix == 'none' )
-            $decimalCont = 0;
+            $decimalCount = 0;
 
         if ( strlen( $namedParameters['decimal_symbol'] ) > 0 )
             $decimalSymbol = $namedParameters['decimal_symbol'];
