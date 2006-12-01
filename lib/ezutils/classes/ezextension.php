02524eb8b0546215b7f41a5b35fead56c44e6d3a
Kåre Køhler Høvik <kk@ez.no>
2006-12-01 13:28:22 +0000

    - Foxed typo.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.7@17680 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezutils/classes/ezextension.php
+++ lib/ezutils/classes/ezextension.php
@@ -335,7 +335,7 @@ class eZExtension
         $infoFileName = eZDir::path( array( eZExtension::baseDirectory(), $extension, 'ezinfo.php' ) );
         if ( file_exists( $infoFileName ) )
         {
-            include_once( $fileName );
+            include_once( $infoFileName );
             $className = $extension . 'Info';
             if ( is_callable( array( $className, 'info' ) ) )
             {
