12e67c1843d9bd0f4946a90ba1b55b2cf539f812
Valentin Doroschuk <vd@ez.no>
2006-12-04 14:21:06 +0000

    # Changed ezhtml url
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@17687 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/extensionscommon.sh
+++ bin/shell/extensionscommon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EXTENSIONS="http://svn.ez.no/svn/extensions/ezpaypal/"
-EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/commercial/projects/ezdhtml/trunk/dhtml/"
+EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezdhtml/trunk/"
 EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezodf/stable/2.0/"
 
 # These are disabled for now
