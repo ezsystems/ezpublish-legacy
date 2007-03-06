71587a1639a349e074d9c93d36f1eca622df80a2
Dmitriy Lakhtyuk <dl@ez.no>
2007-03-06 16:39:01 +0000

    - Fixed bug #10323: Class translation breaks after some translations.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@18265 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/3.10/unstable/dbupdate-3.9.0-to-3.10.0alpha1.sql
+++ update/database/postgresql/3.10/unstable/dbupdate-3.9.0-to-3.10.0alpha1.sql
@@ -1,2 +1,14 @@
 UPDATE ezsite_data SET value='3.10.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+-- extend length of 'serialized_name_list'
+ALTER TABLE ezcontentclass RENAME COLUMN serialized_name_list TO name_tmp;
+ALTER TABLE ezcontentclass ADD COLUMN serialized_name_list text;
+UPDATE ezcontentclass SET serialized_name_list = name_tmp;
+ALTER TABLE ezcontentclass DROP COLUMN name_tmp;
+
+ALTER TABLE ezcontentclass_attribute RENAME COLUMN serialized_name_list TO name_tmp;
+ALTER TABLE ezcontentclass_attribute ADD COLUMN serialized_name_list text;
+UPDATE ezcontentclass_attribute SET serialized_name_list = name_tmp;
+ALTER TABLE ezcontentclass_attribute ALTER serialized_name_list SET NOT NULL;
+ALTER TABLE ezcontentclass_attribute DROP COLUMN name_tmp;
