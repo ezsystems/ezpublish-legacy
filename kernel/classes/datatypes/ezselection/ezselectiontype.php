1a7912a60506a6d9c4c1a1feaff015059c3390c6
Ruslan Lisnyk <rl@ez.no>
2007-03-14 17:45:53 +0000

    - Fixed bug #8996: Wrong title for ezselection.   (Merged from trunk (3.10.0alpha1) rev. 18388)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.9@18389 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezselection/ezselectiontype.php
+++ kernel/classes/datatypes/ezselection/ezselectiontype.php
@@ -305,28 +305,24 @@ class eZSelectionType extends eZDataType
 
     function toString( $contentObjectAttribute )
     {
-
         $selected = $this->objectAttributeContent( $contentObjectAttribute );
-
         $classContent = $this->classAttributeContent( $contentObjectAttribute->attribute( 'contentclass_attribute' ) );
-        $return = '';
-        if ( count( $selected ) == 0)
-        {
-            return '';
-        }
 
-
-        $optionArray = $classContent['options'];
-        foreach ( $selected as $id )
+        if ( count( $selected ) )
         {
-            foreach ( $optionArray as $option )
+            $optionArray = $classContent['options'];
+            foreach ( $selected as $id )
             {
-                $optionID = $option['id'];
-                if ( $optionID == $id )
-                    $returnData[] = $option['name'];
+                foreach ( $optionArray as $option )
+                {
+                    $optionID = $option['id'];
+                    if ( $optionID == $id )
+                        $returnData[] = $option['name'];
+                }
             }
+            return eZStringUtils::implodeStr( $returnData, '|' );
         }
-        return eZStringUtils::implodeStr( $returnData, '|' );
+        return '';
     }
 
 
@@ -359,24 +355,16 @@ class eZSelectionType extends eZDataType
     {
         $selected = $this->objectAttributeContent( $contentObjectAttribute );
         $classContent = $this->classAttributeContent( $contentObjectAttribute->attribute( 'contentclass_attribute' ) );
-        $return = "";
-        if ( count( $selected ) == 0)
+        $return = '';
+        if ( count( $selected ) )
         {
-            return "";
-        }
-
-        $count = 0;
-        foreach ( $selected as $id )
-        {
-            /*if ( $id == 0 ) // first object gets id==0, while rest of objects get id with offset from 1
-                $id++;
-            if ( $count++ != 0 )
-                $return .= ', ';
-            $return .= $classContent['options'][$id-1]['name'];*/
-            if ( $count != 0 )
-                $return .= ', ';
-            $return .= $classContent['options'][$id]['name'];
-            $count++;
+            $selectedNames = array();
+            foreach ( $classContent['options'] as $option )
+            {
+                if ( in_array( $option['id'], $selected ) )
+                    $selectedNames[] = $option['name'];
+            }
+            $return = implode( ', ', $selectedNames );
         }
         return $return;
     }
