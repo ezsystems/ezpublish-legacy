0508c6ea259fb3b16c21d90c5c32940cb354ee62
Ruslan Lisnyk <rl@ez.no>
2007-03-16 23:39:30 +0000

    #- Removed useless comment.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.9@18425 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobjectversion.php
+++ kernel/classes/ezcontentobjectversion.php
@@ -1002,7 +1002,6 @@ class eZContentObjectVersion extends eZPersistentObject
     {
         $statuses = array( EZ_VERSION_STATUS_DRAFT,
                            EZ_VERSION_STATUS_PENDING,
-                           // EZ_VERSION_STATUS_ARCHIVED
                            EZ_VERSION_STATUS_REJECTED,
                            EZ_VERSION_STATUS_INTERNAL_DRAFT );
         if ( $versionStatus === false )
