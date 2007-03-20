a2b25d400b6d3e54e738360814fc1cc50ca8143c
Vladimir Polyanin <vp@ez.no>
2007-03-20 11:22:35 +0000

    - Fixed bug #9953: SiteAccessRules example 3 disables website root   (Merged from stable/3.8 (3.8.8) rev. 18468)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.6@18473 a01eee8c-daba-0310-acae-fa49f3023285

--- index.php
+++ index.php
@@ -545,7 +545,16 @@ while ( $moduleRunRequired )
         }
     }
 
-    $moduleCheck = accessAllowed( $uri );
+    if ( $uri->isEmpty() )
+    {
+        $tmp_uri = new eZURI( $ini->variable( "SiteSettings", "IndexPage" ) );
+        $moduleCheck = accessAllowed( $tmp_uri );
+    }
+    else
+    {
+        $moduleCheck = accessAllowed( $uri );
+    }
+
     if ( !$moduleCheck['result'] )
     {
         if ( $ini->variable( "SiteSettings", "ErrorHandler" ) == "defaultpage" )
@@ -564,7 +573,6 @@ while ( $moduleRunRequired )
 
     if ( $uri->isEmpty() )
     {
-        $tmp_uri = new eZURI( $ini->variable( "SiteSettings", "IndexPage" ) );
         if ( !fetchModule( $tmp_uri, $check, $module, $module_name, $function_name, $params ) )
             $displayMissingModule = true;
     }
