3b668dd049f5b91fe8b82763ed1c446586a8f9d5
Bjørn Reiten <br@ez.no>
2007-05-04 14:16:44 +0000

    - Updated kernel schema for PostgreSQL and ISBN.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@18845 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/sql/postgresql/kernel_schema.sql
+++ kernel/sql/postgresql/kernel_schema.sql
@@ -4375,6 +4375,105 @@ ALTER TABLE ONLY ezworkflow_process
 
 
 
+CREATE SEQUENCE ezisbn_group_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
 
 
 
+
+
+
+
+CREATE TABLE ezisbn_group (
+  id integer DEFAULT nextval('ezisbn_group_s'::text) NOT NULL,
+  description character varying(255) default ''::character varying NOT NULL,
+  group_number integer DEFAULT 0 NOT NULL
+);
+
+
+
+
+
+
+
+ALTER TABLE ONLY ezisbn_group
+    ADD CONSTRAINT ezisbn_group_pkey PRIMARY KEY (id);
+
+
+
+
+
+
+
+CREATE SEQUENCE ezisbn_group_range_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
+
+
+
+
+
+
+CREATE TABLE ezisbn_group_range (
+  id integer DEFAULT nextval('ezisbn_group_range_s'::text) NOT NULL,
+  from_number  integer DEFAULT 0 NOT NULL,
+  to_number  integer DEFAULT 0 NOT NULL,
+  group_from  character varying(32) default ''::character varying NOT NULL,
+  group_to character varying(32) default ''::character varying NOT NULL,
+  group_length integer DEFAULT 0 NOT NULL
+);
+
+
+
+
+
+
+
+ALTER TABLE ONLY ezisbn_group_range
+    ADD CONSTRAINT ezisbn_group_range_pkey PRIMARY KEY (id);
+
+
+
+
+
+
+
+CREATE SEQUENCE ezisbn_registrant_range_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
+
+
+
+
+
+
+CREATE TABLE ezisbn_registrant_range (
+  id integer DEFAULT nextval('ezisbn_registrant_range_s'::text) NOT NULL,
+  from_number integer DEFAULT 0 NOT NULL,
+  to_number integer DEFAULT 0 NOT NULL,
+  registrant_from character varying(32) default ''::character varying NOT NULL,
+  registrant_to character varying(32) default ''::character varying NOT NULL,
+  registrant_length integer DEFAULT 0 NOT NULL,
+  isbn_group_id integer DEFAULT 0 NOT NULL
+);
+
+
+
+
+
+
+
+ALTER TABLE ONLY ezisbn_registrant_range
+    ADD CONSTRAINT ezisbn_registrant_range_pkey PRIMARY KEY (id);
