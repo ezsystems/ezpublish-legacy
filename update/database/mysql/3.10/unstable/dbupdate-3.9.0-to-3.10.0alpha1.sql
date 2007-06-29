b7bfce395d94d14da621049ac2deba872e3c35f1
Dmitriy Lakhtyuk <dl@ez.no>
2007-06-29 08:06:09 +0000

    - Updated mysql sql-updates for isbn datatype to be consistent with   postgresql updates.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@19165 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/3.10/unstable/dbupdate-3.9.0-to-3.10.0alpha1.sql
+++ update/database/mysql/3.10/unstable/dbupdate-3.9.0-to-3.10.0alpha1.sql
@@ -9,28 +9,28 @@ ALTER TABLE ezcontentclass_attribute CHANGE COLUMN serialized_name_list serializ
 -- Enhanced ISBN datatype.
 CREATE TABLE ezisbn_group (
   id int(11) NOT NULL auto_increment,
-  description varchar(255) default NULL,
+  description varchar(255) NOT NULL default '',
   group_number int(11) NOT NULL default '0',
   PRIMARY KEY  (id)
-) TYPE=MyISAM;
+);
 
 CREATE TABLE ezisbn_group_range (
   id int(11) NOT NULL auto_increment,
   from_number int(11) NOT NULL default '0',
   to_number int(11) NOT NULL default '0',
-  group_from varchar(32) default NULL,
-  group_to varchar(32) default NULL,
+  group_from varchar(32) NOT NULL default '',
+  group_to varchar(32) NOT NULL default '',
   group_length int(11) NOT NULL default '0',
   PRIMARY KEY  (id)
-) TYPE=MyISAM;
+);
 
 CREATE TABLE ezisbn_registrant_range (
   id int(11) NOT NULL auto_increment,
   from_number int(11) NOT NULL default '0',
   to_number int(11) NOT NULL default '0',
-  registrant_from varchar(32) default NULL,
-  registrant_to varchar(32) default NULL,
+  registrant_from varchar(32) NOT NULL default '',
+  registrant_to varchar(32) NOT NULL default '',
   registrant_length int(11) NOT NULL default '0',
   isbn_group_id int(11) NOT NULL default '0',
   PRIMARY KEY  (id)
-) TYPE=MyISAM;
+);
