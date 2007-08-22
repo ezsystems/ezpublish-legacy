73bd920b71fa79c238898fbfaac5c9a8ba3ff403
Dmitriy Lakhtyuk <dl@ez.no>
2007-08-22 07:48:56 +0000

    - Updated version info: set 3.10.0beta1
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@19731 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/sql/common/cleandata.sql
+++ kernel/sql/common/cleandata.sql
@@ -22371,14 +22371,14 @@ INSERT INTO ezsite_data (
   value
 ) VALUES (
   'ezpublish-version',
-  '3.10.0alpha1'
+  '3.10.0beta1'
 );
 INSERT INTO ezsite_data (
   name,
   value
 ) VALUES (
   'ezpublish-release',
-  '1'
+  '2'
 );
 
 INSERT INTO ezurl (
