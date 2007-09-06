89d3c08f8797b0ec3b3b9f52a301938da8c192d2
Sergiy Pushchin <sp@ez.no>
2007-09-06 15:30:55 +0000

    - Fixed bug #011284: Varchar(255) is not enough for ezurl.url.   (Manualy merged from trunk (3.10.0rc1) rev. 19853,19854).
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.9@19855 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/3.9/dbupdate-3.9.3-to-3.9.4.sql
+++ update/database/mysql/3.9/dbupdate-3.9.3-to-3.9.4.sql
@@ -2,3 +2,7 @@ UPDATE ezsite_data SET value='3.9.4' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='10' WHERE name='ezpublish-release';
 
 ALTER table ezsearch_word ADD KEY ezsearch_word_obj_count(object_count);
+
+DROP INDEX ezurl_url ON ezurl;
+ALTER TABLE ezurl MODIFY url longtext;
+ALTER table ezurl ADD KEY ezurl_url( url(255) );
--- update/database/postgresql/3.9/dbupdate-3.9.3-to-3.9.4.sql
+++ update/database/postgresql/3.9/dbupdate-3.9.3-to-3.9.4.sql
@@ -2,3 +2,7 @@ UPDATE ezsite_data SET value='3.9.4' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='10' WHERE name='ezpublish-release';
 
 CREATE INDEX ezsearch_word_obj_count ON ezsearch_word(object_count);
+
+DROP INDEX ezurl_url;
+ALTER TABLE ezurl ALTER url TYPE text;
+CREATE INDEX ezurl_url ON ezurl(url);
