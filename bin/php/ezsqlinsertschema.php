d9b7ed462395eee6016558d7153695dd341729f5
Raymond Bosman <rb@ez.no>
2007-10-01 08:00:42 +0000

    - Scripts need autoload too.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/unstable/2007-03-21-php5@20066 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/php/ezsqlinsertschema.php
+++ bin/php/ezsqlinsertschema.php
@@ -29,6 +29,7 @@
 
 //include_once( 'lib/ezutils/classes/ezcli.php' );
 //include_once( 'kernel/classes/ezscript.php' );
+include( 'autoload.php' );
 
 $cli = eZCLI::instance();
 $script = eZScript::instance( array( 'description' => ( "eZ publish SQL Schema insert\n\n" .
