0a87971746626862f83aec6573051b2878ed62eb
Kristof Coomans <kc@ez.no>
2007-10-02 08:44:44 +0000

    Merged revisions 19966 via svnmerge from http://svn.ez.no/svn/nextgen/trunk
    ........
      r19966 | sp | 2007-09-24 17:41:19 +0200 (Mon, 24 Sep 2007) | 3 lines
      
      - Fixed bug #010792: node_id on objectrelations are not updated on trash / restore
    ........
    
    
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/unstable/2007-03-21-php5@20098 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezobjectrelationlist/ezobjectrelationlisttype.php
+++ kernel/classes/datatypes/ezobjectrelationlist/ezobjectrelationlisttype.php
@@ -790,6 +790,7 @@ class eZObjectRelationListType extends eZDataType
     {
         return array( 'identifier' => 'identifier',
                       'priority' => 'priority',
+                      'in-trash' => 'in_trash',
                       'contentobject-id' => 'contentobject_id',
                       'contentobject-version' => 'contentobject_version',
                       'node-id' => 'node_id',
@@ -1188,6 +1189,7 @@ class eZObjectRelationListType extends eZDataType
         $object->sync();
         $relationItem = array( 'identifier' => false,
                                'priority' => $priority,
+                               'in_trash' => false,
                                'contentobject_id' => $object->attribute( 'id' ),
                                'contentobject_version' => $object->attribute( 'current_version' ),
                                'contentobject_remote_id' => $object->attribute( 'remote_id' ),
@@ -1207,6 +1209,7 @@ class eZObjectRelationListType extends eZDataType
         $sectionID = $object->attribute( 'section_id' );
         $relationItem = array( 'identifier' => false,
                                'priority' => $priority,
+                               'in_trash' => false,
                                'contentobject_id' => $object->attribute( 'id' ),
                                'contentobject_version' => $object->attribute( 'current_version' ),
                                'contentobject_remote_id' => $object->attribute( 'remote_id' ),
@@ -1258,7 +1261,9 @@ class eZObjectRelationListType extends eZDataType
         {
             if ( $content['relation_list'][$key]['contentobject_id'] == $objectID )
             {
-                unset($content['relation_list'][$key]);
+                $content['relation_list'][$key]['in_trash'] = true;
+                $content['relation_list'][$key]['node_id'] = null;
+                $content['relation_list'][$key]['parent_node_id']= null;
             }
         }
         eZObjectRelationListType::storeObjectAttributeContent( $contentObjectAttribute, $content );
@@ -1269,7 +1274,15 @@ class eZObjectRelationListType extends eZDataType
     function fixRelationsRestore ( $objectID, $contentObjectAttribute )
     {
         $content =& $contentObjectAttribute->content();
-        $content['relation_list'][] = $this->appendObject( $objectID, 0, $contentObjectAttribute);
+
+        foreach ( array_keys( $content['relation_list'] ) as $key )
+        {
+            if ( $content['relation_list'][$key]['contentobject_id'] == $objectID )
+            {
+                $priority = $content['relation_list'][$key]['priority'];
+                $content['relation_list'][$key] = $this->appendObject( $objectID, $priority, $contentObjectAttribute);
+            }
+        }
         eZObjectRelationListType::storeObjectAttributeContent( $contentObjectAttribute, $content );
         $contentObjectAttribute->setContent( $content );
         $contentObjectAttribute->storeData();
@@ -1290,7 +1303,8 @@ class eZObjectRelationListType extends eZDataType
             $relatedObject =& $content['relation_list'][$key];
             if ( $relatedObject['contentobject_id'] == $objectID )
             {
-                $content['relation_list'][$key] = $this->appendObject($objectID, 0, $contentObjectAttribute );
+                $priority = $content['relation_list'][$key]['priority'];
+                $content['relation_list'][$key] = $this->appendObject($objectID, $priority, $contentObjectAttribute );
             }
         }
 
