e8eff5c2385d1a3760d0abc277e6a5a8afc47df3
Kristof Coomans <kc@ez.no>
2007-10-03 09:37:47 +0000

    Merged revisions 20234 via svnmerge from http://svn.ez.no/svn/nextgen/trunk
    ........
      r20234 | dp | 2007-10-03 11:24:57 +0200 (Wed, 03 Oct 2007) | 2 lines
      
      #011538: php warning in ezwebdav
    ........
    
    
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/unstable/2007-03-21-php5@20236 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezwebdav/classes/ezwebdavserver.php
+++ lib/ezwebdav/classes/ezwebdavserver.php
@@ -373,7 +373,7 @@ class eZWebDAVServer
         // Sanity check & action: if client forgot to ask, we'll still
         // play along revealing some basic/default properties. This is
         // necessary to make it work with Windows XP + SP2.
-        if( count( $requestedProperties ) == 0 )
+        if( !is_array( $requestedProperties ) || count( $requestedProperties ) == 0 )
         {
             $requestedProperties = array( 'displayname',
                                           'creationdate',
