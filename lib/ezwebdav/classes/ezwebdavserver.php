c84c337bf76f351ebb3f78c1ce1948dd20f27200
Kristof Coomans <kc@ez.no>
2007-10-18 11:32:01 +0000

    - Fixed bug #11678: Typo in WebDAV log: "requesed" instead of "requested"   (Merged from trunk (4.0.0beta1) rev. 20519)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.9@20521 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezwebdav/classes/ezwebdavserver.php
+++ lib/ezwebdav/classes/ezwebdavserver.php
@@ -411,7 +411,7 @@ class eZWebDAVServer
             $requestedProperties[] = 'getcontenttype';
         }
 
-        $this->appendLogEntry( 'Client requesed ' .
+        $this->appendLogEntry( 'Client requested ' .
                                count( $requestedProperties ) .
                                ' properties.', 'outputCollectionContent' );
 
