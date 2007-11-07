f3e506dc97292c28881fd211ef6f2d1b051d5a8b
Kristof Coomans <kc@ez.no>
2007-11-07 13:02:07 +0000

    - Fixed bug #11826: Invalid entries in ezcontentclass_classgroup table after fresh install
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@20631 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/4.0/unstable/dbupdate-4.0.0alpha2-to-4.0.0beta1.sql
+++ update/database/postgresql/4.0/unstable/dbupdate-4.0.0alpha2-to-4.0.0beta1.sql
@@ -2,3 +2,5 @@ UPDATE ezsite_data SET value='4.0.0beta1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
 DELETE FROM ezuser_setting where user_id not in (SELECT contentobject_id FROM ezuser);
+
+DELETE FROM ezcontentclass_classgroup WHERE NOT EXISTS (SELECT * FROM ezcontentclass c WHERE c.id=contentclass_id AND c.version=contentclass_version);
