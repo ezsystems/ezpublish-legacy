13e3e5abaf1c6b091a07912d0cf1d2502e3948f5
Dmitriy Lakhtyuk <dl@ez.no>
2007-12-04 11:16:56 +0000

    - Updated changelogs and sql-updates for 3.10.0-to-4.0.0 upgrade path.   (Merged from release/4.0.0 rev. 20989)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.0@20997 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ update/database/postgresql/4.0/dbupdate-3.10.0-to-4.0.0.sql
@@ -0,0 +1,24 @@
+UPDATE ezsite_data SET value='4.0.0' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='5' WHERE name='ezpublish-release';
+
+DELETE FROM ezuser_setting where user_id not in (SELECT contentobject_id FROM ezuser);
+
+DELETE FROM ezcontentclass_classgroup WHERE NOT EXISTS (SELECT * FROM ezcontentclass c WHERE c.id=contentclass_id AND c.version=contentclass_version);
+
+CREATE SEQUENCE ezurlwildcard_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
+CREATE TABLE ezurlwildcard (
+  id integer DEFAULT nextval('ezurlwildcard_s'::text) NOT NULL,
+  source_url text NOT NULL,
+  destination_url text NOT NULL,
+  type integer DEFAULT 0 NOT NULL
+);
+
+ALTER TABLE ONLY ezurlwildcard
+    ADD CONSTRAINT ezurlwildcard_pkey PRIMARY KEY (id);
+
