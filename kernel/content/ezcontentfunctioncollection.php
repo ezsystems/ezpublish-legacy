3b634c303369cd2fe5ae91d241649c37b55b2d84
Sergey Alexeev <sa@ez.no>
2007-12-04 15:42:49 +0000

    - Fixed bug #12109: eZkeyword searches "part of a tag".   (Manually merged from trunk(4.0.0) rev. 20959 )
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.10@21006 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/content/ezcontentfunctioncollection.php
+++ kernel/content/ezcontentfunctioncollection.php
@@ -792,7 +792,8 @@ class eZContentFunctionCollection
                                 $classid, 
                                 $owner = false, 
                                 $parentNodeID = false, 
-                                $includeDuplicates = true )
+                                $includeDuplicates = true,
+                                $strictMatching = false  )
     {
         $classIDArray = array();
         if ( is_numeric( $classid ) )
@@ -833,10 +834,18 @@ class eZContentFunctionCollection
             $sqlToExcludeDuplicates = ' DISTINCT';  
         }
 
+        //composing sql for matching tag word, it could be strict equiality or LIKE clause 
+        //dependent of $strictMatching parameter.
+        $sqlMatching = "ezkeyword.keyword LIKE '$alphabet%'";
+        if ( $strictMatching )
+        {
+            $sqlMatching = "ezkeyword.keyword = '$alphabet'";
+        }
+
         $query = "SELECT COUNT($sqlToExcludeDuplicates ezcontentobject.id) AS count
                   FROM ezkeyword, ezkeyword_attribute_link,ezcontentobject_tree,ezcontentobject,ezcontentclass, ezcontentobject_attribute
                        $sqlPermissionChecking[from]
-                  WHERE ezkeyword.keyword LIKE '$alphabet%'
+                  WHERE $sqlMatching
                   $showInvisibleNodesCond
                   $sqlPermissionChecking[where]
                   $sqlClassIDs
@@ -877,7 +886,8 @@ class eZContentFunctionCollection
                            $owner = false, 
                            $sortBy = array(), 
                            $parentNodeID = false, 
-                           $includeDuplicates = true )
+                           $includeDuplicates = true,
+                           $strictMatching = false )
     {
         $classIDArray = array();
         if ( is_numeric( $classid ) )
@@ -983,13 +993,21 @@ class eZContentFunctionCollection
             $sqlClassIDString = 'AND ezkeyword.class_id IN (' . $db->implodeWithTypeCast( ',', $classIDArray, 'int' ) . ')';
         }
 
+        // composing sql for matching tag word, it could be strict equiality or LIKE clause 
+        // dependent of $strictMatching parameter.
+        $sqlMatching = "ezkeyword.keyword LIKE '$alphabet%'";
+        if ( $strictMatching )
+        {
+            $sqlMatching = "ezkeyword.keyword = '$alphabet'";
+        }
+
         $query = "SELECT $sqlTarget
                   FROM ezkeyword, ezkeyword_attribute_link,ezcontentobject_tree,ezcontentobject,ezcontentclass
                        $sortingInfo[attributeFromSQL]
                        $sqlPermissionChecking[from]
                   WHERE
                   $sortingInfo[attributeWhereSQL]
-                  ezkeyword.keyword LIKE '$alphabet%'
+                  $sqlMatching
                   $showInvisibleNodesCond
                   $sqlPermissionChecking[where]
                   $sqlClassIDString
