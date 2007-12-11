3328d61d70f740395a3b4e392e7850dc06091cd6
Kristof Coomans <kc@ez.no>
2007-12-11 13:22:17 +0000

    - Fixed bug #12209: updateniceurls.php: wrong fetch limit is displayed   (Merged from trunk (4.1.0alpha1) rev. 21027)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.10@21029 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/php/updateniceurls.php
+++ bin/php/updateniceurls.php
@@ -152,6 +152,8 @@ if ( $options['fetch-limit'] !== null )
         $script->shutdown( 1, "The --fetch-limit must be 1 or higher, tried with $fetchLimit" );
     }
 }
+$cli->notice( "Using fetch limit: $fetchLimit" );
+
 $percentLength = 6;
 $timeLength = 12;
 $columnWidth = 72;
