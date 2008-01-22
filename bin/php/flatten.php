e1126b43d1dd76dfdf668737615c6504f18e9dab
Kristof Coomans <kc@ez.no>
2008-01-22 09:37:21 +0000

    - Fixed bug #12414: flatten.php : db-host not defined   (Merged from trunk (4.1.0alpha1) rev. 21189)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.0@21190 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/php/flatten.php
+++ bin/php/flatten.php
@@ -48,7 +48,7 @@ $script = eZScript::instance( array( 'description' => ( "eZ Publish database fla
 
 $script->startup();
 
-$options = $script->getOptions( "[db-user:][db-password:][db-database:][db-type:|db-driver:][sql]",
+$options = $script->getOptions( "[db-host:][db-user:][db-password:][db-database:][db-type:|db-driver:][sql]",
                                 "[name]",
                                 array( 'db-host' => "Database host",
                                        'db-user' => "Database user",
