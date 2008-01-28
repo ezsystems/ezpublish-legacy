1a4db8735805515740be45676d38fc7e614c2b37
Kristof Coomans <kc@ez.no>
2008-01-28 13:26:38 +0000

    - Fixed bug #12175: cache-block subtree_expiry never expires   (Merged from trunk (4.1.0alpha1) rev. 21272)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.0@21273 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/eztemplate/classes/eztemplatecacheblock.php
+++ lib/eztemplate/classes/eztemplatecacheblock.php
@@ -161,13 +161,12 @@ class eZTemplateCacheBlock
 
      See subtreeCacheSubDir() for more details on the $nodeID parameter.
      */
-    static function cachePath( $keyString, $nodeID )
+    static function cachePath( $keyString, $nodeID = false )
     {
         //include_once( 'lib/ezutils/classes/ezsys.php' );
         $filename = eZSys::ezcrc32( $keyString ) . ".cache";
 
         $phpDir = eZTemplateCacheBlock::templateBlockCacheDir();
-        $nodeID = false;
         if ( is_numeric( $nodeID ) )
         {
             $phpDir .= eZTemplateCacheBlock::calculateSubtreeCacheDir( $nodeID, $filename );
