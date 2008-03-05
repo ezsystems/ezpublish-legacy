b78cee3f41126eb41b7cf29db0784ce05b20988e
Kristof Coomans <kc@ez.no>
2008-03-05 07:45:23 +0000

    - Implemented enhancement #11896: sql optimization in ezontentObjectTreeNode::fetch()   (Merged from trunk (4.1.0alpha1) rev. 21415)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.10@21417 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobjecttreenode.php
+++ kernel/classes/ezcontentobjecttreenode.php
@@ -3431,7 +3431,9 @@ class eZContentObjectTreeNode extends eZPersistentObject
                 if ( is_array( $nodeID ) )
                 {
                     if( count( $nodeID ) === 1 )
-                        $sqlCondition = 'node_id IN ( ' . (int) $nodeID[0] . ' ) AND';
+                    {
+                        $sqlCondition = 'node_id = ' . (int) $nodeID[0] . ' AND';
+                    }
                     else
                     {
                         // All elements from $nodeID should be casted to (int)
@@ -3441,7 +3443,7 @@ class eZContentObjectTreeNode extends eZPersistentObject
                 }
                 else
                 {
-                    $sqlCondition = 'node_id IN ( ' . (int) $nodeID . ' ) AND';
+                    $sqlCondition = 'node_id = ' . (int) $nodeID . ' AND';
                 }
             }
 
