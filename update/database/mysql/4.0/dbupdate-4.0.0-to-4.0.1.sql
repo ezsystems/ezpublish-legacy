1acb34cc7c1c27dedbb51c9a8aa871c44212ca87
Kristof Coomans <kc@ez.no>
2008-03-06 15:51:22 +0000

    - Fixed bug #12234: ezcontent_language missing index on name field   (Merged from trunk (4.1.0alpha1) rev. 21449)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.0@21450 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/4.0/dbupdate-4.0.0-to-4.0.1.sql
+++ update/database/mysql/4.0/dbupdate-4.0.0-to-4.0.1.sql
@@ -1,2 +1,5 @@
 UPDATE ezsite_data SET value='4.0.1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='6' WHERE name='ezpublish-release';
+
+ALTER TABLE ezcontent_language ADD INDEX ezcontent_language_name(name);
+
--- update/database/postgresql/4.0/dbupdate-4.0.0-to-4.0.1.sql
+++ update/database/postgresql/4.0/dbupdate-4.0.0-to-4.0.1.sql
@@ -1,2 +1,5 @@
 UPDATE ezsite_data SET value='4.0.1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='6' WHERE name='ezpublish-release';
+
+CREATE INDEX ezcontent_language_name ON ezcontent_language (name);
+
