bce73acc33b3d34bbe244b4d1d8cad83dfc84d11
Kristof Coomans <kc@ez.no>
2008-03-12 08:51:57 +0000

    - Fixed bug #12687: missing index table ezcontentobject   (Merged from trunk (4.1.0alpha1) rev. 21473)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.0@21474 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/4.0/dbupdate-4.0.0-to-4.0.1.sql
+++ update/database/mysql/4.0/dbupdate-4.0.0-to-4.0.1.sql
@@ -3,3 +3,5 @@ UPDATE ezsite_data SET value='6' WHERE name='ezpublish-release';
 
 ALTER TABLE ezcontent_language ADD INDEX ezcontent_language_name(name);
 
+ALTER TABLE ezcontentobject ADD INDEX ezcontentobject_owner(owner_id);
+
