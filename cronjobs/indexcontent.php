0ceb042ce817d3c9735053bb678864d33b09b8db
Kristof Coomans <kc@ez.no>
2008-03-17 07:49:26 +0000

    - Fixed bug #12006: cronjobs/indexcontent.php could fail with oracle
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.9@21530 a01eee8c-daba-0310-acae-fa49f3023285

--- cronjobs/indexcontent.php
+++ cronjobs/indexcontent.php
@@ -49,10 +49,11 @@ while( true )
     $entries = $db->arrayQuery( "SELECT param FROM ezpending_actions WHERE action = 'index_object'",
                                 array( 'limit' => $limit,
                                        'offset' => $offset ) );
-    $inSQL = '';
 
     if ( is_array( $entries ) && count( $entries ) != 0 )
     {
+        $inSQL = '';
+
         $db->begin();
         foreach ( $entries as $entry )
         {
