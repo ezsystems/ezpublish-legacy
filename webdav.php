f14541fdf0d7055a221602c64a477c08c1a153c9
Kristof Coomans <kc@ez.no>
2008-03-18 09:57:50 +0000

    - Fixed bug #11606: php crash if an inexisting login handler class is specified in ini files   (Merged from trunk (4.1.0alpha1) rev. 21557)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.9@21560 a01eee8c-daba-0310-acae-fa49f3023285

--- webdav.php
+++ webdav.php
@@ -194,6 +194,11 @@ if ( $enable === 'true' )
                 {
                     $loginHandler = $loginHandlers[$key];
                     $userClass =& eZUserLoginHandler::instance( $loginHandler );
+                    if ( !is_object( $userClass ) )
+                    {
+                        continue;
+                    }
+
                     $user = $userClass->loginUser( $loginUsername, $loginPassword );
                     if ( get_class( $user ) == 'ezuser' )
                         break;
