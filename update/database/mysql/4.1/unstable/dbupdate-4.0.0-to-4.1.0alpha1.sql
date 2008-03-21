bd996fb6ae8be12d42ecdd37ab46d65bfee4762e
Kristof Coomans <kc@ez.no>
2008-03-21 11:01:30 +0000

    - Fixed: missing markers for changes included in eZ Publish 4.0.1
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@21585 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/4.1/unstable/dbupdate-4.0.0-to-4.1.0alpha1.sql
+++ update/database/mysql/4.1/unstable/dbupdate-4.0.0-to-4.1.0alpha1.sql
@@ -6,9 +6,11 @@ ALTER TABLE ezworkflow_event ADD COLUMN data_text5 LONGTEXT;
 ALTER TABLE ezrss_export ADD COLUMN node_id INT NULL;
 ALTER TABLE ezrss_export_item ADD COLUMN category VARCHAR( 255 ) NULL;
 
+-- START: from 4.0.1
 ALTER TABLE ezcontent_language ADD INDEX ezcontent_language_name(name);
 
 ALTER TABLE ezcontentobject ADD INDEX ezcontentobject_owner(owner_id);
 
 ALTER TABLE ezcontentobject ADD UNIQUE INDEX ezcontentobject_remote_id(remote_id);
+-- END: from 4.0.1
 
--- update/database/postgresql/4.1/unstable/dbupdate-4.0.0-to-4.1.0alpha1.sql
+++ update/database/postgresql/4.1/unstable/dbupdate-4.0.0-to-4.1.0alpha1.sql
@@ -6,9 +6,11 @@ ALTER TABLE ezworkflow_event ADD COLUMN data_text5 TEXT;
 ALTER TABLE ezrss_export ADD COLUMN node_id INT NULL;
 ALTER TABLE ezrss_export_item ADD COLUMN category VARCHAR( 255 ) NULL;
 
+-- START: from 4.0.1
 CREATE INDEX ezcontent_language_name ON ezcontent_language (name);
 
 CREATE INDEX ezcontentobject_owner ON ezcontentobject (owner_id);
 
 CREATE UNIQUE INDEX ezcontentobject_remote_id ON ezcontentobject (remote_id);
+-- END: from 4.0.1
 
