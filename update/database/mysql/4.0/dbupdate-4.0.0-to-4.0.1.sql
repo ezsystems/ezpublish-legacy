fae2413efd95dbdabae39e4faf58f14e908cca57
Kristof Coomans <kc@ez.no>
2008-03-21 11:03:21 +0000

    - Fixed: missing markers for changes included in eZ Publish 3.10.1
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.0@21586 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/4.0/dbupdate-4.0.0-to-4.0.1.sql
+++ update/database/mysql/4.0/dbupdate-4.0.0-to-4.0.1.sql
@@ -1,8 +1,10 @@
 UPDATE ezsite_data SET value='4.0.1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='6' WHERE name='ezpublish-release';
 
+-- START: from 3.10.1
 ALTER TABLE ezcontent_language ADD INDEX ezcontent_language_name(name);
 
 ALTER TABLE ezcontentobject ADD INDEX ezcontentobject_owner(owner_id);
 
 ALTER TABLE ezcontentobject ADD UNIQUE INDEX ezcontentobject_remote_id(remote_id);
+-- END: from 3.10.1
--- update/database/postgresql/4.0/dbupdate-4.0.0-to-4.0.1.sql
+++ update/database/postgresql/4.0/dbupdate-4.0.0-to-4.0.1.sql
@@ -1,9 +1,10 @@
 UPDATE ezsite_data SET value='4.0.1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='6' WHERE name='ezpublish-release';
 
+-- START: from 3.10.1
 CREATE INDEX ezcontent_language_name ON ezcontent_language (name);
 
 CREATE INDEX ezcontentobject_owner ON ezcontentobject (owner_id);
 
 CREATE UNIQUE INDEX ezcontentobject_remote_id ON ezcontentobject (remote_id);
-
+-- END: from 3.10.1
