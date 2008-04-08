486403071eff76259e86c2af10efc863e707d934
Kristof Coomans <kc@ez.no>
2008-04-08 08:16:34 +0000

    - Implemented enhancement #12824: Mention template file and line number for "Missing/malformed array to iterate through." errors
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@21703 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/eztemplate/classes/eztemplateforeachfunction.php
+++ lib/eztemplate/classes/eztemplateforeachfunction.php
@@ -279,14 +279,14 @@ class eZTemplateForeachFunction
         $loop->parseParamValue( 'array', $array );
         if ( !is_array( $array ) )
         {
-            $tpl->error( eZTemplateForeachFunction::FUNCTION_NAME, "Missing/malformed array to iterate through." );
+            $tpl->error( eZTemplateForeachFunction::FUNCTION_NAME, "Missing/malformed array to iterate through.", $functionPlacement );
             return;
         }
 
         $loop->parseParamVarName( 'item_var', $itemVarName );
         if ( !$itemVarName )
         {
-            $tpl->error( eZTemplateForeachFunction::FUNCTION_NAME, "Missing/malformed item variable name." );
+            $tpl->error( eZTemplateForeachFunction::FUNCTION_NAME, "Missing/malformed item variable name.", $functionPlacement );
             return;
         }
 
@@ -322,7 +322,7 @@ class eZTemplateForeachFunction
         {
             if ( $nItems || $offset < 0 )
             {
-                $tpl->warning( eZTemplateForeachFunction::FUNCTION_NAME, "Invalid 'offset' parameter specified." );
+                $tpl->warning( eZTemplateForeachFunction::FUNCTION_NAME, "Invalid 'offset' parameter specified.", $functionPlacement );
             }
             $offset = ( $offset < 0 ) ? 0 : $nItems;
         }
@@ -333,7 +333,7 @@ class eZTemplateForeachFunction
         elseif ( $max < 0 || $offset+$max > $nItems )
         {
             if ( $max <0 )
-                $tpl->warning( eZTemplateForeachFunction::FUNCTION_NAME, "Invalid 'max' parameter specified." );
+                $tpl->warning( eZTemplateForeachFunction::FUNCTION_NAME, "Invalid 'max' parameter specified.", $functionPlacement );
             $max = $nItems - $offset;
         }
 
