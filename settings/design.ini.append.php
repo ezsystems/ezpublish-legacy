23bb9d0f752c701cdc4d70c0fcb277b4ef95702e
André Rømcke <ar@ez.no>
2008-04-09 13:05:49 +0000

    some typoes
    git-svn-id: http://svn.ez.no/svn/extensions/eztinymce/trunk@2824 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -8,15 +8,15 @@ DesignExtensions[]=ezoe
 # A list of stylesheets to use for the editor content.
 # Enable the code button in ezoe.ini to be able to see the
 # generated html content or use a debug tool like firebug!
-EditorCSSFileList[]
-# You can use a variable <skin> anywhere in the path
+# You can use a variable <skin> anywhere in the path / filename
 # this [optional]variable is taken from ezoe.ini
+EditorCSSFileList[]
 EditorCSSFileList[]=skins/<skin>/content.css
 
 
-## here is an example for appending your own css to the editor
-## content you needc to plase it in the stylesheet folder in
-## one of your active  eZ Publish designs.
+## Here is an example for appending your own css to the editor
+## content you need to place it in the stylesheet folder in
+## one of your active eZ Publish designs.
 #[StylesheetSettings]
 #EditorCSSFileList[]=my_custom_editor_styles.css
 
