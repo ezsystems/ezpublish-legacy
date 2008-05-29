b07c4dd5edac16feaf7d0fb074d60007bacd1b1a
Kristof Coomans <kc@ez.no>
2008-05-29 14:28:34 +0000

    - Fixed bug #11781: draft - object name are not update # Partial revert to prevent bug #12493   (Merged from stable/3.10 (3.10.1) rev. 21904)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/3.9@21905 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobject.php
+++ kernel/classes/ezcontentobject.php
@@ -2282,6 +2282,7 @@ class eZContentObject extends eZPersistentObject
                 if ( $contentObjectAttribute->fetchInput( $http, $attributeDataBaseName ) )
                 {
                     $attributeInputMap[$contentObjectAttribute->attribute('id')] = true;
+                    $dataMap[$contentObjectAttribute->attribute( 'contentclass_attribute_identifier' )] =& $contentObjectAttribute;
                 }
 
                 // Custom Action Code
