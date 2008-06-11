315ca40794f056549e923853f2ac5979e05bc8fb
Kristof Coomans <kc@ez.no>
2008-06-11 08:16:48 +0000

    - Preparing for 3.10.1rc1
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/3.10.1@21985 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ update/database/mysql/3.10/unstable/dbupdate-3.10.0-to-3.10.1rc1.sql
@@ -0,0 +1,20 @@
+UPDATE ezsite_data SET value='3.10.1' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='5' WHERE name='ezpublish-release';
+
+CREATE TABLE ezurlwildcard (
+  id int(11) NOT NULL auto_increment,
+  source_url longtext NOT NULL,
+  destination_url longtext NOT NULL,
+  type int(11) NOT NULL default '0',
+  PRIMARY KEY  (id)
+);
+
+-- START: from 3.9.5
+ALTER TABLE ezcontent_language ADD INDEX ezcontent_language_name(name);
+
+ALTER TABLE ezcontentobject ADD INDEX ezcontentobject_owner(owner_id);
+
+ALTER TABLE ezcontentobject ADD UNIQUE INDEX ezcontentobject_remote_id(remote_id);
+-- END: from 3.9.5
+
+ALTER TABLE ezurlalias_ml ADD COLUMN alias_redirects int(1) NOT NULL default 1;
---
+++ update/database/postgresql/3.10/unstable/dbupdate-3.10.0-to-3.10.1rc1.sql
@@ -0,0 +1,29 @@
+UPDATE ezsite_data SET value='3.10.1' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='5' WHERE name='ezpublish-release';
+
+CREATE SEQUENCE ezurlwildcard_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
+CREATE TABLE ezurlwildcard (
+  id integer DEFAULT nextval('ezurlwildcard_s'::text) NOT NULL,
+  source_url text NOT NULL,
+  destination_url text NOT NULL,
+  type integer DEFAULT 0 NOT NULL
+);
+
+ALTER TABLE ONLY ezurlwildcard
+    ADD CONSTRAINT ezurlwildcard_pkey PRIMARY KEY (id);
+
+-- START: from 3.9.5
+CREATE INDEX ezcontent_language_name ON ezcontent_language (name);
+
+CREATE INDEX ezcontentobject_owner ON ezcontentobject (owner_id);
+
+CREATE UNIQUE INDEX ezcontentobject_remote_id ON ezcontentobject (remote_id);
+-- END: from 3.9.5
+
+ALTER TABLE ezurlalias_ml ADD COLUMN alias_redirects INT NOT NULL default 1;
