bfa3d4dca46d311fed72306cb5a35a64d909046d
Kristof Coomans <kc@ez.no>
2008-07-16 09:38:53 +0000

    - Implemented: update script to prevent issue #13057: error trying to apply 3.10.1 upgrade sql # Fixed: content objects with status draft|archived don't show up # Implemented: option "mode" (d for detailed, a for automatic)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@22104 a01eee8c-daba-0310-acae-fa49f3023285

--- update/common/scripts/4.1/fixobjectremoteid.php
+++ update/common/scripts/4.1/fixobjectremoteid.php
@@ -47,14 +47,28 @@ $scriptSettings['use-extensions'] = false;
 $script = eZScript::instance( $scriptSettings );
 $script->startup();
 
-$config = '';
+$config = '[mode:]';
 $argumentConfig = '';
-$optionHelp = false;
+$optionHelp = array( 'mode' => "the fixing mode to use, either d (detailed) or a (automatic)" );
 $arguments = false;
 $useStandardOptions = true;
 
 $options = $script->getOptions( $config, $argumentConfig, $optionHelp, $arguments, $useStandardOptions );
 $script->initialize();
+
+if ( isset( $options['mode'] ) )
+{
+    if ( !in_array( $options['mode'], array( 'a', 'd' ) ) )
+    {
+        $script->shutdown( 1, 'Invalid mode. Use either d for detailed or a for automatic.' );
+    }
+
+    $mode = $options['mode'];
+}
+else
+{
+    $mode = false;
+}
 
 $db = eZDB::instance();
 
@@ -68,42 +82,74 @@ $cli->output( '' );
 
 foreach ( $nonUniqueRemoteIDDataList as $nonUniqueRemoteIDData )
 {
-    $action = readline( "Remote ID '$nonUniqueRemoteIDData[remote_id]' is used for $nonUniqueRemoteIDData[cnt] different content objects. Do you want to see the details (d) or do you want this inconsistency to be fixed automatically (a) ?" );
-
-    while ( !in_array( $action, array( 'a', 'd' ) ) )
+    if ( $mode )
     {
-        $action = readline( 'Invalid option. Type either d for details or a to fix automatically.' );
+        $cli->output( "Remote ID '$nonUniqueRemoteIDData[remote_id]' is used for $nonUniqueRemoteIDData[cnt] different content objects." );
+        $action = $mode;
     }
+    else
+    {
+        $action = readline( "Remote ID '$nonUniqueRemoteIDData[remote_id]' is used for $nonUniqueRemoteIDData[cnt] different content objects. Do you want to see the details (d) or do you want this inconsistency to be fixed automatically (a) ?" );
+
+        while ( !in_array( $action, array( 'a', 'd' ) ) )
+        {
+            $action = readline( 'Invalid option. Type either d for details or a to fix automatically.' );
+        }
+    }
+
+    $contentObjects = eZPersistentObject::fetchObjectList( eZContentObject::definition(),
+                                                           null,
+                                                           array( 'remote_id' => $nonUniqueRemoteIDData['remote_id'] ),
+                                                           array( 'status' => 'desc', 'published' => 'asc' ) );
 
     switch ( $action )
     {
         case 'd':
         {
-            $escapedRemoteID = $db->escapeString( $nonUniqueRemoteIDData['remote_id'] );
-
-            $sql = "SELECT o.id, t.path_identification_string, o.published
-                    FROM ezcontentobject_tree t, ezcontentobject o
-                    WHERE o.id=t.contentobject_id
-                      AND o.remote_id='$escapedRemoteID'
-                      AND t.node_id=t.main_node_id
-                    ORDER BY o.published asc";
-            $rows = $db->arrayQuery( $sql );
-
             $cli->output( '' );
             $cli->output( 'Select the number of the content object that you want to keep the current remote ID. The other listed content objects will get a new one.' );
             $cli->output( '' );
 
-            foreach ( $rows as $i => $row )
+            foreach ( $contentObjects as $i => $contentObject )
             {
-                $dateTime = new eZDateTime( $row['published'] );
-                $formattedDateTime = $dateTime->toString( true );
-                $cli->output( "$i) $row[path_identification_string] (object ID: $row[id], published: $formattedDateTime )" );
+                $status = $contentObject->attribute( 'status' );
+                $objectID = $contentObject->attribute( 'id' );
+
+                switch ( $status )
+                {
+                    case eZContentObject::STATUS_PUBLISHED:
+                    {
+                        $dateTime = new eZDateTime( $contentObject->attribute( 'published' ) );
+                        $formattedDateTime = $dateTime->toString( true );
+                        $mainNode = $contentObject->attribute( 'main_node' );
+                        $pathIdentificationString = $mainNode->attribute( 'path_identification_string' );
+
+                        $message = "$pathIdentificationString (object ID: $objectID, published: $formattedDateTime )";
+                    } break;
+
+                    case eZContentObject::STATUS_DRAFT:
+                    {
+                        $message = "draft content object (object ID: $objectID )";
+                    } break;
+
+                    case eZContentObject::STATUS_ARCHIVED:
+                    {
+                        $message = "trashed content object (object ID: $objectID)";
+                    } break;
+
+                    default:
+                    {
+                        $script->shutdown( 2, "Impossible object status $status for object $objectID" );
+                    }
+                }
+
+                $cli->output( "$i) $message" );
                 $cli->output( '' );
             }
 
             do {
                 $skip = readline( 'Number of object that should keep the current remote ID: ' );
-            } while ( !array_key_exists( $skip, $rows ) );
+            } while ( !array_key_exists( $skip, $contentObjects ) );
         } break;
 
         case 'a':
@@ -113,10 +159,6 @@ foreach ( $nonUniqueRemoteIDDataList as $nonUniqueRemoteIDData )
         }
     }
 
-    $contentObjects = eZPersistentObject::fetchObjectList( eZContentObject::definition(),
-                                                           null,
-                                                           array( 'remote_id' => $nonUniqueRemoteIDData['remote_id'] ),
-                                                           array( 'published' => 'asc' ) );
     foreach ( $contentObjects as $i => $contentObject )
     {
         if ( $i == $skip )
