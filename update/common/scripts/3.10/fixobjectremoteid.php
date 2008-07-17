e4e425b77f8fd0e51ee5e775b3fc94db0c4c71fc
Kristof Coomans <kc@ez.no>
2008-07-17 13:59:28 +0000

    - Implemented: update script to prevent issue #13057: error trying to apply 3.10.1 upgrade sql # Implemented: tell the user when fixing # Implemented: show the total number of content objects that received a new remote ID
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/3.10.1@22124 a01eee8c-daba-0310-acae-fa49f3023285

--- update/common/scripts/3.10/fixobjectremoteid.php
+++ update/common/scripts/3.10/fixobjectremoteid.php
@@ -83,6 +83,8 @@ $cli->output( '' );
 $cli->output( "Found $nonUniqueRemoteIDDataListCount non-unique content object remote IDs." );
 $cli->output( '' );
 
+$totalCount = 0;
+
 foreach ( $nonUniqueRemoteIDDataList as $nonUniqueRemoteIDData )
 {
     if ( $mode )
@@ -161,6 +163,8 @@ foreach ( $nonUniqueRemoteIDDataList as $nonUniqueRemoteIDData )
             $skip = 0;
         }
     }
+    
+    $cli->output( 'Fixing...' );
 
     foreach ( $contentObjects as $i => $contentObject )
     {
@@ -173,10 +177,14 @@ foreach ( $nonUniqueRemoteIDDataList as $nonUniqueRemoteIDData )
         $contentObject->setAttribute( 'remote_id', $newRemoteID );
         $contentObject->store();
     }
+    
+    $totalCount += $nonUniqueRemoteIDData['cnt'] - 1;
 
     $cli->output( '' );
     $cli->output( '' );
 }
+
+$cli->output( "Number of content objects that received a new remote ID : $totalCount" );
 
 $script->shutdown( 0 );
 
