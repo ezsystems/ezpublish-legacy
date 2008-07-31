231f8e3497e4106f28df4b85b3042c9d89ba1a35
Kristof Coomans <kc@ez.no>
2008-07-31 12:09:10 +0000

    - Fixed bug #13434: Problem if xmlblock is required /empty and Add object in objectrelation(s) is clicked   (Merged from release/3.10.1 (3.10.1rc2) rev. 22160)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/3.9.5@22161 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezxmltext/handlers/input/ezsimplifiedxmlinput.php
+++ kernel/classes/datatypes/ezxmltext/handlers/input/ezsimplifiedxmlinput.php
@@ -126,8 +126,7 @@ class eZSimplifiedXMLInput extends eZXMLInputHandler
                 return EZ_INPUT_VALIDATOR_STATE_INVALID;
             }
 
-            $classAttribute =& $contentObjectAttribute->contentClassAttribute();
-            if ( $classAttribute->attribute( "is_required" ) == true )
+            if ( $contentObjectAttribute->validateIsRequired() )
             {
                 $root =& $document->Root;
                 if ( !count( $root->Children ) )
