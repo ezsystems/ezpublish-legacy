d94c4832fc4528c67cd9700e9b376f7d62f661fb
Kristof Coomans <kc@ez.no>
2008-07-31 11:03:04 +0000

    - Fixed bug #12216: query error in eZContentObjectTreeNode::calendar   (Merged from trunk (4.1.0alpha1) rev. 22154)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/3.9.5@22157 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobjecttreenode.php
+++ kernel/classes/ezcontentobjecttreenode.php
@@ -2838,6 +2838,10 @@ class eZContentObjectTreeNode extends eZPersistentObject
         $groupByText        = '';
         eZContentObjectTreeNode::createGroupBySQLStrings( $groupBySelectText, $groupByText, $groupBy );
 
+        $useVersionName     = true;
+        $versionNameTables  = eZContentObjectTreeNode::createVersionNameTablesSQLString( $useVersionName );
+        $versionNameJoins   = eZContentObjectTreeNode::createVersionNameJoinsSQLString( $useVersionName, false );
+
         $limitation = ( isset( $params['Limitation']  ) && is_array( $params['Limitation']  ) ) ? $params['Limitation']: false;
         $limitationList = eZContentObjectTreeNode::getLimitationList( $limitation );
         $sqlPermissionChecking = eZContentObjectTreeNode::createPermissionCheckingSQL( $limitationList );
@@ -2851,6 +2855,7 @@ class eZContentObjectTreeNode extends eZPersistentObject
                    FROM
                       ezcontentobject_tree,
                       ezcontentobject,ezcontentclass
+                      $versionNameTables
                       $attributeFilter[from]
                       $extendedAttributeFilter[tables]
                       $sqlPermissionChecking[from]
@@ -2862,9 +2867,10 @@ class eZContentObjectTreeNode extends eZPersistentObject
                       AND
                       $notEqParentString
                       $mainNodeOnlyCond
+                      ezcontentobject_tree.contentobject_id = ezcontentobject.id AND
+                      ezcontentclass.id = ezcontentobject.contentclass_id AND
                       $classCondition
-                      ezcontentobject_tree.contentobject_id = ezcontentobject.id  AND
-                      ezcontentclass.id = ezcontentobject.contentclass_id
+                      $versionNameJoins
                       $showInvisibleNodesCond
                       $sqlPermissionChecking[where]
                 $groupByText ";
