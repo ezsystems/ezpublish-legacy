12d412791246b181d439b10ef29fd2aa8c57d31d
Kristof Coomans <kc@ez.no>
2008-08-18 07:28:28 +0000

    - Changed paths to ezodf and ezurlaliasmigration extensions
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/4.0.1@22215 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/extensionscommon.sh
+++ bin/shell/extensionscommon.sh
@@ -2,7 +2,8 @@
 
 #EXTENSIONS="http://svn.ez.no/svn/extensions/ezpaypal/"
 EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezdhtml/unstable/ezdhtml_php5"
-EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezodf/trunk"
+EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezodf/stable/2.2"
+EXTENSIONS="$EXTENSIONS http://svn.projects.ez.no/ezurlaliasmigration/tags/1.0.0/extension/ezurlaliasmigration"
 
 # These are disabled for now
 #EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/commercial/projects/ezoracle/trunk"
