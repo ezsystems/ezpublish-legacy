f309b7b65f7f52286cdcfc7cea00caae48c95c32
Ole Marius Smestad <oms@ez.no>
2008-10-08 12:57:48 +0000

    - Reverted rev. 21982. We will be using SPL rather than redefine the   autoload method in config.php
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@22447 a01eee8c-daba-0310-acae-fa49f3023285

--- autoload.php
+++ autoload.php
@@ -11,48 +11,45 @@
 
 // config.php can set the components path like:
 // ini_set( 'include_path', ini_get( 'include_path' ). ':../ezcomponents/trunk' );
-// or it can include it`s own autoload mechanism
 
 if ( file_exists( "config.php" ) )
 {
     require "config.php";
 }
-if ( !function_exists( '__autoload' ) )
+
+// require 'Base/src/base.php';
+$baseEnabled = @include( 'ezc/Base/base.php' );
+if ( !$baseEnabled )
 {
-    // require 'Base/src/base.php';
-    $baseEnabled = @include( 'ezc/Base/base.php' );
-    if ( !$baseEnabled )
+    $baseEnabled = @include( 'Base/src/base.php' );
+}
+
+define( 'EZCBASE_ENABLED', $baseEnabled );
+function __autoload( $className )
+{
+    static $ezpClasses = null;
+    if ( is_null( $ezpClasses ) )
     {
-        $baseEnabled = @include( 'Base/src/base.php' );
+        $ezpKernelClasses = require 'autoload/ezp_kernel.php';
+        $ezpExtensionClasses = require 'autoload/ezp_extension.php';
+        $ezpTestClasses = @include_once 'autoload/ezp_tests.php';
+        if ( $ezpTestClasses )
+        {
+            $ezpClasses = array_merge( $ezpKernelClasses, $ezpExtensionClasses, $ezpTestClasses );
+        }
+        else
+        {
+            $ezpClasses = array_merge( $ezpKernelClasses, $ezpExtensionClasses );
+        }
     }
 
-    define( 'EZCBASE_ENABLED', $baseEnabled );
-    function __autoload( $className )
+    if ( array_key_exists( $className, $ezpClasses ) )
     {
-        static $ezpClasses = null;
-        if ( is_null( $ezpClasses ) )
-        {
-            $ezpKernelClasses = require 'autoload/ezp_kernel.php';
-            $ezpExtensionClasses = require 'autoload/ezp_extension.php';
-            $ezpTestClasses = @include_once 'autoload/ezp_tests.php';
-            if ( $ezpTestClasses )
-            {
-                $ezpClasses = array_merge( $ezpKernelClasses, $ezpExtensionClasses, $ezpTestClasses );
-            }
-            else
-            {
-                $ezpClasses = array_merge( $ezpKernelClasses, $ezpExtensionClasses );
-            }
-        }
-
-        if ( array_key_exists( $className, $ezpClasses ) )
-        {
-            require( $ezpClasses[$className] );
-        }
-        elseif ( EZCBASE_ENABLED )
-        {
-            ezcBase::autoload( $className );
-        }
+        require( $ezpClasses[$className] );
+    }
+    elseif ( EZCBASE_ENABLED )
+    {
+        ezcBase::autoload( $className );
     }
 }
 ?>
