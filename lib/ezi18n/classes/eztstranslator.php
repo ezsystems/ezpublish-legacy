c774940603ec8d44f97340ffb04050e19b752c20
Kristof Coomans <kc@ez.no>
2008-11-19 15:42:44 +0000

    - Fixed bug #13029: ez4: Error: eZTSTranslator::loadTranslationFile, Unknown element name: defaultcodec
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@22594 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezi18n/classes/eztstranslator.php
+++ lib/ezi18n/classes/eztstranslator.php
@@ -285,9 +285,6 @@ class eZTSTranslator extends eZTranslatorHandler
                     {
                         $this->handleContextNode( $child );
                     }
-                    else
-                        eZDebug::writeError( "Unknown element name: " . $child->tagName,
-                                             "eZTSTranslator::loadTranslationFile" );
                 }
             }
             eZDebug::accumulatorStop( 'tstranslator_load' );
