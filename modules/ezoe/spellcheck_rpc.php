7c15cad20c38353b0466ea86bca0fccde33e53fa
André Rømcke <ar@ez.no>
2008-11-26 10:05:29 +0000

    cs
    git-svn-id: http://svn.ez.no/svn/extensions/eztinymce/trunk@3355 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- modules/ezoe/spellcheck_rpc.php
+++ modules/ezoe/spellcheck_rpc.php
@@ -16,20 +16,22 @@ require_once( 'extension/ezoe/modules/ezoe/classes/SpellChecker.php' );
  * @param String $default_value Default value to return if value not found.
  * @return String request value by name without magic quoting or default value.
  */
-function getRequestParam($name, $default_value = false, $sanitize = false) {
-	if (!isset($_REQUEST[$name]))
-		return $default_value;
+function getRequestParam($name, $default_value = false, $sanitize = false)
+{
+    if (!isset($_REQUEST[$name]))
+        return $default_value;
 
-	if (is_array($_REQUEST[$name])) {
-		$newarray = array();
+    if (is_array($_REQUEST[$name]))
+    {
+        $newarray = array();
 
-		foreach ($_REQUEST[$name] as $name => $value)
-			$newarray[formatParam($name, $sanitize)] = formatParam($value, $sanitize);
+        foreach ($_REQUEST[$name] as $name => $value)
+            $newarray[formatParam($name, $sanitize)] = formatParam($value, $sanitize);
 
-		return $newarray;
-	}
+        return $newarray;
+    }
 
-	return formatParam($_REQUEST[$name], $sanitize);
+    return formatParam($_REQUEST[$name], $sanitize);
 }
 
 
@@ -43,14 +45,14 @@ $ezoeIni  = eZINI::instance( 'ezoe.ini' );
 if ( $ezoeIni->hasVariable( 'SpellChecker', 'config' )
   && count( $ezoeIni->variable( 'SpellChecker', 'config' ) ) )
 {
-	$config = $ezoeIni->variable( 'SpellChecker', 'config' );
+    $config = $ezoeIni->variable( 'SpellChecker', 'config' );
 }
 
 
 if ( defined('PSPELL_FAST') )
 {
-	$config['PSpell.mode'] = PSPELL_FAST;
-	$config['PSpellShell.mode'] = PSPELL_FAST;
+    $config['PSpell.mode'] = PSPELL_FAST;
+    $config['PSpellShell.mode'] = PSPELL_FAST;
 }
 
 
@@ -58,7 +60,7 @@ if ( defined('PSPELL_FAST') )
 
 if ( isset( $config['general.engine'] ) )
 {
-	include_once( 'extension/ezoe/modules/ezoe/classes/' . $config['general.engine'] . '.php' );
+    include_once( 'extension/ezoe/modules/ezoe/classes/' . $config['general.engine'] . '.php' );
 }
 
 // Set RPC response headers
@@ -74,73 +76,77 @@ $raw = '';
 
 // Try param
 if (isset($_POST["json_data"]))
-	$raw = getRequestParam("json_data");
+    $raw = getRequestParam("json_data");
 
 // Try globals array
 if (!$raw && isset($_GLOBALS["HTTP_RAW_POST_DATA"]))
-	$raw = $_GLOBALS["HTTP_RAW_POST_DATA"];
+    $raw = $_GLOBALS["HTTP_RAW_POST_DATA"];
 else if (!$raw && isset($HTTP_RAW_POST_DATA))
-	$raw = $HTTP_RAW_POST_DATA;
+    $raw = $HTTP_RAW_POST_DATA;
 
 // Try stream
-if (!$raw) {
-	if (!function_exists('file_get_contents')) {
-		$fp = fopen("php://input", "r");
-		if ($fp) {
-			$raw = "";
+if (!$raw)
+{
+    if (!function_exists('file_get_contents'))
+    {
+        $fp = fopen("php://input", "r");
+        if ($fp)
+        {
+            $raw = "";
 
-			while (!feof($fp))
-				$raw = fread($fp, 1024);
+            while (!feof($fp))
+                $raw = fread($fp, 1024);
 
-			fclose($fp);
-		}
-	} else
-		$raw = "" . file_get_contents("php://input");
+            fclose($fp);
+        }
+    } else
+        $raw = "" . file_get_contents("php://input");
 }
 
 // No input data
 if (!$raw)
 {
-	echo '{"result":null,"id":null,"error":{"errstr":"Could not get raw post data.","errfile":"","errline":null,"errcontext":"","level":"FATAL"}}';
-	eZDB::checkTransactionCounter();
-	eZExecution::cleanExit();
+    echo '{"result":null,"id":null,"error":{"errstr":"Could not get raw post data.","errfile":"","errline":null,"errcontext":"","level":"FATAL"}}';
+    eZDB::checkTransactionCounter();
+    eZExecution::cleanExit();
 }
 
 // Passthrough request to remote server
-if (isset($config['general.remote_rpc_url'])) {
-	$url = parse_url($config['general.remote_rpc_url']);
+if (isset($config['general.remote_rpc_url']))
+{
+    $url = parse_url($config['general.remote_rpc_url']);
 
-	// Setup request
-	$req = "POST " . $url["path"] . " HTTP/1.0\r\n";
-	$req .= "Connection: close\r\n";
-	$req .= "Host: " . $url['host'] . "\r\n";
-	$req .= "Content-Length: " . strlen($raw) . "\r\n";
-	$req .= "\r\n" . $raw;
+    // Setup request
+    $req = "POST " . $url["path"] . " HTTP/1.0\r\n";
+    $req .= "Connection: close\r\n";
+    $req .= "Host: " . $url['host'] . "\r\n";
+    $req .= "Content-Length: " . strlen($raw) . "\r\n";
+    $req .= "\r\n" . $raw;
 
-	if (!isset($url['port']) || !$url['port'])
-		$url['port'] = 80;
+    if (!isset($url['port']) || !$url['port'])
+        $url['port'] = 80;
 
-	$errno = $errstr = "";
+    $errno = $errstr = "";
 
-	$socket = fsockopen($url['host'], intval($url['port']), $errno, $errstr, 30);
-	if ($socket) {
-		// Send request headers
-		fputs($socket, $req);
+    $socket = fsockopen($url['host'], intval($url['port']), $errno, $errstr, 30);
+    if ($socket) {
+        // Send request headers
+        fputs($socket, $req);
 
-		// Read response headers and data
-		$resp = "";
-		while (!feof($socket))
-				$resp .= fgets($socket, 4096);
+        // Read response headers and data
+        $resp = "";
+        while (!feof($socket))
+                $resp .= fgets($socket, 4096);
 
-		fclose($socket);
+        fclose($socket);
 
-		// Split response header/data
-		$resp = explode("\r\n\r\n", $resp);
-		echo $resp[1]; // Output body
-	}
+        // Split response header/data
+        $resp = explode("\r\n\r\n", $resp);
+        echo $resp[1]; // Output body
+    }
 
-	eZDB::checkTransactionCounter();
-	eZExecution::cleanExit();
+    eZDB::checkTransactionCounter();
+    eZExecution::cleanExit();
 }
 
 // Get JSON data
@@ -150,21 +156,21 @@ $input = $json->decode($raw);
 // Execute RPC
 if (isset($config['general.engine']) && class_exists( $config['general.engine'] ))
 {
-	$spellchecker = new $config['general.engine']($config);
-	$result = call_user_func_array(array($spellchecker, $input['method']), $input['params']);
+    $spellchecker = new $config['general.engine']($config);
+    $result = call_user_func_array(array($spellchecker, $input['method']), $input['params']);
 }
 else
 {
-	echo '{"result":null,"id":null,"error":{"errstr":"You must choose an valid spellchecker engine in the ezoe.ini file.","errfile":"","errline":null,"errcontext":"","level":"FATAL"}}';
-	eZDB::checkTransactionCounter();
-	eZExecution::cleanExit();
+    echo '{"result":null,"id":null,"error":{"errstr":"You must choose an valid spellchecker engine in the ezoe.ini file.","errfile":"","errline":null,"errcontext":"","level":"FATAL"}}';
+    eZDB::checkTransactionCounter();
+    eZExecution::cleanExit();
 }
 
 // Request and response id should always be the same
 $output = array(
-	"id" => $input->id,
-	"result" => $result,
-	"error" => null
+    "id" => $input->id,
+    "result" => $result,
+    "error" => null
 );
 
 // Return JSON encoded string
