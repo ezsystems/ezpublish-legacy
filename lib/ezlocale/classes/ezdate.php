df54e8e5505439058168d44442241afe890815ab
Kristof Coomans <kc@ez.no>
2008-12-17 12:44:09 +0000

    - Fixed bug #14157: eZDate::create() and eZDateTime::create() should be marked as static
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@22723 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezlocale/classes/ezdate.php
+++ lib/ezlocale/classes/ezdate.php
@@ -146,7 +146,7 @@ class eZDate
     }
 
     /*!
-     Returns a reference to the current locale.
+     Returns the current locale.
     */
     function locale()
     {
@@ -293,7 +293,7 @@ class eZDate
      Creates a new eZDate object with the date values $month, $day and $year and returns a reference to it.
      Any value can be ommitted or set to 0 to use the current date value.
     */
-    function create( $month, $day = 0, $year = 0 )
+    static function create( $month, $day = 0, $year = 0 )
     {
         if ( $year != 0 )
             $date = mktime( 0, 0, 0, $month, $day, $year );
