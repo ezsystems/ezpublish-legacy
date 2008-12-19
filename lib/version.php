9e40dcb360297be9c2f1e466cef6e66f1129efd2
Kristof Coomans <kc@ez.no>
2008-12-19 08:07:29 +0000

    - Setting version to 4.1.0alpha2
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@22739 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/version.php
+++ lib/version.php
@@ -35,8 +35,8 @@ class eZPublishSDK
     const VERSION_MAJOR = 4;
     const VERSION_MINOR = 1;
     const VERSION_RELEASE = 0;
-    const VERSION_STATE = 'alpha1';
-    const VERSION_DEVELOPMENT = false;
+    const VERSION_STATE = 'alpha2';
+    const VERSION_DEVELOPMENT = true;
     const VERSION_REVISION_STRING = '$Rev$';
     const VERSION_ALIAS = '4.1';
     const VERSION_REVISION = 19785;
