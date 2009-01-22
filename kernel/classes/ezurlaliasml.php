4a198391f5e34f9eabc6c64c8122413408359235
Kristof Coomans <kc@ez.no>
2009-01-22 14:01:32 +0000

    - Fixed bug #14077: eZURLAliasML database table lock and unlock code causes implicit commit of database transaction # can not use VALUES(DEFAULT), because of http://bugs.mysql.com/bug.php?id=42270   (Merged from trunk (4.1.0alpha2) rev. 22868)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.0@22869 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezurlaliasml.php
+++ kernel/classes/ezurlaliasml.php
@@ -2174,7 +2174,8 @@ class eZURLAliasML extends eZPersistentObject
         }
         else
         {
-            $db->query( 'INSERT INTO ezurlalias_ml_incr(id) VALUES(DEFAULT)' );
+            // can not use VALUES(DEFAULT), because of http://bugs.mysql.com/bug.php?id=42270
+            $db->query( 'INSERT INTO ezurlalias_ml_incr(id) VALUES(NULL)' );
         }
 
         return $db->lastSerialID( 'ezurlalias_ml_incr', 'id' );
