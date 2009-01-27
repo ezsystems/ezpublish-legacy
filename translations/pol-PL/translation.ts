bcfa3b9b97b89e038610428f7de94096ad1d3fba
André Rømcke <ar@ez.no>
2009-01-27 23:53:29 +0000

    defultcodec warning
    git-svn-id: http://svn.ez.no/svn/extensions/eztinymce/trunk@3552 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- translations/pol-PL/translation.ts
+++ translations/pol-PL/translation.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="pl_PL">
-<defaultcodec></defaultcodec>
 <context>
     <name>design/admin/content/edit</name>
     <message>
