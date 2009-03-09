57fd2a57783b34aeeefa24507b58e79d3ab92616
Gaetano Giunta <gg@ez.no>
2009-03-09 22:27:37 +0000

    - Fix db update scripts: set eZPublish version
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@23128 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/4.1/unstable/dbupdate-4.1.0beta1-to-4.1.0.sql
+++ update/database/mysql/4.1/unstable/dbupdate-4.1.0beta1-to-4.1.0.sql
@@ -1,3 +1,6 @@
+UPDATE ezsite_data SET value='4.1.0' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
 ALTER TABLE ezpending_actions ADD COLUMN created int(11) DEFAULT NULL;
 
 ALTER TABLE ezpending_actions ADD INDEX ezpending_actions_created ( created );
--- update/database/postgresql/4.1/unstable/dbupdate-4.1.0beta1-to-4.1.0.sql
+++ update/database/postgresql/4.1/unstable/dbupdate-4.1.0beta1-to-4.1.0.sql
@@ -1,3 +1,6 @@
+UPDATE ezsite_data SET value='4.1.0' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
 ALTER TABLE ezpending_actions ADD COLUMN created integer;
 
 CREATE INDEX ezpending_actions_created ON ezpending_actions USING btree ( created );
