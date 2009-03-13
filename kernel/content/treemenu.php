b6adad7d030ff74d025dc743fbd0425fb636396b
Jérôme Renard <jr@ez.no>
2009-03-13 13:52:38 +0000

    - Fixed bug #14600: object name is shown with unknown number, when the object name is a number, and starts with zeros
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@23169 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/content/treemenu.php
+++ kernel/content/treemenu.php
@@ -55,7 +55,7 @@ function arrayToJSON( $array )
             {
                 $value = arrayToJSON( $value );
             }
-            else if ( !is_numeric( $value ) )
+            else if ( !is_numeric( $value ) or $key == 'name' )
             {
                 $value = '"' . washJS( $value ) . '"';
             }
