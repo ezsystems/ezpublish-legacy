342100697af78efb49b52c46d8ff6ea1cfd976ad
Ole Marius Smestad <oms@ez.no>
2009-03-16 00:51:57 +0000

    - Updated: Updated name of dbupdate files
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@23181 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/4.1/unstable/dbupdate-4.1.0beta1-to-4.1.0rc1.sql
+++ update/database/postgresql/4.1/unstable/dbupdate-4.1.0beta1-to-4.1.0rc1.sql
@@ -1,4 +1,4 @@
-UPDATE ezsite_data SET value='4.1.0' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='4.1.0rc1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
 ALTER TABLE ezpending_actions ADD COLUMN created integer;
