c65df2f298a374ef9b537a62fb4efdcfc2aadf34
André Rømcke <ar@ez.no>
2009-03-17 13:17:38 +0000

    Updated copyright year
    git-svn-id: http://svn.ez.no/svn/extensions/eztinymce/trunk@3720 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- ezxmltext/handlers/input/ezoexmlinput.php
+++ ezxmltext/handlers/input/ezoexmlinput.php
@@ -6,7 +6,7 @@
 // ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 // SOFTWARE NAME: eZ Online Editor extension for eZ Publish
 // SOFTWARE RELEASE: 5.0
-// COPYRIGHT NOTICE: Copyright (C) 2008 eZ Systems AS
+// COPYRIGHT NOTICE: Copyright (C) 1999-2009 eZ Systems AS
 // SOFTWARE LICENSE: GNU General Public License v2.0
 // NOTICE: >
 //   This program is free software; you can redistribute it and/or
