bfbfc538c7a231a89c0aea51e8b43d314bed6054
Ole Marius Smestad <oms@ez.no>
2009-03-19 15:50:15 +0000

    - Updated: configured branch for next maintenance release # Added missing file.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.1@23256 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/checkversionnumbers.sh
+++ bin/shell/checkversionnumbers.sh
@@ -6,16 +6,16 @@
 # The last version which changelogs and db updates are related to
 # For the first development release this should be empty, in
 # wich case $LAST_STABLE is used.
-PREVIOUS_VERSION="4.0.0"
+PREVIOUS_VERSION="4.1.0"
 # The last version of the newest stable branch
-LAST_STABLE="4.0.0"
+LAST_STABLE="4.1.0"
 # Set this to true if the LAST_STABLE has been modified from the last release
 # This will be set to true automatically if the release is a final release
 LAST_STABLE_CHANGED="true"
 
 MAJOR=4
 MINOR=1
-RELEASE=0
+RELEASE=1
 # Starts at 1 for the first release in a branch and increases with one
 REAL_RELEASE=1
 STATE=""
@@ -28,7 +28,7 @@ PACKAGE_DEVELOPMENT="false"
 # Is automatically set to 'true' when $STATE contains some text, do not modify
 DEVELOPMENT="false"
 # Whether the previous release is a development release or not.
-DEVELOPMENT_PREVIOUS="true"
+DEVELOPMENT_PREVIOUS="false"
 # Is only true when the release is a final release (ie. the first of the stable ones)
 # Will be automatically set to true when $RELEASE is 0 and $DEVELOPMENT is false
 FINAL="false"
