75a24f953c75ce597cb9ab11756ce2fd430a01b9
Ole Marius Smestad <oms@ez.no>
2009-03-19 15:29:00 +0000

    - Updated: Adjusted update SQLs
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@23254 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/4.2/unstable/dbupdate-4.1.0-to-4.2.0alpha1.sql
+++ update/database/postgresql/4.2/unstable/dbupdate-4.1.0-to-4.2.0alpha1.sql
@@ -1,5 +1,5 @@
-UPDATE ezsite_data set value='4.2.0alpha1' WHERE name='ezpublish-version';
-UPDATE ezsite_data set value='1' WHERE name='ezpublish-release';
+UPDATE ezsite_data SET value='4.2.0alpha1' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
 -- START: from 4.1.0
 -- END: from 4.1.0
