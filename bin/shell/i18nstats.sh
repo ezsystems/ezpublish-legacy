179eb7ef92485c6c212bd0915c8e7a8012a2c4a0
Gunnstein Lye <gl@ez.no>
2009-03-19 15:04:59 +0000

    - Added %-complete calculation to i18nstats.sh (from trunk rev. 23250)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.1@23251 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/i18nstats.sh
+++ bin/shell/i18nstats.sh
@@ -30,24 +30,26 @@ if [ -z "$LOCALE_LIST" ]; then
     LOCALE_LIST=`ls share/translations`
 fi
 
-echo "+---------------------------------------------------+"
-echo "| Locale | Total | Obsolete | Finished | Unfinished |"
-echo "+---------------------------------------------------+"
+echo "+--------+-------+----------+----------+------------+------------+"
+echo "| Locale | Total | Obsolete | Finished | Unfinished | % Complete |"
+echo "+--------+-------+----------+----------+------------+------------+"
 for LOCALE in $LOCALE_LIST; do
     if [ $LOCALE = "untranslated" ]; then
         continue
     fi
     FILE=share/translations/$LOCALE/translation.ts
     if [ -f "$FILE" ]; then
+        FINISHED=`grep "<translation" $FILE | grep -v obsolete | grep -v unfinished | wc -l`
+        UNFINISHED=`grep "<translation" $FILE | grep unfinished | wc -l`
         printf "| %-6s " $LOCALE
         printf "| %5s " `grep "<translation" $FILE | wc -l`
         printf "| %8s " `grep "<translation" $FILE | grep obsolete | wc -l`
-        printf "| %8s " `grep "<translation" $FILE | grep -v obsolete | grep -v unfinished | wc -l`
-        printf "| %10s |\n" `grep "<translation" $FILE | grep unfinished | wc -l`
+        printf "| %8s " $FINISHED
+        printf "| %10s " $UNFINISHED
+        printf "| %9i%% |\n" $(( ( $FINISHED * 100 ) / ( $FINISHED + $UNFINISHED ) ))
     else
         printf "| %-6s " $LOCALE
-        printf "| %-31s" "No translation found"
-        printf "%11s\n" "|"
+        printf "| No translation found %34s\n" "|"
     fi
 done
-echo "+---------------------------------------------------+"
+echo "+--------+-------+----------+----------+------------+------------+"
