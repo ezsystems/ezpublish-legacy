941af0955891509f7d1e88acbeb891d3f8f137ba
Gaetano Giunta <gg@ez.no>
2009-04-16 17:00:24 +0000

    - fix: add proper skip notices for people upgrading from 4.1.1 to 4.20alpha update script
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@23409 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/4.2/unstable/dbupdate-4.1.0-to-4.2.0alpha1.sql
+++ update/database/mysql/4.2/unstable/dbupdate-4.1.0-to-4.2.0alpha1.sql
@@ -2,11 +2,13 @@ SET storage_engine=InnoDB;
 UPDATE ezsite_data SET value='4.2.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
+-- START: from 4.1.1
 ALTER TABLE ezworkflow_event
 CHANGE data_text1 data_text1 VARCHAR( 255 ),
 CHANGE data_text2 data_text2 VARCHAR( 255 ),
 CHANGE data_text3 data_text3 VARCHAR( 255 ),
 CHANGE data_text4 data_text4 VARCHAR( 255 );
+-- END: from 4.1.0
 
 -- START: from 4.1.0
 -- END: from 4.1.0
