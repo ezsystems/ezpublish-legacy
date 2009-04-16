bb40ef67889641c6aa468b0976c371dc5c972c9b
Ole Marius Smestad <oms@ez.no>
2009-04-16 10:01:22 +0000

    - Updated: postgresql dbupdate script
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.1@23404 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/4.1/dbupdate-4.1.0-to-4.1.1.sql
+++ update/database/postgresql/4.1/dbupdate-4.1.0-to-4.1.1.sql
@@ -1,2 +1,26 @@
 UPDATE ezsite_data SET value='4.1.1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+ALTER TABLE ezworkflow_event RENAME COLUMN data_text1 TO data_text1_tmp;
+ALTER TABLE ezworkflow_event ADD COLUMN data_text1 character varying(255);
+ALTER TABLE ezworkflow_event ALTER data_text1 SET DEFAULT NULL ;
+UPDATE ezworkflow_event SET data_text1=data_text1_tmp;
+ALTER TABLE ezworkflow_event DROP COLUMN data_text1_tmp;
+
+ALTER TABLE ezworkflow_event RENAME COLUMN data_text2 TO data_text2_tmp;
+ALTER TABLE ezworkflow_event ADD COLUMN data_text2 character varying(255);
+ALTER TABLE ezworkflow_event ALTER data_text2 SET DEFAULT NULL ;
+UPDATE ezworkflow_event SET data_text2=data_text2_tmp;
+ALTER TABLE ezworkflow_event DROP COLUMN data_text2_tmp;
+
+ALTER TABLE ezworkflow_event RENAME COLUMN data_text3 TO data_text3_tmp;
+ALTER TABLE ezworkflow_event ADD COLUMN data_text3 character varying(255);
+ALTER TABLE ezworkflow_event ALTER data_text3 SET DEFAULT NULL ;
+UPDATE ezworkflow_event SET data_text3=data_text3_tmp;
+ALTER TABLE ezworkflow_event DROP COLUMN data_text3_tmp;
+
+ALTER TABLE ezworkflow_event RENAME COLUMN data_text4 TO data_text4_tmp;
+ALTER TABLE ezworkflow_event ADD COLUMN data_text4 character varying(255);
+ALTER TABLE ezworkflow_event ALTER data_text4 SET DEFAULT NULL ;
+UPDATE ezworkflow_event SET data_text4=data_text4_tmp;
+ALTER TABLE ezworkflow_event DROP COLUMN data_text4_tmp;
