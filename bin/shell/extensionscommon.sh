2d38334a58b10ae5d1145f49a2d65668fba19176
Ole Marius Smestad <oms@ez.no>
2009-04-16 13:45:32 +0000

    - Updated: Build properties.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.1@23408 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/extensionscommon.sh
+++ bin/shell/extensionscommon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EXTENSIONS="http://svn.ez.no/svn/extensions/eztinymce/trunk/ezoe/"
-EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezodf/trunk/"
+EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezodf/stable/2.3/"
 #EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezpaypal/trunk"
 
 # These are disabled for now
