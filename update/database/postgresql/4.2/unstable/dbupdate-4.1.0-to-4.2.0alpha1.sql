2dedab9bd714b2f37e1f2463eedf10d549715123
Gaetano Giunta <gg@ez.no>
2009-04-16 17:01:16 +0000

    - fix: add 4.1.1 modifications to postgres db update script
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@23410 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/4.2/unstable/dbupdate-4.1.0-to-4.2.0alpha1.sql
+++ update/database/postgresql/4.2/unstable/dbupdate-4.1.0-to-4.2.0alpha1.sql
@@ -1,5 +1,31 @@
 UPDATE ezsite_data SET value='4.2.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
+-- START: from 4.1.1
+ALTER TABLE ezworkflow_event RENAME COLUMN data_text1 TO data_text1_tmp;
+ALTER TABLE ezworkflow_event ADD COLUMN data_text1 character varying(255);
+ALTER TABLE ezworkflow_event ALTER data_text1 SET DEFAULT NULL ;
+UPDATE ezworkflow_event SET data_text1=data_text1_tmp;
+ALTER TABLE ezworkflow_event DROP COLUMN data_text1_tmp;
+
+ALTER TABLE ezworkflow_event RENAME COLUMN data_text2 TO data_text2_tmp;
+ALTER TABLE ezworkflow_event ADD COLUMN data_text2 character varying(255);
+ALTER TABLE ezworkflow_event ALTER data_text2 SET DEFAULT NULL ;
+UPDATE ezworkflow_event SET data_text2=data_text2_tmp;
+ALTER TABLE ezworkflow_event DROP COLUMN data_text2_tmp;
+
+ALTER TABLE ezworkflow_event RENAME COLUMN data_text3 TO data_text3_tmp;
+ALTER TABLE ezworkflow_event ADD COLUMN data_text3 character varying(255);
+ALTER TABLE ezworkflow_event ALTER data_text3 SET DEFAULT NULL ;
+UPDATE ezworkflow_event SET data_text3=data_text3_tmp;
+ALTER TABLE ezworkflow_event DROP COLUMN data_text3_tmp;
+
+ALTER TABLE ezworkflow_event RENAME COLUMN data_text4 TO data_text4_tmp;
+ALTER TABLE ezworkflow_event ADD COLUMN data_text4 character varying(255);
+ALTER TABLE ezworkflow_event ALTER data_text4 SET DEFAULT NULL ;
+UPDATE ezworkflow_event SET data_text4=data_text4_tmp;
+ALTER TABLE ezworkflow_event DROP COLUMN data_text4_tmp;
+-- END: from 4.1.1
+
 -- START: from 4.1.0
 -- END: from 4.1.0
