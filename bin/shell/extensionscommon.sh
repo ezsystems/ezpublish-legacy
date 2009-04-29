20602dae48457ae955812813cfcf4e914b279ff3
Ole Marius Smestad <oms@ez.no>
2009-04-29 15:53:10 +0000

    - Updated: Configured release branch.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/4.0.4@23509 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/extensionscommon.sh
+++ bin/shell/extensionscommon.sh
@@ -3,7 +3,7 @@
 #EXTENSIONS="http://svn.ez.no/svn/extensions/ezpaypal/"
 EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezdhtml/unstable/ezdhtml_php5"
 EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezodf/stable/2.2"
-EXTENSIONS="$EXTENSIONS http://svn.projects.ez.no/ezurlaliasmigration/tags/1.0.0/extension/ezurlaliasmigration"
+EXTENSIONS="$EXTENSIONS http://svn.projects.ez.no/ezurlaliasmigration/tags/1.0.1/extension/ezurlaliasmigration"
 
 # These are disabled for now
 #EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/commercial/projects/ezoracle/trunk"
