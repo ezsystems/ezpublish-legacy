774aa7ab561b9fb792a5e83119e1f7b923ae8164
Gunnstein Lye <gl@ez.no>
2009-06-12 14:42:20 +0000

    - Fixed bug #15051: Fatal Error accessing collection information (infocollector/overview)   (Merged from trunk Rev. 23706)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.1@23707 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/4.1/dbupdate-4.1.3-to-4.1.4.sql
+++ update/database/postgresql/4.1/dbupdate-4.1.3-to-4.1.4.sql
@@ -2,3 +2,4 @@ UPDATE ezsite_data SET value='4.1.4' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
 CREATE INDEX ezkeyword_attr_link_oaid ON ezkeyword_attribute_link USING btree (objectattribute_id);
+CREATE INDEX ezinfocollection_co_id_created ON ezinfocollection USING btree (contentobject_id, created);
