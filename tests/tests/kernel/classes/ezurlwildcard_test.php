f39a78d4c8e5e8f39ef1a5883694909d75096f93
Bertrand Dunogier <bd@ez.no>
2009-08-14 13:02:15 +0000

    - Added license header to eZURLWildcardTest
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@23875 a01eee8c-daba-0310-acae-fa49f3023285

--- tests/tests/kernel/classes/ezurlwildcard_test.php
+++ tests/tests/kernel/classes/ezurlwildcard_test.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * File containing the eZURLWildcardTest class
+ *
+ * @copyright Copyright (C) 1999-2009 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ * @package tests
+ */
+
 class eZURLWildcardTest extends ezpDatabaseTestCase
 {
     /**
