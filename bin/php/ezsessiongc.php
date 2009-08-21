f4b294e2ad59ca0f3b7944759b979a25f0e1a39f
André Rømcke <ar@ez.no>
2009-08-21 15:52:36 +0000

    - Implemented enhancement #15093: Missing session cleanup script / cronjob for Debian based distroes   (Merged from trunk (4.2.0Beta1) Rev. 23909)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.1@23910 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ bin/php/ezsessiongc.php
@@ -0,0 +1,58 @@
+#!/usr/bin/env php
+<?php
+//
+// Created on: <21-Aug-2009 11:52:57 ar>
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: eZ Publish
+// SOFTWARE RELEASE: 4.1.x
+// COPYRIGHT NOTICE: Copyright (C) 1999-2009 eZ Systems AS
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
+
+set_time_limit( 0 );
+$isQuiet = false;
+
+require 'autoload.php';
+
+$cli = eZCLI::instance();
+$script = eZScript::instance( array( 'description' => ( "eZ Publish Session Garbage Collector\n\n" .
+                                                        "Allows manual cleaning up expired sessions as defined by site.ini[Session]SessionTimeout\n" .
+                                                        "\n" .
+                                                        "./bin/php/ezsessiongc.php" ),
+                                     'use-session' => false,
+                                     'use-modules' => false,
+                                     'use-extensions' => false ) );
+
+$script->startup();
+
+$options = $script->getOptions( "",
+                                "[]",
+                                array() );
+$script->initialize();
+
+if ( !$isQuiet )
+    $cli->notice( "Cleaning up expired sessions." );
+
+eZSession::garbageCollector();
+
+$script->shutdown();
+
+?>
