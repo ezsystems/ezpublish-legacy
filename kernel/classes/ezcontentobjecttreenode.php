1b6136d8d22a9b9793f2eee713291255e991c9af
Bertrand Dunogier <bd@ez.no>
2009-08-28 14:57:05 +0000

    - Fixed bug #13497: attribute operator throws a PHP fatal error on a node without parent in a displayable language (Merged from trunk rev. 23943)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.1@23944 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobjecttreenode.php
+++ kernel/classes/ezcontentobjecttreenode.php
@@ -460,8 +460,10 @@ class eZContentObjectTreeNode extends eZPersistentObject
     */
     function canRemoveLocation()
     {
+        echo __FUNCTION__;
         if ( !isset( $this->Permissions['can_remove_location'] ) )
         {
+            echo "Calling checkAccess()\n";
             $this->Permissions['can_remove_location'] = $this->checkAccess( 'can_remove_location' );
         }
         return ( $this->Permissions['can_remove_location'] == 1 );
@@ -4238,11 +4240,17 @@ class eZContentObjectTreeNode extends eZPersistentObject
         $accessWord = $accessResult['accessWord'];
         if ( $origFunctionName == 'can_remove_location' )
         {
+            // echo "checkAccess( can_remove_location )\n";
             if ( $this->ParentNodeID <= 1 )
             {
                 return 0;
             }
             $currentNode = eZContentObjectTreeNode::fetch( $this->ParentNodeID );
+            if ( !$currentNode instanceof eZContentObjectTreeNode )
+            {
+                eZDebug::writeWarning( func_get_args(), __METHOD__ );
+                return 0;
+            }
             $contentObject = $currentNode->attribute( 'object' );
         }
         else
