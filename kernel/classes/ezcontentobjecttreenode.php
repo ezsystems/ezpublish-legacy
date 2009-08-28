7ce19d77a8b90ca803f4d14ecac044278494c761
Bertrand Dunogier <bd@ez.no>
2009-08-28 15:23:06 +0000

    - Removed forgotten debug statements...
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.1@23946 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcontentobjecttreenode.php
+++ kernel/classes/ezcontentobjecttreenode.php
@@ -460,10 +460,8 @@ class eZContentObjectTreeNode extends eZPersistentObject
     */
     function canRemoveLocation()
     {
-        echo __FUNCTION__;
         if ( !isset( $this->Permissions['can_remove_location'] ) )
         {
-            echo "Calling checkAccess()\n";
             $this->Permissions['can_remove_location'] = $this->checkAccess( 'can_remove_location' );
         }
         return ( $this->Permissions['can_remove_location'] == 1 );
@@ -4240,7 +4238,6 @@ class eZContentObjectTreeNode extends eZPersistentObject
         $accessWord = $accessResult['accessWord'];
         if ( $origFunctionName == 'can_remove_location' )
         {
-            // echo "checkAccess( can_remove_location )\n";
             if ( $this->ParentNodeID <= 1 )
             {
                 return 0;
