ed752d86a4c2cbc7cacb86297f9fc4b7ac65436f
Bertrand Dunogier <bd@ez.no>
2009-08-28 15:30:53 +0000

    - Fixed bug #15328: Problem in function validateObjectAttributeHTTPInput in class ezemailtype.php   (Merged from trunk rev. 23947)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.1@23948 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/datatypes/ezemail/ezemailtype.php
+++ kernel/classes/datatypes/ezemail/ezemailtype.php
@@ -77,10 +77,10 @@ class eZEmailType extends eZDataType
     */
     function validateObjectAttributeHTTPInput( $http, $base, $contentObjectAttribute )
     {
+        $classAttribute = $contentObjectAttribute->contentClassAttribute();
         if ( $http->hasPostVariable( $base . '_data_text_' . $contentObjectAttribute->attribute( 'id' ) ) )
         {
             $email = $http->postVariable( $base . '_data_text_' . $contentObjectAttribute->attribute( 'id' ) );
-            $classAttribute = $contentObjectAttribute->contentClassAttribute();
 
             $trimmedEmail = trim( $email );
 
