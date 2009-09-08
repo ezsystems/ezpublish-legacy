665e9f294ce8448e8449544ff66574ab24e4c1c3
Bertrand Dunogier <bd@ez.no>
2009-09-08 12:25:07 +0000

    - Added a test for #14371 so that both module/view and operation are covered
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@24040 a01eee8c-daba-0310-acae-fa49f3023285

--- tests/tests/kernel/classes/ezworkflowevent_regression.php
+++ tests/tests/kernel/classes/ezworkflowevent_regression.php
@@ -9,6 +9,8 @@
 
 class eZWorkflowEventRegression extends ezpDatabaseTestCase
 {
+    protected $backupGlobals = false;
+
     public function __construct()
     {
         parent::__construct();
@@ -18,7 +20,7 @@ class eZWorkflowEventRegression extends ezpDatabaseTestCase
     }
 
     /**
-     * Test regression for issue #14371:
+     * Test regression for issue #14371 in a module/view context:
      * Workflow template repeat broken by security patch.
      *
      * Test Outline
@@ -33,7 +35,7 @@ class eZWorkflowEventRegression extends ezpDatabaseTestCase
      * @expected: The object gets published without being redirected
      * @link http://issues.ez.no/14371
      **/
-    public function testEditAfterFetchTemplateRepeat()
+    public function testEditAfterFetchTemplateRepeatModule()
     {
         // first, we need to create an appropriate test workflow
         $adminUser = eZUser::fetchByName( 'admin' );
@@ -80,23 +82,93 @@ class eZWorkflowEventRegression extends ezpDatabaseTestCase
         // Before this fix, a redirection to content/history was performed
         $this->assertNotEquals( $contentModule->ExitStatus, eZModule::STATUS_REDIRECT, "A redirection was performed by content/edit" );
 
-        // Remove trigger
-        eZTrigger::removeTriggerForWorkflow( $this->workflow->attribute( 'id' ) );
+        $this->removeWorkflow( $this->workflow );
 
         // Log in as whoever was logged in
         eZUser::setCurrentlyLoggedInUser( $this->currentUser, $this->currentUser->attribute( 'id' ) );
     }
 
     /**
+     * Test regression for issue #14371 in a module/view context:
+     * Workflow template repeat broken by security patch.
+     *
+     * Test Outline
+     * ------------
+     * 1. Setup a workflow that features a custom workflow event that expects a
+     *    value to be submitted before
+     * 2. Create & publish an article
+     * 3. Add a global POST variable that would be sent interactively from POST
+     * 4. Publish again with this variable
+     *
+     * @result: Redirection to content/history
+     * @expected: The object gets published without being redirected
+     * @link http://issues.ez.no/14371
+     **/
+    public function testEditAfterFetchTemplateRepeatOperation()
+    {
+        // first, we need to create an appropriate test workflow
+        $adminUser = eZUser::fetchByName( 'admin' );
+        $adminUserID = $adminUser->attribute( 'contentobject_id' );
+
+        // Create approval workflow and set up pre publish trigger
+        $this->workflow = $this->createWorkFlow( $adminUserID );
+        $this->trigger = $this->createTrigger( $this->workflow->attribute( 'id' ) );
+
+        // Log in as a user who's allowed to publish content
+        $this->currentUser = eZUser::currentUser();
+        eZUser::setCurrentlyLoggedInUser( $adminUser, $adminUserID );
+
+        // required to avoid a notice
+        $GLOBALS['eZSiteBasics']['user-object-required'] = false;
+
+        $contentModule = eZModule::findModule( 'content' );
+
+        $adminUserID = eZUser::fetchByName( 'admin' )->attribute( 'contentobject_id' );
+
+        // STEP 1: Create an article
+        // This should start the publishing process, and interrupt it because
+        // of the fetch template repeat workflow (expected)
+        $article = new ezpObject( "article", 2, $adminUserID );
+        $article->name = "Article (with interactive workflow) for issue/regression #14371";
+        $objectID = $article->publish();
+        $version = eZContentObjectVersion::fetchVersion( 1, $objectID );
+
+        // STEP 2: Add the POST variables that will allow the operation to continue
+        $_POST['CompletePublishing'] = 1;
+
+        // STEP 3: run content/edit again in order to simulate a POST from the custom TPL
+        $operationResult = eZOperationHandler::execute( 'content', 'publish', array( 'object_id' => $objectID,
+                                                                                     'version'   => 1 ) );
+
+        // Remove trigger
+        eZTrigger::removeTriggerForWorkflow( $this->workflow->attribute( 'id' ) );
+
+        // Log in as whoever was logged in
+        eZUser::setCurrentlyLoggedInUser( $this->currentUser, $this->currentUser->attribute( 'id' ) );
+
+        // remove the workflow events
+        eZWorkflow::removeEvents( false, $this->workflow->attribute( 'id' ), $this->workflow->attribute( 'version' ) );
+
+        // remove the workflow
+        eZWorkflow::removeWorkflow( $this->workflow->attribute( 'id' ), $this->workflow->attribute( 'version' ) );
+    }
+
+    /**
     * Creates the test workflow.
     * @todo Currently only handles the fetchtemplaterepeat event. Will have to be
     *       refactored to handle more events when necessary
+    *
+    * @return eZWorkflow
     **/
     function createWorkFlow( $adminUserID )
     {
-        $this->registerCustomWorkflowEvent(
+        $registerResult = $this->registerCustomWorkflowEvent(
             'fetchtemplaterepeat',
             'tests/toolkit/extras/tests-files/workflowevent_regression_fetchtemplaterepeat.php' );
+        if ( $registerResult == false )
+        {
+            $this->markTestSkipped( "Unable to register the workflow event" );
+        }
 
         $workflow = eZWorkflow::create( $adminUserID );
         $workflow->setAttribute( "name", "eZWorkflowEventRegression Workflow" );
@@ -129,7 +201,9 @@ class eZWorkflowEventRegression extends ezpDatabaseTestCase
 
         // Remove version 1
         eZWorkflowGroupLink::removeWorkflowMembers( $workflowID, 1 );
-        eZWorkflow::removeEvents( false, $workflowID, 0 );
+        // eZWorkflow::removeEvents( false, $workflowID, 0 );
+        eZWorkflow::removeEvents( false, $workflowID, 1 );
+        eZWorkflow::removeWorkflow( $workflowID, 1 );
 
         $workflow->setVersion( 0, $eventList );
         $workflow->adjustEventPlacements( $eventList );
@@ -146,7 +220,7 @@ class eZWorkflowEventRegression extends ezpDatabaseTestCase
     * @todo Currently registers a content/publish/before event. Refactor when required
     * @return void
     **/
-    function createTrigger( $workflowID )
+    protected function createTrigger( $workflowID )
     {
         // @todo Also test with before publish
         return eZTrigger::createNew( 'content', 'publish', 'b', $workflowID );
@@ -159,32 +233,63 @@ class eZWorkflowEventRegression extends ezpDatabaseTestCase
      * @param mixed $includeFile Relative path to the workflow definition (class) file
      * @return bool false if an error occurs
      */
-    function registerCustomWorkflowEvent( $workflowTypeString, $includeFile )
+    protected function registerCustomWorkflowEvent( $workflowTypeString, $includeFile )
     {
+        static $includedFiles = array();
+
+        // include the file if it hasn't been included yet
+        if ( !in_array( $includeFile, $includedFiles ) )
+        {
+            if ( file_exists( $includeFile ) )
+            {
+                include( $includeFile );
+                $includedFiles[] = $includeFile;
+            }
+            else
+            {
+                echo __FUNCTION__ . ": custom workflow event file '$includeFile' doesn't exist\n";
+                return false;
+            }
+        }
+
+        // register the event if it hasn't been registered yet
+        $eventString = "event_{$workflowTypeString}";
         $wfINI = eZINI::instance( 'workflow.ini' );
         $availableEventTypesSetting = $wfINI->variable( 'EventSettings', 'AvailableEventTypes' );
-        $availableEventTypesSetting[] = "event_{$workflowTypeString}";
-        $wfINI->setVariable( 'EventSettings', 'AvailableEventTypes', $availableEventTypesSetting );
-        if ( file_exists( $includeFile ) )
+        if ( !in_array( $eventString, $availableEventTypesSetting ) )
         {
-            include( $includeFile );
-            return true;
+            $availableEventTypesSetting[] = $eventString;
+            $wfINI->setVariable( 'EventSettings', 'AvailableEventTypes', $availableEventTypesSetting );
         }
-        else
-        {
-            return false;
-        }
+        return true;
     }
 
     /**
+     * Helper function used to remove a workflow.
+     * Will remove the workflow, its events, its triggers and its group link.
+     *
+     * @param eZWorkflow $workflow
+     * @return void
+     **/
+    protected function removeWorkflow( eZWorkflow $workflow )
+    {
+        // Remove trigger
+        eZTrigger::removeTriggerForWorkflow( $workflow->attribute( 'id' ) );
+
+        // remove the workflow, its events and the group link
+        eZWorkflow::removeEvents( false, $workflow->attribute( 'id' ), $workflow->attribute( 'version' ) );
+        eZWorkflow::removeWorkflow( $this->workflow->attribute( 'id' ), $workflow->attribute( 'version' ) );
+        eZWorkflowGroupLink::removeWorkflowMembers( $workflow->attribute( 'id' ), $workflow->attribute( 'version' ) );
+    }
+    /**
      * @var eZWorkflow
      **/
-    private $workflow;
+    protected $workflow;
 
-    /**ti
+    /**
      * @var eZTrigger
      **/
-    private $trigger;
+    protected $trigger;
 
     /**
     * Currently logged in user backup
