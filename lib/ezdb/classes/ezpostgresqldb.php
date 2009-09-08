06c20f0f6829de1b5b4bbef00fccb34480dc557b
Ole Marius Smestad <oms@ez.no>
2009-09-08 01:57:29 +0000

    - CS: Removed extra blank line.   (Merged from trunk (4.2.0beta1) rev. 24032)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.0@24034 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezdb/classes/ezpostgresqldb.php
+++ lib/ezdb/classes/ezpostgresqldb.php
@@ -540,7 +540,6 @@ class eZPostgreSQLDB extends eZDBInterface
         return $this->query( "ROLLBACK WORK" );
     }
 
-
     /**
      * Returns the last serial ID generated with an auto increment field.
      * 
