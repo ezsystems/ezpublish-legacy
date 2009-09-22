2cc97ce0d057ce82c3387910a6ccc19d9d4b39a9
Gunnstein Lye <gl@ez.no>
2009-09-22 14:03:30 +0000

    - Fixed issue #15532: Bug with eZHTTPTool::getDataByURL and output buffering   (Merged from trunk (4.3.0alpha1) rev. 24153)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.0@24156 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezutils/classes/ezhttptool.php
+++ lib/ezutils/classes/ezhttptool.php
@@ -787,7 +787,10 @@ class eZHTTPTool
             if ( $justCheckURL )
             {
                 if ( !curl_exec( $ch ) )
+                {
+                    curl_close( $ch );
                     return false;
+                }
 
                 curl_close( $ch );
                 return true;
@@ -795,7 +798,11 @@ class eZHTTPTool
             // Getting data
             ob_start();
             if ( !curl_exec( $ch ) )
+            {
+                curl_close( $ch );
+                ob_end_clean();
                 return false;
+            }
 
             curl_close ( $ch );
             $data = ob_get_contents();
