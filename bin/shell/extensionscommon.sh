76e355c461f2286dee88b7c9f5d3ea6d650ba517
Ole Marius Smestad <oms@ez.no>
2009-09-25 16:50:38 +0000

    - Updated: extension paths
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/release/4.2.0@24174 a01eee8c-daba-0310-acae-fa49f3023285

--- bin/shell/extensionscommon.sh
+++ bin/shell/extensionscommon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EXTENSIONS="http://svn.projects.ez.no/ezoe/stable/5.0/ezoe/"
-EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezodf/trunk/"
+EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezodf/stable/2.4/"
 #EXTENSIONS="$EXTENSIONS http://svn.ez.no/svn/extensions/ezpaypal/trunk"
 
 # These are disabled for now
