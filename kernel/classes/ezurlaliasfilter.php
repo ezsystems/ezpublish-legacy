e8f2b54bcfcbf1b82cf0a1e246e82417eabdcf47
Jérôme Renard <jr@ez.no>
2009-10-07 11:54:22 +0000

    - Implemented PHPDoc comments in regards to issue #012014: Switch to PHPDoc completely - Added also a few extra keywords on class type as well as method visibility
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@24227 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezurlaliasfilter.php
+++ kernel/classes/ezurlaliasfilter.php
@@ -28,54 +28,64 @@
 // ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 //
 
-/*! \file
-*/
+/**
+ * The eZURLAliasFilter class the interface for all url alias filters,
+ * the filter implementation must implement the process method.
+ *
+ * For execution of the configured filters use the static method processFilters.
+ * Help with configuration is found in settings/site.ini under the group URLTranslator.
+ *
+ * @abstract
+ */
 
-/*!
-  \class eZURLAliasFilter ezurlaliasfilter.php
-  \brief Handles filtering of URL aliases
-
-  This class defines the interface for all url alias filters, the filter implementation must implement the process method.
-
-  For execution of the configured filters use the static method processFilters.
-  Help with configuration is found in settings/site.ini under the group URLTranslator.
-*/
-
-class eZURLAliasFilter
+abstract class eZURLAliasFilter
 {
     /**
-     Initialize the filter object.
+     * Initialize the filter object.
      */
-    function eZURLAliasFilter()
+    public function eZURLAliasFilter()
     {
     }
 
-    /*!
-     \abstract
-     Process the url alias element $text and return the new element as a string.
-
-     \param $languageObject The current language object used for the string $text.
-     \param $caller The object which called the filtering process, can be null.
+    /*
+     *
+     * Process the url alias element $text and return the new element as a string.
+     * This method must be overriden in custom URL alias filters.
+     *
+     * This function has not been declared as "abstract" because of backward compatibility
+     * but you should see it as an abstract method.
+     *
+     * @abstract
+     * @param string $text           The URL alias
+     * @param string $languageObject The current language object used for the string $text.
+     * @param object $caller         The object which called the filtering process, can be null.
+     * @return string the processed URL alias
      */
-    function process( $text, &$languageObject, &$caller )
+    public function process( $text, &$languageObject, &$caller )
     {
         return $text;
     }
 
-    /*!
-     \static
-     Process all configured filters and return the resulting text.
-
-     Filters are found in the INI group URLTranslator and the setting Filters.
-     This is done in combination with the setting Extensions which controls
-     which extensions have filter classes.
-
-     The parameters $text, $languageObject and $caller are sent to the method
-     process on the filter object.
-
-     Note: The filter list will be cached in memory to improve performance of subsequent calls.
+    /**
+     *
+     * Process all configured filters and return the resulting text.
+     *
+     * Filters are found in the INI group URLTranslator and the setting Filters.
+     * This is done in combination with the setting Extensions which controls
+     * which extensions have filter classes.
+     *
+     * The parameters $text, $languageObject and $caller are sent to the method
+     * process on the filter object.
+     *
+     * Note: The filter list will be cached in memory to improve performance of subsequent calls.
+     *
+     * @static
+     * @param string $text           The URL alias
+     * @param string $languageObject The current language object used for the string $text.
+     * @param object $caller         The object which called the filtering process, can be null.
+     * @return string the URL alias processed by the process() method
      */
-    static function processFilters( $text, $languageObject, $caller )
+    public static function processFilters( $text, $languageObject, $caller )
     {
         $filters = array();
         if ( isset( $GLOBALS['eZURLAliasFilters'] ) )
