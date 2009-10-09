fae8fb918d2501cb6d60b26c2389a2a7539a5743
André Rømcke <ar@ez.no>
2009-10-09 14:12:17 +0000

    - Added note on permissions
    git-svn-id: http://svn.projects.ez.no/ezjscore/trunk@63 94d18f98-15bb-4cfc-85c1-4239715ba301

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -8,6 +8,9 @@ ExtensionAutoloadPath[]=ezjscore
 [SSLZoneSettings] 
 ModuleViewAccessMode[ezjscore/*]=keep
 
+
+# Permissions to the specific ajax calls are handled inside the ezjscore/call
+# view, so only comment this out if you want to disable it!
 [RoleSettings]
 PolicyOmitList[]=ezjscore/hello
 PolicyOmitList[]=ezjscore/call
