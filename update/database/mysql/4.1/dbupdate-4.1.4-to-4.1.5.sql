0f9dae00daffa96828d3429e200e9abee863100e
André Rømcke <ar@ez.no>
2009-10-26 13:52:27 +0000

    - Fixed bug #14616: classattributeidentifiers cache doesn't work (at all) with clustering + ezdb   (Merged from trunk (4.3.0alpha1) rev. 24281)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.1@24283 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/4.1/dbupdate-4.1.4-to-4.1.5.sql
+++ update/database/mysql/4.1/dbupdate-4.1.4-to-4.1.5.sql
@@ -1,3 +1,8 @@
 SET storage_engine=InnoDB;
 UPDATE ezsite_data SET value='4.1.5' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+-- START: using cluster setup
+UPDATE ezdbfile set scope='classattridentifiers' WHERE scope='classattributeidenti';
+UPDATE ezdbfile set scope='classattridentifiers' WHERE scope='classattributeidenfiers';
+-- END: using cluster setup
--- update/database/postgresql/4.1/dbupdate-4.1.4-to-4.1.5.sql
+++ update/database/postgresql/4.1/dbupdate-4.1.4-to-4.1.5.sql
@@ -1,2 +1,7 @@
 UPDATE ezsite_data SET value='4.1.5' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+-- START: using cluster setup
+UPDATE ezdbfile set scope='classattridentifiers' WHERE scope='classattributeidenti';
+UPDATE ezdbfile set scope='classattridentifiers' WHERE scope='classattributeidenfiers';
+-- END: using cluster setup
