49cd472d1f0bfaa1f6e667ea6c42a70a54197dc2
Gunnstein Lye <gl@ez.no>
2009-11-19 14:13:25 +0000

    - Fixed bug #15740: Current imagemanager can`t execute image aliases that have different image handler`s filters.   (Merged from trunk (4.3.0alpha1) rev. 24338)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.1@24340 a01eee8c-daba-0310-acae-fa49f3023285

---
+++ tests/tests/lib/ezimage/suite.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * File containing the eZImageTestSuite class
+ *
+ * @copyright Copyright (C) 1999-2009 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ * @package tests
+ */
+
+class eZImageTestSuite extends ezpDatabaseTestSuite
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setName( "eZImage Test Suite" );
+
+        $this->addTestSuite( 'eZImageManagerTest' );
+    }
+
+    public static function suite()
+    {
+        return new self();
+    }
+}
+
+?>
