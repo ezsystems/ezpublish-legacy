e1f8b2e5fdfbdf951535bd33ba8912cb3d1038de
Ole Marius Smestad <oms@ez.no>
2009-11-24 13:10:04 +0000

    - Updated: schema updates to clustering tables are best kept in separate files.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@24384 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/mysql/4.3/unstable/dbupdate-4.2.0-to-4.3.0alpha1.sql
+++ update/database/mysql/4.3/unstable/dbupdate-4.2.0-to-4.3.0alpha1.sql
@@ -2,10 +2,4 @@ SET storage_engine=InnoDB;
 UPDATE ezsite_data SET value='4.3.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
--- START: from 4.2.0 using cluster setup
-UPDATE ezdbfile set scope='classattridentifiers' WHERE scope='classattributeidenti';
-UPDATE ezdbfile set scope='classattridentifiers' WHERE scope='classattributeidenfiers';
--- END: from 4.2.0 using cluster setup
-
-
 ALTER TABLE ezrss_export_item ADD COLUMN enclosure VARCHAR( 255 ) NULL;
