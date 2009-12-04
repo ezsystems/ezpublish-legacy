4df5b8b77a3cf0ae76f6db3cdb9bc394cfa12ec3
Ole Marius Smestad <oms@ez.no>
2009-12-04 10:46:05 +0000

    - Fixed: Moved one test which expects Oracle, out of the default set.
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@24430 a01eee8c-daba-0310-acae-fa49f3023285

--- tests/tests/lib/ezdb/ezdbinterface_test.php
+++ tests/tests/lib/ezdb/ezdbinterface_test.php
@@ -37,10 +37,15 @@ class eZDBInterfaceTest extends ezpDatabaseTestCase
             // Standard IN queries with a real cast (alpha => 0)
             array( array( 1, 2, 'a', 4 ), 'testrow', false, false, 'int',
                 'testrow IN ( 1, 2, 0, 4 )' ),
+        );
+    }
 
-            // Unicity test
+    public static function providerForTestGenerateSQLINStatement2()
+    {
+        // Unicity test
+        return array(
             array( array( 1, 2, 3, 3, 3, 4 ), 'testrow', false, true, 'int',
-                'testrow IN ( 1, 2, 3, 4 )' ),
+            'testrow IN ( 1, 2, 3, 4 )' )
         );
     }
 
@@ -60,6 +65,21 @@ class eZDBInterfaceTest extends ezpDatabaseTestCase
     public function testGenerateSQLINStatement( $elements, $columnName, $not, $unique, $type, $expected )
     {
         $db = eZDB::instance();
+        $ret = $db->generateSQLINStatement(
+            $elements, $columnName, $not, $unique, $type );
+        $this->assertEquals( $expected, $ret );
+    }
+
+    /**
+     * @dataProvider providerForTestGenerateSQLINStatement2
+     **/
+    public function testGenerateSQLINStatementOracle( $elements, $columnName, $not, $unique, $type, $expected )
+    {
+        $db = eZDB::instance();
+
+        if ( $db->databaseName() !== "oracle" )
+            self::markTestSkipped( "Only implemented in the Oracle driver, skipping" );
+
         $ret = $db->generateSQLINStatement(
             $elements, $columnName, $not, $unique, $type );
         $this->assertEquals( $expected, $ret );
