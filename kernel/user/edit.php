16b69577b74e21ee3cf43d2f098e760846c218bb
André Rømcke <ar@ez.no>
2010-01-25 16:22:45 +0000

    - Implemented enhancement #15339: Add view_parameters support in user/edit # Incl support for usign view param to redirect to edit user object, for use in admin2 left menu
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@24675 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/user/edit.php
+++ kernel/user/edit.php
@@ -27,49 +27,71 @@
 //
 
 
-$currentUser = eZUser::currentUser();
-$http = eZHTTPTool::instance();
 $Module = $Params['Module'];
 
-if ( isset( $Params["UserID"] ) && is_numeric( $Params["UserID"] ) )
-    $UserID = $Params["UserID"];
-else if ( !$currentUser->isAnonymous() )
-    $UserID = $currentUser->attribute( "contentobject_id" );
+if ( isset( $Params['UserID'] ) && is_numeric( $Params['UserID'] ) )
+{
+    $UserID = $Params['UserID'];
+}
+else
+{
+    $currentUser = eZUser::currentUser();
+    $UserID      = $currentUser->attribute( 'contentobject_id' );
+	if ( $currentUser->isAnonymous() )
+	{
+	    return $Module->handleError( eZError::KERNEL_ACCESS_DENIED, 'kernel' );
+	}
+}
+
+if ( isset( $Params['UserParameters'] ) )
+{
+    $UserParameters = $Params['UserParameters'];
+}
+else
+{
+    $UserParameters = array();
+}
 
 if ( $Module->isCurrentAction( "ChangePassword" ) )
 {
-    $Module->redirectTo( $Module->functionURI( "password" ) . "/" . $UserID  );
-    return;
+    return $Module->redirectTo( "user/password/" . $UserID  );
 }
 
 if ( $Module->isCurrentAction( "ChangeSetting" ) )
 {
-    $Module->redirectTo( $Module->functionURI( "setting" ) . "/" . $UserID );
-    return;
+    return $Module->redirectTo( "user/setting/" . $UserID );
 }
 
 if ( $Module->isCurrentAction( "Cancel" ) )
 {
-    $Module->redirectTo( '/content/view/sitemap/5/' );
-    return;
+    return $Module->redirectTo( '/content/view/sitemap/5/' );
 }
 
-if ( $Module->isCurrentAction( "Edit" ) )
+$http = eZHTTPTool::instance();
+
+if ( $Module->isCurrentAction( "Edit" ) || ( isset( $UserParameters['action'] ) && $UserParameters['action'] === 'edit' ) )
 {
     $selectedVersion = $http->hasPostVariable( 'SelectedVersion' ) ? $http->postVariable( 'SelectedVersion' ) : 'f';
     $editLanguage = $http->hasPostVariable( 'ContentObjectLanguageCode' ) ? $http->postVariable( 'ContentObjectLanguageCode' ) : '';
-    $Module->redirectTo( '/content/edit/' . $UserID . '/' . $selectedVersion . '/' . $editLanguage );
-    return;
+    return $Module->redirectTo( '/content/edit/' . $UserID . '/' . $selectedVersion . '/' . $editLanguage );
 }
 
 $userAccount = eZUser::fetch( $UserID );
 if ( !$userAccount )
+{
     return $Module->handleError( eZError::KERNEL_NOT_AVAILABLE, 'kernel' );
+}
+    
 $userObject = $userAccount->attribute( 'contentobject' );
-if ( !$userObject )
+if ( !$userObject instanceof eZContentObject  )
+{
     return $Module->handleError( eZError::KERNEL_NOT_AVAILABLE, 'kernel' );
+}
+
 if ( !$userObject->canEdit( ) )
+{
     return $Module->handleError( eZError::KERNEL_ACCESS_DENIED, 'kernel' );
+}
 
 require_once( "kernel/common/template.php" );
 $tpl = templateInit();
@@ -77,6 +99,8 @@ $tpl->setVariable( "module", $Module );
 $tpl->setVariable( "http", $http );
 $tpl->setVariable( "userID", $UserID );
 $tpl->setVariable( "userAccount", $userAccount );
+$tpl->setVariable( 'view_parameters', $UserParameters );
+$tpl->setVariable( 'site_access', $GLOBALS['eZCurrentAccess'] );
 
 $Result = array();
 $Result['content'] = $tpl->fetch( "design:user/edit.tpl" );
