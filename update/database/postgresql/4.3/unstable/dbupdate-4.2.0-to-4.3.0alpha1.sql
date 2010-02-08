744f2aa5d096ac149e1ab5588496759fe880a85b
André Rømcke <ar@ez.no>
2010-02-08 12:02:38 +0000

    - Fixed update sql for postgres
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@24886 a01eee8c-daba-0310-acae-fa49f3023285

--- update/database/postgresql/4.3/unstable/dbupdate-4.2.0-to-4.3.0alpha1.sql
+++ update/database/postgresql/4.3/unstable/dbupdate-4.2.0-to-4.3.0alpha1.sql
@@ -1,8 +1,14 @@
 UPDATE ezsite_data SET value='4.3.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
-ALTER TABLE ezrss_export_item ADD COLUMN enclosure VARCHAR( 255 ) NULL;
-ALTER TABLE ezcontentclass ADD COLUMN serialized_description_list text NULL;
-ALTER TABLE ezcontentclass_attribute ADD COLUMN serialized_data_text text NULL;
-ALTER TABLE ezcontentclass_attribute ADD COLUMN serialized_description_list text NULL;
-ALTER TABLE ezcontentclass_attribute ADD COLUMN category VARCHAR( 25 ) NOT NULL;
+ALTER TABLE ezrss_export_item ADD COLUMN enclosure character varying(255);
+ALTER TABLE ezrss_export_item ALTER enclosure SET DEFAULT NULL;
+
+ALTER TABLE ezcontentclass ADD COLUMN serialized_description_list character;
+ALTER TABLE ezcontentclass ALTER serialized_description_list SET DEFAULT NULL;
+
+ALTER TABLE ezcontentclass_attribute ADD COLUMN serialized_data_text character;
+ALTER TABLE ezcontentclass_attribute ALTER serialized_data_text SET DEFAULT NULL;
+ALTER TABLE ezcontentclass_attribute ADD COLUMN serialized_description_list character;
+ALTER TABLE ezcontentclass_attribute ALTER serialized_description_list SET DEFAULT NULL;
+ALTER TABLE ezcontentclass_attribute ADD COLUMN category character varying(25);
