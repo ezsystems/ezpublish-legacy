befe4d368715e1b628c0e4b57cd03bbe0eac63bd
Gunnstein Lye <gl@ez.no>
2010-02-11 10:32:38 +0000

    - Fixed bug #11038: eZP might go into an infinite loop if MySQL closes it connections causing apache to segfault (now also fixed for mysqli)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@24902 a01eee8c-daba-0310-acae-fa49f3023285

--- lib/ezdb/classes/ezmysqlidb.php
+++ lib/ezdb/classes/ezmysqlidb.php
@@ -442,7 +442,7 @@ class eZMySQLiDB extends eZDBInterface
                 $oldRecordError = $this->RecordError;
                 // Turn off error handling while we unlock
                 $this->RecordError = false;
-                $this->unlock();
+                mysqli_query( $connection, 'UNLOCK TABLES' );
                 $this->RecordError = $oldRecordError;
 
                 $this->reportError();
