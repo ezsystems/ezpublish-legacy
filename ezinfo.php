1adb57a78d90973d5c073f1b2a5b9e1879f91720
André Rømcke <ar@ez.no>
2010-02-13 11:39:46 +0000

    * Fixed #15872: suggestions of spell checker have the wrong encoding # And bumped version number to beta1
    git-svn-id: http://svn.projects.ez.no/ezoe/trunk@170 0f997322-f765-4293-a636-cd5f354860a4

--- ezinfo.php
+++ ezinfo.php
@@ -31,7 +31,7 @@ class ezoeInfo
         $eZCopyrightString = 'Copyright (C) 1999-' . date('Y') . ' eZ Systems AS';
 
         return array( 'Name'      => '<a href="http://projects.ez.no/ezoe">eZ Online Editor</a> extension',
-                      'Version'   => '5.1.0Alpha1',
+                      'Version'   => '5.1.0Beta1',
                       'Copyright' => $eZCopyrightString,
                       'License'   => 'GNU General Public License v2.0',
                       'Includes the following third-party software' => array( 'Name' => 'TinyMce Javascript HTML WYSIWYG editor',
--- modules/ezoe/classes/GoogleSpell.php
+++ modules/ezoe/classes/GoogleSpell.php
@@ -3,7 +3,7 @@
  * $Id: editor_plugin_src.js 201 2007-02-12 15:56:56Z spocke $
  *
  * @author Moxiecode
- * @copyright Copyright © 2004-2007, Moxiecode Systems AB, All rights reserved.
+ * @copyright Copyright ï¿½ 2004-2007, Moxiecode Systems AB, All rights reserved.
  */
 
 class GoogleSpell extends SpellChecker {
@@ -38,7 +38,7 @@ class GoogleSpell extends SpellChecker {
 		$matches = $this->_getMatches($lang, $word);
 
 		if (count($matches) > 0)
-			$sug = explode("\t", utf8_encode($this->_unhtmlentities($matches[0][4])));
+			$sug = explode("\t", $this->_unhtmlentities($matches[0][4]));
 
 		// Remove empty
 		foreach ($sug as $item) {
@@ -61,7 +61,7 @@ class GoogleSpell extends SpellChecker {
 
 		$header  = "POST ".$path." HTTP/1.0 \r\n";
 		$header .= "MIME-Version: 1.0 \r\n";
-		$header .= "Content-type: application/PTI26 \r\n";
+		$header .= "Content-type: application/PTI26; charset=UTF-8 \r\n";
 		$header .= "Content-length: ".strlen($xml)." \r\n";
 		$header .= "Content-transfer-encoding: text \r\n";
 		$header .= "Request-number: 1 \r\n";
