f05e22b22b08da7b20348ce1e926f5e91f6772e1
Jérôme Renard <jr@ez.no>
2010-02-15 12:10:16 +0000

    - Fixed class name in phpdoc
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@24930 a01eee8c-daba-0310-acae-fa49f3023285

--- tests/tests/kernel/datatypes/ezxmltext/handlers/output/ezxhtmlxmloutput_regression.php
+++ tests/tests/kernel/datatypes/ezxmltext/handlers/output/ezxhtmlxmloutput_regression.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the eZRoleTest class
+ * File containing the eZXHTMLXMLOutputRegression class
  *
  * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
