8f9bc2c98380ec2216489a2e18f0e4a4b4a456d9
André Rømcke <ar@ez.no>
2010-02-17 10:07:26 +0000

    - changed to use admin2 design
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/trunk@24968 a01eee8c-daba-0310-acae-fa49f3023285

--- settings/siteaccess/admin/site.ini.append.php
+++ settings/siteaccess/admin/site.ini.append.php
@@ -16,7 +16,8 @@ LoginPage=custom
 RequireUserLogin=true
 
 [DesignSettings]
-SiteDesign=admin
+SiteDesign=admin2
+AdditionalSiteDesignList[]=admin
 
 [ContentSettings]
 CachedViewPreferences[full]=admin_navigation_states=0;admin_navigation_content=0;admin_navigation_details=0;admin_navigation_languages=0;admin_navigation_locations=0;admin_navigation_relations=0;admin_navigation_roles=0;admin_navigation_policies=0;admin_navigation_content=0;admin_navigation_translations=0;admin_children_viewmode=list;admin_list_limit=1;admin_edit_show_locations=0;admin_leftmenu_width=10;admin_url_list_limit=10;admin_url_view_limit=10;admin_section_list_limit=1;admin_orderlist_sortfield=user_name;admin_orderlist_sortorder=desc;admin_search_stats_limit=1;admin_treemenu=1;admin_bookmarkmenu=1;admin_left_menu_width=13
