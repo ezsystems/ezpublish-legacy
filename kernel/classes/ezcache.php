0833c04d21cc79457b0be084ec77560edcb2442d
Ole Marius Smestad <oms@ez.no>
2010-03-08 02:56:55 +0000

    - Fixed bug #16332: In-memory cache for eZContentClass when clearing class identifier cache   (Merged from trunk (4.3.0beta2) rev. 25115)
    git-svn-id: file:///home/patrick.allaert/svn-git/ezp-repo/ezpublish/stable/4.2@25141 a01eee8c-daba-0310-acae-fa49f3023285

--- kernel/classes/ezcache.php
+++ kernel/classes/ezcache.php
@@ -496,6 +496,7 @@ class eZCache
         $fileHandler = eZClusterFileHandler::instance();
         $fileHandler->fileDelete( $cachePath, 'classidentifiers_' );
         $fileHandler->fileDelete( $cachePath, 'classattributeidentifiers_' );
+        eZContentClass::expireCache();
     }
 
     /*!
