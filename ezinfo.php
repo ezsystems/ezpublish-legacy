35ef16e72b94176a5b64e187abdd8fc4c2b987ae
Jérôme Renard <jr@ez.no>
2010-03-24 09:17:18 +0000

    - Added correct copyright headers
    git-svn-id: http://svn.projects.ez.no/ezjscore/trunk@157 94d18f98-15bb-4cfc-85c1-4239715ba301

--- ezinfo.php
+++ ezinfo.php
@@ -28,7 +28,7 @@ class ezjscoreInfo
 {
     static function info()
     {
-        $eZCopyrightString = 'Copyright (C) 1999-' . date('Y') . ' eZ Systems AS';
+        $eZCopyrightString = 'Copyright (C) 1999-2009 eZ Systems AS';
 
         return array( 'Name'      => '<a href="http://projects.ez.no/ezjscore">eZ JSCore</a> extension',
                       'Version'   => '1.1.0Alpha1',
