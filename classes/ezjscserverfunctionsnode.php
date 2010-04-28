4dba72de249acd19f63de6f17190e30afbdce6f3
Łukasz Serwatka <ls@ez.no>
2010-04-28 12:35:26 +0000

    - Added node server functions
    git-svn-id: http://svn.projects.ez.no/ezjscore/trunk@172 94d18f98-15bb-4cfc-85c1-4239715ba301

---
+++ classes/ezjscserverfunctionsnode.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * ezjscServerFunctionsNode class definition that provide node fetch functions
+ * 
+ */
+class ezjscServerFunctionsNode extends ezjscServerFunctions
+{
+    /**
+     * Returns a subtree node items for given parent node
+     *
+     * Following parameters are supported:
+     * ezjscnode::subtree::parent_node_id::limit::offset::sort::order
+     * 
+     * @static
+     * @param mixed $args
+     * @return array
+     */
+    public static function subTree( $args )
+    {
+        $parentNodeID = isset( $args[0] ) ? $args[0] : null;
+        $limit = isset( $args[1] ) ? $args[1] : 25;
+        $offset = isset( $args[2] ) ? $args[2] : 0;
+        $sort = isset( $args[3] ) ? $args[3] : 'published';
+        $order = isset( $args[4] ) ? $args[4] : false;
+
+        if ( !$parentNodeID )
+        {
+            throw new ezcBaseFunctionalityNotSupportedException( 'Fetch node list', 'Parent node id is not valid' );
+        }
+
+        $node = eZContentObjectTreeNode::fetch( $parentNodeID );
+        if ( !$node instanceOf eZContentObjectTreeNode )
+        {
+            throw new ezcBaseFunctionalityNotSupportedException( 'Fetch node list', "Parent node '$parentNodeID' is not valid" );
+        }
+
+        $params = array( 'Depth' => 1,
+                         'Limit' => $limit,
+                         'Offset' => $offset,
+                         'SortBy' => array( array( $sort, $order ) ),
+                         'DepthOperator' => 'eq',
+                         'AsObject' => true );
+
+       // fetch nodes and total node count
+        $count = $node->subTreeCount( $params );
+        if ( $count )
+        {
+            $nodeArray = $node->subTree( $params );
+        }
+        else
+        {
+            $nodeArray = false;
+        }
+
+        // generate json response from node list
+        if ( $nodeArray )
+        {
+            $list = ezjscAjaxContent::nodeEncode( $nodeArray, array( 'fetchChildrenCount' => true, 'loadImages' => false ), 'raw' );
+        }
+        else
+        {
+            $list = array();
+        }
+
+        return array( 'parent_node_id' => $parentNodeID,
+                      'count' => count( $nodeArray ),
+                      'total_count' => (int)$count,
+                      'list' => $list,
+                      'limit' => $limit,
+                      'offset' => $offset,
+                      'sort' => $sort,
+                      'order' => $order );
+    }
+}
+
+?>
