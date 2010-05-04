3e60107ef57321c8945a82aff31ef3127afde1cc
André Rømcke <ar@ez.no>
2010-05-04 15:27:34 +0000

    - Fixed version number (currently 5.2.0alpha2)
    git-svn-id: http://svn.projects.ez.no/ezoe/trunk@222 0f997322-f765-4293-a636-cd5f354860a4

--- ezinfo.php
+++ ezinfo.php
@@ -31,7 +31,7 @@ class ezoeInfo
         $eZCopyrightString = 'Copyright (C) 1999-2010 eZ Systems AS';
 
         return array( 'Name'      => '<a href="http://projects.ez.no/ezoe">eZ Online Editor</a> extension',
-                      'Version'   => '5.2.0alpha1',
+                      'Version'   => '5.2.0alpha2',
                       'Copyright' => $eZCopyrightString,
                       'License'   => 'GNU General Public License v2.0',
                       'Includes the following third-party software' => array( 'Name' => 'TinyMce Javascript HTML WYSIWYG editor',
