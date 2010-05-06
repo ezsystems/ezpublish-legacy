dca58be37c61c73e06701b6eed0c3a6ef1628a56
Łukasz Serwatka <ls@ez.no>
2010-05-06 12:49:20 +0000

    - Added updatepriority
    git-svn-id: http://svn.projects.ez.no/ezjscore/trunk@186 94d18f98-15bb-4cfc-85c1-4239715ba301

--- classes/ezjscserverfunctionsnode.php
+++ classes/ezjscserverfunctionsnode.php
@@ -100,6 +100,46 @@ class ezjscServerFunctionsNode extends ezjscServerFunctions
 
         return $sortKey;
     }
+
+    /**
+     * Updating priority sorting for given node
+     * 
+     * @param mixed $args
+     * @return array
+     */
+    public static function updatePriority( $args )
+    {
+        $http = eZHTTPTool::instance();
+
+        if ( !$http->hasPostVariable('ContentNodeID') 
+                || !$http->hasPostVariable('PriorityID')
+                    || !$http->hasPostVariable('Priority') )
+        {
+            return array();
+        }
+
+        $contentNodeID = $http->postVariable('ContentNodeID');
+        $priorityArray = $http->postVariable('Priority');
+        $priorityIDArray = $http->postVariable('PriorityID');
+        
+        if ( eZOperationHandler::operationIsAvailable( 'content_updatepriority' ) )
+        {
+            $operationResult = eZOperationHandler::execute( 'content', 'updatepriority',
+                                                             array( 'node_id' => $contentNodeID,
+                                                                    'priority' => $priorityArray,
+                                                                    'priority_id' => $priorityIDArray ), null, true );
+        }
+        else
+        {
+            eZContentOperationCollection::updatePriority( $contentNodeID, $priorityArray, $priorityIDArray );
+        }
+
+        if ( $http->hasPostVariable( 'ContentObjectID' ) )
+        {
+            $objectID = $http->postVariable( 'ContentObjectID' );
+            eZContentCacheManager::clearContentCache( $objectID );
+        }
+    }
 }
 
 ?>
