6f2962b1fe19ead52dbbf384bbfcfcc194283233
Bertrand Dunogier <bd@ez.no>
2010-05-21 12:02:02 +0200

    - Added a few missing files

---
+++ content_location.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * File containing the ezpContentLocation class
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package API
+ */
+
+/**
+ * This class is used to manipulate a specific content location
+ * @package API
+ */
+class ezpContentLocation extends ezpLocation
+{
+
+}
+?>
---
+++ location.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * File containing the ezpLocation class
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package API
+ */
+
+/**
+ * This class is used to manipulate locations
+ * @package API
+ */
+class ezpLocation
+{
+
+}
+?>
