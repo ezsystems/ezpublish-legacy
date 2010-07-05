e4a781f9bd08f5c50ce4c66ecfb09744fe3f6c27
André Rømcke <ar@ez.no>
2010-07-05 21:57:28 +0200

    Cleanup ezdatepicker slightly, removing handleSelect from global scope

--- design/admin2/javascript/ezdatepicker.js
+++ design/admin2/javascript/ezdatepicker.js
@@ -1,27 +1,6 @@
-// JavaScript Document
-function handleSelect(type,args,obj) {
-    var dates = args[0];
-    var date = dates[0];
-    var year = date[0], month = date[1], day = date[2];
-
-    var objIDArray = obj.id.split( '_' );
-    var id = objIDArray[3];
-    var datatype = objIDArray[1];
-    var base = objIDArray[0];
-
-    var txtYear = document.getElementsByName( base + '_' + datatype + '_year_' + id );
-    txtYear[0].value = year;
-
-    var txtMonth = document.getElementsByName( base + '_' + datatype + '_month_' + id );
-    txtMonth[0].value = month;
-
-    var txtDay = document.getElementsByName( base + '_' + datatype + '_day_' + id );
-    txtDay[0].value = day;
-
-    window['cal'+id].hide();
-}
-
-function showDatePicker( base, id, datatype ) {
+// Date picker functions for YUI Calendar
+function showDatePicker( base, id, datatype )
+{
     var calIconID = base + '_' + datatype + '_cal_' + id;
     var calContainerID = base + '_' + datatype + '_cal_container_' + id;
     var calContainer = document.getElementById( calContainerID );
@@ -36,5 +15,21 @@ function showDatePicker( base, id, datatype ) {
                                                                                               mindate: "1/1/1970",
                                                                                               LOCALE_WEEKDAYS: "medium" } );
     window['cal'+id].render();
-    window['cal'+id].selectEvent.subscribe( handleSelect, window['cal'+id], true );
+    window['cal'+id].selectEvent.subscribe( function( type, args, obj )
+    {
+        var dates = args[0], date = dates[0], year = date[0], month = date[1], day = date[2];
+
+        var idArray = obj.id.split( '_' ), id = idArray[3], datatype = idArray[1], base = idArray[0];
+
+        var txtYear = document.getElementsByName( base + '_' + datatype + '_year_' + id );
+        txtYear[0].value = year;
+
+        var txtMonth = document.getElementsByName( base + '_' + datatype + '_month_' + id );
+        txtMonth[0].value = month;
+
+        var txtDay = document.getElementsByName( base + '_' + datatype + '_day_' + id );
+        txtDay[0].value = day;
+
+        this.hide();
+    }, window['cal'+id], true );
 }
