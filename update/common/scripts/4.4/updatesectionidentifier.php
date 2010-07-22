e64002c193867bc182dde5992dc641cbc50c6b87
Łukasz Serwatka <ls@ez.no>
2010-07-22 10:43:34 +0200

    - Fixed bug #17020: Provide a upgrade script for section identifiers

---
+++ update/common/scripts/4.4/updatesectionidentifier.php
@@ -0,0 +1,64 @@
+#!/usr/bin/env php
+<?php
+/**
+ * File containing the section identifier upgrade script.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package update
+ */
+
+require 'autoload.php';
+
+$script = eZScript::instance( array( 'description' => 'eZ Publish section identifier update script. ' .
+                                                      'This script will update existing sections with missing identifiers.',
+                                     'use-session' => false,
+                                     'use-modules' => false,
+                                     'use-extension' => false ) );
+$script->startup();
+$options = $script->getOptions( '', '', array( '-q' => 'Quiet mode' ) );
+
+$script->initialize();
+$isQuiet = $script->isQuiet();
+
+$cli = eZCLI::instance();
+$trans = eZCharTransform::instance();
+
+// Fetch 50 items per iteration
+$limit = 50;
+$offset = 0;
+
+do
+{
+    // Fetch items with empty section_identifier
+    $rows = eZSection::fetchFilteredList( array( 'section_identifier' => '' ), $offset, $limit );
+
+    if ( !$rows )
+        break;
+
+    foreach ( $rows as $row )
+    {
+        if ( $row->attribute( 'section_identifier' ) == '' )
+        {
+            // Create a new section identifier with NAME_ID pattern
+            $name = $row->attribute( 'name' );
+            $identifier = $trans->transformByGroup( $name, 'identifier' ) . '_' . $row->attribute( 'id' );
+
+            // Set new section identifier and store it
+            $row->setAttribute( 'section_identifier', $identifier );
+            $row->store();
+
+            if ( !$isQuiet )
+                $cli->output( "Setting identifier '{$identifier}' for section '{$name}'" );
+        }
+    }
+
+} while ( true );
+
+if ( !$isQuiet )
+    $cli->output( "Update has been completed." );
+
+$script->shutdown();
+
+?>
--- update/database/postgresql/4.4/unstable/dbupdate-4.4.0alpha4-to-4.4.0alpha5.sql
+++ update/database/postgresql/4.4/unstable/dbupdate-4.4.0alpha4-to-4.4.0alpha5.sql
@@ -1,4 +1,3 @@
 UPDATE ezsite_data SET value='4.4.0alpha5' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 ALTER TABLE ezsection ADD COLUMN section_identifier character varying(255);
-UPDATE ezsection set section_identifier = LOWER(name);
