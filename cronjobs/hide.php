24210d24d9db80c436518539c27db2fc86ec7a6e
Bertrand Dunogier <bd@ez.no>
2010-08-04 17:17:49 +0200

    - Implemented enhancement #014831: hide cronjob is totally useless for big sites   Slightly modified version of Georg Franz's contribution.

--- cronjobs/hide.php
+++ cronjobs/hide.php
@@ -1,93 +1,85 @@
 <?php
-//
-// Created on: <21-Jan-05 16:00:52 kk>
-//
-// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
-// SOFTWARE NAME: eZ Publish
-// SOFTWARE RELEASE: 4.1.x
-// COPYRIGHT NOTICE: Copyright (C) 1999-2010 eZ Systems AS
-// SOFTWARE LICENSE: GNU General Public License v2.0
-// NOTICE: >
-//   This program is free software; you can redistribute it and/or
-//   modify it under the terms of version 2.0  of the GNU General
-//   Public License as published by the Free Software Foundation.
-//
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of version 2.0 of the GNU General
-//   Public License along with this program; if not, write to the Free
-//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//   MA 02110-1301, USA.
-//
-//
-// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
-//
-
-/*! \file
-*/
+/**
+ * File containing the hide.php cronjob.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package kernel
+ */
 
 $ini = eZINI::instance( 'content.ini' );
 $rootNodeIDList = $ini->variable( 'HideSettings','RootNodeList' );
 $hideAttributeArray = $ini->variable( 'HideSettings', 'HideDateAttributeList' );
-$hideClasses = array_keys( $hideAttributeArray );
 
 $currrentDate = time();
 
 $offset = 0;
-$limit = 20;
+$limit = 50;
 
+$hiddenNodesParams = array(
+    'LoadDataMap' => false,
+    'Limit' => $limit,
+    'Offset' => $offset,
+    'SortBy' => array( array( 'published', true ) ) );
+
+$newMemoryUsage = $oldMemoryUsage = 0;
 foreach( $rootNodeIDList as $nodeID )
 {
     $rootNode = eZContentObjectTreeNode::fetch( $nodeID );
-
-    while( true )
+    if ( !$isQuiet )
     {
-        $nodeArray = $rootNode->subTree( array( 'ClassFilterType' => 'include',
-                                                'ClassFilterArray' => $hideClasses,
-                                                'Offset' => $offset,
-                                                'Limit' => $limit ) );
-        if ( !$nodeArray ||
-             count( $nodeArray ) == 0 )
+        $cli->output( 'Hiding content of node "' . $rootNode->attribute( 'name' ) . '" (' . $nodeID . ')' );
+        $cli->output();
+    }
+    foreach ( $hideAttributeArray as $hideClass => $attributeIdentifier )
+    {
+        $offset = 0;
+        $counter = 0;
+        $countParams = array( 'ClassFilterType' => 'include',
+                              'ClassFilterArray' => array( $hideClass ),
+                              'Limitation' => array(),
+                              'IgnoreVisibility' => false,
+                              'AttributeFilter' => array( 'and',
+                                  array( "{$hideClass}/{$attributeIdentifier}", '<=', $currrentDate ),
+                                  array( "{$hideClass}/$attributeIdentifier", '>', 0 ) ) );
+
+        $nodeArrayCount = $rootNode->subTreeCount( $countParams );
+        if ( $nodeArrayCount > 0 )
         {
-            break;
-        }
-
-        $offset += $limit;
-
-        foreach ( $nodeArray as $node )
-        {
-            $dataMap = $node->attribute( 'data_map' );
-
-            $dateAttributeName = $hideAttributeArray[$node->attribute( 'class_identifier' )];
-
-            if ( !$dateAttributeName )
+            if ( !$isQuiet )
             {
-                continue;
+                $cli->output( "Hiding {$nodeArrayCount} node(s) of class {$hideClass}." );
             }
 
-            $dateAttribute = $dataMap[$dateAttributeName];
-
-            if ( $dateAttribute === null || !$dateAttribute->hasContent() )
+            do
             {
-                continue;
-            }
+                $nodeArray = $rootNode->subTree( $hiddenNodesParams );
 
-            $date = $dateAttribute->content();
-            $retractDate = $date->attribute( 'timestamp' );
-            if ( $retractDate > 0 && $retractDate < $currrentDate && !$node->attribute( 'is_hidden' ) )
-            {
-                eZContentObjectTreeNode::hideSubTree( $node );
-                if ( !$isQuiet )
+                foreach ( $nodeArray as $node )
                 {
-                    $cli->output( 'Hiding node : ' . $node->attribute( 'node_id' ) );
+                    eZContentObjectTreeNode::hideSubTree( $node );
+                    if ( !$isQuiet )
+                    {
+                        $cli->output( 'Hiding node: "' . $node->attribute( 'name' ) . '" (' . $node->attribute( 'node_id' ) . ')' );
+                    }
                 }
+                $counter += $limit;
+
+                // clear memory after every batch
+                eZContentObject::clearCache();
+            } while( is_array( $nodeArray ) and count( $nodeArray ) > 0 );
+
+            if ( !$isQuiet )
+            {
+                $cli->output();
             }
         }
     }
+    if ( !$isQuiet )
+    {
+        $cli->output();
+    }
 }
-
 
 ?>
