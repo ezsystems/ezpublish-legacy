efbcea7b99114281aec615fefc1c7c6b797bf3f8
Ole Marius Smestad <oms@ez.no>
2010-08-06 06:37:34 +0200

    Added: New table definition for PostgreSQL

--- sql/postgresql/schema.sql
+++ sql/postgresql/schema.sql
@@ -21,3 +21,20 @@ CREATE TABLE ezprest_clients (
 ALTER TABLE ONLY ezprest_clients ADD CONSTRAINT ezprest_clients_pkey PRIMARY KEY ( id );
 CREATE INDEX client_id ON ezprest_clients USING btree ( client_id );
 CREATE UNIQUE INDEX client_id_UNIQUE ON ezprest_clients USING btree ( client_id, "version" );
+
+CREATE SEQUENCE ezprest_authorized_clients_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
+CREATE TABLE ezprest_authorized_clients (
+    created integer,
+    id integer DEFAULT nextval('ezprest_authorized_clients_s'::text) NOT NULL,
+    rest_client_id integer,
+    user_id integer
+);
+
+CREATE INDEX client_user ON ezprest_authorized_clients USING btree (rest_client_id, user_id);
+ALTER TABLE ONLY ezprest_authorized_clients ADD CONSTRAINT ezprest_authorized_clients_pkey PRIMARY KEY (id);
