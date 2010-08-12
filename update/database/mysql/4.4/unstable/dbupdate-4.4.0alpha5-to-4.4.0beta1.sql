eb0f075594d55fc230ebf3b146fcad3585ae879d
Ole Marius Smestad <oms@ez.no>
2010-08-12 09:33:34 +0200

    Updated: Configured branch for beta1

--- update/database/mysql/4.4/unstable/dbupdate-4.4.0alpha5-to-4.4.0beta1.sql
+++ update/database/mysql/4.4/unstable/dbupdate-4.4.0alpha5-to-4.4.0beta1.sql
@@ -1,5 +1,5 @@
 SET storage_engine=InnoDB;
-UPDATE ezsite_data SET value='4.4.0alpha6' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='4.4.0beta1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 ALTER TABLE ezinfocollection_attribute ADD INDEX ezinfocollection_attr_cca_id ( contentclass_attribute_id );
 ALTER TABLE ezinfocollection_attribute ADD INDEX ezinfocollection_attr_coa_id ( contentobject_attribute_id );
