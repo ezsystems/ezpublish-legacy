3daca5cae7075f07f0dd1677e6685bd0f41c3b69
André Rømcke <ar@ez.no>
2010-08-24 09:17:52 +0000

    - Simplify extension loading by taking advantage of ezpExtensionHelper in ezp master tests/
    git-svn-id: http://svn.projects.ez.no/ezoe/trunk@275 0f997322-f765-4293-a636-cd5f354860a4

--- tests/suite.php
+++ tests/suite.php
@@ -25,28 +25,17 @@ class eZOeTestSuite extends ezpDatabaseTestSuite
 
     public function setUp()
     {
-        // make sure ezoe settings are read
-        $ini = eZINI::instance();
-        $activeExtensions = $ini->variable( 'ExtensionSettings', 'ActiveExtensions' );
-        if ( !in_array( 'ezoe', $activeExtensions, true ) )
-        {
-            $this->ezoeIsLoaded = true;
-            $activeExtensions[] = 'ezoe';
-            $ini->setVariable( 'ExtensionSettings', 'ActiveExtensions', $activeExtensions );
-            $extensionDirectory = eZExtension::baseDirectory();
-            $ini->prependOverrideDir( $extensionDirectory . '/ezoe/settings', true, 'extension:ezoe', 'extension' );
-        }
+        // make sure ezoe is enabled and settings are read
+        $this->ezoeWasLoaded = ezpExtensionHelper::load( 'ezoe' );
+
         parent::setUp();
     }
-    
+
     public function tearDown()
     {
-        if ( $this->ezoeIsLoaded )
+        if ( $this->ezoeWasLoaded )
         {
-            $ini = eZINI::instance();
-            $activeExtensions = $ini->variable( 'ExtensionSettings', 'ActiveExtensions' );
-            $ini->setVariable( 'ExtensionSettings', 'ActiveExtensions', array_diff( $activeExtensions, array('ezoe') ) );
-            $ini->removeOverrideDir( 'extension:ezoe' );
+            $this->ezoeWasLoaded = !ezpExtensionHelper::unload( 'ezoe' );
         }
         parent::tearDown();
     }
