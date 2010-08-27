51d004a88ea50d49d73f3750206157e6d36211f4
Bertrand Dunogier <bd@ez.no>
2010-08-27 10:39:20 +0200

    Fixed a bad copy/paste error in the beta1 to beta2 SQL update script

--- update/database/mysql/4.4/unstable/dbupdate-4.4.0beta1-to-4.4.0beta2.sql
+++ update/database/mysql/4.4/unstable/dbupdate-4.4.0beta1-to-4.4.0beta2.sql
@@ -3,4 +3,4 @@ UPDATE ezsite_data SET value='4.4.0beta2' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 ALTER TABLE ezpreferences MODIFY COLUMN value longtext;
 ALTER TABLE ezpolicy ADD original_id INT(11);
-ALTER TABLE ezinfocollection_attribute ADD INDEX ezoriginal_policy_id ( original_id );
+ALTER TABLE ezpolicy ADD INDEX ezoriginal_policy_id ( original_id );
