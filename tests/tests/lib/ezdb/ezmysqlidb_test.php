ea08662ce49ba990e1636fb94800c937e00288cb
Bertrand Dunogier <bd@ez.no>
2010-08-27 09:46:08 +0200

    Coding standards & comments

--- tests/tests/lib/ezdb/ezmysqlidb_test.php
+++ tests/tests/lib/ezdb/ezmysqlidb_test.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * File containing the eZMySQLiDBTest class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package tests
+ */
 class eZMySQLiDBTest extends ezpDatabaseTestCase
 {
     function __construct()
@@ -14,6 +21,7 @@ class eZMySQLiDBTest extends ezpDatabaseTestCase
 
         parent::setUp();
 
+        // clean up the database so that the tests are independant from the ezp database
         ezpTestDatabaseHelper::clean( $this->sharedFixture );
     }
 
@@ -37,6 +45,9 @@ class eZMySQLiDBTest extends ezpDatabaseTestCase
         $this->assertEquals( 'eztestfk_3_fk1', $foreignKeys[1]['keys'][0] );
     }
 
+    /**
+    * Helper function that creates 3 tables, two of them having foreign keys towards the first one
+    */
     public function createFKTables()
     {
         $db = $this->sharedFixture;
