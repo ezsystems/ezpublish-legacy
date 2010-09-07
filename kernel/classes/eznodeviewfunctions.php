d54175c39a17dec5a539292872ed8a365de05940
André Rømcke <ar@ez.no>
2010-09-07 23:51:47 +0200

    Add file_exists calls where there where @filemtime before as it did cause a warning

--- kernel/classes/eznodeviewfunctions.php
+++ kernel/classes/eznodeviewfunctions.php
@@ -410,7 +410,13 @@ class eZNodeviewfunctions
                 {
                     foreach ( $Result['template_list'] as $templateFile )
                     {
-                        if ( filemtime( $templateFile ) > $mtime )
+                        if ( !file_exists( $templateFile ) )
+                        {
+                            $cacheExpired = true;
+                            $expiryReason = "Content cache is expired by template file '" . $templateFile . "', it does not exist anymore";
+                            break;
+                        }
+                        else if ( filemtime( $templateFile ) > $mtime )
                         {
                             $cacheExpired = true;
                             $expiryReason = "Content cache is expired by template file '" . $templateFile . "'";
