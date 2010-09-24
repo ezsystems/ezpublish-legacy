cef9effd064a97f447e933775415c78345c7ab20
André Rømcke <ar@ez.no>
2010-09-24 13:26:29 +0000

    - missing license header
    git-svn-id: http://svn.projects.ez.no/ezoe/trunk@312 0f997322-f765-4293-a636-cd5f354860a4

--- autoloads/eztemplateautoload.php
+++ autoloads/eztemplateautoload.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Template autoload definition for eZ Online Editor
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ *
+ */
 
 $eZTemplateOperatorArray = array();
 
