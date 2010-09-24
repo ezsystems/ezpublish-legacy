b426ec414a312daae6bfd7fbbf2a9fe7004b54a7
André Rømcke <ar@ez.no>
2010-09-24 13:21:41 +0000

    - missing license header
    git-svn-id: http://svn.projects.ez.no/ezjscore/trunk@239 94d18f98-15bb-4cfc-85c1-4239715ba301

--- classes/ezjscserverfunctionsnode.php
+++ classes/ezjscserverfunctionsnode.php
@@ -1,4 +1,32 @@
 <?php
+//
+// Definition of ezjscServerFunctionsNode class
+//
+// Created on: <01-Jun-2010 00:00:00 ls>
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: eZ JSCore extension for eZ Publish
+// SOFTWARE RELEASE: 1.x
+// COPYRIGHT NOTICE: Copyright (C) 1999-2010 eZ Systems AS
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
 
 /**
  * ezjscServerFunctionsNode class definition that provide node fetch functions
