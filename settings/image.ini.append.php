7c89ffa5f8e7d1e27edf94b93413b96fd460b84f
André Rømcke <ar@ez.no>
2010-09-25 15:18:26 +0000

    - Fixed file names to please nit pickers (read: waste of time since these files does not contain anything secret)
    git-svn-id: http://svn.projects.ez.no/ezoe/trunk@314 0f997322-f765-4293-a636-cd5f354860a4

--- settings/image.ini.append.php
+++ settings/image.ini.append.php
@@ -1,4 +1,5 @@
-#?ini charset="utf-8"?
+<?php /* #?ini charset="utf-8"?
+# eZ publish configuration file for images
 
 
 ## Example for making image size name more human readable in OE gui
@@ -14,3 +15,5 @@
 #Reference=
 #Filters[]
 #Filters[]=geometry/scaledownonly=360;440
+
+*/ ?>
