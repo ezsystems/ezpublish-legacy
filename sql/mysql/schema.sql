849500541e69082bbb9da59bbcc1de6d0cb0fcb1
Łukasz Serwatka <ls@ez.no>
2010-10-19 11:19:45 +0200

    - Fixed MySQL schema

--- sql/mysql/schema.sql
+++ sql/mysql/schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE "ezprest_token" (
+CREATE TABLE `ezprest_token` (
   `id` varchar(200) NOT NULL,
   `expirytime` bigint(20) NOT NULL DEFAULT '0',
   `client_id` varchar(200) NOT NULL,
-  `scope` varchar(200) DEFAULT 'NULL',
-  PRIMARY KEY ("id"),
+  `scope` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `token_client_id` (`client_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
