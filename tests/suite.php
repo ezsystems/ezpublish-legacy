ba048688923ee09d03700dbfee51f0a34cb8dfdd
Ole Marius Smestad <oms@ez.no>
2010-11-30 05:43:39 +0100

    Added tests for new request parser

---
+++ tests/suite.php
@@ -0,0 +1,34 @@
+<?php
+
+class ezpRestTestSuite extends ezpTestSuite
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setName( "eZ Publish REST test suite" );
+        $this->addTestSuite( 'ezpRestHttpRequestParserRegression' );
+    }
+
+    public static function suite()
+    {
+        return new self();
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // make sure extension is enabled and settings are read
+        // give a warning if it is already enabled
+        if ( !ezpExtensionHelper::load( 'rest' ) )
+            trigger_error( __METHOD__ . ': extension is already loaded, this hints about missing cleanup in other tests that uses it!', E_USER_WARNING );
+    }
+
+    public function tearDown()
+    {
+        ezpExtensionHelper::unload( 'rest' );
+        parent::tearDown();
+    }
+}
+
+?>
