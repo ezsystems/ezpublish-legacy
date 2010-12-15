616066f6e247b08e1eca76e9c312b7d447d15f2b
André Rømcke <ar@ez.no>
2010-12-15 10:26:31 +0100

    Fixed #017550: Login unsuccessful after upgrade to 4.4 with DebugByUser enabled
    Code relies on session, and does not work if executed before session start anymore thaks to session rewrite(specifically the changes done in eZUser::instance()).

--- index.php
+++ index.php
@@ -348,9 +348,6 @@ $access = eZSiteAccess::match( $uri,
 $access = eZSiteAccess::change( $access );
 eZDebugSetting::writeDebug( 'kernel-siteaccess', $access, 'current siteaccess' );
 
-// Check for activating Debug by user ID (Final checking. The first was in eZDebug::updateSettings())
-eZDebug::checkDebugByUser();
-
 // Check for siteaccess extension
 eZExtension::activateExtensions( 'access' );
 // Siteaccess extension check end
@@ -449,6 +446,12 @@ if ( $dbRequired )
 // pre check, RequireUserLogin & FORCE_LOGIN related so needs to be after session init
 if ( !isset( $check ) )
     $check = eZUserLoginHandler::preCheck( $siteBasics, $uri );
+
+/**
+ * Check for activating Debug by user ID (Final checking. The first was in eZDebug::updateSettings())
+ * @uses eZUser::instance() So needs to be executed after eZSession::start()|lazyStart()
+ */
+eZDebug::checkDebugByUser();
 
 // Initialize with locale settings
 $locale = eZLocale::instance();
