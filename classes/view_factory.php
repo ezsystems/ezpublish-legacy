5aa3733419c932a672bcc4abec99e96e6e28e293
Lukasz Serwatka <ls@ez.no>
2010-12-15 12:37:55 +0100

    Updated class license header

--- classes/view_factory.php
+++ classes/view_factory.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the ezpContentRestController class.
+ * File containing the ezpRestViewFactory class.
  *
  * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
