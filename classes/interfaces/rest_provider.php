e8e8c8876c7065e5cb1dd7a17762939d5da1d510
Ole Marius Smestad <oms@ez.no>
2010-12-16 13:05:41 +0100

    Added initial version of ezpRestProvider factory.
    Pair programmed by:
    Łukasz Serwatka <ls@ez.no>
    Ole Marius Smestad <oms@ez.no>

---
+++ classes/interfaces/rest_provider.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * File containing the ezpRestProviderInterface interface.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ *
+ */
+
+interface ezpRestProviderInterface {}
