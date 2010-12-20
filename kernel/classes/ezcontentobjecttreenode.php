43c624eeccf203348205a883829b5d1b2f45c698
xc <xc@ez.no>
2010-12-20 15:30:11 +0800

    Added: missed IgnoreVisibility parameter (bug 017743) (cherry picked from commit 373840ae9f2d2b5a586b7f9b9307957b7e97496e)

--- kernel/classes/ezcontentobjecttreenode.php
+++ kernel/classes/ezcontentobjecttreenode.php
@@ -3798,6 +3798,7 @@ class eZContentObjectTreeNode extends eZPersistentObject
         $totalChildCount = 0;
         $totalLoneNodeCount = 0;
         $canRemoveAll = true;
+        $hasPendingObject = false;
 
         $db = eZDB::instance();
         $db->begin();
@@ -3807,7 +3808,6 @@ class eZContentObjectTreeNode extends eZPersistentObject
 
         foreach ( $deleteIDArray as $deleteID )
         {
-            $hasPendingObject = false;
             $node = eZContentObjectTreeNode::fetch( $deleteID );
             if ( $node === null )
                 continue;
@@ -3870,7 +3870,7 @@ class eZContentObjectTreeNode extends eZPersistentObject
                     if ( $accessResult['accessWord'] == 'limited' )
                     {
                         $limitationList = $accessResult['policies'];
-                        $removeableChildCount = $node->subTreeCount( array( 'Limitation' => $limitationList ) );
+                        $removeableChildCount = $node->subTreeCount( array( 'Limitation' => $limitationList, 'IgnoreVisibility' => true ) );
                         $canRemoveSubtree = ( $removeableChildCount == $childCount );
                         $canRemove = $canRemoveSubtree;
                     }
