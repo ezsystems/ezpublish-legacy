1321c27b6c5f37bb3801dfa6c64b08d05dbf3701
Ole Marius Smestad <oms@ez.no>
2010-12-21 11:24:40 +0100

    Added model for ezpRestAuthcode

--- sql/mysql/schema.sql
+++ sql/mysql/schema.sql
@@ -8,3 +8,14 @@ CREATE TABLE `ezprest_token` (
   PRIMARY KEY (`id`),
   KEY `token_client_id` (`client_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ezprest_authcode` (
+  `id` varchar(200) NOT NULL,
+  `expirytime` bigint(20) NOT NULL DEFAULT '0',
+  `client_id` varchar(200) NOT NULL,
+  `user_id` varchar(200) NOT NULL,
+  `scope` varchar(200) DEFAULT NULL,
+
+  PRIMARY KEY (`id`),
+  KEY `authcode_client_id` (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- sql/postgresql/schema.sql
+++ sql/postgresql/schema.sql
@@ -12,3 +12,16 @@ CREATE TABLE ezprest_token (
 ALTER TABLE ONLY ezprest_token ADD CONSTRAINT ezprest_token_pkey PRIMARY KEY ( id );
 -- Unsure this is need as of yet.
 CREATE INDEX token_client_id ON ezprest_token USING btree ( client_id );
+
+CREATE TABLE ezprest_authcode (
+    -- with sha1 40 would presumably be enough
+    id character varying(200) NOT NULL,
+    expirytime integer DEFAULT 0 NOT NULL,
+    client_id character varying(200) NOT NULL,
+    user_id integer,
+    -- datatype?
+    scope character varying(200) DEFAULT NULL
+);
+
+ALTER TABLE ONLY ezprest_authcode ADD CONSTRAINT ezprest_authcode_pkey PRIMARY KEY ( id );
+CREATE INDEX authcode_client_id ON ezprest_authcode USING btree ( client_id );
