065b5ebf85c3fe26c23973a1727e0ef8dbbe8c2c
Christian Pfeffer Gjengedal <cpg@ez.no>
2011-01-11 15:59:21 +0100

    Asynchronous publishing: additional design keys and navigation part based on the section

--- kernel/content/queued.php
+++ kernel/content/queued.php
@@ -24,13 +24,20 @@ $contentObject = eZContentObject::fetch( $pContentObjectId );
 $contentObjectVersion = $contentObject->version( $pVersion );
 $nodeAssignments = $contentObjectVersion->attribute( 'node_assignments' );
 $contentClass = eZContentClass::fetch( $contentObject->attribute( 'contentclass_id' ) );
+$section = eZSection::fetch( $contentObject->attribute( 'section_id' ) );
+$navigationPartIdentifier = $section->attribute( 'navigation_part_identifier' );
 
 $res = eZTemplateDesignResource::instance();
 $designKeys = array( array( 'object', $contentObject->attribute( 'id' ) ), // Object ID
-                     array( 'node', $virtualNodeID ), // Node id
+                     array( 'node', $virtualNodeID ), // Fake node id
                      array( 'remote_id', $contentObject->attribute( 'remote_id' ) ),
                      array( 'class', $contentClass->attribute( 'id' ) ), // Class ID
-                     array( 'class_identifier', $contentClass->attribute( 'identifier' ) ) // Class identifier
+                     array( 'class_identifier', $contentClass->attribute( 'identifier' ) ), // Class identifier
+                     array( 'class_group', $contentObject->attribute( 'match_ingroup_id_list' ) ),
+                     array( 'state', $contentObject->attribute( 'state_id_array' ) ),
+                     array( 'state_identifier', $contentObject->attribute( 'state_identifier_array' ) ),
+                     array( 'section', $contentObject->attribute( 'section_id' ) ),
+                     array( 'section_identifier', $section->attribute( 'identifier' ) )
               );
 $res->setKeys( $designKeys );
 
@@ -41,8 +48,14 @@ $tpl->setVariable( 'content_object', $contentObject );
 $tpl->setVariable( 'content_object_version', $contentObjectVersion );
 $tpl->setVariable( 'content_class', $contentClass );
 
-$Result['path'] = array( array( 'url' => false,
-                                'text' => ezpI18n::tr( 'kernel/content', 'Content queued for publishing' ) ) );
+$Result['path'] = array( array( 'text' => ezpI18n::tr( 'kernel/content', 'Content' ),
+                                 'url' => false ),
+                          array( 'text' => ezpI18n::tr( 'kernel/content', 'Publishing queue' ),
+                                 'url' => false ),
+                          array( 'text' => $contentObject->attribute( 'name' ),
+                                 'url' => false ) );
+
 $Result['content'] = $tpl->fetch( 'design:content/queued.tpl' );
+$Result['navigation_part'] = $navigationPartIdentifier;
 return $Result;
 ?>
