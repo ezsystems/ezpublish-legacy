91deaec6a34875ae67f6281324f7f73fda3f9723
Christian Pfeffer Gjengedal <cpg@ez.no>
2011-01-12 15:02:49 +0100

    Removed old header info

--- bin/php/publishingqueueprocessor.php
+++ bin/php/publishingqueueprocessor.php
@@ -1,20 +1,11 @@
 #!/usr/bin/env php
 <?php
 /**
-* File containing the contentwaittimeout.php script
 *
 * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
 * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
 * @version //autogentag//
 * @package
-*/
-
-/**
-* This script starts parallel publishing processes in order to trigger lock wait timeouts
-* Launch it using $./bin/php/ezexec.phhp contentwaittimeout.php
-*
-* To customize the class, parent node or concurrency level, modify the 3 variables below.
-* @package tests
 */
 
 
