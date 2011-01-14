ezsystems/ezpublish-legacy

commit 88f609940b0e9a39faa8b1db96f7e2a7c42d535d

Fixed #017594: embeding image not working correctly when more than one image attribute in a class

design/standard/javascript/ezoe/popup_utils.js
@@ -809,11 +809,12 @@ var eZOEPopupUtils = {
                    tr.appendChild( td );
                    
                    td = document.createElement("td");
-                   if ( n.image_attributes && n.data_map[ n.image_attributes[0] ] && n.data_map[ n.image_attributes[0] ].content['small'] )
+                   var imageIndex = eZOEPopupUtils.indexOfImage( n, 'small' );
+                   if ( imageIndex !== -1 )
                    {
                        tag = document.createElement("span");
                        tag.className = 'image_preview';
-                       tag.innerHTML += ' <a href="#">' + ed.getLang('preview.preview_desc')  + '<img src="' + ed.settings.ez_root_url + n.data_map[ n.image_attributes[0] ].content['small'].url + '" /></a>';
+                       tag.innerHTML += ' <a href="#">' + ed.getLang('preview.preview_desc')  + '<img src="' + ed.settings.ez_root_url + n.data_map[ n.image_attributes[imageIndex] ].content['small'].url + '" /></a>';
                        td.appendChild( tag );
                        hasImage = true;
                    }
@@ -878,6 +879,18 @@ var eZOEPopupUtils = {
             tr.appendChild( td );
             tbody.appendChild( tr );
         } );
+    },
+
+    indexOfImage: function( jsonNode, alias )
+    {
+        if ( !alias ) alias = 'small';
+        var index = -1;
+        jQuery.each( jsonNode.image_attributes, function( i, attr )
+        {
+            if ( index === -1 && n.data_map[ attr ] && n.data_map[ attr ].content[ alias ] )
+                index = i;
+        });
+        return index;
     },
 
     // some reusable functions from ezcore

commit a0d72562c7ead395e5c73b843397fc41184e4685

Fixed problem with browse mode for images introduced in commit 88f609940b0e9a39faa8b1db96f7e2a7c42d535d

design/standard/javascript/ezoe/popup_utils.js
@@ -887,7 +887,7 @@ var eZOEPopupUtils = {
         var index = -1;
         jQuery.each( jsonNode.image_attributes, function( i, attr )
         {
-            if ( index === -1 && n.data_map[ attr ] && n.data_map[ attr ].content[ alias ] )
+            if ( index === -1 && jsonNode.data_map[ attr ] && jsonNode.data_map[ attr ].content[ alias ] )
                 index = i;
         });
         return index;