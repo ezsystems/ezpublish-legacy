378eafe375163557ba9cd1145ceb3b19baa69315
Bertrand Dunogier <bd@ez.no>
2011-01-14 17:56:19 +0100

    Asynchronous publishing: added (incomplete) handling of option files for the debian init.d script

---
+++ bin/shell/daemon_settings.sh
@@ -0,0 +1,7 @@
+# Edit the following variable's value to set options for the eZ Asynchronous Publishing daemon
+
+# User to run the service as, usually your webserver's user
+#RUN_AS_USER=www-data
+
+# Full path to your PHP executable. This is only required if your executable isn't set by default in the environnement
+#PHP_EXECUTABLE=/usr/bin/php
