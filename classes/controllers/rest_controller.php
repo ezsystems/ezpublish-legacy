631641543899bb69daccb1ecaf44091d9ac3e361
lolautruche <lolautruche@gmail.com>
2011-01-21 16:13:44 +0100

    * Added ezpRestMvcController abstract class, to be extended by every REST controller * Added ezpRestMvcResult class. An object of this class has to be returned in REST controllers actions

---
+++ classes/controllers/rest_controller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * File containing ezpRestMvcController class
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ */
+
+abstract class ezpRestMvcController extends ezcMvcController
+{
+    
+}
+?>
---
+++ classes/response/result.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * File containing ezpRestResult class
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ */
+
+/**
+ * This struct contains the result data to be returned by eZP REST controllers
+ */
+class ezpRestMvcResult extends ezcMvcResult
+{
+    public $responseGroups;
+    
+    /**
+     * Returns a new instance of this class with the data specified by $array.
+     *
+     * $array contains all the data members of this class in the form:
+     * array('member_name'=>value).
+     *
+     * __set_state makes this class exportable with var_export.
+     * var_export() generates code, that calls this method when it
+     * is parsed with PHP.
+     *
+     * @param array(string=>mixed) $array
+     * @return ezpRestMvcResult
+     */
+    public static function __set_state( array $array )
+    {
+        $obj = new self( $array['status'], $array['date'],
+                         $array['generator'], $array['cache'], $array['cookies'],
+                         $array['content'], $array['variables'] );
+        
+        $obj->responseGroups = $array['responseGroups'];
+        return $obj;
+    }
+}
+?>
