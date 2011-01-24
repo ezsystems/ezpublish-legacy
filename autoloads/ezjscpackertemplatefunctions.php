28ebe1f261a7a9fb144d04ab65c0c6386fcc1e5d
André Rømcke <ar@ez.no>
2011-01-24 14:27:05 +0100

    Make ::setPersistentArray() more reusable by not implicit flattening array values
    Preperation for reusing these functions in ezpagedata (ezwebin).

--- autoloads/ezjscpackertemplatefunctions.php
+++ autoloads/ezjscpackertemplatefunctions.php
@@ -32,13 +32,13 @@
  * ezjsPacker related template operators
  * For merging and packing javascript or stylesheet files together to reduce size and
  * number of files(as in reduces client connections).
- * 
+ *
  * ezscript[_require|_load]( array|string $scripts
  *                                   [, string $type='text/javascript'
  *                                   [, string $language='javascript'
  *                                   [, string $charset='utf-8'
  *                                   [, int $pack_level=2]]]])
- *                                   
+ *
  * ezcss[_require|_load]( array|string $css_files
  *                                   [, string $media='all'
  *                                   [, string $type='text/css'
@@ -48,10 +48,10 @@
  *
  * ezscriptfiles( array|string $scripts[, int $pack_level=2[, bool $ignore_loaded=false]] )
  * ezcssfiles( array|string $css_files[, int $pack_level=3[, bool $ignore_loaded=false]] )
- * 
+ *
  * These are alternatives to ezscript and ezcss that return array of files to be included
  * instead of generating the (x)html for them.
- * 
+ *
  * Genal note:
  * Packing has 4 levels:
  *  0 = off
@@ -64,18 +64,18 @@
  *
  * You can also use css / js generators to generate content dynamically.
  * This is better explained in ezjscore.ini[Packer_<function>]
- * 
+ *
  * Brief (ezscript|ezcss)_require + (ezscript|ezcss)_load:
  * Lets you do on demand loading of javscript and css files instead of loading
  * them on every page using JavaScriptList & CSSFileList witch tends to also
  * load files no matter what design you use.
- * 
+ *
  * (ezscript|ezcss)_require : Stores list of required files in persistent_variable hash
  * values (js|css)_files or if not defined (on non content pages) on protected variable
- * ezjscPackerTemplateFunctions::$persistentVariable so they can be loaded later 
+ * ezjscPackerTemplateFunctions::$persistentVariable so they can be loaded later
  * by (ezscript|ezcss)_load. If already loaded, then executed right away just like
  * calling (ezscript|ezcss) operators.
- * 
+ *
  * (ezscript|ezcss)_load : Packs the files you (optionally) pass to it + the files marked
  * to be loaded by (ezscript|ezcss)_require.
  */
@@ -176,7 +176,7 @@ class ezjscPackerTemplateFunctions
             {
                 if ( !isset( self::$loaded['js_files'] ) )
                 {
-                    $depend = self::setPersistentArray( 'js_files', $namedParameters['script_array'], $tpl, false, true );
+                    $depend = self::setPersistentArray( 'js_files', self::flattenArray( $namedParameters['script_array'] ), $tpl, false, true );
                     $ret = ezjscPacker::buildJavascriptTag( $depend,
                                                          $namedParameters['type'],
                                                          $namedParameters['language'],
@@ -185,21 +185,21 @@ class ezjscPackerTemplateFunctions
                     self::$loaded['js_files'] = true;
                     break;
                 }// let 'ezscript' handle loaded calls
-                else
+                elseif ( $operatorName === 'ezscript_load' )
                 {
-                    $namedParameters['script_array'] = self::setPersistentArray( 'js_files', $namedParameters['script_array'], $tpl, true, true, true );
+                    $namedParameters['script_array'] = self::setPersistentArray( 'js_files', self::flattenArray( $namedParameters['script_array'] ), $tpl, true, true, true );
                 }
             }
             case 'ezscript_require':
             {
                 if ( !isset( self::$loaded['js_files'] ) )
                 {
-                    self::setPersistentArray( 'js_files', $namedParameters['script_array'], $tpl, true );
+                    self::setPersistentArray( 'js_files', self::flattenArray( $namedParameters['script_array'] ), $tpl, true );
                     break;
                 }// let 'ezscript' handle loaded calls
-                else
+                elseif ( $operatorName === 'ezscript_require' )
                 {
-                    $namedParameters['script_array'] = self::setPersistentArray( 'js_files', $namedParameters['script_array'], $tpl, true, true, true );
+                    $namedParameters['script_array'] = self::setPersistentArray( 'js_files', self::flattenArray( $namedParameters['script_array'] ), $tpl, true, true, true );
                 }
             }
             case 'ezscript':
@@ -218,7 +218,7 @@ class ezjscPackerTemplateFunctions
                 }
                 else
                 {
-                    $diff = self::setPersistentArray( 'js_files', $namedParameters['script_array'], $tpl, true, true, true );
+                    $diff = self::setPersistentArray( 'js_files', self::flattenArray( $namedParameters['script_array'] ), $tpl, true, true, true );
                     $ret = ezjscPacker::buildJavascriptFiles( $diff, $namedParameters['pack_level'] );
                 }
             } break;
@@ -226,7 +226,7 @@ class ezjscPackerTemplateFunctions
             {
                 if ( !isset( self::$loaded['css_files'] ) )
                 {
-                    $depend = self::setPersistentArray( 'css_files', $namedParameters['css_array'], $tpl, false, true );
+                    $depend = self::setPersistentArray( 'css_files', self::flattenArray( $namedParameters['css_array'] ), $tpl, false, true );
                     $ret = ezjscPacker::buildStylesheetTag( $depend,
                                                          $namedParameters['media'],
                                                          $namedParameters['type'],
@@ -235,21 +235,21 @@ class ezjscPackerTemplateFunctions
                     self::$loaded['css_files'] = true;
                     break;
                 }// let 'ezcss' handle loaded calls
-                else
+                elseif ( $operatorName === 'ezcss_load' )
                 {
-                    $namedParameters['css_array'] = self::setPersistentArray( 'css_files', $namedParameters['css_array'], $tpl, true, true, true );
+                    $namedParameters['css_array'] = self::setPersistentArray( 'css_files', self::flattenArray( $namedParameters['css_array'] ), $tpl, true, true, true );
                 }
             }
             case 'ezcss_require':
             {
                 if ( !isset( self::$loaded['css_files'] ) )
                 {
-                    self::setPersistentArray( 'css_files', $namedParameters['css_array'], $tpl, true );
+                    self::setPersistentArray( 'css_files', self::flattenArray( $namedParameters['css_array'] ), $tpl, true );
                     break;
                 }// let 'ezcss' handle loaded calls
-                else
+                elseif ( $operatorName === 'ezcss_require' )
                 {
-                    $namedParameters['css_array'] = self::setPersistentArray( 'css_files', $namedParameters['css_array'], $tpl, true, true, true );
+                    $namedParameters['css_array'] = self::setPersistentArray( 'css_files', self::flattenArray( $namedParameters['css_array'] ), $tpl, true, true, true );
                 }
             }
             case 'ezcss':
@@ -268,7 +268,7 @@ class ezjscPackerTemplateFunctions
                 }
                 else
                 {
-                    $diff = self::setPersistentArray( 'css_files', $namedParameters['css_array'], $tpl, true, true, true );
+                    $diff = self::setPersistentArray( 'css_files', self::flattenArray( $namedParameters['css_array'] ), $tpl, true, true, true );
                     $ret = ezjscPacker::buildStylesheetFiles( $diff, $namedParameters['pack_level'] );
                 }
             } break;
@@ -280,15 +280,15 @@ class ezjscPackerTemplateFunctions
      * Function for setting values to deal with persistent_variable either from
      * template or internally on {@link self::$persistentVariable}
      *
+     * @internal
      * @param string $key Key to store values on
      * @param string|array $value Value(s) to store
      * @param object $tpl Template object to get values from
      * @param bool $append Append or prepend value?
      * @param bool $arrayUnique Make sure array is unique to remove duplicates
      * @param bool $returnArrayDiff Return diff against existing values instead of resulting array
-     * $param bool $override Override/Wipe out values or merge?
+     * @param bool $override Override/Wipe out values or merge?
      * @return array
-     * 
      */
     static public function setPersistentArray( $key, $value, $tpl, $append = true, $arrayUnique = false, $returnArrayDiff = false, $override = false )
     {
@@ -313,7 +313,7 @@ class ezjscPackerTemplateFunctions
             $persistentVariable = self::$persistentVariable;
         }
 
-        if ( !is_array( $persistentVariable ) )
+        if ( $persistentVariable === false || !is_array( $persistentVariable ) )
         {
             // Give warning if value is not array as we depend on it
             if ( !$isPageLayout && $persistentVariable )
@@ -331,7 +331,12 @@ class ezjscPackerTemplateFunctions
             if ( isset( $persistentVariable[ $key ] ) && is_array( $persistentVariable[ $key ] ) )
             {
                 if ( is_array( $value ) )
-                    $persistentVariable[ $key ] = self::flattenArrayMerge( $persistentVariable[ $key ], $value, $append );
+                {
+                    if ( $append )
+                        $persistentVariable[ $key ] = array_merge( $persistentVariable[ $key ], $value );
+                    else
+                        $persistentVariable[ $key ] = array_merge( $value, $persistentVariable[ $key ] );
+                }
                 else if ( $append )
                     $persistentVariable[ $key ][] = $value;
                 else
@@ -340,7 +345,7 @@ class ezjscPackerTemplateFunctions
             else
             {
                 if ( is_array( $value ) )
-                    $persistentVariable[ $key ] = self::flattenArray( $value );
+                    $persistentVariable[ $key ] = $value;
                 else
                     $persistentVariable[ $key ] = array( $value );
             }
@@ -379,27 +384,18 @@ class ezjscPackerTemplateFunctions
     }
 
     /**
-     * Merge array2 with array1, but flatten array2 first
-     * 
-     * @param array $array1
-     * @param array $array2
-     * @param bool $append Append or Prepend array2 on array1
-     * @return array
-     */
-    static protected function flattenArrayMerge( $array1, $array2, $append = true )
-    {
-        $array2 = self::flattenArray( $array2 );
-        return $append ? array_merge( $array1, $array2 ) : array_merge( $array2, $array1 );
-    }
-
-    /**
-     * Flatten array so {@link self::setPersistentArray()} is able to proporly make it unique
-     * 
-     * @param array $array
+     * Flatten css_array/script_array so {@link self::setPersistentArray()} is able to proporly make it unique
+     *
+     * @param array|string $value
      * @return array
      */
     static protected function flattenArray( $array )
     {
+        if ( !is_array( $array ) )
+        {
+            return array( $array );
+        }
+
         $arrayFlatten = array();
         while( isset( $array[0] ) )
         {
@@ -412,7 +408,13 @@ class ezjscPackerTemplateFunctions
         return $arrayFlatten;
     }
 
-    // reusable function for getting internal persistent_variable
+    /**
+     * Reusable function for getting internal persistent_variable
+     *
+     * @internal
+     * @param string $key Optional, return all values if null
+     * @return array|string
+     */
     static public function getPersistentVariable( $key = null )
     {
         if ( $key !== null )
@@ -426,7 +428,7 @@ class ezjscPackerTemplateFunctions
 
     // Internal version of the $persistent_variable used on view that don't support it
     static protected $persistentVariable = null;
-    
+
     // Internal flag for already loaded types
     static protected $loaded = array();
 }
