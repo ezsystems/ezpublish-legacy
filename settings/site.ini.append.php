23b1bd33be01ec4d2137f3ff9534873233c2db82
lolautruche <lolautruche@gmail.com>
2011-01-31 10:44:33 +0100

    cache clear conf

---
+++ settings/site.ini.append.php
@@ -0,0 +1,17 @@
+<?php /* #?ini charset="utf-8"?
+
+
+# Cache item entry (for eZ Publish 4.3 and up)
+[Cache]
+CacheItems[]=rest
+
+[Cache_rest]
+name=REST Application cache
+id=rest
+tags[]=content
+path=rest
+isClustered=true
+
+
+
+*/ ?>
