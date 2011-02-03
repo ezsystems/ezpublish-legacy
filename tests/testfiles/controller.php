07657611ffc457163fd4c6af7f50fa71e486ca3a
lolautruche <lolautruche@gmail.com>
2011-02-03 18:00:15 +0100

    Added some virtual properties and a setRestINI() method for convenience in tests dev

--- tests/testfiles/controller.php
+++ tests/testfiles/controller.php
@@ -3,7 +3,7 @@ class ezpRestTestController extends ezpRestMvcController
 {
     public function doTest()
     {
-        $ret = ezpRestMvcResult();
+        $ret = new ezpRestMvcResult();
         $ret->variables['testVar'] = 'test';
         if( isset( $this->dummyVar ) )
         {
@@ -11,6 +11,39 @@ class ezpRestTestController extends ezpRestMvcController
         }
         
         return $ret;
+    }
+    
+    /**
+     * Helper method to access some private/protected methods
+     * @see lib/ezc/MvcTools/src/interfaces/ezcMvcController::__get()
+     */
+    public function __get( $name )
+    {
+        $ret = null;
+        switch( $name )
+        {
+            case 'cacheLocation':
+                $ret = $this->getCacheLocation();
+            break;
+            
+            case 'cacheId':
+                $ret = $this->generateCacheId();
+            break;
+            
+            case 'cacheTTL':
+                $ret = $this->getActionTTL();
+            break;
+            
+            default:
+                $ret = parent::__get( $name );
+        }
+        
+        return $ret;
+    }
+    
+    public function setRestINI( eZINI $restINI )
+    {
+        $this->restINI = $restINI;
     }
 }
 ?>
