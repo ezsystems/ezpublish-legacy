c7975fc16d170b7719c5645ecee8109c61b6a3e6
xc <xc@ez.no>
2011-02-10 23:57:16 +0100

    Updated: executed operation in event

--- kernel/classes/workflowtypes/event/ezfinishuserregister/ezfinishuserregistertype.php
+++ kernel/classes/workflowtypes/event/ezfinishuserregister/ezfinishuserregistertype.php
@@ -8,23 +8,24 @@ class eZFinishUserRegisterType extends eZWorkflowEventType {
 
     const WORKFLOW_TYPE_STRING = "ezfinishuserregister";
 
-
     public function  __construct()
     {
-        $this->eZWorkflowEventType( eZFinishUserRegisterType::WORKFLOW_TYPE_STRING, ezpI18n::tr( 'kernel/workflow/event', "Finish User Register" ) );
+        $this->eZWorkflowEventType( eZFinishUserRegisterType::WORKFLOW_TYPE_STRING, ezpI18n::tr( 'kernel/workflow/event', "Finish User Registration" ) );
         $this->setTriggerTypes( array( 'content' => array( 'publish' => array( 'after' ) ) ) );
     }
 
     function execute($process, $event)
     {
         //execute user register operation
-
         $parameterList = $process->attribute( 'parameter_list' );
         $objectID = $parameterList['object_id'];
-        eZUserOperationCollection::sendUserNotification( $objectID );
-        //todo: execute register instead of invoke sendnotificaiton directly.
-//        $result = eZOperationHandler::execute( 'user', 'register', array( 'user_id' => $objectID ) );
-//      return parent::execute($process, $event);
+        $object = eZContentObject::fetch( $objectID );
+        // @todo: improve the possible performance.
+        if( $object->attribute( 'class_name' ) == 'user' )
+        {
+            $result = eZOperationHandler::execute( 'user', 'register', array( 'user_id' => $objectID ) );
+            return $result;
+        }
     }
 
     function typeFunctionalAttributes( )
