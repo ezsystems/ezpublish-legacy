1be8efbb608a00be8ea04c2f18f800513a721eaa
lolautruche <lolautruche@gmail.com>
2011-02-11 18:28:11 +0100

    Fixed bug #017932: Indexer crashes on trashed objects (cherry picked from commit a9ecd651d17fa61abbca391d03c626a35e15189c)

---
+++ tests/tests/kernel/content/suite.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * File containing the eZKernelTestSuite class
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ * @package tests
+ */
+
+class eZKernelContentTestSuite extends ezpDatabaseTestSuite
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setName( "eZ Publish Kernel Content Module Test Suite" );
+
+        $this->addTestSuite( 'eZContentOperationDeleteObjectRegression' );
+    }
+
+    public static function suite()
+    {
+        return new self();
+    }
+}
+
+?>
