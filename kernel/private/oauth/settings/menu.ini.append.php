07ee7f887191e95d82d3389159b87153c2b1b6d8
Bertrand Dunogier <bd@ez.no>
2011-02-17 02:02:06 +0100

    REST merge: 1st refine batch of oauth

--- kernel/private/oauth/settings/menu.ini.append.php
+++
@@ -1,6 +0,0 @@
-<?php /* #?ini charset="utf-8"?
-
-[Leftmenu_setup]
-Links[oauth_list]=oauthadmin/list
-LinkNames[oauth_list]=oAuth admin
-*/ ?>
--- kernel/private/oauth/settings/module.ini.append.php
+++
@@ -1,8 +0,0 @@
-<?php /* #?ini charset="utf-8"?
-
-[ModuleSettings]
-ExtensionRepositories[]=oauth
-ModuleList[]=oauthadmin
-ModuleList[]=oauth
-
-*/ ?>
