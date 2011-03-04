74fca7b834be09f83b2789931b571b775a423c2e
Ole Marius Smestad <oms@ez.no>
2011-03-04 15:23:46 +0100

    Fixed: dbupdate scripts. Moved updates to the proper version.

--- update/database/postgresql/4.5/unstable/dbupdate-4.4.0-to-4.5.0alpha1.sql
+++ update/database/postgresql/4.5/unstable/dbupdate-4.4.0-to-4.5.0alpha1.sql
@@ -1,17 +1,2 @@
 UPDATE ezsite_data SET value='4.5.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
-
-CREATE TABLE ezpublishingqueueprocesses (
-    created integer,
-    ezcontentobject_version_id integer DEFAULT 0 NOT NULL,
-    finished integer,
-    pid integer,
-    started integer,
-    status integer
-);
-
-ALTER TABLE ONLY ezpublishingqueueprocesses
-    ADD CONSTRAINT ezpublishingqueueprocesses_pkey PRIMARY KEY (ezcontentobject_version_id);
-
-ALTER TABLE ezsection ALTER COLUMN identifier SET DEFAULT '';
-UPDATE ezsection SET identifier='' WHERE identifier IS NULL;
