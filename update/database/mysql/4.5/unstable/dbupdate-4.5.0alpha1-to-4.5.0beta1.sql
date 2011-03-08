68874fd084386c4dcd7d2de2f599eff4c2be9dd5
Ole Marius Smestad <oms@ez.no>
2011-03-08 14:00:49 +0100

    Synced dbupdate files with a schema.

--- update/database/mysql/4.5/unstable/dbupdate-4.5.0alpha1-to-4.5.0beta1.sql
+++ update/database/mysql/4.5/unstable/dbupdate-4.5.0alpha1-to-4.5.0beta1.sql
@@ -14,3 +14,50 @@ CREATE TABLE ezpublishingqueueprocesses (
 
 ALTER TABLE ezsection MODIFY COLUMN identifier VARCHAR(255) DEFAULT '';
 UPDATE ezsection SET identifier='' WHERE identifier IS NULL;
+
+CREATE TABLE `ezprest_token` (
+  `id` varchar(200) NOT NULL,
+  `refresh_token` varchar(200) NOT NULL,
+  `expirytime` bigint(20) NOT NULL DEFAULT '0',
+  `client_id` varchar(200) NOT NULL,
+  `user_id` varchar(200) NOT NULL,
+  `scope` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `token_client_id` (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ezprest_authcode` (
+  `id` varchar(200) NOT NULL,
+  `expirytime` bigint(20) NOT NULL DEFAULT '0',
+  `client_id` varchar(200) NOT NULL,
+  `user_id` varchar(200) NOT NULL,
+  `scope` varchar(200) DEFAULT NULL,
+
+  PRIMARY KEY (`id`),
+  KEY `authcode_client_id` (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ezprest_clients` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `description` text,
+  `client_id` varchar(200) DEFAULT NULL,
+  `client_secret` varchar(200) DEFAULT NULL,
+  `endpoint_uri` varchar(200) DEFAULT NULL,
+  `owner_id` int(11) NOT NULL DEFAULT '0',
+  `created` int(11) NOT NULL DEFAULT '0',
+  `updated` int(11) NOT NULL DEFAULT '0',
+  `version` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `client_id_UNIQUE` (`client_id`,`version`),
+  KEY `client_id` (`client_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `ezprest_authorized_clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rest_client_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client_user` (`rest_client_id`,`user_id`)
+) ENGINE=InnoDB;
