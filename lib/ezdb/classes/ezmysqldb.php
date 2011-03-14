25493ad00f088541873318c932978c01ffb4e41c
Jérôme Vieilledent <jv@ez.no>
2011-03-14 14:26:29 +0100

    Rel #018127 : errors not correctly registered if the callers passes in a specific $connection parameter when $db is connected

--- lib/ezdb/classes/ezmysqldb.php
+++ lib/ezdb/classes/ezmysqldb.php
@@ -905,9 +905,11 @@ class eZMySQLDB extends eZDBInterface
     {
         if ( $this->IsConnected )
         {
-            if ( $connection === false && is_resource( $this->DBConnection ) )
-            {
+            if ( $connection === false )
                 $connection = $this->DBConnection;
+
+            if ( $connection === false && is_resource( $connection ) )
+            {
                 $this->ErrorMessage = mysql_error( $connection );
                 $this->ErrorNumber = mysql_errno( $connection );
             }
