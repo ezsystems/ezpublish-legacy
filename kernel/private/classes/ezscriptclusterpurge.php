8fae0d8886ad9b2cd315b551bb1e4b0a2b3f5dff
Patrick Allaert <pa@ez.no>
2011-03-15 15:59:52 +0100

    CS: changed "GNU GPL" -> "GNU General Public License" in file headers

--- kernel/private/classes/ezscriptclusterpurge.php
+++ kernel/private/classes/ezscriptclusterpurge.php
@@ -9,7 +9,7 @@
  * should therefore be required to run it.
  *
  * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
- * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ * @license http://ez.no/licenses/gnu_gpl GNU General Public License v2
  *
  * @property bool optDryRun
  * @property int optIterationLimit
--- tests/tests/kernel/private/rest/tools/generate_test_data.php
+++ tests/tests/kernel/private/rest/tools/generate_test_data.php
@@ -6,7 +6,7 @@
  * it, to capture raw server side request data.
  *
  * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
- * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ * @license http://ez.no/licenses/gnu_gpl GNU General Public License v2
  *
  */
 
