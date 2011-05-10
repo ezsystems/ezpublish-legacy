94e44949b5754d1f8c8dbf135150b7d2d1885f6a
Vidar L <vl@ubuntu.(none)>
2011-05-10 14:48:24 +0200

    - Readded copyright header in ezptestrunner

--- tests/toolkit/ezptestrunner.php
+++ tests/toolkit/ezptestrunner.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * File containing the ezpTestRunner class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ * @package tests
+ */
+
 
 class ezpTestRunner extends PHPUnit_TextUI_Command
 {
