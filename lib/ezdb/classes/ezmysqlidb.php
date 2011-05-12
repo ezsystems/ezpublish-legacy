051fbc991e538c5632d20a4c9a1d04ac953992f6
Jérôme Gamez <jerome.gamez@gmail.com>
2011-05-12 13:32:28 +0200

    Merged changes from ezmysqldb.php

--- lib/ezdb/classes/ezmysqlidb.php
+++ lib/ezdb/classes/ezmysqlidb.php
@@ -216,6 +216,7 @@ class eZMySQLiDB extends eZDBInterface
 
         if ( is_array( $charset ) )
         {
+            $realCharset = array();
             foreach ( $charset as $charsetItem )
                 $realCharset[] = eZCharsetInfo::realCharsetCode( $charsetItem );
         }
@@ -348,9 +349,9 @@ class eZMySQLiDB extends eZDBInterface
                             $columns[$col]['size'] = max( $columns[$col]['size'], strlen( $data ) );
                         }
                     }
-
-                    $analysisText = '';
+                    
                     $delimiterLine = array();
+                    $colLine = array();
                     // Generate the column line and the vertical delimiter
                     // The look of the table is taken from the MySQL CLI client
                     // It looks like this:
@@ -380,7 +381,7 @@ class eZMySQLiDB extends eZDBInterface
                             $data = isset( $row[$name] ) ? $row[$name] : '';
                             // Align numerical values to the right (ie. pad left)
                             $rowLine[] = ' ' . str_pad( $row[$name], $size, ' ',
-                                                        is_numeric( $row[$name] ) ? STR_PAD_LEFT : STR_PAD_RIGHT ) . ' ';
+                                                        is_numeric( $data ) ? STR_PAD_LEFT : STR_PAD_RIGHT ) . ' ';
                         }
                         $analysisText .= '|' . join( '|', $rowLine ) . "|\n";
                         $analysisText .= $delimiterLine;
