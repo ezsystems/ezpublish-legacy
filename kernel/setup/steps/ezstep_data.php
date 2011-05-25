6c7a9d37ba742a0ea54326bc78514def12701182
Alexandre SEBBANE <alex@sebbane.org>
2011-05-25 06:45:32 +0200

    add proxy step to ezpublish install
    Signed-off-by: Alexandre SEBBANE <alex@sebbane.org>

--- kernel/setup/steps/ezstep_data.php
+++ kernel/setup/steps/ezstep_data.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /**
  * File containing the eZStepData class.
  *
@@ -148,6 +148,8 @@ class eZStepData
                                    'class' => 'SystemFinetune' ),
                             array( 'file' => 'email_settings',
                                    'class' => 'EmailSettings' ),
+                            array( 'file' => 'proxy_settings',
+                                   'class' => 'ProxySettings' ),
                             array( 'file' => 'database_choice',
                                    'class' => 'DatabaseChoice' ),
                             array( 'file' => 'database_init',
