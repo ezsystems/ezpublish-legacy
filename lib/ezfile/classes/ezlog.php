9ee123295d5e9c69cd45cc5d1f6b37ed66de3ad1
Jérôme Gamez <jerome.gamez@gmail.com>
2011-06-22 17:53:15 +0200

    Migrated doxygen comments to phpdoc

--- lib/ezfile/classes/ezlog.php
+++ lib/ezfile/classes/ezlog.php
@@ -21,19 +21,24 @@ class eZLog
     const MAX_LOGROTATE_FILES = 3;
     const MAX_LOGFILE_SIZE = 204800; // 200*1024
 
-    /*!
-      Creates a new log object.
-    */
-    function eZLog( )
+    /**
+     * Creates a new log object.
+     */
+    public function __construct()
     {
+
     }
 
-    /*!
-     \static
-     \public
-     Writes a message $message to a given file name $name and directory $dir for logging
-    */
-    static function write( $message, $logName = 'common.log', $dir = 'var/log' )
+    /**
+     * Writes a message $message to a given file name $name and directory $dir for logging
+     *
+     * @static
+     * @param string $message
+     * @param string $logName
+     * @param string $dir
+     * @return void
+     */
+    public static function write( $message, $logName = 'common.log', $dir = 'var/log' )
     {
         $fileName = $dir . '/' . $logName;
         $oldumask = @umask( 0 );
@@ -71,11 +76,15 @@ class eZLog
         }
     }
 
-    /*!
-     \private
-     Writes file name $name and storage directory $dir to storage log
-    */
-    static function writeStorageLog( $name, $dir = false )
+    /**
+     * Writes file name $name and storage directory $dir to storage log
+     *
+     * @static
+     * @param string $name
+     * @param bool $dir
+     * @return void
+     */
+    public static function writeStorageLog( $name, $dir = false )
     {
         $ini = eZINI::instance();
         $varDir = $ini->variable( 'FileSettings', 'VarDir' );
@@ -126,12 +135,14 @@ class eZLog
         }
     }
 
-    /*!
-     \static
-     \deprecated Use self::MAX_LOGFILE_SIZE instead
-     \return the maxium size for a log file in bytes.
-    */
-    static function maxLogSize()
+    /**
+     * Returns the maxium size for a log file in bytes.
+     *
+     * @deprecated since 4.6, use eZLog::MAX_LOGFILE_SIZE instead
+     * @static
+     * @return int
+     */
+    public static function maxLogSize()
     {
         $maxLogSize =& $GLOBALS['eZMaxLogSize'];
         if ( isset( $maxLogSize ) )
@@ -139,37 +150,44 @@ class eZLog
         return self::MAX_LOGFILE_SIZE;
     }
 
-    /*!
-     \static
-     \deprecated
-     Sets the maxium size for a log file to \a $size.
-    */
-    static function setMaxLogSize( $size )
+    /**
+     * Sets the maxium size for a log file to \a $size.
+     *
+     * @deprecated since 4.6
+     * @static
+     * @param  $size
+     * @return void
+     */
+    public static function setMaxLogSize( $size )
     {
         $GLOBALS['eZMaxLogSize'] = $size;
     }
 
-    /*!
-     \static
-     \deprecated Use self::MAX_LOGROTATE_FILES instead
-     \return the maxium number of logrotate files to keep.
-    */
-    static function maxLogrotateFiles()
+    /**
+     * Returns the maxium number of logrotate files to keep.
+     *
+     * @deprecated since 4.6, use eZLog::MAX_LOGROTATE_FILES instead
+     * @static
+     * @return int
+     */
+    public static function maxLogrotateFiles()
     {
         $maxLogrotateFiles =& $GLOBALS['eZMaxLogrotateFiles'];
         if ( isset( $maxLogrotateFiles ) )
             return $maxLogrotateFiles;
         return self::MAX_LOGROTATE_FILES;
     }
-    
-    /*!
-     \static
-     Rotates logfiles so the current logfile is backed up,
-     old rotate logfiles are rotated once more and those that
-     exceed self::MAX_LOGROTATE_FILES will be removed.
-     Rotated files will get the extension .1, .2 etc.
-    */
-    static function rotateLog( $fileName )
+
+    /**
+     * Rotates logfiles so the current logfile is backed up,
+     * old rotate logfiles are rotated once more and those that
+     * exceed self::MAX_LOGROTATE_FILES will be removed.
+     * Rotated files will get the extension .1, .2 etc.
+     * @static
+     * @param  $fileName
+     * @return bool
+     */
+    public static function rotateLog( $fileName )
     {
         for ( $i = self::MAX_LOGROTATE_FILES; $i > 0; --$i )
         {
@@ -195,12 +213,15 @@ class eZLog
         }
         return false;
     }
-
-    /*!
-     \static
-     \deprecated
-     Sets the maxium number of logrotate files to keep to \a $files.
-    */
+    
+    /**
+     * Sets the number of files to be rotated
+     *
+     * @deprecated since 4.6
+     * @static
+     * @param  $files
+     * @return void
+     */
     static function setLogrotateFiles( $files )
     {
         $GLOBALS['eZMaxLogrotateFiles'] = $files;
