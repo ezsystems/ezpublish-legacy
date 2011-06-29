bde06a55db71b82772fe6d178dc30b5ff053aa38
Gaetano Giunta <gg@ez.no>
2011-06-29 15:43:49 +0200

    optimize clearstatcache calls for php 5.3 in ezdb, ezdfscluster file handlers

--- kernel/classes/clusterfilehandlers/dbbackends/mysqli.php
+++ kernel/classes/clusterfilehandlers/dbbackends/mysqli.php
@@ -493,7 +493,14 @@ class eZDBFileHandlerMysqliBackend
         mysqli_free_result( $res );
 
         // Make sure all data is written correctly
-        clearstatcache();
+        if ( version_compare( PHP_VERSION, '5.3.0' ) >= 0 )
+        {
+            clearstatcache( false, $tmpFilePath );
+        }
+        else
+        {
+            clearstatcache();
+        }
         $tmpSize = filesize( $tmpFilePath );
         if ( $tmpSize != $metaData['size'] )
         {
@@ -747,7 +754,14 @@ class eZDBFileHandlerMysqliBackend
     function _storeInner( $filePath, $datatype, $scope, $fname )
     {
         // Insert file metadata.
-        clearstatcache();
+        if ( version_compare( PHP_VERSION, '5.3.0' ) >= 0 )
+        {
+            clearstatcache( false, $filePath );
+        }
+        else
+        {
+            clearstatcache();
+        }
         $fileMTime = filemtime( $filePath );
         $contentLength = filesize( $filePath );
         $filePathHash = md5( $filePath );
