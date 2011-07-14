5f6aa1af0eac7fa4fd77a97cebcd4319585e6c23
Christian Pfeffer Gjengedal <cpg@ez.no>
2011-07-14 11:05:01 +0200

    Changed: Integers in json for thumbnail dimentions

--- classes/ezjscajaxcontent.php
+++ classes/ezjscajaxcontent.php
@@ -285,8 +285,8 @@ class ezjscAjaxContent
         if ( isset( $params['fetchThumbPreview'] ) )
         {
             $thumbUrl = '';
-            $thumbWidth = '';
-            $thumbHeight = '';
+            $thumbWidth = 0;
+            $thumbHeight = 0;
             $thumbDataType = isset( $params['thumbDataType'] ) ? $params['thumbDataType'] : 'ezimage';
             $thumbImageSize = isset( $params['thumbImageSize'] ) ? $params['thumbImageSize'] : 'small';
 
@@ -303,8 +303,8 @@ class ezjscAjaxContent
                             __METHOD__ );
 
                     $thumbUrl = isset( $imageAlias['full_path'] ) ? $imageAlias['full_path'] : '';
-                    $thumbWidth = isset( $imageAlias['width'] ) ? $imageAlias['width'] : '';
-                    $thumbHeight = isset( $imageAlias['height'] ) ? $imageAlias['height'] : '';
+                    $thumbWidth = isset( $imageAlias['width'] ) ? (int) $imageAlias['width'] : 0;
+                    $thumbHeight = isset( $imageAlias['height'] ) ? (int) $imageAlias['height'] : 0;
 
                     if ( $thumbUrl !== '' )
                         eZURI::transformURI( $thumbUrl, true );
