b889ef81aaf6b7e566c2e8c0c7cf62d5649f1d03
Damien Pobel <dp@ez.no>
2011-07-29 11:42:46 +0200

    Fixed #18149: special character not supported by eZXMLInputParser

--- tests/tests/kernel/datatypes/suite.php
+++ tests/tests/kernel/datatypes/suite.php
@@ -23,6 +23,7 @@ class eZDatatypeTestSuite extends ezpDatabaseTestSuite
         $this->addTestSuite( 'eZTextFileUserTest' );
         $this->addTestSuite( 'eZEmailTypeTest' );
         $this->addTestSuite( 'eZXMLInputParserTest' );
+        $this->addTestSuite( 'eZSimplifiedXMLInputParserRegression' );
 
         $this->addTestSuite( 'eZBinaryFileTypeRegression' );
         $this->addTestSuite( 'eZImageTypeRegression' );
