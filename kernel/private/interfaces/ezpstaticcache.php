bf09d2655e55a17572f9c37487b5575416dbe4c0
Philipp Kamps <pek@pek-MacBookPro>
2011-08-11 12:26:49 +0200

    Inline doc added

--- kernel/private/interfaces/ezpstaticcache.php
+++ kernel/private/interfaces/ezpstaticcache.php
@@ -14,6 +14,13 @@
  */
 interface ezpStaticCache
 {
+    /**
+     * Generates the caches for all URLs that must always be generated.
+     *
+     * @param bool $quiet If true then the function will not output anything.
+     * @param eZCLI|false $cli The eZCLI object or false if no output can be done.
+     * @param bool $delay
+     */
     public function generateAlwaysUpdatedCache( $quiet = false, $cli = false, $delay = true );
     
     /**
@@ -24,12 +31,37 @@ interface ezpStaticCache
      */
     public function generateNodeListCache( $nodeList );
     
+    /**
+     * Generates the static cache from the configured INI settings.
+     *
+     * @param bool $force If true then it will create all static caches even if it is not outdated.
+     * @param bool $quiet If true then the function will not output anything.
+     * @param eZCLI|false $cli The eZCLI object or false if no output can be done.
+     * @param bool $delay
+     */
     public function generateCache( $force = false, $quiet = false, $cli = false, $delay = true );
     
+    /**
+     * Generates the caches for the url $url using the currently configured storageDirectory().
+     *
+     * @param string $url The URL to cache, e.g /news
+     * @param int|false $nodeID The ID of the node to cache, if supplied it will also cache content/view/full/xxx.
+     * @param bool $skipExisting If true it will not unlink existing cache files.
+     * @return bool
+     */
     public function cacheURL( $url, $nodeID = false, $skipExisting = false, $delay = true );
 
+    /**
+     * Removes the static cache file (index.html) and its directory if it exists.
+     * The directory path is based upon the URL $url and the configured static storage dir.
+     *
+     * @param string $url The URL for the current item, e.g /news
+     */
     public function removeURL( $url );
 
+    /**
+     * This function goes over the list of recorded actions and excecutes them.
+     */
     static function executeActions();
 }
 ?>
