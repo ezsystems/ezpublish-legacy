fa24b19a0ce8145511ac713a0401a3526aa6bc19
xc <xc@ez.no>
2011-09-13 12:00:41 +0200

    Added: ezorder_nr_incr for postgresql upgrade

--- update/database/postgresql/4.6/unstable/dbupdate-4.5.0-to-4.6.0alpha1.sql
+++ update/database/postgresql/4.6/unstable/dbupdate-4.5.0-to-4.6.0alpha1.sql
@@ -1,2 +1,17 @@
 UPDATE ezsite_data SET value='4.6.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+CREATE SEQUENCE ezorder_nr_incr_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
+CREATE TABLE ezorder_nr_incr (
+    id integer DEFAULT nextval('ezorder_nr_incr_s'::text) NOT NULL
+);
+
+ALTER TABLE ONLY ezorder_nr_incr
+    ADD CONSTRAINT ezorder_nr_incr_pkey PRIMARY KEY (id);
+
