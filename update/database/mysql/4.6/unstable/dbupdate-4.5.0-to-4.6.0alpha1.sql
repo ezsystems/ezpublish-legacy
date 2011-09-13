2975b41d8bc184a449e098f1c4648185c5dba5cd
xc <xc@ez.no>
2011-09-13 11:59:03 +0200

    Fixed: #018625: Upgrading from 4.5 to 4.6alpha1 database (mysql) consistency check fails

--- update/database/mysql/4.6/unstable/dbupdate-4.5.0-to-4.6.0alpha1.sql
+++ update/database/mysql/4.6/unstable/dbupdate-4.5.0-to-4.6.0alpha1.sql
@@ -1,3 +1,8 @@
 SET storage_engine=InnoDB;
 UPDATE ezsite_data SET value='4.6.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+CREATE TABLE ezorder_nr_incr (
+id int(11) NOT NULL AUTO_INCREMENT,
+PRIMARY KEY ( id )
+);
