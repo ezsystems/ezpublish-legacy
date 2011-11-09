12c1295cc910a9851446c1641b306579d786e813
Jérôme Vieilledent <jv@ez.no>
2011-11-09 15:29:19 +0100

    Fixed eZURLAliasML regression tests

--- tests/tests/kernel/datatypes/ezobjectrelationlist/ezobjectrelationlist_regression.php
+++ tests/tests/kernel/datatypes/ezobjectrelationlist/ezobjectrelationlist_regression.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing eZContentOperationDeleteRegression class
+ * File containing eZObjectRelationListDatatypeRegression class
  *
  * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
