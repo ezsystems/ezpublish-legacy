da21fb19e9a5f4cf67025170e68a74a1d2926495
Jerome Despatis <jerome@despatis.com>
2011-12-11 10:38:34 +0100

    database change for ezscriptmonitor

--- update/database/mysql/4.7/unstable/dbupdate-4.6.0-to-4.7.0alpha1.sql
+++ update/database/mysql/4.7/unstable/dbupdate-4.6.0-to-4.7.0alpha1.sql
@@ -1,3 +1,6 @@
 SET storage_engine=InnoDB;
 UPDATE ezsite_data SET value='4.7.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+ALTER TABLE `ezscheduled_script` CHANGE `name` `name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+CHANGE `command` `command` VARCHAR( 2000 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
