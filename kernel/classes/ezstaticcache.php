a180612ab7352cc07a93f850333fd51548b11c6c
Jerome Despatis <jerome@despatis.com>
2011-12-15 11:31:55 +0100

    Cloning same error text when same error is encountered in 2 different cases, in order to not loose any information on the error

--- kernel/classes/ezstaticcache.php
+++ kernel/classes/ezstaticcache.php
@@ -415,8 +415,7 @@ class eZStaticCache implements ezpStaticCache
                             }
                             if ( $content === false )
                             {
-                                eZDebug::writeError( "Could not grab content (from $fileName), is the hostname correct and Apache running?",
-                                                      'Static Cache' );
+                                eZDebug::writeError( "Could not grab content (from $fileName), is the hostname correct and Apache running?", 'Static Cache' );
                             }
                             else
                             {
@@ -624,7 +623,7 @@ class eZStaticCache implements ezpStaticCache
                         }
                         if ( $fileContentCache[$source] === false )
                         {
-                            eZDebug::writeError( 'Could not grab content, is the hostname correct and Apache running?', 'Static Cache' );
+                            eZDebug::writeError( "Could not grab content (from $source), is the hostname correct and Apache running?", 'Static Cache' );
                         }
                         else
                         {
