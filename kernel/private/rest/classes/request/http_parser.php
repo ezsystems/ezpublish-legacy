ezsystems/ezpublish-legacy

commit 5128c35a296559c497d1ef5ad232e77332a2bd02

Changed: The REST Infrastructure should not make any restrictions on http-methods.
Furthermore the http protocol also allows to define custom methods.

This is part of EZPNEXT-177: Refactoring REST API Infrastructure

kernel/private/rest/classes/request/http_parser.php
@@ -56,7 +56,6 @@ class ezpRestHttpRequestParser extends ezcMvcHttpRequestParser
     {
         $this->processEncryption();
         parent::processStandardHeaders( );
-        $this->processProtocolOverride();
     }
 
     /**
@@ -161,17 +160,17 @@ class ezpRestHttpRequestParser extends ezcMvcHttpRequestParser
     }
 
     /**
-     * Adds support for using POST for PUT and DELETE for legacy browsers that does not support these.
-     *
-     * If a post param "_method" is set to either PUT or DELETE, then ->protocol is changed to that.
-     * ( original protocol is kept on ->originalProtocol param  )
-     * Post is used as this is only meant for forms in legacy browsers.
+     * Processes the request protocol.
      */
-    protected function processProtocolOverride()
+    protected function processProtocol()
     {
         $req = $this->request;
-        $req->originalProtocol = $req->protocol;
+        $req->originalProtocol = $req->protocol = 'http-' . ( isset( $_SERVER['REQUEST_METHOD'] ) ? strtolower( $_SERVER['REQUEST_METHOD'] ) : "get" );
 
+        // Adds support for using POST for PUT and DELETE for legacy browsers that does not support these.
+        // If a post param "_method" is set to either PUT or DELETE, then ->protocol is changed to that.
+        // (original protocol is kept on ->originalProtocol param)
+        // Post is used as this is only meant for forms in legacy browsers.
         if ( $req->protocol === 'http-post' && isset( $req->post['_method'] ) )
         {
             $method = strtolower( $req->post['_method'] );

commit 8a5870665356247541ba427490f80330fd8b43e4

Fixed EZPNEXT-214: ci.ez.no down because of commits on the 11th possibly caused by REST commits
Regression introduced in 5128c35a296559c497d1ef5ad232e77332a2bd02, the
request->post property is set at a later step. Have to work with $_POST.

kernel/private/rest/classes/request/http_parser.php
@@ -166,13 +166,13 @@ class ezpRestHttpRequestParser extends ezcMvcHttpRequestParser
         // If a post param "_method" is set to either PUT or DELETE, then ->protocol is changed to that.
         // (original protocol is kept on ->originalProtocol param)
         // Post is used as this is only meant for forms in legacy browsers.
-        if ( $req->protocol === 'http-post' && isset( $req->post['_method'] ) )
+        if ( $req->protocol === 'http-post' && isset( $_POST['_method'] ) )
         {
-            $method = strtolower( $req->post['_method'] );
+            $method = strtolower( $_POST['_method'] );
             if ( $method  === 'put' || $method === 'delete' )
                 $req->protocol = "http-{$method}";
 
-            unset( $req->post['_method'] );
+            unset( $_POST['_method'] );
         }
     }
 