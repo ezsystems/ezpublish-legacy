fa3e311b5d864c60f25d3e58621cc01bf7b6c956
crevillo <crevillo@gmail.com>
2012-02-16 18:09:11 +0100

    Fixed #019146: useless? comment in ezdbinterface. Useless brackets too.

--- lib/ezdb/classes/ezdbinterface.php
+++ lib/ezdb/classes/ezdbinterface.php
@@ -96,31 +96,20 @@ class eZDBInterface
         $this->OutputTextCodec = null;
         $this->InputTextCodec = null;
 
-        /*
-        This is pseudocode, there is no such function as
-        mysql_supports_charset() of course
-        if ( $this->UseBuiltinEncoding and mysql_supports_charset( $charset ) )
-        {
-            mysql_session_set_charset( $charset );
-        }
-        else
-        */
-        {
-            $tmpOutputTextCodec = eZTextCodec::instance( $charset, false, false );
-            $tmpInputTextCodec = eZTextCodec::instance( false, $charset, false );
-            unset( $this->OutputTextCodec );
-            unset( $this->InputTextCodec );
-            $this->OutputTextCodec = null;
-            $this->InputTextCodec = null;
+        $tmpOutputTextCodec = eZTextCodec::instance( $charset, false, false );
+        $tmpInputTextCodec = eZTextCodec::instance( false, $charset, false );
+        unset( $this->OutputTextCodec );
+        unset( $this->InputTextCodec );
+        $this->OutputTextCodec = null;
+        $this->InputTextCodec = null;
 
-            if ( $tmpOutputTextCodec && $tmpInputTextCodec )
+        if ( $tmpOutputTextCodec && $tmpInputTextCodec )
+        {
+            if ( $tmpOutputTextCodec->conversionRequired() && $tmpInputTextCodec->conversionRequired() )
             {
-                if ( $tmpOutputTextCodec->conversionRequired() && $tmpInputTextCodec->conversionRequired() )
-                {
-                    $this->OutputTextCodec =& $tmpOutputTextCodec;
-                    $this->InputTextCodec =& $tmpInputTextCodec;
-                }
-            }
+                $this->OutputTextCodec =& $tmpOutputTextCodec;
+                $this->InputTextCodec =& $tmpInputTextCodec;
+            }        
         }
 
         $this->OutputSQL = false;
