e758c0af01c936c9db36d48433d5e3cad3dd237a
crevillo <crevillo@gmail.com>
2012-03-15 15:49:09 +0100

    Reverted unwanted version change

--- kernel/classes/datatypes/ezuser/ezuser.php
+++ kernel/classes/datatypes/ezuser/ezuser.php
@@ -4,7 +4,7 @@
  *
  * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
- * @version  2012.2
+ * @version //autogentag//
  * @package kernel
  */
 
