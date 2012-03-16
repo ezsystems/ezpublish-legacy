a80870194b26fb496b774ca22b2d1e0538ee4dbb
André R <ar@ez.no>
2012-03-16 20:50:47 +0100

    Reconfigured branch (cherry picked from commit 6c879fd2f3dde2f6eaeec17190c9c56ac5ecd04f)
    Conflicts:
    
    	update/database/mysql/4.7/unstable/dbupdate-4.7.0alpha1-to-4.7.0beta1.sql
    	update/database/postgresql/4.7/unstable/dbupdate-4.7.0alpha1-to-4.7.0beta1.sql

--- bin/shell/checkversionnumbers.sh
+++ bin/shell/checkversionnumbers.sh
@@ -6,19 +6,19 @@
 # The last version which changelogs and db updates are related to
 # For the first development release this should be empty, in
 # wich case $LAST_STABLE is used.
-PREVIOUS_VERSION="4.6.0"
+PREVIOUS_VERSION="4.7.0alpha1"
 # The last version of the newest stable branch
 LAST_STABLE="4.6.0"
 # Set this to true if the LAST_STABLE has been modified from the last release
 # This will be set to true automatically if the release is a final release
-LAST_STABLE_CHANGED="true"
+LAST_STABLE_CHANGED="false"
 
 MAJOR=4
 MINOR=7
 RELEASE=0
 # Starts at 1 for the first release in a branch and increases with one
 REAL_RELEASE=1
-STATE="alpha1"
+STATE="beta1"
 VERSION=$MAJOR"."$MINOR"."$RELEASE""$STATE
 VERSION_ONLY=$MAJOR"."$MINOR
 BRANCH_VERSION=$MAJOR"."$MINOR
@@ -28,7 +28,7 @@ PACKAGE_DEVELOPMENT="false"
 # Is automatically set to 'true' when $STATE contains some text, do not modify
 DEVELOPMENT="true"
 # Whether the previous release is a development release or not.
-DEVELOPMENT_PREVIOUS="false"
+DEVELOPMENT_PREVIOUS="true"
 # Is only true when the release is a final release (ie. the first of the stable ones)
 # Will be automatically set to true when $RELEASE is 0 and $DEVELOPMENT is false
 FINAL="false"
