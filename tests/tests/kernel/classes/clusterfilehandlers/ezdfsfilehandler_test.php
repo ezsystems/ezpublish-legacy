c329471f206599c0c1340209e61948e0bed58d5a
Damien Pobel <dp@ez.no>
2012-04-04 14:26:38 +0200

    Fixed the initialization of unit tests related to file handlers and cluster systems

--- tests/tests/kernel/classes/clusterfilehandlers/ezdfsfilehandler_test.php
+++ tests/tests/kernel/classes/clusterfilehandlers/ezdfsfilehandler_test.php
@@ -46,6 +46,7 @@ class eZDFSFileHandlerTest extends eZDBBasedClusterFileHandlerAbstractTest
     public function setUp()
     {
         parent::setUp();
+        eZClusterFileHandler::resetHandler();
 
         self::setUpDatabase( $this->clusterClass );
 
@@ -82,9 +83,6 @@ class eZDFSFileHandlerTest extends eZDBBasedClusterFileHandlerAbstractTest
         // We need to clear the existing handler if it was loaded before the INI
         // settings changes
         eZClusterFileHandler::resetHandler();
-        if ( isset( $GLOBALS['eZClusterFileHandler_chosen_handler'] ) and
-            !$GLOBALS['eZClusterFileHandler_chosen_handler'] instanceof eZDFSFileHandler )
-            unset( $GLOBALS['eZClusterFileHandler_chosen_handler'] );
 
         unset( $GLOBALS['eZClusterInfo'] );
 
@@ -104,9 +102,7 @@ class eZDFSFileHandlerTest extends eZDBBasedClusterFileHandlerAbstractTest
     public function tearDown()
     {
         ezpINIHelper::restoreINISettings();
-
-        if ( isset( $GLOBALS['eZClusterFileHandler_chosen_handler'] ) )
-            unset( $GLOBALS['eZClusterFileHandler_chosen_handler'] );
+        eZClusterFileHandler::resetHandler();
 
         if ( $this->haveToRemoveDFSPath )
         {
