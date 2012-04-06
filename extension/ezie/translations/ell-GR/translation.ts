c1ea2fea7d2b5a8e06356f8895e428c282d518d3
Theodoros Papageorgiou <theo@webwiz.gr>
2012-04-06 11:26:01 +0300

    localization: added ezautosave ell-GR translation.ts

---
+++ extension/ezie/translations/ell-GR/translation.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="en_US">
+<defaultcodec></defaultcodec>
+<context>
+    <name>design/standard/content/datatype</name>
+    <message>
+        <source>Current image</source>
+        <translation>Τρέχουσα εικόνα</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Προεπισκόπηση</translation>
+    </message>
+    <message>
+        <source>Filename</source>
+        <translation>Όνομα αρχείου</translation>
+    </message>
+    <message>
+        <source>MIME type</source>
+        <translation>Τύπος MIME</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Μέγεθος</translation>
+    </message>
+    <message>
+        <source>There is no image file.</source>
+        <translation>Δεν υπάρχει αρχείο εικόνας.</translation>
+    </message>
+    <message>
+        <source>Remove image</source>
+        <translation>Αφαίρεση εικόνας</translation>
+    </message>
+    <message>
+        <source>New image file for upload</source>
+        <translation>Νέο αρχείο εικόνας για μεταφόρτωση</translation>
+    </message>
+    <message>
+        <source>Alternative image text</source>
+        <translation>Εναλλακτικό κείμενο εικόνας</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/ezie</name>
+    <message>
+        <source>Edit</source>
+        <translation>Επεξεργασία</translation>
+    </message>
+    <message>
+        <source>eZ Image Editor</source>
+        <translation>eZ Image Editor</translation>
+    </message>
+    <message>
+        <source>Save and Close</source>
+        <translation>Αποθήκευση και Κλείσιμο</translation>
+    </message>
+    <message>
+        <source>Save &amp;amp; Close</source>
+        <translation>Αποθήκευση &amp;amp; Κλείσιμο</translation>
+    </message>
+    <message>
+        <source>Close without saving</source>
+        <translation>Κλείσιμο χωρίς αποθήκευση</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Έξοδος</translation>
+    </message>
+    <message>
+        <source>Thumbnail</source>
+        <translation>Μικρογραφία</translation>
+    </message>
+    <message>
+        <source>Rotation</source>
+        <translation>Περιστροφή</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Επιλογή</translation>
+    </message>
+    <message>
+        <source>Dimensions</source>
+        <translation>Διαστάσεις</translation>
+    </message>
+    <message>
+        <source>Keep ratio</source>
+        <translation>Διατήρηση αναλογιών</translation>
+    </message>
+    <message>
+        <source>Free</source>
+        <translation>Ελεύθερο</translation>
+    </message>
+    <message>
+        <source>Crop</source>
+        <translation>Περικοπή</translation>
+    </message>
+    <message>
+        <source>Zoom</source>
+        <translation>Μεγέθυνση</translation>
+    </message>
+    <message>
+        <source>Actual pixels</source>
+        <translation>Πραγματικά pixels</translation>
+    </message>
+    <message>
+        <source>Fit on screen</source>
+        <translation>Προσαρμογή στην οθόνη</translation>
+    </message>
+    <message>
+        <source>Watermarks</source>
+        <translation>Υδατογραφήματα</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Εφαρμογή</translation>
+    </message>
+    <message>
+        <source>Contrast</source>
+        <translation>Αντίθεση</translation>
+    </message>
+    <message>
+        <source>Brightness</source>
+        <translation>Φωτεινότητα</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation>Ενέργειες</translation>
+    </message>
+    <message>
+        <source>Tools</source>
+        <translation>Εργαλεία</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Αναίρεση</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Επανάληψη</translation>
+    </message>
+    <message>
+        <source>Image</source>
+        <translation>Εικόνα</translation>
+    </message>
+    <message>
+        <source>Horizontal Flip</source>
+        <translation>Οριζόντια αντιστροφή</translation>
+    </message>
+    <message>
+        <source>Vertical Flip</source>
+        <translation>Κάθετη αντιστροφή</translation>
+    </message>
+    <message>
+        <source>Perform Crop</source>
+        <translation>Εκτέλεση Περικοπής</translation>
+    </message>
+    <message>
+        <source>Watermark</source>
+        <translation>Υδατογράφημα</translation>
+    </message>
+    <message>
+        <source>Effects</source>
+        <translation>Εφφέ</translation>
+    </message>
+    <message>
+        <source>Pixelate</source>
+        <translation>Pixelate</translation>
+    </message>
+    <message>
+        <source>Black and White</source>
+        <translation>Ασπρόμαυρο</translation>
+    </message>
+    <message>
+        <source>Sepia</source>
+        <translation>Sepia</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Επιλογές</translation>
+    </message>
+    <message>
+        <source>The image is not loaded because of a server error.</source>
+        <translation>Η εικόνα δε φορτώθηκε λόγω σφάλματος διακομιστή.</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Κλείσιμο</translation>
+    </message>
+</context>
+</TS>
