1b688ceddf6a5c16c3647d577c46063e9f50cceb
Patrick Allaert <pa@ez.no>
2012-04-19 19:53:33 +0200

    Fixed #019337: Database Check Failed after upgrading from Ez Publish 4.6 > 4.7 rc1 with Postgres (step 1) (cherry picked from commit bb1e7d59930b2de932986377551a74d010f16c71)

--- update/database/postgresql/4.7/unstable/dbupdate-4.7.0beta1-to-4.7.0rc1.sql
+++ update/database/postgresql/4.7/unstable/dbupdate-4.7.0beta1-to-4.7.0rc1.sql
@@ -1,2 +1,5 @@
 UPDATE ezsite_data SET value='4.7.0rc1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+ALTER TABLE ONLY ezpending_actions
+    ADD CONSTRAINT ezpending_actions_pkey PRIMARY KEY (id);
