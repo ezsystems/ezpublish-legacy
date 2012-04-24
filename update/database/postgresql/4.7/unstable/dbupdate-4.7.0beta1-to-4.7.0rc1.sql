824f24f0caf8cf7cfd2ce2abb66994aab71cb2f3
Lukasz Serwatka <l.serwatka@gazeta.pl>
2012-04-24 16:26:52 +0200

    Fixed #019354: ezcontentstaging : changing main node assignment is not synced on postgres

--- update/database/postgresql/4.7/unstable/dbupdate-4.7.0beta1-to-4.7.0rc1.sql
+++ update/database/postgresql/4.7/unstable/dbupdate-4.7.0beta1-to-4.7.0rc1.sql
@@ -3,3 +3,6 @@ UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
 ALTER TABLE ONLY ezpending_actions
     ADD CONSTRAINT ezpending_actions_pkey PRIMARY KEY (id);
+
+UPDATE eztrigger SET name = 'pre_updatemainassignment', function_name = 'updatemainassignment'
+  WHERE name = 'pre_UpdateMainAssignment' AND function_name = 'UpdateMainAssignment';
