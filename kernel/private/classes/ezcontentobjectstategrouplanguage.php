8823cc7b38216feb85df08c6cf5ef654a6ce975a
Damien Pobel <dp@ez.no>
2012-07-05 11:00:31 +0200

    Fixed #19169: duplicated object state groups after changing default language

--- kernel/private/classes/ezcontentobjectstategrouplanguage.php
+++ kernel/private/classes/ezcontentobjectstategrouplanguage.php
@@ -38,14 +38,16 @@ class eZContentObjectStateGroupLanguage extends eZPersistentObject
                                          "description" => array( "name" => "Description",
                                                                  "datatype" => "text",
                                                                  "required" => false ),
+                                         "real_language_id" => array( "name" => "RealLanguageID",
+                                                                      "datatype" => "integer",
+                                                                      "required" => true ),
                                          "language_id" => array( "name" => "LanguageID",
                                                                  "datatype" => "integer",
                                                                  "required" => false ) ),
                       "keys" => array( "contentobject_state_group_id",
-                                       "language_id" ),
+                                       "real_language_id" ),
                       "function_attributes" => array( "language" => "language",
-                                                      "is_valid" => "isValid",
-                                                      "real_language_id" => "realLanguageID",
+                                                      "is_valid" => "isValid"
                                                     ),
                       "increment_key" => false,
                       "class_name" => "eZContentObjectStateGroupLanguage",
@@ -96,7 +98,7 @@ class eZContentObjectStateGroupLanguage extends eZPersistentObject
      */
     public function language()
     {
-        return eZContentLanguage::fetch( $this->LanguageID & ~1 );
+        return eZContentLanguage::fetch( $this->RealLanguageID );
     }
 
     /**
@@ -116,7 +118,9 @@ class eZContentObjectStateGroupLanguage extends eZPersistentObject
      */
     public function realLanguageID()
     {
-        return $this->LanguageID & ~1;
+        return $this->RealLanguageID;
     }
+
+
 }
 ?>
