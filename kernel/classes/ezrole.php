cfacdb8ae4348a4728ba2b4a402a637eba5bae49
J. J. Cavalleri <joaquim.cavalleri@ez.no>
2012-08-17 17:16:19 +0100

    Rolling back removal of duplicate policies

--- kernel/classes/ezrole.php
+++ kernel/classes/ezrole.php
@@ -588,24 +588,7 @@ class eZRole extends eZPersistentObject
                 }
             }
         }
-        $accessArray = self::super_unique( $accessArray );
         return $accessArray;
-    }
-
-    /*!
-     Array unique recursive
-     */
-    static function super_unique( $array )
-    {
-        $result = array_map( "unserialize", array_unique( array_map( "serialize", $array ) ) );
-        foreach ( $result as $key => $value )
-        {
-            if ( is_array( $value ) )
-            {
-                $result[$key] = self::super_unique( $value );
-            }
-        }
-        return $result;
     }
 
     /*!
