ezsystems/ezpublish-legacy

commit 4856b2328d6fd019cf573ffa429139d438bca210

Fix hardcoded use of 'index.php' in ezpKernelWeb
Making it work with index_dev.php correctly, generating correct urls.
Cache might still be an issue not all ezp4 cache types will include indexFile as part of key.

kernel/private/classes/ezpkernelweb.php
@@ -232,7 +232,20 @@ class ezpKernelWeb implements ezpKernelHandler
         $GLOBALS['eZGlobalRequestURI'] = eZSys::serverVariable( 'REQUEST_URI' );
 
         // Initialize basic settings, such as vhless dirs and separators
-        eZSys::init( 'index.php', $ini->variable( 'SiteAccessSettings', 'ForceVirtualHost' ) === 'true' );
+        if ( $this->getServiceContainer()->has( 'request' ) )
+        {
+            eZSys::init(
+                basename( $this->getServiceContainer()->get( 'request' )->getScriptName() ),
+                $ini->variable( 'SiteAccessSettings', 'ForceVirtualHost' ) === 'true'
+            );
+        }
+        else
+        {
+            eZSys::init(
+                'index.php',
+                $ini->variable( 'SiteAccessSettings', 'ForceVirtualHost' ) === 'true'
+            );
+        }
 
         // Check for extension
         eZExtension::activateExtensions( 'default' );

commit 27bff23fab846fb02818771ae9a4485cf58506b6

Fix fatal error in standalone execution introduced in 4856b2328d6fd019cf573ffa429139d438bca210

kernel/private/classes/ezpkernelweb.php
@@ -232,7 +232,7 @@ class ezpKernelWeb implements ezpKernelHandler
         $GLOBALS['eZGlobalRequestURI'] = eZSys::serverVariable( 'REQUEST_URI' );
 
         // Initialize basic settings, such as vhless dirs and separators
-        if ( $this->getServiceContainer()->has( 'request' ) )
+        if ( $this->hasServiceContainer() && $this->getServiceContainer()->has( 'request' ) )
         {
             eZSys::init(
                 basename( $this->getServiceContainer()->get( 'request' )->getScriptName() ),