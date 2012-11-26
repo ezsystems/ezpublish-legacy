eb3efd71c2611e773cab5f54a43e6c5b2aab994b
Paul Borgermans <paul.borgermans@gmail.com>
2012-11-26 14:39:06 +0100

    Fixed EZP-19889: datatype eZSelection returns allways true in function hasObjectAttributeContent

--- kernel/classes/datatypes/ezselection/ezselectiontype.php
+++ kernel/classes/datatypes/ezselection/ezselectiontype.php
@@ -374,7 +374,8 @@ class eZSelectionType extends eZDataType
 
     function hasObjectAttributeContent( $contentObjectAttribute )
     {
-        return true;
+        $selected = $this->objectAttributeContent( $contentObjectAttribute );
+        return !empty( $selected[0] );
     }
 
     function sortKey( $contentObjectAttribute )
