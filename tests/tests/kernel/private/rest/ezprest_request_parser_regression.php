17df87e5cb36fd23f5ddd618a3b4ed7e2e1c65ae
Damien Pobel <dp@ez.no>
2012-12-11 18:31:39 +0100

    Fixed failing unit test on ezpRestHttpRequestParser

--- tests/tests/kernel/private/rest/ezprest_request_parser_regression.php
+++ tests/tests/kernel/private/rest/ezprest_request_parser_regression.php
@@ -46,6 +46,8 @@ class ezpRestHttpRequestParserRegression extends ezpRegressionTest
         $_FILES  = $this->filesArray;
         $_REQUEST = $this->requestArray;
         $_COOKIE = $this->cookieArray;
+        // make sure eZSys::serverPort() really runs
+        unset( $GLOBALS['eZSysServerPort'] );
     }
 
     public function testRunRegression( $name )
@@ -70,7 +72,7 @@ class ezpRestHttpRequestParserRegression extends ezpRegressionTest
         else
         {
             $expected = file_get_contents( $expectedFileName );
-            self::assertEquals( $expected, var_export( $req, true ) );
+            self::assertEquals( $expected, var_export( $req, true ), $expectedFileName );
         }
     }
 
