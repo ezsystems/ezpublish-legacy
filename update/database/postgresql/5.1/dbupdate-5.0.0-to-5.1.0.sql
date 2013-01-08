dab2bb185c9edd61ebc9ebd8223a494b679ec82d
Petar Spanja <petar.spanja@gmail.com>
2013-01-08 16:06:07 +0100

    dbupdate: added queries to cleanup ezcontentboject_link table

--- update/database/postgresql/5.1/dbupdate-5.0.0-to-5.1.0.sql
+++ update/database/postgresql/5.1/dbupdate-5.0.0-to-5.1.0.sql
@@ -25,3 +25,6 @@ CREATE INDEX ezurlalias_ml_par_lnk_txt ON ezurlalias_ml USING btree (parent, tex
 DROP INDEX ezurlalias_ml_action;
 DROP INDEX ezurlalias_ml_par_act_id_lnk;
 CREATE INDEX ezurlalias_ml_par_act_id_lnk ON ezurlalias_ml USING btree ("action", id, link, parent);
+
+DELETE FROM ezcontentobject_link WHERE op_code <> 0;
+DELETE FROM ezcontentobject_link WHERE relation_type = 0;
