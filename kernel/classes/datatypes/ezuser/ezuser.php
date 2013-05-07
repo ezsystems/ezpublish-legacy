ezsystems/ezpublish-legacy

commit e39d2077d6dbf2a2b1de4597733d2f89cbc0f338

EZP-20455: eZUser::isLoggedIn does not correctly check user's state
Deprecate isLoggedIn() in favor of the new isRegistered() and the
existing isUserLoggedIn() methods.

kernel/classes/datatypes/ezuser/ezuser.php
@@ -88,7 +88,7 @@ class eZUser extends eZPersistentObject
                                                       'roles' => 'roles',
                                                       'role_id_list' => 'roleIDList',
                                                       'limited_assignment_value_list' => 'limitValueList',
-                                                      'is_logged_in' => 'isLoggedIn',
+                                                      'is_logged_in' => 'isRegistered',
                                                       'is_enabled' => 'isEnabled',
                                                       'is_locked' => 'isLocked',
                                                       'last_visit' => 'lastVisit',
@@ -1107,7 +1107,7 @@ WHERE user_id = '" . $userID . "' AND
         // - the user has not logged out,
         // - the user is not logged in,
         // - and if a automatic single sign on plugin is enabled.
-        if ( !self::$userHasLoggedOut and is_object( $currentUser ) and !$currentUser->isLoggedIn() )
+        if ( !self::$userHasLoggedOut && is_object( $currentUser ) && !$currentUser->isRegistered() )
         {
             $ssoHandlerArray = $ini->variable( 'UserSettings', 'SingleSignOnHandlerArray' );
             if ( !empty( $ssoHandlerArray ) )
@@ -2304,18 +2304,35 @@ WHERE user_id = '" . $userID . "' AND
         return eZUserAccountKey::fetchByUserID( $this->ContentObjectID );
     }
 
-    /*!
-     Returns true if it's a real user which is logged in. False if the user
-     is the default user or the fallback buildtin user.
-    */
-    function isLoggedIn()
+    /**
+     * Returns true if the user is a registered and not anonymous user.
+     *
+     * @deprecated since 5.1 Use isRegistered() / isUserLoggedIn() instead.
+     */
+    public function isLoggedIn()
     {
-        if ( $this->ContentObjectID == self::anonymousId() or
-             $this->ContentObjectID == -1 )
-        {
-            return false;
-        }
-        return true;
+        eZDebug::writeStrict( "Method " . __METHOD__ . " has been deprecated in 5.1. Use isRegistered() / isUserLoggedIn() instead.", "Deprecation" );
+        return $this->isRegistered();
+    }
+
+    /**
+     * Returns true if the user is a registered and not anonymous user.
+     *
+     * @deprecated since 5.1 Use isRegistered() / isUserLoggedIn() instead.
+     */
+    public function isRegistered()
+    {
+        return $this->ContentObjectID != self::anonymousId() && $this->ContentObjectID != -1;
+    }
+
+    /**
+     * Returns whether the current user is a registered user.
+     *
+     * @since 5.1
+     */
+    static public function isCurrentUserRegistered()
+    {
+        return self::currentUser()->isRegistered();
     }
 
     /*!

extension/ezformtoken/event/ezxformtoken.php
@@ -252,7 +252,7 @@ class ezxFormToken
         if ( !eZSession::hasStarted() )
             return false;
 
-        if ( !eZUser::currentUser()->isLoggedIn() )
+        if ( !eZUser::isCurrentUserRegistered() )
             return false;
 
         return true;

kernel/classes/ezhttpheader.php
@@ -39,8 +39,7 @@ class eZHTTPHeader
                  && $ini->hasVariable( 'HTTPHeaderSettings', 'OnlyForAnonymous' )
                  && $ini->variable( 'HTTPHeaderSettings', 'OnlyForAnonymous' ) === 'enabled' )
             {
-                $user = eZUser::currentUser();
-                $GLOBALS['eZHTTPHeaderCustom'] = !$user->isLoggedIn();
+                $GLOBALS['eZHTTPHeaderCustom'] = !eZUser::isCurrentUserRegistered();
             }
             else
             {

kernel/classes/ezpreferences.php
@@ -64,8 +64,8 @@ class eZPreferences
         // We must store the database changes if:
         // a - The current user is logged in (ie. not anonymous)
         // b - We have specified a specific user (not the current).
-        //    in which case isLoggedIn() will fail.
-        if ( $storeUserID !== false or $user->isLoggedIn() )
+        //    in which case isRegistered() will fail.
+        if ( $storeUserID !== false or $user->isRegistered() )
         {
             // Only store in DB if user is logged in or we have
             // a specific user ID defined

kernel/classes/shopaccounthandlers/ezdefaultshopaccounthandler.php
@@ -21,12 +21,7 @@ class eZDefaultShopAccountHandler
     */
     function verifyAccountInformation()
     {
-        // Check login
-        $user = eZUser::currentUser();
-        if ( !$user->isLoggedIn() )
-            return false;
-        else
-            return true;
+        return eZUser::isCurrentUserRegistered();
     }
 
     /*!

kernel/content/action.php
@@ -1142,8 +1142,7 @@ else if ( $http->hasPostVariable( "ContentObjectID" )  )
     }
     else if ( $http->hasPostVariable( "ActionAddToWishList" ) )
     {
-        $user = eZUser::currentUser();
-        if ( !$user->isLoggedIn() )
+        if ( !eZUser::isCurrentUserRegistered() )
             return $module->handleError( eZError::KERNEL_ACCESS_DENIED, 'kernel' );
 
         $shopModule = eZModule::exists( "shop" );

kernel/content/draft.php
@@ -13,7 +13,7 @@ $Offset = $Params['Offset'];
 $viewParameters = array( 'offset' => $Offset );
 
 $user = eZUser::currentUser();
-if ( !$user->isLoggedIn() )
+if ( !$user->isRegistered() )
     return $Module->handleError( eZError::KERNEL_ACCESS_DENIED, 'kernel' );
 
 $userID = $user->id();

kernel/notification/addtonotification.php
@@ -20,7 +20,7 @@ $redirectURI = $http->postVariable( 'RedirectURI', $http->sessionVariable( 'Last
 
 $viewMode = $http->hasPostVariable( 'ViewMode' ) ? $http->postVariable( 'ViewMode' ) : 'full';
 
-if ( !$user->isLoggedIn() )
+if ( !$user->isRegistered() )
 {
     eZDebug::writeError( 'User not logged in trying to subscribe for notification, node ID: ' . $nodeID,
                          'kernel/content/action.php' );

commit a456dee3610bba0697c2d12cccd5378451414b22

Changed: fixed doc issue from copy/paste
Introduced in e39d2077d6dbf2a2b1de4597733d2f89cbc0f338 while fixing:
"EZP-20455: eZUser::isLoggedIn does not correctly check user's state"

kernel/classes/datatypes/ezuser/ezuser.php
@@ -2336,8 +2336,6 @@ WHERE user_id = '" . $userID . "' AND
 
     /**
      * Returns true if the user is a registered and not anonymous user.
-     *
-     * @deprecated since 5.1 Use isRegistered() / isUserLoggedIn() instead.
      */
     public function isRegistered()
     {