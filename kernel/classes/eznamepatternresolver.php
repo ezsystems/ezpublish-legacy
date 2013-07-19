cfa2a7263ee6adbe612f3f1f2104abf52033757a
Peter Keung <peter@mugo.ca>
2013-07-19 12:48:34 -0700

    Fix for EZP-21305: Allow object relation attribute to be used in object name pattern as first condition in fallback

--- kernel/classes/eznamepatternresolver.php
+++ kernel/classes/eznamepatternresolver.php
@@ -241,7 +241,7 @@ class eZNamePatternResolver
             }
             else
             {
-                if ( array_key_exists( $tokenPart, $this->attributeArray ) and $this->attributeArray[$tokenPart] !== '' and $this->attributeArray[$tokenPart] !== NULL )
+                if ( array_key_exists( $tokenPart, $this->attributeArray ) && $this->attributeArray[$tokenPart] !== '' && $this->attributeArray[$tokenPart] !== null && $this->attributeArray[$tokenPart] !== false )
                 {
                     $replaceString = $this->attributeArray[$tokenPart];
                     // We want to stop after the first matching token part / identifier is found
