64f590e66daa9e6e1479297c856ce4abbd5f89b4
Yannick ROGER <yannick.roger@ez.no>
2013-09-03 18:23:24 +0200

    Fix EZP-21465: Cleanup extra lines in the ezurl_object_link table

--- update/database/postgresql/5.2/dbupdate-5.1.0-to-5.2.0.sql
+++ update/database/postgresql/5.2/dbupdate-5.1.0-to-5.2.0.sql
@@ -42,3 +42,13 @@ ALTER TABLE ezcobj_state_language
 
 ALTER TABLE ezurlalias_ml
     ALTER COLUMN lang_mask TYPE BIGINT;
+
+-- Start ezp-21465 : Cleanup extra lines in the ezurl_object_link table
+DELETE
+FROM ezurl_object_link AS T1
+WHERE T1.url_id < ANY (SELECT url_id
+      FROM ezurl_object_link T2
+      WHERE T1.url_id <> T2.url_id
+      AND T1.contentobject_attribute_id = T2.contentobject_attribute_id
+      AND T1.contentobject_attribute_version = T2.contentobject_attribute_version);
+-- End ezp-21465
