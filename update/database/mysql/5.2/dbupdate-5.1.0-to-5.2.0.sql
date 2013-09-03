4936cf0980d97aa4dfdfa1f85b0a01bd4dfd88c5
Yannick ROGER <yannick.roger@ez.no>
2013-09-03 18:23:24 +0200

    Fix EZP-21465: Cleanup extra lines in the ezurl_object_link table

--- update/database/mysql/5.2/dbupdate-5.1.0-to-5.2.0.sql
+++ update/database/mysql/5.2/dbupdate-5.1.0-to-5.2.0.sql
@@ -1,3 +1,21 @@
 SET storage_engine=InnoDB;
 UPDATE ezsite_data SET value='5.2.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+-- Start ezp-21465 : Cleanup extra lines in the ezurl_object_link table
+DROP TEMPORARY TABLE IF EXISTS ezurl_object_link_temp ;
+
+CREATE TEMPORARY TABLE ezurl_object_link_temp (
+   contentobject_attribute_id int(11) NOT NULL DEFAULT '0',
+   contentobject_attribute_version int(11) NOT NULL DEFAULT '0',
+   url_id int(11) NOT NULL DEFAULT '0',
+   KEY ezurl_ol_coa_id (contentobject_attribute_id),
+   KEY ezurl_ol_coa_version (contentobject_attribute_version),
+   KEY ezurl_ol_url_id (url_id),
+   UNIQUE KEY unique_key (contentobject_attribute_id, contentobject_attribute_version)
+) IGNORE SELECT * FROM ezurl_object_link;
+
+TRUNCATE TABLE ezurl_object_link;
+
+INSERT INTO ezurl_object_link SELECT * FROM ezurl_object_link_temp;
+-- End ezp-21465
--- update/database/postgresql/5.2/dbupdate-5.1.0-to-5.2.0.sql
+++ update/database/postgresql/5.2/dbupdate-5.1.0-to-5.2.0.sql
@@ -1,2 +1,13 @@
 UPDATE ezsite_data SET value='5.2.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+-- Start ezp-21465 : Cleanup extra lines in the ezurl_object_link table
+DELETE
+FROM ezurl_object_link AS T1
+WHERE T1.url_id < ANY (SELECT url_id
+      FROM ezurl_object_link T2
+      WHERE T1.url_id <> T2.url_id
+      AND T1.contentobject_attribute_id = T2.contentobject_attribute_id
+      AND T1.contentobject_attribute_version = T2.contentobject_attribute_version);
+-- End ezp-21465
+
