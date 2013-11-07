87129dead264bf15040fd2c0c3014e5fc83f0483
Bertrand Dunogier <bd@ez.no>
2013-11-07 19:04:42 +0100

    EZP-21913: partially fixed ezwebincommon.php

--- bin/php/ezwebincommon.php
+++ bin/php/ezwebincommon.php
@@ -19,13 +19,6 @@ define( "EZ_INSTALL_PACKAGE_EXTRA_ACTION_QUIT", 'q' );
 define( "EZ_INSTALL_PACKAGE_EXTRA_ACTION_SKIP_PACKAGE", 's' );
 
 /*!
- define global vars
-*/
-global $cli;
-global $script;
-
-
-/*!
  includes
 */
 require_once 'autoload.php';
@@ -35,8 +28,8 @@ require_once 'autoload.php';
 ***************************************************************/
 function showError( $message, $addEOL = true, $bailOut = true )
 {
-    global $cli;
-    global $script;
+    $cli = eZCLI::instance();
+    $script = eZCLI::instance();
 
     $cli->output( $cli->stylize( 'error', "Error: " .  $message ), $addEOL );
 
@@ -48,25 +41,25 @@ function showError( $message, $addEOL = true, $bailOut = true )
 
 function showWarning( $message, $addEOL = true )
 {
-    global $cli;
+    $cli = eZCLI::instance();
     $cli->output( $cli->stylize( 'warning', "Warning: " . $message ), $addEOL );
 }
 
 function showNotice( $message, $addEOL = true )
 {
-    global $cli;
+    $cli = eZCLI::instance();
     $cli->output( $cli->stylize( 'notice', "Notice: " ) .  $message, $addEOL );
 }
 
 function showMessage( $message, $addEOL = true )
 {
-    global $cli;
+    $cli = eZCLI::instance();
     $cli->output( $cli->stylize( 'blue', $message ), $addEOL );
 }
 
 function showMessage2( $message, $addEOL = true )
 {
-    global $cli;
+    $cli = eZCLI::instance();
     $cli->output( $cli->stylize( 'red', $message ), $addEOL );
 }
 
@@ -188,8 +181,6 @@ function repositoryByVendor( $vendor )
 */
 function downloadPackages( $packageList, $packageURL, $packageDir, $packageRepository )
 {
-    global $cli;
-
     showMessage2( "Configuring..." );
 
     if ( !is_array( $packageList ) || count( $packageList ) == 0 )
@@ -302,7 +293,7 @@ function downloadPackages( $packageList, $packageURL, $packageDir, $packageRepos
 */
 function installPackages( $packageList, $params = array() )
 {
-    global $cli;
+    $cli = eZCLI::instance();
 
     showMessage2( "Installing..." );
 
