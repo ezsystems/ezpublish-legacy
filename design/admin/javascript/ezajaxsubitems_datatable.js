07de12622320c4648dd03d20647ac012a1411d90
Yannick ROGER <yannick.roger@ez.no>
2014-07-04 15:22:33 +0200

    Fix EZP-23086: Image thumbnail not shown on backend if alias contains quotes

--- design/admin/javascript/ezajaxsubitems_datatable.js
+++ design/admin/javascript/ezajaxsubitems_datatable.js
@@ -60,9 +60,9 @@ var sortableSubitems = function () {
         }
 
         var thumbView = function(cell, record, column, data) {
-            var url = record.getData('thumbnail_url');
+            var url = encodeURI(record.getData('thumbnail_url'));
             if (url) {
-                var thBack = 'background: url(' + url + ') no-repeat;';
+                var thBack = 'background: url(\'' + url.replace("'", "\\'") + '\') no-repeat;';
                 var thWidth = ' width: ' + record.getData('thumbnail_width') + 'px;';
                 var thHeight = ' height: ' + record.getData('thumbnail_height') + 'px;';
                 cell.innerHTML = '<div class="thumbview"><div id="thumbfield" class="thumbfield"></div><span><div style="' + thBack + thWidth + thHeight + '"></div></span></div>';
