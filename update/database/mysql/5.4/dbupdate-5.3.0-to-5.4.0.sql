93be9a718846b774c26a517026c7e7f5a847934c
Joao Inacio <joao.inacio@ez.no>
2014-11-07 13:35:55 +0000

    Fix EZP-23595: update sql to cleanup extra rows in ezuser_setting table

--- update/database/mysql/5.4/dbupdate-5.3.0-to-5.4.0.sql
+++ update/database/mysql/5.4/dbupdate-5.3.0-to-5.4.0.sql
@@ -16,3 +16,6 @@ WHERE
     ezcontentclass_attribute.id = ezinfocollection_attribute.contentclass_attribute_id
     AND ezinfocollection_attribute.data_int = 0
     AND ezcontentclass_attribute.data_type_string IN ( 'ezdate', 'ezdatetime' );
+
+-- See https://jira.ez.no/browse/EZP-23595 - cleanup extra lines in the ezuser_setting table
+DELETE FROM ezuser_setting where user_id not in (SELECT contentobject_id FROM ezuser);
