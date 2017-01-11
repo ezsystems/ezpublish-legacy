13a1d0f880125bc16d27ea5bcb46cd724bc4f35f
Philipp Kamps <pkamps@mugo.ca>
2017-01-11 21:03:41 +0100

    Adding function "createNew" to simplify the creation of a ezsitedata entry

--- kernel/classes/ezsitedata.php
+++ kernel/classes/ezsitedata.php
@@ -8,38 +8,64 @@
  * @package kernel
  */
 
+/**
+ * eZPersistentObject implementation for ezsite_data table.
+ * Enables you to store and fetch key/value pairs
+ *
+ * Class eZSiteData
+ */
 class eZSiteData extends eZPersistentObject
 {
     /**
      * Schema definition
-     * eZPersistentObject implementation for ezsite_data table
      * @see kernel/classes/ezpersistentobject.php
      * @return array
      */
     public static function definition()
     {
-        return array( 'fields'       => array( 'name'               => array( 'name'     => 'name',
-                                                                              'datatype' => 'string',
-                                                                              'default'  => null,
-                                                                              'required' => true ),
-
-                                               'value'             => array( 'name'     => 'value',
-                                                                             'datatype' => 'string',
-                                                                             'default'  => null,
-                                                                             'required' => true ),
-
-                                            ),
-
-                      'keys'                 => array( 'name' ),
-                      'class_name'           => 'eZSiteData',
-                      'name'                 => 'ezsite_data',
-                      'function_attributes'  => array()
+        return array(
+            'fields' => array(
+                'name' => array(
+                    'name'     => 'name',
+                    'datatype' => 'string',
+                    'default'  => null,
+                    'required' => true
+                ),
+                'value' => array(
+                    'name'     => 'value',
+                    'datatype' => 'string',
+                    'default'  => null,
+                    'required' => true
+                ),
+            ),
+            'keys'                => array( 'name' ),
+            'class_name'          => 'eZSiteData',
+            'name'                => 'ezsite_data',
+            'function_attributes' => array()
         );
     }
 
     /**
+     * Constructs a new eZSiteData instance. You need to call 'store()'
+     * in order to store it into the DB.
+     *
+     * @param string $key
+     * @param string $value
+     * @return eZSiteData
+     */
+    public static function create( $name, $value )
+    {
+        return new eZSiteData( array(
+            'name' => $name,
+            'value' => $value
+        ) );
+    }
+
+    /**
      * Fetches a site data by name
+	 *
      * @param string $name
+     * @return eZPersistentObject
      */
     public static function fetchByName( $name )
     {
