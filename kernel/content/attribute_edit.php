30c1260b840d747740b9cd93bc76a8ed1db5a7ae
pkamps <pkamps@mugo.ca>
2017-10-21 12:26:25 +0200

    [Travis] Setup PHP7 testing on travis, bump requriment to PHP 5.5 (#73)

--- kernel/content/attribute_edit.php
+++ kernel/content/attribute_edit.php
@@ -219,35 +219,40 @@ if ( $_SERVER['REQUEST_METHOD'] === 'POST' )
 {
     // Need to detect if post_max_size has been reached. If so, all post variables are gone...
     $postMaxSize = trim( ini_get( 'post_max_size' ) );
-    $postMaxSizeBytes = $postMaxSize;
-    $postMaxSizeUnit = 'b';
-    // post_max_size can have values like 8M which needs to be converted to bytes
-    $last = strtolower( $postMaxSize[strlen($postMaxSize)-1] );
-    if ( !is_numeric( $last ) )
-        $postMaxSize = substr( $postMaxSize, 0, -1 );
-    switch ( $last )
+    $postMaxSizeBytes = (int)$postMaxSize;
+
+    // As of PHP 5.3.2 it is possible to configure unlimited post_max_size, further checking
+    if ($postMaxSizeBytes !== 0)
     {
-        case 'g':
-            $postMaxSizeBytes *= 1073741824; // = 1024 * 1024 * 1024
-            $postMaxSizeUnit = 'Gb';
-            break;
-        case 'm':
-            $postMaxSizeBytes *= 1048576; // = 1024 * 1024
-            $postMaxSizeUnit = 'Mb';
-            break;
-        case 'k':
-            $postMaxSizeBytes *= 1024;
-            $postMaxSizeUnit = 'Kb';
-            break;
-    }
-    if ( (int)$_SERVER['CONTENT_LENGTH'] > $postMaxSizeBytes &&  // This is not 100% acurrate as $_SERVER['CONTENT_LENGTH'] doesn't only count post data but also other things
-        count( $_POST ) === 0 )                                 // Therefore we also check if request got no post variables.
-    {
-        $validation['attributes'][] = array( 'id' => '1',
-                                             'identified' => 'generalid',
-                                             'name' => ezpI18n::tr( 'kernel/content', 'Error' ),
-                                             'description' => ezpI18n::tr( 'kernel/content', 'The request sent to the server was too big to be accepted. This probably means that you uploaded a file which was too big. The maximum allowed request size is %max_size_string.', null, array( '%max_size_string' => "$postMaxSize $postMaxSizeUnit" ) ) );
-        $validation['processed'] = true;
+        $postMaxSizeUnit = 'b';
+        // post_max_size can have values like 8M which needs to be converted to bytes
+        $last = strtolower( $postMaxSize[strlen($postMaxSize)-1] );
+        if ( !is_numeric( $last ) )
+            $postMaxSize = substr( $postMaxSize, 0, -1 );
+        switch ( $last )
+        {
+            case 'g':
+                $postMaxSizeBytes *= 1073741824; // = 1024 * 1024 * 1024
+                $postMaxSizeUnit = 'Gb';
+                break;
+            case 'm':
+                $postMaxSizeBytes *= 1048576; // = 1024 * 1024
+                $postMaxSizeUnit = 'Mb';
+                break;
+            case 'k':
+                $postMaxSizeBytes *= 1024;
+                $postMaxSizeUnit = 'Kb';
+                break;
+        }
+        if ( (int)$_SERVER['CONTENT_LENGTH'] > $postMaxSizeBytes &&  // This is not 100% acurrate as $_SERVER['CONTENT_LENGTH'] doesn't only count post data but also other things
+            count( $_POST ) === 0 )                                 // Therefore we also check if request got no post variables.
+        {
+            $validation['attributes'][] = array( 'id' => '1',
+                                                 'identified' => 'generalid',
+                                                 'name' => ezpI18n::tr( 'kernel/content', 'Error' ),
+                                                 'description' => ezpI18n::tr( 'kernel/content', 'The request sent to the server was too big to be accepted. This probably means that you uploaded a file which was too big. The maximum allowed request size is %max_size_string.', null, array( '%max_size_string' => "$postMaxSize $postMaxSizeUnit" ) ) );
+            $validation['processed'] = true;
+        }
     }
 }
 
