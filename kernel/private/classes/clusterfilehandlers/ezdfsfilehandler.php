be3ecf4975ac1e35374839ce6e27a9a75b293f98
André R <ar@ez.no>
2018-08-13 14:28:07 +0200

    Use ternary operator to shorten closure like others

--- kernel/private/classes/clusterfilehandlers/ezdfsfilehandler.php
+++ kernel/private/classes/clusterfilehandlers/ezdfsfilehandler.php
@@ -156,18 +156,13 @@ class eZDFSFileHandler implements eZClusterFileHandlerInterface, ezpDatabaseBase
             usort( $GLOBALS['eZClusterInfo'], function ( $a, $b ) {
                 $a = $a['cnt'];
                 $b = $b['cnt'];
-
-                if ( $a > $b )
-                {
-                    return -1;
-                }
                 
                 if ( $a == $b )
                 {
                     return 0;
                 }
 
-                return 1;
+                return $a > $b ? -1 : 1;
             });
             array_pop( $GLOBALS['eZClusterInfo'] );
         }
