9c0ff40a784057bc2cd5c0eb2600510ce8c7ba1c
pkamps <pkamps@mugo.ca>
2018-11-06 23:10:27 +0100

    Fetch function parameter "class_filter_type" with smart default value (#118)
    * Allows to not specify the class_filter_type parameter in fetch functions - assuming the value "include"
    
    * Adding default value for the tree_count and list_count fetch functions

--- kernel/content/ezcontentfunctioncollection.php
+++ kernel/content/ezcontentfunctioncollection.php
@@ -342,6 +342,12 @@ class eZContentFunctionCollection
             $treeParameters['DepthOperator'] = $depthOperator;
         }
 
+        // Set an unset ClassFilterType to 'include' if 'ClassFilterArray' is set
+        if( !empty( $treeParameters[ 'ClassFilterArray' ] ) && !$treeParameters[ 'ClassFilterType' ] )
+        {
+            $treeParameters[ 'ClassFilterType' ] = 'include';
+        }
+
         $children = null;
         if ( is_numeric( $parentNodeID ) or is_array( $parentNodeID ) )
         {
@@ -368,6 +374,12 @@ class eZContentFunctionCollection
 
         if ( is_numeric( $parentNodeID ) or is_array( $parentNodeID ) )
         {
+            // Set an unset ClassFilterType to 'include' if 'ClassFilterArray' is set
+            if( !empty( $class_filter_array ) && !$class_filter_type )
+            {
+                $class_filter_type = 'include';
+            }
+
             $childrenCount = eZContentObjectTreeNode::subTreeCountByNodeID( array( 'Limitation' => $limitation,
                                                                            'ClassFilterType' => $class_filter_type,
                                                                            'ClassFilterArray' => $class_filter_array,
