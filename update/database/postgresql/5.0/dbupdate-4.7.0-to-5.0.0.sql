59ed553dcd84bffcb844b3f70bb8af6a6dc6d99a
pkamps <pkamps@mugo.ca>
2019-12-03 11:06:48 +0100

    Updated dbupdate scripts so they will no longer fail when there are multiple users with the same e-mail (#169)

--- update/database/postgresql/5.0/dbupdate-4.7.0-to-5.0.0.sql
+++ update/database/postgresql/5.0/dbupdate-4.7.0-to-5.0.0.sql
@@ -14,7 +14,7 @@ UPDATE ezcobj_state_group_language SET real_language_id = language_id & ~1;
 ALTER TABLE ezgeneral_digest_user_settings ADD user_id integer DEFAULT 0 NOT NULL;
 DELETE FROM ezgeneral_digest_user_settings WHERE address NOT IN (SELECT email FROM ezuser);
 UPDATE ezgeneral_digest_user_settings SET user_id = (SELECT ezuser.contentobject_id
-           FROM ezuser WHERE ezuser.email = ezgeneral_digest_user_settings.address);
+           FROM ezuser WHERE ezuser.email = ezgeneral_digest_user_settings.address LIMIT 1);
 CREATE INDEX ezgeneral_digest_user_id ON ezgeneral_digest_user_settings USING btree (user_id);
 ALTER TABLE ezgeneral_digest_user_settings DROP COLUMN address;
 CREATE INDEX ezuser_login ON ezuser USING btree (login);
