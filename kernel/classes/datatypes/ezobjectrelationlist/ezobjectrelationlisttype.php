114f40a4daff3ea4b1382c304511c8ce41d7667a
Stian Torjussen <53934716+StianTorjussen@users.noreply.github.com>
2020-02-26 16:51:59 +0100

    Apply suggestions from code review
    Check for existence of post variable
    
    Co-Authored-By: André R. <289757+andrerom@users.noreply.github.com>

--- kernel/classes/datatypes/ezobjectrelationlist/ezobjectrelationlisttype.php
+++ kernel/classes/datatypes/ezobjectrelationlist/ezobjectrelationlisttype.php
@@ -76,7 +76,7 @@ class eZObjectRelationListType extends eZDataType
             // If in browse mode and relations have been added using the search field
             // items are stored in the post variable
             if (
-                is_array( $http->postVariable( $postVariableName ) )
+                $http->hasPostVariable( $postVariableName )
                 && $http->postVariable( $postVariableName ) != array( "no_relation" )
                 && count( $http->postVariable( $postVariableName ) ) > 0
             )
